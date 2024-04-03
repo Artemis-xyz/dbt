2730d23d79230ba7fe3bca8b2c292d3529d61331
0xnirmal <nirmal@artemisanalytics.xyz>
2024-04-03 13:49:40 -0400

    reduce false positive on shows

--- dbt_scripts/show_changed.sh
+++ dbt_scripts/show_changed.sh
@@ -5,5 +5,7 @@ echo $changed_models
 echo "\n"
 echo "$changed_models" | while IFS= read -r model; do
     echo "SHOWING $model"
-    dbt show --select "$model" --limit 10
+    dbt show --select "$model" --limit 10 || true
 done
+
+exit 0
