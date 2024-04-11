647ebdd7f41cecd6f0dd2f1bf6a6ddba69581424
Sebastian Melendez <sebm@seas.upenn.edu>
2024-04-11 17:24:23 -0400

    adding nft trading volume

--- models/projects/cardano/core/ez_cardano_metrics.sql
+++ models/projects/cardano/core/ez_cardano_metrics.sql
@@ -33,7 +33,8 @@ with
     ),
     price_data as ({{ get_coingecko_metrics("cardano") }}),
     defillama_data as ({{ get_defillama_metrics("cardano") }}),
-    github_data as ({{ get_github_metrics("cardano") }})
+    github_data as ({{ get_github_metrics("cardano") }}),
+    nft_metrics as ({{ get_nft_metrics("cardano") }})
 select
     fundamental_data.date,
     fundamental_data.chain,
@@ -49,9 +50,11 @@ select
     weekly_commits_core_ecosystem,
     weekly_commits_sub_ecosystem,
     weekly_developers_core_ecosystem,
-    weekly_developers_sub_ecosystem
+    weekly_developers_sub_ecosystem,
+    nft_trading_volume
 from fundamental_data
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
 left join github_data on fundamental_data.date = github_data.date
+left join nft_metrics on fundamental_data.date = nft_metrics.date
 where fundamental_data.date < to_date(sysdate())
