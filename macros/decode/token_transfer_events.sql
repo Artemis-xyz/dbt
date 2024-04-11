41da8fd7c76cc59483599e59d2975d07223c4bdc
Sebastian Melendez <sebm@seas.upenn.edu>
2024-04-11 14:11:37 -0400

    creating celo stablecoin tables

--- macros/decode/token_transfer_events.sql
+++ macros/decode/token_transfer_events.sql
@@ -1,4 +1,4 @@
-{% macro transfer_events(chain) %}
+{% macro token_transfer_events(chain) %}
     select
         block_timestamp,
         block_number,
@@ -7,12 +7,15 @@
         origin_from_address,
         origin_to_address,
         contract_address,
-        decoded_log:"from"::string as "from",
-        decoded_log:"to"::string as "to",
-        decoded_log:"value"::string as amount,
+        decoded_log:"from"::string as from_address,
+        decoded_log:"to"::string as to_address,
+        try_to_number(decoded_log:"value"::string) as amount,
         tx_status
     from {{ ref("fact_" ~ chain ~ "_decoded_events") }}
     where topic_zero = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+        and data != '0x'
+        and amount is not null
+        and tx_status = 1
     {% if is_incremental() %}
         and
             block_timestamp
--- models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
@@ -6,4 +6,5 @@ from
             ('USDC', '0xcebA9300f2b948710d2653dD7B07f33A8B32118C', 6, 'usd-coin', 0),
             ('USDT', '0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e', 6, 'tether', 0),
             ('cUSD', '0x765DE816845861e75A25fCA122bb6898B8B1282a', 18, 'celo-dollar', 0),
+            ('cEUR', '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73', 18, 'celo-euro', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
