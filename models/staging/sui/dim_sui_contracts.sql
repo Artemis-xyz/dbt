acc15405246b9a0a70383e2c424cbcd49e82fec5
Son Do <sondo077@gmail.com>
2024-04-17 14:10:41 -0400

    Fix Sui Contracts

--- models/staging/sui/dim_sui_contracts.sql
+++ models/staging/sui/dim_sui_contracts.sql
@@ -3,15 +3,16 @@ with
 sui_contracts_sigma_over as (
     select 
         package_id as address,
-        package_name as name, 
-        coalesce(overwrite.overwrite_namespace, overwrite_w_exisiting_namespace, namespace) as namespace,
-        friendly_name,
-        project_img as icon,
-        category,
-        sub_category,
+        max(package_name) as name, 
+        max(coalesce(overwrite.overwrite_namespace, overwrite_w_exisiting_namespace, namespace)) as namespace,
+        max(friendly_name) as friendly_name,
+        max(project_img) as icon,
+        max(category) as category,
+        max(sub_category) as sub_category,
         'sui' as chain
     from {{ ref("fact_sui_contracts_silver") }} as sui_contracts full join {{ source("SIGMA", "sui_overwrite_namespace") }} overwrite
     on sui_contracts.namespace = overwrite.sui_namespace
+    group by package_id
 ) 
 select 
     address,
