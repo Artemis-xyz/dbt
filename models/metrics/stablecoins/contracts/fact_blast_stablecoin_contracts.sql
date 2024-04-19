866dff195d28870cddd4120e59640fbec436a5bb
0xnirmal <nirmal@artemisanalytics.xyz>
2024-04-19 10:02:25 -0400

    fix compilation error

--- models/metrics/stablecoins/contracts/fact_blast_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_blast_stablecoin_contracts.sql
@@ -3,5 +3,5 @@ select symbol, contract_address, num_decimals, coingecko_id, initial_supply
 from
     (
         values
-            ('USDB', '0x4300000000000000000000000000000000000003', 18, 'usdb', 0),
+            ('USDB', '0x4300000000000000000000000000000000000003', 18, 'usdb', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
