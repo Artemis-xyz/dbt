7e56375baea6a5f2a2a1ea4cf0dba353e8b4d425
aqkan <154107106+aqkan@users.noreply.github.com>
2024-04-25 14:55:25 -0400

    Add daily sleep model for Solana

---
+++ models/staging/solana/fact_solana_daily_sleep.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key=["from_address", "raw_date"],
+        snowflake_warehouse="ETHEREUM",
+    )
+}}
+
+{{ fact_daily_sleep("solana", "signers[0]") }}
