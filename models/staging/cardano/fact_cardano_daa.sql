1de8a7e7999eb6c273018e2bb6d1f7c2550a0ea1
Anthony Yim <anthonyyim@gmail.com>
2024-05-07 19:06:33 -0400

    Use new SF table for Cardano DAA

--- models/staging/cardano/fact_cardano_daa.sql
+++ models/staging/cardano/fact_cardano_daa.sql
@@ -2,11 +2,11 @@
 with
     max_extraction as (
         select max(extraction_date) as max_date
-        from {{ source("PROD_LANDING", "raw_cardano_daa") }}
+        from {{ source("PROD_LANDING", "raw_cardano_daa_partitioned") }}
     ),
     cardano_data as (
         select parse_json(source_json) as data
-        from {{ source("PROD_LANDING", "raw_cardano_daa") }}
+        from {{ source("PROD_LANDING", "raw_cardano_daa_partitioned") }}
         where extraction_date = (select max_date from max_extraction)
     )
 select date(value[0]) as date, value[1] as daa, value as source, 'cardano' as chain
