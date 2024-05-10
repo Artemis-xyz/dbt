dd360f9fac25fc66f77e0420dd6e7f1242a01aaa
Alex Weseley <pampawes@gmail.com>
2024-05-10 00:22:00 -0400

    adding refs so dagster shows lineages

--- models/projects/jupiter/fact_jupiter_fees_silver.sql
+++ models/projects/jupiter/fact_jupiter_fees_silver.sql
@@ -2,10 +2,10 @@ select
    lo.date,
    coalesce(dca.fees,0) + coalesce(lo.fees,0) + coalesce(perps.fees, 0) as fees
 from
-    pc_dbt_db.prod.fact_jupiter_limit_order_fees_silver lo
+    {{ ref("fact_jupiter_limit_order_fees_silver") }} lo
 left join
-     pc_dbt_db.prod.fact_jupiter_dca_fees_silver dca on dca.date = lo.date
+     {{ ref("fact_jupiter_dca_fees_silver") }} dca on dca.date = lo.date
 left join
-    pc_dbt_db.prod.fact_jupiter_perps_silver perps on perps.date = dca.date
+    {{ ref("fact_jupiter_perps_silver") }} perps on perps.date = dca.date
 where lo.date < to_date(sysdate())
 order by 1 asc
