f4fc0d11141f39c09dff8c9be49248420f18a60c
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-05-10 09:21:37 -0400

    Revert "Alw jupiter perps volume traders"

--- models/projects/jupiter/ez_jupiter_metrics.sql
+++ models/projects/jupiter/ez_jupiter_metrics.sql
@@ -10,25 +10,18 @@
 
 with
     fees_data as (
-        select date, fees
+        select date, fees, 'solana' as chain, 'jupiter' as protocol
         from {{ ref("fact_jupiter_fees_silver") }}
-    ),
-    perps_data as (
-        select date, volume, traders
-        from {{ ref("fact_jupiter_perps_silver")}}
     ),
     price_data as ({{ get_coingecko_metrics("jupiter-exchange-solana") }})
 select
-    fees_data.date as date,
-    'solana' as chain,
-    'jupiter' as protocol,
-    fees_data.fees as fees,
-    volume as trading_volume,
-    traders as unique_traders,
+    fees_data.date,
+    fees_data.chain,
+    fees_data.protocol,
+    fees,
     price,
     market_cap,
     fdmc
 from fees_data
-left join perps_data on perps_data.date = fees_data.date
 left join price_data on fees_data.date = price_data.date
 where fees_data.date < to_date(sysdate())
--- models/projects/jupiter/fact_jupiter_fees_silver.sql
+++ models/projects/jupiter/fact_jupiter_fees_silver.sql
@@ -2,10 +2,10 @@ select
    lo.date,
    coalesce(dca.fees,0) + coalesce(lo.fees,0) + coalesce(perps.fees, 0) as fees
 from
-    {{ ref("fact_jupiter_limit_order_fees_silver") }} lo
+    pc_dbt_db.prod.fact_jupiter_limit_order_fees_silver lo
 left join
-     {{ ref("fact_jupiter_dca_fees_silver") }} dca on dca.date = lo.date
+     pc_dbt_db.prod.fact_jupiter_dca_fees_silver dca on dca.date = lo.date
 left join
-    {{ ref("fact_jupiter_perps_silver") }} perps on perps.date = dca.date
+    pc_dbt_db.prod.fact_jupiter_perps_fees_silver perps on perps.date = dca.date
 where lo.date < to_date(sysdate())
 order by 1 asc
