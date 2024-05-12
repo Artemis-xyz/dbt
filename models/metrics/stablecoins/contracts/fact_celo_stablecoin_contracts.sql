5f77f7ef987410ed5d8e9565e82bd5cb6fe8553f
Sebastian Melendez <sebm@seas.upenn.edu>
2024-05-12 17:41:16 -0400

    adding cusd

--- models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
@@ -7,5 +7,6 @@ from
             ('USDT', '0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e', 6, 'tether', 0),
             ('cEUR', '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73', 18, 'celo-euro', 0),
             ('cREAL', '0xe8537a3d056da446677b9e9d6c5db704eaab4787', 18, 'celo-real-creal', 0),
-            ('USDGLO', '0x4F604735c1cF31399C6E711D5962b2B3E0225AD3', 18, 'glo-dollar', 0)
+            ('USDGLO', '0x4F604735c1cF31399C6E711D5962b2B3E0225AD3', 18, 'glo-dollar', 0),
+            ('cUSD', '0x765DE816845861e75A25fCA122bb6898B8B1282a', 18, 'celo-dollar', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
