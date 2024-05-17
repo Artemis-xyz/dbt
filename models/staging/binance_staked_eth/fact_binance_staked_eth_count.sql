c65f089d220dab338b773891d97dd0b4256e9db6
0xnirmal <nirmal@artemisanalytics.xyz>
2024-05-17 16:23:14 -0400

    add binance the right way - with repeg

--- models/staging/binance_staked_eth/fact_binance_staked_eth_count.sql
+++ models/staging/binance_staked_eth/fact_binance_staked_eth_count.sql
@@ -1,13 +1,26 @@
 {{ config(materialized="table") }}
-
-with eth_supply as (
-    {{ daily_erc20_total_supply("0xa2E3356610840701BDf5611a53974510Ae27E2e1", 18, "ethereum") }}
+-- depends_on: {{ source("PROD_LANDING", "raw_beth_staked_count_ethereum") }}
+-- depends_on: {{ source("PROD_LANDING", "raw_beth_staked_count_bsc") }}
+with raw_eth_supply as (
+    {{ raw_partitioned_array_to_fact_table_many_columns('landing_database.prod_landing.raw_beth_staked_count_ethereum', 'date', ['totalSupply', 'exchangeRate']) }}
+),
+eth_supply as (
+    select 
+        date,
+        totalSupply * exchangeRate as total_supply
+    from raw_eth_supply
 ),
 eth_supply_forward_filled as (
     {{ forward_fill('date', 'total_supply', 'eth_supply') }}
 ),
+raw_bsc_supply as (
+    {{ raw_partitioned_array_to_fact_table_many_columns('landing_database.prod_landing.raw_beth_staked_count_bsc', 'date', ['totalSupply', 'exchangeRate']) }}
+),
 bsc_supply as (
-    ({{ daily_erc20_total_supply("0xa2E3356610840701BDf5611a53974510Ae27E2e1", 18,"bsc") }})
+    select 
+        date,
+        totalSupply * exchangeRate as total_supply
+    from raw_bsc_supply
 ),
 bsc_supply_forward_filled as (
     {{ forward_fill('date', 'total_supply', 'bsc_supply') }}
