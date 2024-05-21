77a7774eac789628acaa1d38a123c057eaae205e
Sebastian Melendez <sebm@seas.upenn.edu>
2024-05-21 15:26:42 -0400

    updating ton dex data

--- models/projects/ton/core/ez_ton_metrics.sql
+++ models/projects/ton/core/ez_ton_metrics.sql
@@ -24,6 +24,12 @@ with
     ),
     price_data as ({{ get_coingecko_metrics("the-open-network") }}),
     defillama_data as ({{ get_defillama_metrics("ton") }}),
+    dex_data as (
+        select
+            date,
+            dex_volumes
+        from {{ ref("fact_ton_dex_volumes") }}
+    ),
     github_data as ({{ get_github_metrics("ton") }})
 select
     fundamental_data.date,
@@ -39,7 +45,7 @@ select
     market_cap,
     fdmc,
     tvl,
-    dex_volumes,
+    dex_data.dex_volumes,
     weekly_commits_core_ecosystem,
     weekly_commits_sub_ecosystem,
     weekly_developers_core_ecosystem,
@@ -48,4 +54,5 @@ from fundamental_data
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
 left join github_data on fundamental_data.date = github_data.date
+left join dex_data on fundamental_data.date = dex_data.date
 where fundamental_data.date < to_date(sysdate())
