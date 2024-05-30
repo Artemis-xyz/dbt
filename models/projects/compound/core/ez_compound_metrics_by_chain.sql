45c911b22450fb120b4f02f28ad37dc17d718c27
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-05-30 09:44:45 -0400

    Add ez compound_metrics_by_chain

---
+++ models/projects/compound/core/ez_compound_metrics_by_chain.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="COMPOUND",
+        database="compound",
+        schema="core",
+        alias="ez_metrics_by_chain",
+    )
+}}
+
+with
+    compound_by_chain as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    ref("fact_compound_lending_arbitrum_gold"),
+                    ref("fact_compound_lending_base_gold"),
+                    ref("fact_compound_lending_ethereum_gold"),
+                    ref("fact_compound_lending_polygon_gold"),
+                ],
+            )
+        }}
+    )
+select
+    compound_by_chain.date,
+    'compound' as app,
+    'DeFi' as category,
+    compound_by_chain.chain,
+    compound_by_chain.daily_borrows_usd,
+    compound_by_chain.daily_supply_usd
+from compound_by_chain
+where compound_by_chain.date < to_date(sysdate())
