596beea541bc5945d5bc4246ed964e45cfc73ec4
Son Do <sondo077@gmail.com>
2024-05-30 16:17:29 -0400

    Added sei v2 metrics and split between wasm and evm

--- models/staging/sei/fact_sei_avg_block_time_silver.sql
+++ models/staging/sei/fact_sei_avg_block_time_silver.sql
@@ -1,9 +1,14 @@
-{{ config(materialized="table") }}
+{{ config(materialized="incremental", unique_key="date", snowflake_warehouse="sei") }}
 
 select 
     date(a.block_timestamp) as date, 
     avg(datediff(second,a.block_timestamp, b.block_timestamp)) as avg_block_time
 from sei_flipside.core.fact_blocks a, sei_flipside.core.fact_blocks b
 where a.block_id = b.block_id-1 
-and date(a.block_timestamp) < date(sysdate())
+and date(a.block_timestamp) < date(sysdate()) 
+and date(b.block_timestamp) < date(sysdate())
+{% if is_incremental() %}
+    and a.block_timestamp >= (select dateadd('day', -5, max(date)) from {{ this }})
+    and b.block_timestamp >= (select dateadd('day', -5, max(date)) from {{ this }})
+{% endif %}
 group by 1
