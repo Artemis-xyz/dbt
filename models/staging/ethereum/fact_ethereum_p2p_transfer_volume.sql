3f46173c94b91f2b5a2542ab5ee705cb70152a9e
Sebastian Melendez <sebm@seas.upenn.edu>
2024-06-04 10:02:07 -0400

    fixing c911 issues with l1 models

--- models/staging/ethereum/fact_ethereum_p2p_transfer_volume.sql
+++ models/staging/ethereum/fact_ethereum_p2p_transfer_volume.sql
@@ -1,3 +1,8 @@
+-- depends_on {{ ref("fact_ethereum_p2p_native_transfers") }}
+-- depends_on {{ ref("fact_ethereum_p2p_token_transfers") }}
+-- depends_on {{ ref("fact_ethereum_p2p_stablecoin_transfers") }}
+
+
 {{
     config(
         materialized="table",
