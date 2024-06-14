f9dcbcc06594c87d416f0131cb48bc91d7607aef
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-06-14 14:41:20 -0400

    creating fuse ez_metrics (#178)

---
+++ models/projects/fuse/core/ez_fuse_metrics.sql
@@ -0,0 +1,44 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="FUSE",
+        database="fuse",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+
+with
+    fundamental_data as (
+        select
+            date
+            , txns
+            , daa as dau
+            , gas_usd as fees
+            , gas as fees_native
+        from {{ ref("fact_fuse_daa_txns_gas_gas_usd") }}
+    )
+    , github_data as ({{ get_github_metrics("fuse") }})
+    , defillama_data as ({{ get_defillama_metrics("fuse") }})
+    , price_data as ({{ get_coingecko_metrics("fuse-network-token") }})
+select
+    fundamental_data.date
+    , 'fuse' as chain
+    , txns
+    , dau
+    , fees
+    , fees_native
+    , tvl
+    , dex_volumes
+    , weekly_commits_core_ecosystem
+    , weekly_commits_sub_ecosystem
+    , weekly_developers_core_ecosystem
+    , weekly_developers_sub_ecosystem
+    , price
+    , market_cap
+    , fdmc
+from fundamental_data
+left join github_data using (date)
+left join defillama_data using (date)
+left join price_data using (date)
+where fundamental_data.date < to_date(sysdate())
--- models/staging/fuse/fact_fuse_daa_txns_gas_gas_usd.sql
+++ models/staging/fuse/fact_fuse_daa_txns_gas_gas_usd.sql
@@ -1,4 +1,4 @@
-{{ config(materialized="view") }}
+{{ config(materialized="view", snowflake_warehouse="FUSE") }}
 select t1.date, t1.chain, daa, txns, gas, gas_usd
 from {{ ref("fact_fuse_daa") }} t1
 left join {{ ref("fact_fuse_gas_gas_usd") }} t2 on t1.date = t2.date
