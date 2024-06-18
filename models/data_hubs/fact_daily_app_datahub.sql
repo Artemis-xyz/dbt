cc325e8c0ffe86e8d62162e23228666d69b98632
Son Do <sondo077@gmail.com>
2024-06-18 16:19:11 -0400

    remove deprecated model from datahub

--- models/data_hubs/fact_daily_app_datahub.sql
+++ models/data_hubs/fact_daily_app_datahub.sql
@@ -4,7 +4,6 @@ with
         {{
             dbt_utils.union_relations(
                 relations=[
-                    ref("agg_daily_app_fundamental_usage"),
                     ref("fact_stargate_bridge_volume_gold"),
                     ref("fact_venus_lending_bsc_gold"),
                     ref("fact_sushiswap_arbitrum_gold"),
