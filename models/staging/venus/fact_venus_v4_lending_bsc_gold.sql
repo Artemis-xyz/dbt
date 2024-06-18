ee3d56bd1aeced3354d5fbaf9ae7dd440316f903
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-06-18 11:57:11 -0400

    rm unnecessary model

--- models/staging/venus/fact_venus_v4_lending_bsc_gold.sql
+++ models/staging/venus/fact_venus_v4_lending_bsc_gold.sql
@@ -1,3 +1,3 @@
 {{ config(materialized="table", snowflake_warehouse="VENUS") }}
-select date, chain, app, category, daily_borrows_usd, daily_supply_usd
+select date, chain, 'venus' as app, category, daily_borrows_usd, daily_supply_usd
 from {{ ref("fact_venus_v4_lending_bsc") }}
