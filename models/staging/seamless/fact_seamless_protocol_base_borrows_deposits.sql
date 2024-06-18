fd4cd3211de238f6c38e4963be767b57e48ec30d
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-06-18 15:29:54 -0400

    Update name of seamless db and wh

--- models/staging/seamless/fact_seamless_protocol_base_borrows_deposits.sql
+++ models/staging/seamless/fact_seamless_protocol_base_borrows_deposits.sql
@@ -1,4 +1,4 @@
-{{ config(materialized="table", snowflake_warehouse="SEAMLESS") }}
+{{ config(materialized="table", snowflake_warehouse="SEAMLESSPROTOCOL") }}
 {{
     fact_aave_fork_lending(
         "raw_seamless_protocol_base_borrows_deposits", "base", "seamlessprotocol"
