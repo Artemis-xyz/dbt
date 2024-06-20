6feb0767b15d3598e3cd993ac679ebef4a85c76e
Sebastian Melendez <sebm@seas.upenn.edu>
2024-06-20 19:21:04 -0400

    updating to add pricing

--- macros/stablecoins/agg_daily_stablecoin_metrics_breakdown.sql
+++ macros/stablecoins/agg_daily_stablecoin_metrics_breakdown.sql
@@ -66,33 +66,67 @@ with
             ) as p2p_stablecoin_dau
         from transfer_transactions
         group by 1, 2, 3, 4
+    ),
+    results as (
+        select
+            transfer_transactions_agg.date
+            --stablecoin idenifiers
+            , transfer_transactions_agg.contract_address
+            , transfer_transactions_agg.symbol as symbol
+            --sender idenifiers
+            , transfer_transactions_agg.from_address
+            , filtered_contracts.name as contract_name
+            , coalesce(filtered_contracts.name, transfer_transactions_agg.from_address) as contract
+            , filtered_contracts.friendly_name as application
+            , dim_apps_gold.icon as icon
+            , filtered_contracts.app as app
+            , filtered_contracts.category as category
+            --metrics
+            , coalesce(stablecoin_transfer_volume, 0) as stablecoin_transfer_volume
+            , coalesce(stablecoin_daily_txns, 0) as stablecoin_daily_txns
+            , coalesce(stablecoin_dau, 0) stablecoin_dau
+            --p2p metrics
+            , coalesce(p2p_stablecoin_transfer_volume, 0) as p2p_stablecoin_transfer_volume
+            , coalesce(p2p_stablecoin_daily_txns, 0) as p2p_stablecoin_daily_txns
+            , coalesce(p2p_stablecoin_dau, 0) as p2p_stablecoin_dau
+            , '{{ chain }}' as chain
+        from transfer_transactions_agg
+        left join filtered_contracts
+            on lower(transfer_transactions_agg.from_address) = lower(filtered_contracts.address)
+        left join pc_dbt_db.prod.dim_apps_gold dim_apps_gold 
+            on filtered_contracts.app = dim_apps_gold.namespace
+    ),
+    results_dollar_denom as (
+        select
+            results.date
+            , results.contract_address
+            , results.symbol
+
+            , from_address
+            , contract_name
+            , contract
+            , application
+            , icon
+            , app
+            , category
+            , stablecoin_transfer_volume * coalesce(
+                d.token_current_price, 1
+            ) as stablecoin_transfer_volume
+            , stablecoin_daily_txns
+            , stablecoin_dau
+            , p2p_stablecoin_transfer_volume * coalesce(
+                d.token_current_price, 1
+            ) as p2p_stablecoin_transfer_volume
+            , p2p_stablecoin_daily_txns
+            , p2p_stablecoin_dau
+            , chain
+        from results
+        left join {{ ref( "fact_" ~ chain ~ "_stablecoin_contracts") }} c
+            on lower(results.contract_address) = lower(c.contract_address)
+        left join {{ ref( "fact_coingecko_token_realtime_data") }} d
+            on lower(c.coingecko_id) = lower(d.token_id)
     )
-    select
-        transfer_transactions_agg.date
-        --stablecoin idenifiers
-        , transfer_transactions_agg.contract_address
-        , transfer_transactions_agg.symbol as symbol
-        --sender idenifiers
-        , transfer_transactions_agg.from_address
-        , filtered_contracts.name as contract_name
-        , coalesce(filtered_contracts.name, transfer_transactions_agg.from_address) as contract
-        , filtered_contracts.friendly_name as application
-        , dim_apps_gold.icon as icon
-        , filtered_contracts.app as app
-        , filtered_contracts.category as category
-        --metrics
-        , coalesce(stablecoin_transfer_volume, 0) as stablecoin_transfer_volume
-        , coalesce(stablecoin_daily_txns, 0) as stablecoin_daily_txns
-        , coalesce(stablecoin_dau, 0) stablecoin_dau
-        --p2p metrics
-        , coalesce(p2p_stablecoin_transfer_volume, 0) as p2p_stablecoin_transfer_volume
-        , coalesce(p2p_stablecoin_daily_txns, 0) as p2p_stablecoin_daily_txns
-        , coalesce(p2p_stablecoin_dau, 0) as p2p_stablecoin_dau
-        , '{{ chain }}' as chain
-    from transfer_transactions_agg
-    left join filtered_contracts
-        on lower(transfer_transactions_agg.from_address) = lower(filtered_contracts.address)
-    left join pc_dbt_db.prod.dim_apps_gold dim_apps_gold 
-        on filtered_contracts.app = dim_apps_gold.namespace
-    where transfer_transactions_agg.date < to_date(sysdate())
+select *
+from results_dollar_denom
+where date < to_date(sysdate())
 {% endmacro %}
