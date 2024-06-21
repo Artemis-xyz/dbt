8ec0154b565c66051e306ef44f2d2f4d06c419b2
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-06-21 15:10:13 -0400

    rm scroll orgpaned tables (#227)

--- models/staging/scroll/fact_scroll_gas_gas_usd_revenue_gold.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized="table") }}
-select date, gas, gas_usd, revenue, chain
-from {{ ref("fact_scroll_gas_gas_usd_revenue") }}
--- models/staging/scroll/fact_scroll_txns_gold.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized="table", snowflake_warehouse="SCROLL") }}
-select date, txns, chain
-from {{ ref("fact_scroll_txns") }}
