173e5e826c5c0e9cf534f1647c4fc97f08ff4ba8
Sebastian Melendez <sebm@seas.upenn.edu>
2024-06-21 12:27:08 -0400

    rm orphaned

--- models/staging/stacks/fact_stacks_daa_txns_gold.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized="table") }}
-
-select date, chain, daa, txns
-from {{ ref("fact_stacks_daa_txns") }}
--- models/staging/stacks/fact_stacks_native_fees_gold.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized="table") }}
-select date, native_token_fees, fees, chain
-from {{ ref("fact_stacks_native_fees") }}
