1b174e3265ddce9d38fc7a96ef601fb6f20f475f
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-06-21 15:09:02 -0400

    rm flow orphaned (#237)

--- models/staging/flow/fact_flow_daa_txns_gold.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized="table") }}
-select date, daa, txns, chain
-from {{ ref("fact_flow_daa_txns") }}
-where date < to_date(sysdate())
--- models/staging/flow/fact_flow_fees_revs_gold.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized="table") }}
-select date, total_fees_usd as fees, fees_burned_usd as revenue, 'flow' as chain
-from {{ ref("fact_flow_fees_revs") }}
-where date < to_date(sysdate())
