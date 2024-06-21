7a72f644adc1a8bbcf55707401ada8c74e71c2cd
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-06-21 14:53:24 -0400

    rm axie, aptos, acala, axelar orphaned (#243)

--- models/staging/axelar/fact_axelar_daa_txns_gold.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized="table") }}
-select date, daa, txns, chain
-from {{ ref("fact_axelar_daa_txns") }}
--- models/staging/axie/fact_axie_sales_data_gold.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized="table") }}
-select *
-from {{ ref("fact_axie_sales_data") }}
-where date < to_date(sysdate())
