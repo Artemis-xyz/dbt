ff69b312a20c00e901f5563faa7bd1b1fa2e1aed
Sebastian Melendez <sebm@seas.upenn.edu>
2024-06-27 18:51:43 -0400

    updating commit mistake

--- models/staging/solana/agg_solana_daily_stablecoin_metrics_breakdown.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized="incremental",
-        unique_key=["date", "symbol", "from_address"],
-        snowflake_warehouse="BALANCES_LG",
-    )
-}}
-
-{{ agg_daily_stablecoin_metrics_breakdown("solana") }}
