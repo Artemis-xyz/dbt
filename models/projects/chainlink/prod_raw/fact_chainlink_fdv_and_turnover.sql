502b420c1cbe37cf3b081088f6b5f50391ec005c
alexwes <pampawes@gmail.com>
2024-06-28 16:40:53 -0400

    Chainlink financials treasury (#262)

---
+++ models/projects/chainlink/prod_raw/fact_chainlink_fdv_and_turnover.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="CHAINLINK",
+        database="chainlink",
+        schema="raw",
+        alias="fact_fdv_and_turnover",
+    )
+}}
+
+
+SELECT
+    date,
+    shifted_token_price_usd * 1000000000 as fully_diluted_market_cap,
+    shifted_token_h24_volume_usd / shifted_token_market_cap as token_turnover_circulating,
+    shifted_token_h24_volume_usd / fully_diluted_market_cap as token_turnover_fdv
+FROM pc_dbt_db.prod.fact_coingecko_token_date_adjusted_gold
+where coingecko_id = 'chainlink'
+order by date desc
