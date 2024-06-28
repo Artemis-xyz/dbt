041e390439551009df2283ea7f3af21aee842561
Anthony Yim <anthonyyim@gmail.com>
2024-06-28 14:19:35 -0400

    Add metrics breakdown table for EVM chains (#259)

---
+++ models/staging/polygon/agg_polygon_daily_stablecoin_metrics_breakdown.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key=["date", "contract_address", "from_address"],
+        snowflake_warehouse="BALANCES_LG_2",
+    )
+}}
+
+{{ agg_daily_stablecoin_metrics_breakdown("polygon") }}
