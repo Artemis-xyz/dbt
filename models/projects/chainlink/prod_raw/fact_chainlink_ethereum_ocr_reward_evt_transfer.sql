8dc0055575594ff2bd5108c6d5abd0d99768a284
Sebastian Melendez <sebm@seas.upenn.edu>
2024-06-28 13:51:19 -0400

    fixing merge error

--- models/projects/chainlink/prod_raw/fact_chainlink_ethereum_ocr_reward_evt_transfer.sql
+++ models/projects/chainlink/prod_raw/fact_chainlink_ethereum_ocr_reward_evt_transfer.sql
@@ -18,11 +18,7 @@ select
 from ethereum_flipside.core.ez_token_transfers reward_evt_transfer
 right join {{ ref('fact_chainlink_ethereum_ocr_reward_transmission_logs') }} ocr_reward_transmission_logs 
     on lower(ocr_reward_transmission_logs.contract_address) = lower(reward_evt_transfer.from_address)
-<<<<<<< HEAD
 left join {{ ref('dim_chainlink_ethereum_ocr_operator_admin_meta') }} ocr_operator_admin_meta 
-=======
-left join {{ ref('fact_chainlink_ethereum_ocr_operator_admin_meta') }} ocr_operator_admin_meta 
->>>>>>> origin/main
     on lower(ocr_operator_admin_meta.admin_address) = lower(reward_evt_transfer.to_address)
 where lower(reward_evt_transfer.from_address) in (select lower(contract_address) from {{ ref('fact_chainlink_ethereum_ocr_reward_transmission_logs') }})
 GROUP BY
