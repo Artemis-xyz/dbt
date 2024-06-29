7d09b80ce2188a1b8540bb1b38bd795209133422
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-06-28 20:10:34 -0400

    Chainlink: EZ Metrics (#267)

--- models/projects/chainlink/prod_raw/fact_chainlink_fdv_and_turnover.sql
+++ models/projects/chainlink/prod_raw/fact_chainlink_fdv_and_turnover.sql
@@ -11,6 +11,7 @@
 
 SELECT
     date,
+    shifted_token_h24_volume_usd as token_volume,
     shifted_token_price_usd * 1000000000 as fully_diluted_market_cap,
     shifted_token_h24_volume_usd / shifted_token_market_cap as token_turnover_circulating,
     shifted_token_h24_volume_usd / fully_diluted_market_cap as token_turnover_fdv
