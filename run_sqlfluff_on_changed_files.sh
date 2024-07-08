d6d3784e683128766bc13acdd42dc7c999349a14
0xnirmal <nirmal@artemisanalytics.xyz>
2024-07-08 17:43:17 -0400

    more changes

---
+++ run_sqlfluff_on_changed_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Get the list of changed SQL files
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACM | grep '\.sql$')
+
+if [ -z "$CHANGED_FILES" ]; then
+  echo "No changed SQL files to lint"
+  exit 0
+fi
+
+# Run SQLFluff on each changed SQL file
+for file in $CHANGED_FILES; do
+  sqlfluff lint "$file"
+  if [ $? -ne 0 ]; then
+    echo "SQLFluff found issues in $file"
+    exit 1
+  fi
+done
+
+exit 0
