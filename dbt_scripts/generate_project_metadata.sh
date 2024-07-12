546f6da259dcad1fc57f8b2927033216da44a05b
Sebastian Melendez <sebm@seas.upenn.edu>
2024-07-12 10:19:04 -0400

    creating bash script to generate group metadata file

---
+++ dbt_scripts/generate_project_metadata.sh
@@ -0,0 +1,20 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+directory=$1
+project=$(basename "$directory")
+
+output_file="$directory/__${project}__metadata.yml"
+
+echo "models:" >> "$output_file"
+
+find "$directory" -type f -name "*.sql" -exec basename {} \; | while read -r filename; do
+    filename_without_extension="${filename%.sql}"
+    echo "  - name: $filename_without_extension" >> "$output_file"
+    echo "    config:" >> "$output_file"
+    echo "      meta:" >> "$output_file"
+    echo "        dagster:" >> "$output_file"
+    echo "          group: chainlink" >> "$output_file"
+done
