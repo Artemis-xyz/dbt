7e7867e69a8ccaa07540c6ba70e3d45f3cf2f852
Sebastian Melendez <sebm@seas.upenn.edu>
2024-07-17 12:29:47 -0400

    separating premint and bridge addresses

--- models/metrics/stablecoins/transfers/fact_tron_stablecoin_transfers.sql
+++ models/metrics/stablecoins/transfers/fact_tron_stablecoin_transfers.sql
@@ -1,7 +1,7 @@
 -- depends_on: {{ ref('fact_tron_stablecoin_contracts') }}
--- depends_on: {{ ref('fact_tron_stablecoin_premint_addresses') }}
+-- depends_on: {{ ref('fact_tron_stablecoin_bridge_addresses') }}
 {{ config(
-    materialized="incremental", 
+    materialized="table", 
         snowflake_warehouse="STABLECOIN_LG_2", 
         unique_key=["tx_hash", "index"],
     ) 
