9b9d922e50efc8e9e78a18f2fc87389936907c22
Sebastian Melendez <sebm@seas.upenn.edu>
2024-07-17 12:33:57 -0400

    changing back to incremental

--- models/metrics/stablecoins/transfers/fact_ethereum_stablecoin_transfers.sql
+++ models/metrics/stablecoins/transfers/fact_ethereum_stablecoin_transfers.sql
@@ -3,7 +3,7 @@
 -- depends_on: {{ ref('fact_ethereum_stablecoin_bridge_addresses') }}
 
 {{ config(
-        materialized="table", 
+        materialized="incremental", 
         snowflake_warehouse="STABLECOIN_LG_2", 
         unique_key=["tx_hash", "index"],
     ) 
--- models/metrics/stablecoins/transfers/fact_tron_stablecoin_transfers.sql
+++ models/metrics/stablecoins/transfers/fact_tron_stablecoin_transfers.sql
@@ -1,7 +1,7 @@
 -- depends_on: {{ ref('fact_tron_stablecoin_contracts') }}
 -- depends_on: {{ ref('fact_tron_stablecoin_bridge_addresses') }}
 {{ config(
-    materialized="table", 
+    materialized="incremental", 
         snowflake_warehouse="STABLECOIN_LG_2", 
         unique_key=["tx_hash", "index"],
     ) 
