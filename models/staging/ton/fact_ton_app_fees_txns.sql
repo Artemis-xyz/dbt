f34a00e54af93fc42578d7938b4f7dabd2ae3045
Son Do <sondo077@gmail.com>
2024-07-18 09:51:04 -0400

    Added Ton Apps Fundamental Data

---
+++ models/staging/ton/fact_ton_app_fees_txns.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key="date",
+    )
+}}
+with raw_data as (
+    select
+        extraction_date
+        , source_json
+        , source_json:"raw_date"::date as date
+        , source_json:"fees_native"::double as fees_native
+        , source_json:"txns"::bigint as txns
+    from
+        {{ source("PROD_LANDING", "raw_ton_app_txns_fees") }}
+    {% if is_incremental() %}
+        where source_json:"date_timestamp"::timestamp > (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
+    {% endif %}
+)
+select 
+    date
+    , max_by(fees_native, extraction_date) as fees_native
+    , max_by(txns, extraction_date) as txns
+from raw_data
+group by date
