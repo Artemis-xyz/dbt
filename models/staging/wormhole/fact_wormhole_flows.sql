79f4da08d6555585ca9522c41b1033773bea43ee
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-07-17 23:17:11 -0400

    Add missing commas

--- models/staging/wormhole/fact_wormhole_flows.sql
+++ models/staging/wormhole/fact_wormhole_flows.sql
@@ -69,8 +69,8 @@ with
                     (4001, 'evmos'),
                     (4002, 'kujira'),
                     (4003, 'neutron'),
-                    (4004, 'celestia')
-                    (4005, 'stargaze')
+                    (4004, 'celestia'),
+                    (4005, 'stargaze'),
                     (4006, 'seda'),
                     (4007, 'dymension'),
                     (4008, 'provenance'),
