8ccc0d9fc0441f5911e17095fffb4a1f438a6930
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-07-21 23:06:52 -0400

    Col name in inj bridge flows should be block_date

--- models/staging/injective/fact_injective_bridge_flows.sql
+++ models/staging/injective/fact_injective_bridge_flows.sql
@@ -1,4 +1,4 @@
 SELECT
-    * EXCLUDE date,
-    DATEADD('day', 1, DATE_TRUNC('day', TO_TIMESTAMP_NTZ(date))) AS date
+    * EXCLUDE block_date,
+    DATE_TRUNC('day', TO_TIMESTAMP_NTZ(block_date)) AS date
 FROM {{ source('PROD_LANDING', 'injective_bridge_flows') }}
