851d6b45b6636b0ec0bafca265909bc82599cd0a
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-07-22 22:19:06 -0400

    Stablecoin V2: Polygon, Tron (#328)

--- models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_silver.sql
+++ models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_silver.sql
@@ -9,6 +9,8 @@ with
                     ref("ez_optimism_stablecoin_metrics_by_address"),
                     ref("ez_avalanche_stablecoin_metrics_by_address"),
                     ref("ez_ethereum_stablecoin_metrics_by_address"),
+                    ref("ez_polygon_stablecoin_metrics_by_address"),
+                    ref("ez_tron_stablecoin_metrics_by_address"),
                 ]
             )
         }}
