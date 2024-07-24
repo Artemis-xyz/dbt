beae4c90a438096f9e958cf690de9d47ff0eba17
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-07-23 21:14:09 -0400

    Rollup ibc

--- models/metrics/bridges/agg_daily_bridge_flows_metrics_silver.sql
+++ models/metrics/bridges/agg_daily_bridge_flows_metrics_silver.sql
@@ -6,20 +6,64 @@ with
             dbt_utils.union_relations(
                 relations=[
                     ref("fact_across_flows"),
-                    ref("fact_synapse_flows"),
-                    ref("fact_wormhole_flows"),
-                    ref("fact_arbitrum_one_bridge_flows"),
-                    ref("fact_avalanche_bridge_flows"),
-                    ref("fact_base_bridge_flows"),
-                    ref("fact_optimism_bridge_flows"),
-                    ref("fact_injective_bridge_flows"),
-                    ref("fact_polygon_pos_bridge_flows"),
-                    ref("fact_starknet_bridge_flows"),
-                    ref("fact_zksync_era_bridge_flows"),
-                    ref("fact_rainbow_bridge_flows"),
                 ]
             )
         }}
+    ), cosmos_ecosystem AS (
+        -- Roll up all Cosmos chains into a single record for the whole ecosystem
+        -- Get IBC flows from Injective
+        SELECT
+            date,
+            'injective_ibc' AS app,
+            'injective' AS source_chain,
+            'cosmos_ecosystem' AS destination_chain,
+            category,
+            SUM(amount_usd) AS amount_usd,
+            NULL AS fee_usd
+        FROM {{ ref("fact_injective_bridge_flows") }}
+        WHERE
+            source_chain = 'injective'
+            AND app = 'ibc'
+        GROUP BY
+            date, category
+
+        UNION ALL
+
+        -- Get IBC flows to Injective
+        SELECT
+            date,
+            'injective_ibc' AS app,
+            'cosmos_ecosystem' AS source_chain,
+            'injective' AS destination_chain,
+            category,
+            SUM(amount_usd) AS amount_usd,
+            NULL AS fee_usd
+        FROM {{ ref("fact_injective_bridge_flows") }}
+        WHERE
+            destination_chain = 'injective'
+            AND app = 'ibc'
+        GROUP BY
+            date, category
+
+        UNION ALL
+
+        -- Get flows to/from Injective from Peggy
+        SELECT
+            date,
+            app,
+            source_chain,
+            destination_chain,
+            category,
+            amount_usd,
+            NULL AS fee_usd
+        FROM {{ ref("fact_injective_bridge_flows") }}
+        WHERE
+            app = 'peggy'
+    ),
+    unioned AS (
+        SELECT * EXCLUDE _dbt_source_relation FROM daily_data
+        UNION ALL
+        SELECT * FROM cosmos_ecosystem
     )
 select
     *,
@@ -32,5 +76,5 @@ select
     || destination_chain
     || '-'
     || category as unique_id
-from daily_data
+from unioned
 where date < to_date(sysdate())
