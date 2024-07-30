b27a8b198fae3ecc23f4bf6e39be85a5ab10030d
Son Do <34179343+Sunny77D@users.noreply.github.com>
2024-07-30 18:49:30 -0400

    Fixed Sui to not count failed transactions in DAU and transaction counts (#343)

--- models/projects/sui/core/ez_sui_metrics_by_category.sql
+++ models/projects/sui/core/ez_sui_metrics_by_category.sql
@@ -20,6 +20,16 @@ with
         from min_date
         group by start_date, category
     ),
+    dau_txns as (
+        select
+            raw_date,
+            category,
+            count(*) txns,
+            count(distinct sender) dau
+        from {{ ref("ez_sui_transactions") }}
+        where not equal_null(category, 'EOA') and status = 'success'
+        group by raw_date, category
+    ),
     agg_data as (
         select
             raw_date,
@@ -27,9 +37,7 @@ with
             max(chain) as chain,
             max(friendly_name) friendly_name,
             sum(tx_fee) gas,
-            sum(gas_usd) gas_usd,
-            count(*) txns,
-            count(distinct sender) dau
+            sum(gas_usd) gas_usd
         from {{ ref("ez_sui_transactions") }}
         where not equal_null(category, 'EOA')
         group by raw_date, category
@@ -50,6 +58,10 @@ select
     null as sybil_users,
     null as non_sybil_users
 from agg_data
+left join
+    dau_txns
+    on equal_null(agg_data.category, dau_txns.category)
+    and agg_data.raw_date = dau_txns.raw_date
 left join
     new_users
     on equal_null(agg_data.category, new_users.category)
