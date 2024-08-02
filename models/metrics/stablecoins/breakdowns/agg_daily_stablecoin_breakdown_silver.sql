5f528adff2edfa15303a2a7213d19c075317c2b7
Sebastian Melendez <sebm@seas.upenn.edu>
2024-08-02 15:19:51 -0400

    adding solana stablecoin models and create clustering on final table

--- models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_silver.sql
+++ models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_silver.sql
@@ -10,6 +10,7 @@ with
                     ref("ez_avalanche_stablecoin_metrics_by_address"),
                     ref("ez_polygon_stablecoin_metrics_by_address"),
                     ref("ez_ethereum_stablecoin_metrics_by_address"),
+                    ref("ez_solana_stablecoin_metrics_by_address"),
                 ]
             )
         }}
