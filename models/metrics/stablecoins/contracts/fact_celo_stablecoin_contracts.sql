435fb3faf3f4172ba8988ee78b0c1fe4eb8cafbe
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-08-08 14:17:17 -0400

    Stablecoin V2: Celo (#372)

--- models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_celo_stablecoin_contracts.sql
@@ -8,5 +8,6 @@ from
             ('cEUR', '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73', 18, 'celo-euro', 0),
             ('cREAL', '0xe8537a3d056da446677b9e9d6c5db704eaab4787', 18, 'celo-real-creal', 0),
             ('USDGLO', '0x4F604735c1cF31399C6E711D5962b2B3E0225AD3', 18, 'glo-dollar', 0),
-            ('cUSD', '0x765DE816845861e75A25fCA122bb6898B8B1282a', 18, 'celo-dollar', 0)
+            ('cUSD', '0x765DE816845861e75A25fCA122bb6898B8B1282a', 18, 'celo-dollar', 0),
+            ('cKES', '0x456a3d042c0dbd3db53d5489e98dfb038553b0d0', 18, 'celo-kenyan-shilling', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
