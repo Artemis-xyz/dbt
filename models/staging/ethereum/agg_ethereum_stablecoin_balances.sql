7cd8fb5bb6451f51d4ac8eff73b0abb493aab221
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-08-08 14:20:23 -0400

    Stablecoin V2: Clean up (#373)

--- models/staging/ethereum/agg_ethereum_stablecoin_balances.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized="incremental",
-        unique_key=["address", "contract_address", "date"],
-        snowflake_warehouse="BALANCES_LG",
-    )
-}}
-
-{{ agg_foward_filled_stablecoin_balances_by_addresses("ethereum") }}
--- models/staging/optimism/agg_optimism_daily_stablecoin_metrics_breakdown.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized="incremental",
-        unique_key=["date", "contract_address", "from_address"],
-        snowflake_warehouse="STABLECOIN_V2_LG_2",
-    )
-}}
-
-{{ agg_daily_stablecoin_metrics_breakdown("optimism") }}
