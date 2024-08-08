b6c35bb4a367327ee7ca03fa6028b51090df6a73
Son Do <34179343+Sunny77D@users.noreply.github.com>
2024-08-08 14:32:33 -0400

    Remove Blast stablecoin from data pipeline (#374)

--- models/metrics/stablecoins/breakdowns/weekly/agg_blast_stablecoin_breakdown_weekly.sql
+++
@@ -1,5 +0,0 @@
--- depends_on: {{ ref('fact_blast_stablecoin_contracts') }}
--- depends_on: {{ ref('fact_blast_address_balances_by_token') }}
--- depends_on: {{ ref('fact_blast_stablecoin_transfers') }}
-{{ config(materialized="table", snowflake_warehouse="STABLECOIN_LG_2") }}
-{{ agg_chain_stablecoin_breakdown("blast", "week") }}
