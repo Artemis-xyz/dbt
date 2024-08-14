7809f6c74c29d62798927fb3db0783c14dec91c2
Son Do <34179343+Sunny77D@users.noreply.github.com>
2024-08-14 14:55:37 -0400

    Adding usdc as an Asset (#398)

---
+++ models/projects/usdc/core/ez_usdc_metrics.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="USDC",
+        database="usdc",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+
+select 
+    'usdc' as symbol
+    , date
+    , sum(transfer_volume) as stablecoin_transfer_volume
+    , sum(deduped_transfer_volume) as deduped_stablecoin_transfer_volume
+    , sum(dau) as stablecoin_dau
+    , sum(txns) as stablecoin_txns
+    , sum(total_supply) as stablecoin_total_supply
+from {{ ref("agg_daily_stablecoin_metrics") }}
+where symbol = 'USDC'
+group by date
