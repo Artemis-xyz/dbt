d53fa33c38c7a0c61bbf533b6e6a93ab90e83367
Son Do <sondo077@gmail.com>
2024-08-14 14:33:39 -0400

    renamed columns to use match helper function seb made

--- models/projects/usdc/core/ez_usdc_metrics.sql
+++ models/projects/usdc/core/ez_usdc_metrics.sql
@@ -11,11 +11,11 @@
 select 
     symbol
     , date
-    , sum(transfer_volume) as transfer_volume
-    , sum(deduped_transfer_volume) as deduped_transfer_volume
-    , sum(dau) as dau
-    , sum(txns) as txns
-    , sum(total_supply) as total_supply
+    , sum(transfer_volume) as stablecoin_transfer_volume
+    , sum(deduped_transfer_volume) as deduped_stablecoin_transfer_volume
+    , sum(dau) as stablecoin_dau
+    , sum(txns) as stablecoin_txns
+    , sum(total_supply) as stablecoin_total_supply
 from {{ ref("agg_daily_stablecoin_metrics") }}
 where symbol = 'USDC'
 group by symbol, date
