1280f2f49ecbb8f77b456f78594c0eff142f3950
Son Do <sondo077@gmail.com>
2024-08-14 11:57:53 -0400

    Added USDC Projects

---
+++ models/projects/usdc/core/ez_usdc_metrics.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="USDC",
+        database="usdc",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+
+select 
+    symbol
+    , date
+    , sum(transfer_volume) as transfer_volume
+    , sum(deduped_transfer_volume) as deduped_transfer_volume
+    , sum(dau) as dau
+    , sum(txns) as txns
+    , sum(total_supply) as total_supply
+from {{ ref("agg_daily_stablecoin_metrics") }}
+where symbol = 'USDC'
+group by symbol, date 
+order by date desc
