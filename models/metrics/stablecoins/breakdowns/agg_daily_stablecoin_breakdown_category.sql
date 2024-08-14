0ab26b9ddabcbb7a134027dd8d532d81810e4a97
Sebastian Melendez <sebm@seas.upenn.edu>
2024-08-14 18:50:41 -0400

    adding new macro

--- models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_category.sql
+++ models/metrics/stablecoins/breakdowns/agg_daily_stablecoin_breakdown_category.sql
@@ -1,31 +1,3 @@
 {{ config(materialized="table", snowflake_warehouse="STABLECOIN_V2_LG") }}
-select
-    date
-    , category
-    , count(distinct case when stablecoin_daily_txns > 0 then from_address end) as stablecoin_dau
-    , sum(stablecoin_transfer_volume) as stablecoin_transfer_volume
-    , sum(stablecoin_daily_txns) as stablecoin_daily_txns
-    , case 
-        when sum(stablecoin_daily_txns) > 0 then sum(stablecoin_transfer_volume) / sum(stablecoin_daily_txns) 
-        else 0
-    end as stablecoin_avg_txn_value
-    , count(distinct case when artemis_stablecoin_daily_txns > 0 then from_address end) as artemis_stablecoin_dau
-    , sum(artemis_stablecoin_transfer_volume) as artemis_stablecoin_transfer_volume
-    , sum(artemis_stablecoin_daily_txns) as artemis_stablecoin_daily_txns
-    , case
-        when sum(artemis_stablecoin_daily_txns) > 0 then sum(artemis_stablecoin_transfer_volume) / sum(artemis_stablecoin_daily_txns) 
-        else 0
-    end as artemis_stablecoin_avg_txn_value
-    , count(distinct case when p2p_stablecoin_daily_txns > 0 then from_address end) as p2p_stablecoin_dau
-    , sum(p2p_stablecoin_transfer_volume) as p2p_stablecoin_transfer_volume
-    , sum(p2p_stablecoin_daily_txns) as p2p_stablecoin_daily_txns
-    , case
-        when sum(p2p_stablecoin_daily_txns) > 0 then sum(p2p_stablecoin_transfer_volume) / sum(p2p_stablecoin_daily_txns) 
-        else 0
-    end as p2p_stablecoin_avg_txn_value
 
-    , sum(stablecoin_supply) as stablecoin_supply
-    , sum(case when is_wallet::number = 1 then stablecoin_supply else 0 end) as p2p_stablecoin_supply
-from {{ ref("agg_daily_stablecoin_breakdown_silver") }}
-group by date, category
-order by date
+{{ stablecoin_breakdown(["category"]) }}
