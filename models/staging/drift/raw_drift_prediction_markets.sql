be4b0d815775b56c893565d211c026a1d2f9bf26
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-08-19 16:56:31 -0400

    trump/kamala 2024

---
+++ models/staging/drift/raw_drift_prediction_markets.sql
@@ -0,0 +1,25 @@
+{{ config(materialized="table") }}
+with
+    trump_data as (
+        select
+            DATE_TRUNC('DAY', extraction_date) as date,
+            source_json as trump_price
+        from {{ source("PROD_LANDING", "raw_drift_fill_price_trump") }}
+    ), kamala_data as (
+        select
+            DATE_TRUNC('DAY', extraction_date) as date,
+            source_json as kamala_price
+        from {{ source("PROD_LANDING", "raw_drift_fill_price_kamala") }}
+    )
+
+select
+    trump_data.date,
+    'drift' as app,
+    'DeFi' as category,
+    'solana' as chain,
+    trump_price,
+    kamala_price
+from trump_data
+left join kamala_data
+    on trump_data.date = kamala_data.date
+    
