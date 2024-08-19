2ec1f66aaed69da3df6a0b13ef5d490ce7d21fca
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-08-19 17:49:18 -0400

    Add prediction market metrics to ez_drift_metrics_by_chain

--- models/projects/drift/core/ez_drift_metrics_by_chain.sql
+++ models/projects/drift/core/ez_drift_metrics_by_chain.sql
@@ -10,15 +10,24 @@
 
 
 with
-    drfit_data as (
+    trading_data as (
         select date, trading_volume, chain
         from {{ ref("fact_drift_trading_volume") }}
+    ),
+    prediction_data as (
+        select date, trump_price, kamala_price, chain
+        from {{ ref("fact_drift_prediction_markets") }}
     )
+
 select
-    date,
+    trading_data.date,
     'drift' as app,
     'DeFi' as category,
-    chain,
-    trading_volume
-from drfit_data
-where date < to_date(sysdate())
+    trading_data.chain,
+    trading_volume,
+    trump_price,
+    kamala_price
+from trading_data
+left join prediction_data
+    on trading_data.date = prediction_data.date
+where trading_data.date < to_date(sysdate())
