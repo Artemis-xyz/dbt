5f0f4ec6645f0850e0951fc1ced60e5a9b83cc0b
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-08-19 08:51:32 -0400

    Starknet: Dune (#406)

--- models/staging/starknet/fact_starknet_transactions.sql
+++ models/staging/starknet/fact_starknet_transactions.sql
@@ -30,27 +30,32 @@ with
             {{ get_coingecko_price_with_latest("starknet") }}
         )
     )
-select
-    t2.tx_hash,
-    coalesce(t1.to_address, t2.from_address) as contract_address,
-    t2.block_timestamp,
-    date_trunc('day', t2.block_timestamp) raw_date,
-    coalesce(t2.from_address, t2.to_address) as from_address,
-    t2.actual_fee / 1E18 as tx_fee,
-    t2.actual_fee_unit as currency,
+select 
+    t1.block_time as block_timestamp,
+    t1.transaction_hash_hex as tx_hash,
+    t2.contract_address_hex as contract_address,
+    t1.block_date as raw_date,
+    coalesce(t1.sender_address_hex, t1.contract_address_hex) as from_address,
+    t1.actual_fee_amount / 1E18 as tx_fee,
+    t1.actual_fee_unit as currency,
     (tx_fee * price) gas_usd,
     'starknet' as chain,
+    t1.execution_status as status,
+
     new_contracts.name,
     new_contracts.app,
     new_contracts.friendly_name,
     new_contracts.sub_category,
     new_contracts.category --EOA Transfers are more complicated on Starknet
-from starknet_data_warehouse__t1.starknet.calls t1
-left join starknet_data_warehouse__t1.starknet.transactions t2 on t1.tx_hash = t2.tx_hash
-left join new_contracts on lower(t1.to_address) = lower(new_contracts.address)
-left join prices on raw_date = prices.date and curr = t2.actual_fee_unit
-where
-    t1.invocation_type = 'execute' and t1.call_depth = 0
+
+from zksync_dune.starknet.transactions t1
+left join zksync_dune.starknet.calls t2 
+    on t1.transaction_hash_hex = t2.transaction_hash_hex
+    and t1.sender_address_hex = t2.caller_address_hex
+    and callstack_index = [0]
+left join new_contracts on lower(t2.contract_address_hex) = lower(new_contracts.address)
+left join prices on raw_date = prices.date and curr = t1.actual_fee_unit
+where coalesce(t1.sender_address_hex, t1.contract_address_hex) is not null
     {% if is_incremental() %}
         -- this filter will only be applied on an incremental run 
         and t2.block_timestamp
