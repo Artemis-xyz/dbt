5fca851d728f5b2e8ac7d74574da295696592673
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-08-20 19:25:47 -0400

    Add polymarket trump and kamala metrics

---
+++ models/staging/polymarket/fact_polymarket_prediction_markets.sql
@@ -0,0 +1,10 @@
+{{ config(materialized="table") }}
+
+SELECT 
+    DATEADD('DAY', -1, DATE_TRUNC('DAY', extraction_date)::date) AS date,
+    value:market::varchar as market,
+    AVG(value:average_fill_price) as prediction_market_100k_buy_order_price
+FROM {{ source("PROD_LANDING", "raw_polymarket_fill_price_by_market") }},
+lateral flatten(input => parse_json(source_json))
+GROUP BY 
+    1, 2
