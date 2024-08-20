518cfd989ef206c45e6833c2710e5420c30c6d53
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-08-20 16:31:27 -0400

    Remove by_market model

--- models/staging/drift/fact_drift_prediction_markets.sql
+++ models/staging/drift/fact_drift_prediction_markets.sql
@@ -2,8 +2,8 @@
 
 SELECT 
     DATE_TRUNC('DAY', extraction_date)::date AS date,
-    value:market as market,
-    AVG(value:average_fill_price) as daily_average_fill_price
+    value:market::varchar as market,
+    AVG(value:average_fill_price) as prediction_market_100k_buy_order_price
 FROM {{ source("PROD_LANDING", "raw_drift_fill_price_by_market") }},
 lateral flatten(input => parse_json(source_json))
 GROUP BY 
