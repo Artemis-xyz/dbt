d6ec9585ff8d2951626555842cc1754113a9230c
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-08-21 12:50:52 -0700

    Add sell metrics for drift + polymarket (#430)

--- models/projects/drift/core/ez_drift_metrics.sql
+++ models/projects/drift/core/ez_drift_metrics.sql
@@ -12,7 +12,8 @@ SELECT
     date,
     'drift' AS app,
     'DeFi' AS category,
-    "'TRUMP-WIN-2024-BET'" AS trump_prediction_market_100k_buy_order_price,
-    "'KAMALA-POPULAR-VOTE-2024-BET'" AS kamala_prediction_market_100k_buy_order_price
+    trump_prediction_market_100k_buy_order_price,
+    kamala_prediction_market_100k_buy_order_price,
+    trump_prediction_market_100k_sell_order_price,
+    kamala_prediction_market_100k_sell_order_price
 FROM {{ ref("fact_drift_prediction_markets") }}
-PIVOT(SUM(PREDICTION_MARKET_100K_BUY_ORDER_PRICE) FOR market IN (ANY ORDER BY market))
--- models/staging/polymarket/fact_polymarket_prediction_markets.sql
+++ models/staging/polymarket/fact_polymarket_prediction_markets.sql
@@ -1,10 +1,45 @@
 {{ config(materialized="table") }}
 
+WITH buys AS (
+    SELECT 
+        DATEADD('DAY', -1, DATE_TRUNC('DAY', extraction_date)::date) AS date,
+        value:market::varchar as market,
+        AVG(value:average_fill_price) as prediction_market_100k_buy_order_price
+    FROM {{ source("PROD_LANDING", "raw_polymarket_fill_price_by_market") }},
+    lateral flatten(input => parse_json(source_json))
+    GROUP BY 
+        1, 2
+), sells AS (
+    SELECT 
+        DATEADD('DAY', -1, DATE_TRUNC('DAY', extraction_date)::date) AS date,
+        value:market::varchar as market,
+        AVG(value:average_fill_price) as prediction_market_100k_sell_order_price
+    FROM {{ source("PROD_LANDING", "raw_polymarket_sell_fill_price_by_market") }},
+    lateral flatten(input => parse_json(source_json))
+    GROUP BY 
+        1, 2
+), pivoted_buys AS (
+    SELECT 
+        date,
+        "'21742633143463906290569050155826241533067272736897614950488156847949938836455'" AS trump_prediction_market_100k_buy_order_price,
+        "'21271000291843361249209065706097167029083067325856089903026951915683588703117'" AS kamala_prediction_market_100k_buy_order_price
+    FROM buys
+    PIVOT(SUM(PREDICTION_MARKET_100K_BUY_ORDER_PRICE) FOR market IN (ANY ORDER BY market))
+    
+), pivoted_sells AS (
+    SELECT 
+        date,
+        "'21742633143463906290569050155826241533067272736897614950488156847949938836455'" AS trump_prediction_market_100k_sell_order_price,
+        "'21271000291843361249209065706097167029083067325856089903026951915683588703117'" AS kamala_prediction_market_100k_sell_order_price
+    FROM sells
+    PIVOT(SUM(PREDICTION_MARKET_100K_SELL_ORDER_PRICE) FOR market IN (ANY ORDER BY market))
+)
 SELECT 
-    DATEADD('DAY', -1, DATE_TRUNC('DAY', extraction_date)::date) AS date,
-    value:market::varchar as market,
-    AVG(value:average_fill_price) as prediction_market_100k_buy_order_price
-FROM {{ source("PROD_LANDING", "raw_polymarket_fill_price_by_market") }},
-lateral flatten(input => parse_json(source_json))
-GROUP BY 
-    1, 2
+    pivoted_buys.date,
+    trump_prediction_market_100k_buy_order_price,
+    kamala_prediction_market_100k_buy_order_price,
+    trump_prediction_market_100k_sell_order_price,
+    kamala_prediction_market_100k_sell_order_price
+FROM pivoted_buys
+LEFT JOIN pivoted_sells 
+    ON pivoted_buys.date = pivoted_sells.date
