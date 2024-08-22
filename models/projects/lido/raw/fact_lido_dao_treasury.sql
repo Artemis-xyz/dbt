35389ade8b8e286b2f94e991adc6c44895954f3c
Alex Weseley <pampawes@gmail.com>
2024-08-22 10:49:09 -0400

    adapting lido treasury to new macro

--- models/projects/lido/raw/fact_lido_dao_treasury.sql
+++ models/projects/lido/raw/fact_lido_dao_treasury.sql
@@ -8,4 +8,4 @@
     )
 }}
 
-{{ get_treasury_balance('ethereum', '0x3e40d73eb977dc6a537af587d48316fee66e9c8c')}}
+{{ get_treasury_balance('ethereum', '0x3e40d73eb977dc6a537af587d48316fee66e9c8c', '2020-12-17')}}
