fdb0d37f65c8e8062e0ed81f2d9c17caee221d90
Alex Weseley <pampawes@gmail.com>
2024-09-04 22:11:06 -0400

    coalesce to avoid nulls

--- models/projects/jupiter/fact_jupiter_fees_silver.sql
+++ models/projects/jupiter/fact_jupiter_fees_silver.sql
@@ -1,5 +1,5 @@
 select
-   lo.date,
+   coalesce(lo.date, dca.date, perps.date) as date,
    coalesce(dca.fees,0) + coalesce(lo.fees,0) + coalesce(perps.fees, 0) as fees
 from
     {{ ref("fact_jupiter_limit_order_fees_silver") }} lo
@@ -7,5 +7,5 @@ left join
      {{ ref("fact_jupiter_dca_fees_silver") }} dca on dca.date = lo.date
 left join
     {{ ref("fact_jupiter_perps_silver") }} perps on perps.date = dca.date
-where lo.date < to_date(sysdate())
+where coalesce(lo.date, dca.date, perps.date) < to_date(sysdate())
 order by 1 asc
