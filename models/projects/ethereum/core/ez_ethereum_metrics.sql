0180e2757bc4fa293668d78634d7ee6a1394c459
Sebastian Melendez <sebm@seas.upenn.edu>
2024-09-06 17:50:58 -0400

    updating tables to fix issue with near and tron

--- models/projects/ethereum/core/ez_ethereum_metrics.sql
+++ models/projects/ethereum/core/ez_ethereum_metrics.sql
@@ -85,7 +85,7 @@ select
     p2p_stablecoin_txns,
     p2p_stablecoin_dau,
     p2p_stablecoin_mau,
-    p2p_stablecoin_transfer_volume,
+    stablecoin_data.p2p_stablecoin_transfer_volume,
     
     censored_blocks,
     semi_censored_blocks,
@@ -103,7 +103,7 @@ select
     p2p_native_transfer_volume,
     p2p_token_transfer_volume,
     p2p_transfer_volume,
-    coalesce(artemis_stablecoin_transfer_volume, 0) - coalesce(p2p_stablecoin_transfer_volume, 0) as non_p2p_stablecoin_transfer_volume,
+    coalesce(artemis_stablecoin_transfer_volume, 0) - coalesce(stablecoin_data.p2p_stablecoin_transfer_volume, 0) as non_p2p_stablecoin_transfer_volume,
     coalesce(dex_volumes, 0) + coalesce(nft_trading_volume, 0) + coalesce(p2p_transfer_volume, 0) as settlement_volume,
     blob_fees_native,
     blob_fees,
