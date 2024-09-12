d4d54d4428692ea666b1b625220e403c9454466e
Alex Weseley <pampawes@gmail.com>
2024-09-12 09:31:11 -0400

    rocketpool quickfix

--- models/projects/rocketpool/core/ez_rocketpool_metrics_by_chain.sql
+++ models/projects/rocketpool/core/ez_rocketpool_metrics_by_chain.sql
@@ -8,7 +8,41 @@
     )
 }}
 
+with
+    staked_eth_metrics as (
+        select
+            date,
+            'ethereum' as chain,
+            num_staked_eth,
+            amount_staked_usd,
+            num_staked_eth_net_change,
+            amount_staked_usd_net_change
+        from {{ ref('fact_rocketpool_staked_eth_count_with_USD_and_change') }}
+    )
 select 
-    'ethereum' as chain,
-    *
-from {{ ref('ez_rocketpool_metrics') }}
+    s.date
+    , 'ethereum' as chain
+    , s.num_staked_eth
+    , s.amount_staked_usd
+    , s.num_staked_eth_net_change
+    , s.amount_staked_usd_net_change
+    , cl_rewards_usd
+    , el_rewards_usd
+    , deposit_fees
+    , fees
+    , primary_supply_side_revenue
+    , secondary_supply_side_revenue
+    , total_supply_side_revenue
+    , protocol_revenue
+    , token_incentives
+    , operating_expenses
+    , total_expenses
+    , protocol_earnings
+    , net_deposits
+    , outstanding_supply
+    , tvl
+    , treasury_value
+    , treasury_value_native
+    , net_treasury_value
+from staked_eth_metrics s
+left join {{ ref('ez_rocketpool_metrics') }} using(date)
