4d28d614bd40de1e289063b4ee29a72a4acd4883
Andrew Van Aken <andrewvanaken@Andrews-MBP.attlocal.net>
2024-09-12 12:07:31 -0400

    pushing celo mau dau

--- macros/rolling_active_addresses.sql
+++ macros/rolling_active_addresses.sql
@@ -140,6 +140,21 @@
             from gnosis_flipside.core.fact_transactions
             where from_address is not null
         ),
+    {% elif chain == 'celo' %}
+        distinct_dates as (
+            select 
+                block_timestamp::date as raw_date
+            from {{ ref("fact_" ~ chain ~ "_transactions")}}  
+            {% if is_incremental() %}
+                where raw_date > (select dateadd('day', -1, max(date)) from {{ this }})
+            {% endif %}
+        ),
+        distinct_dates_for_rolling_active_address as (
+            select 
+                block_timestamp::date as raw_date
+                , from_address as from_address
+            from {{ref("fact_" ~ chain ~ "_transactions")}}
+        ),
     {% else %}
         distinct_dates as (
             select distinct 
