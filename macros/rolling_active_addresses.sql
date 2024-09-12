1cb0896826c0a82bdee73f1b802ffaeb4db21be9
Andrew Van Aken <andrewvanaken@andrews-mbp.local.meter>
2024-09-12 10:49:15 -0400

    adding celo asssets

--- macros/rolling_active_addresses.sql
+++ macros/rolling_active_addresses.sql
@@ -124,20 +124,20 @@
             lateral flatten(input => outputs)
             where from_address is not null
         ),
-    {% else %}
+    {% elif chain == 'celo' %}
         distinct_dates as (
-            select distinct 
-                raw_date
-            from {{ ref("ez_" ~ chain ~ "_transactions") }}
+            select 
+                block_timestamp::date as raw_date
+            from {{ ref("fact_" ~ chain ~ "_transactions")}}  
             {% if is_incremental() %}
                 where raw_date > (select dateadd('day', -1, max(date)) from {{ this }})
             {% endif %}
         ),
         distinct_dates_for_rolling_active_address as (
-            select distinct 
-                raw_date,
-                from_address
-            from {{ ref("ez_" ~ chain ~ "_transactions") }}
+            select 
+                block_timestamp::date as raw_date
+                , from_address as from_address
+            from {{ref("fact_" ~ chain ~ "_transactions")}}
         ),
     {% endif %}
 
