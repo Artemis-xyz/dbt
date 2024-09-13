ee05f6a335404b558925f65b2053672e1326bb16
artemis-barto <159951763+artemis-barto@users.noreply.github.com>
2024-09-13 15:57:42 -0400

    adding starknet mau wau (#492)
    Co-authored-by: Andrew Van Aken <andrewvanaken@Andrews-MBP.attlocal.net>

--- macros/rolling_active_addresses.sql
+++ macros/rolling_active_addresses.sql
@@ -140,6 +140,22 @@
             from gnosis_flipside.core.fact_transactions
             where from_address is not null
         ),
+    {% elif chain == 'starknet' %}
+        distinct_dates as (
+            select 
+                block_timestamp::date as raw_date
+            from {{ ref("ez_" ~ chain ~ "_transactions") }}
+            {% if is_incremental() %}
+                where raw_date > (select dateadd('day', -1, max(date)) from {{ this }})
+            {% endif %}
+        ),
+        distinct_dates_for_rolling_active_address as (
+            select distinct
+                block_timestamp::date as raw_date
+                , from_address as from_address
+            from {{ ref("ez_" ~ chain ~ "_transactions") }}
+            where from_address is not null
+        ),
     {% elif chain == 'celo' %}
         distinct_dates as (
             select 
