fa70e0c7206eed38cda1fb157201630c1f50fb8e
Sebastian Melendez <sebm@seas.upenn.edu>
2024-10-01 10:28:07 -0400

    naming the columns

--- models/common/ez_stablecoin_metrics.sql
+++ models/common/ez_stablecoin_metrics.sql
@@ -9,5 +9,21 @@
 }}
 
 SELECT
-    *
+    date_granularity as date
+    , chain
+    , symbol
+    , stablecoin_dau
+    , stablecoin_transfer_volume
+    , stablecoin_daily_txns
+    , stablecoin_avg_txn_value
+    , artemis_stablecoin_dau
+    , artemis_stablecoin_transfer_volume
+    , artemis_stablecoin_daily_txns
+    , artemis_stablecoin_avg_txn_value
+    , p2p_stablecoin_dau
+    , p2p_stablecoin_transfer_volume
+    , p2p_stablecoin_daily_txns
+    , p2p_stablecoin_avg_txn_value
+    , stablecoin_supply
+    , p2p_stablecoin_supply
 FROM {{ref("agg_daily_stablecoin_breakdown_symbol_chain")}}
