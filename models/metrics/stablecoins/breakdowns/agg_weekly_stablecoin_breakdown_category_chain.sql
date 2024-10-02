3a11fd3e4df5b88b3d51cbad1d4577a3f6aadf15
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-10-02 09:21:56 -0400

    Customer 911: fixing `agg_weekly_stablecoin_breakdown_category_chain` (#520)

--- models/metrics/stablecoins/breakdowns/agg_weekly_stablecoin_breakdown_category_chain.sql
+++ models/metrics/stablecoins/breakdowns/agg_weekly_stablecoin_breakdown_category_chain.sql
@@ -1,3 +1,3 @@
-{{ config(materialized="incremental", unique_key=["date_granularity" "category"], snowflake_warehouse="STABLECOIN_V2_LG") }}
+{{ config(materialized="incremental", unique_key=["date_granularity", "category"], snowflake_warehouse="STABLECOIN_V2_LG") }}
 
 {{stablecoin_breakdown(["chain", "category"], "week")}}
