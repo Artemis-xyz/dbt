ad471dd8e9f23d505c4793bdc624b87df25056bc
Alex Weseley <pampawes@gmail.com>
2024-10-05 21:14:42 -0400

    keep ez_metrics_by_chain as is, modify ez_metrics

--- models/projects/trader_joe/core/ez_trader_joe_metrics.sql
+++ models/projects/trader_joe/core/ez_trader_joe_metrics.sql
@@ -30,7 +30,17 @@ with
         SELECT date(date) as date, sum(tvl) as tvl FROM agg
             group by 1 order by 1 desc
     )
-
+, v2_data as (
+    select
+        date,
+        sum(total_volume) as total_volume,
+        sum(total_fees) as total_fees,
+        sum(protocol_fees) as protocol_fees,
+        sum(unique_traders) as unique_traders,
+        sum(daily_txns) as daily_txns
+    from {{ ref("fact_trader_joe_v2_all_versions_metrics")}}
+    group by 1
+)
 , ez_metrics_agg as (    
     select
         date(date) as date,
@@ -39,7 +49,6 @@ with
         sum(tvl) as tvl,
         sum(trading_volume) as trading_volume,
         sum(trading_fees) as trading_fees,
-        sum(revenue) as revenue,
         sum(unique_traders) as unique_traders,
         sum(gas_cost_native) as gas_cost_native,
         sum(gas_cost_usd) as gas_cost_usd,
@@ -48,20 +57,20 @@ with
     group by 1, 2, 3
 )
 
-
 select
     date(coalesce(v2_tvl.date, e.date)) as date,
     app,
     category,
     sum(coalesce(e.tvl,0) + coalesce(v2_tvl.tvl, 0)) as tvl,
-    sum(trading_volume) as trading_volume,
-    sum(trading_fees) as trading_fees,
-    sum(revenue) as revenue,
-    sum(unique_traders) as unique_traders,
-    sum(gas_cost_native) as gas_cost_native,
-    sum(gas_cost_usd) as gas_cost_usd,
-    sum(txns) as txns
+    sum(coalesce(e.trading_volume, 0) + coalesce(v2_data.total_volume, 0))   as trading_volume,
+    sum(coalesce(e.trading_fees, 0) + coalesce(v2_data.total_fees, 0)) as trading_fees,
+    sum(coalesce(v2_data.protocol_fees, 0)) as revenue,
+    sum(coalesce(e.unique_traders, 0) + coalesce(v2_data.unique_traders, 0)) as unique_traders,
+    sum(coalesce(e.gas_cost_native, 0)) as gas_cost_native,
+    sum(coalesce(e.gas_cost_usd, 0)) as gas_cost_usd,
+    sum(coalesce(e.txns, 0) + coalesce(v2_data.daily_txns, 0)) as txns
 from ez_metrics_agg e
+left join v2_data using (date)
 left join v2_tvl using (date)
 group by 1, 2, 3
 order by 1 desc
--- models/projects/trader_joe/core/ez_trader_joe_metrics_by_chain.sql
+++ models/projects/trader_joe/core/ez_trader_joe_metrics_by_chain.sql
@@ -57,33 +57,20 @@ with
         from {{ ref("ez_trader_joe_dex_swaps")}}
         group by 1, 2
     )
-    , v2_data as (
-        select
-            date,
-            chain,
-            sum(total_volume) as total_volume,
-            sum(total_fees) as total_fees,
-            sum(protocol_fees) as protocol_fees,
-            sum(unique_traders) as unique_traders,
-            sum(daily_txns) as daily_txns
-        from {{ ref("fact_trader_joe_v2_all_versions_metrics")}}
-        group by 1, 2
-    )
+
 select
     tvl_by_chain.date,
     'trader_joe' as app,
     'DeFi' as category,
     tvl_by_chain.chain,
     tvl_by_chain.tvl,
-    coalesce(trading_volume_by_chain.trading_volume, 0) + coalesce(v2_data.total_volume, 0) as trading_volume,
-    coalesce(trading_volume_by_chain.trading_fees, 0) + coalesce(v2_data.total_fees, 0) as trading_fees,
-    coalesce(v2_data.protocol_fees, 0) as revenue,
-    coalesce(trading_volume_by_chain.unique_traders, 0) + coalesce(v2_data.unique_traders, 0) as unique_traders,
+    coalesce(trading_volume_by_chain.trading_volume, 0) as trading_volume,
+    coalesce(trading_volume_by_chain.trading_fees, 0) as trading_fees,
+    coalesce(trading_volume_by_chain.unique_traders, 0) as unique_traders,
     trading_volume_by_chain.gas_cost_native,
     trading_volume_by_chain.gas_cost_usd,
-    coalesce(daily_txns_data.daily_txns, 0) + coalesce(v2_data.daily_txns, 0) as txns
+    coalesce(daily_txns_data.daily_txns, 0) as txns
 from tvl_by_chain
 left join trading_volume_by_chain using(date, chain)
 left join daily_txns_data using (date, chain)
-left join v2_data using (date, chain)
 where tvl_by_chain.date < to_date(sysdate())
