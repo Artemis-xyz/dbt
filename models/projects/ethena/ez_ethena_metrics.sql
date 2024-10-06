24f3c5318cc0cbe2ac7a52e33755afd435412d53
Alex Weseley <pampawes@gmail.com>
2024-10-06 16:22:20 -0400

    ethena ez metrics join fix

--- models/projects/ethena/ez_ethena_metrics.sql
+++ models/projects/ethena/ez_ethena_metrics.sql
@@ -17,11 +17,10 @@ with usde_metrics as (
 select
     usde_metrics.date,
     sum(usde_metrics.stablecoin_dau) as stablecoin_dau,
-    sum(usde_metrics.stablecoin_daily_txns) as stablecoin_daily_txns,
+    sum(usde_metrics.stablecoin_daily_txns) as stablecoin_txns,
     sum(coalesce(collateral_fees.collateral_fee, 0) + coalesce(yield_fees.fees, 0)) as fees
 from usde_metrics
-left join {{ ref('fact_ethena_yield_fees') }} yield_fees
-left join  {{ ref('fact_ethena_collateral_fees') }} collateral_fees
-    on collateral_fees.date = usde_metrics.date
+left join {{ ref('fact_ethena_yield_fees') }} yield_fees using (date)
+left join  {{ ref('fact_ethena_collateral_fees') }} collateral_fees using (date)
 where usde_metrics.date < to_date(sysdate())
 group by 1
