dd4cccb839b2a15aa6a027d1c5981ea68cdcaf12
Alex Weseley <pampawes@gmail.com>
2024-10-07 19:08:26 -0400

    created wormhole ez by chain, removed from datahub model

--- models/projects/wormhole/core/ez_wormhole_metrics.sql
+++ models/projects/wormhole/core/ez_wormhole_metrics.sql
@@ -18,12 +18,18 @@ with txns as (
         bridge_daa
     from {{ ref("fact_wormhole_bridge_daa_gold") }}
 )
+, bridge_volume as (
+    select date, bridge_volume
+    from {{ ref("fact_wormhole_bridge_volume_gold") }}
+)
 
 select
     coalesce(txns.date, daa.date) as date,
     coalesce(txns.txns, 0) as bridge_txns,
     coalesce(daa.bridge_daa, 0) as bridge_daa,
-    0 as fees
+    0 as fees,
+    bridge_volume.bridge_volume
 from txns
 left join daa on txns.date = daa.date
+left join bridge_volume on txns.date = bridge_volume.date
 where coalesce(txns.date, daa.date) < to_date(sysdate())
---
+++ models/projects/wormhole/core/ez_wormhole_metrics_by_chain.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized="table",
+    warehouse="WORMHOLE",
+    database="WORMHOLE",
+    schema="core",
+    alias="ez_metrics_by_chain"
+) }}
+
+with
+    bridge_volume as (
+        select date, chain, inflow, outflow
+        from {{ ref("fact_wormhole_bridge_volume_gold") }}
+        where chain is not null
+    )
+select
+    date,
+    'Wormhole' as app,
+    'Bridge' as category,
+    chain,
+    inflow,
+    outflow
+from bridge_volume
+where date < to_date(sysdate())
