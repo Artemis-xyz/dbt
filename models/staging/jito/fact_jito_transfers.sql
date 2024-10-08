b9cf1450f4a97dcc3ffdeb1330c4959e6346e61d
Alex Weseley <pampawes@gmail.com>
2024-10-08 10:04:32 -0400

    jito fundamentals clean up lineage

--- models/staging/jito/fact_jito_transfers.sql
+++ models/staging/jito/fact_jito_transfers.sql
@@ -4,24 +4,11 @@
         snowflake_warehouse='jito'
     )
 }}
-with prices as (
-    SELECT
-        date(hour) as date
-        , avg(price) as price
-    FROM solana_flipside.price.ez_prices_hourly
-    where is_native = 'True'
-    GROUP BY 1
-)
+
 
 SELECT 
-    date_trunc('day', t.block_timestamp) as day
-    , sum(t.amount * p.price) as fees
-    , sum(t.amount * p.price) * 0.05 as revenue
-    , sum(t.amount * p.price) * 0.95 as supply_side_fees
-    , count(*) as txns
-    , count(distinct t.tx_from) as dau
-FROM fact_jito_transfers t
-LEFT JOIN prices p on p.date = date_trunc('day',t.block_timestamp)
+    *
+FROM {{ source('SOLANA_FLIPSIDE', 'fact_transfers') }} t
 WHERE tx_to IN ('96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5' -- all the tip payment accounts 
                 ,'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe'
                 ,'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY'
@@ -30,9 +17,6 @@ WHERE tx_to IN ('96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5' -- all the tip pa
                 ,'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt'
                 ,'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL'
                 ,'3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT')
-and t.mint = 'So11111111111111111111111111111111111111111'
 {% if is_incremental() %}
     AND block_timestamp > (select dateadd('day', -1, max(day)) from {{ this }})
 {% endif %}
-group by 1
-order by 1 desc
