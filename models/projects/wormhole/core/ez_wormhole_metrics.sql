6a5f6ca5e07be3a035441e4ac3ad192d14cad7e9
Alex Weseley <pampawes@gmail.com>
2024-10-08 01:08:38 -0400

    wormhole to use api fees

--- models/projects/wormhole/core/ez_wormhole_metrics.sql
+++ models/projects/wormhole/core/ez_wormhole_metrics.sql
@@ -19,16 +19,17 @@ with txns as (
     from {{ ref("fact_wormhole_bridge_daa_gold") }}
 )
 , bridge_volume as (
-    select date, bridge_volume
+    select date, sum(bridge_volume) as bridge_volume, sum(fees) as fees
     from {{ ref("fact_wormhole_bridge_volume_gold") }}
+    group by 1
 )
 
 select
     coalesce(txns.date, daa.date) as date,
     coalesce(txns.txns, 0) as bridge_txns,
     coalesce(daa.bridge_daa, 0) as bridge_daa,
-    0 as fees,
-    bridge_volume.bridge_volume
+    coalesce(bridge_volume.fees, 0) as fees,
+    coalesce(bridge_volume.bridge_volume, 0) as bridge_volume
 from txns
 left join daa on txns.date = daa.date
 left join bridge_volume on txns.date = bridge_volume.date
