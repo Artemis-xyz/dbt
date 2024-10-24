94f290e7422218a9fef48f0d5e607b472d45d3ef
Son Do <sondo077@gmail.com>
2024-10-24 11:28:57 -0400

    Remove null protocol ids

--- models/metrics/defillama/agg_defillama_protocol_fees_rev_tvl_dex_vol.sql
+++ models/metrics/defillama/agg_defillama_protocol_fees_rev_tvl_dex_vol.sql
@@ -67,6 +67,7 @@ select
     max(combined_data.dex_volumes) as dex_volumes,
     max(combined_data.tvl) as tvl
 from combined_data
+where defillama_protocol_id is not null
 left join
     {{ ref("fact_defillama_protocols") }} as protocol_names
     on combined_data.defillama_protocol_id = protocol_names.id
