e79ad45543c151daa47121028cf6221bf123a114
Alex Weseley <pampawes@gmail.com>
2024-10-29 20:59:11 -0400

    Revert "jupiter perps pricing data"
    This reverts commit 02d7be6eb2adf47b8867ae0185ccbfb6a2a3289c.
    
    Reverting jupiter pricing commit (merged in diff PR)

--- models/projects/jupiter/fact_jupiter_perps_silver.sql
+++ models/projects/jupiter/fact_jupiter_perps_silver.sql
@@ -1,16 +1,18 @@
 {{
     config(
-        materialized='incremental',
-        snowflake_warehouse='JUPITER',
+        materialized="incremental",
+        unique_key="date",
+        snowflake_warehouse="JUPITER",
     )
 }}
 
+
 with hex_cte as (
     SELECT
-        block_timestamp,
-        tx_id,
+        date(block_timestamp) as date,
         PC_DBT_DB.PROD.BASE58_TO_HEX(f.value:data) as hex_data,
-        f.value:data as base58_data
+        f.value:data as base58_data,
+        tx_id
     FROM solana_flipside.core.fact_events,
     LATERAL FLATTEN(input => get_path(inner_instruction, 'instructions')) AS f
     where
@@ -23,35 +25,28 @@ with hex_cte as (
     and program_id = 'PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu'
     and f.value:data is not null
     and succeeded = 1
-    and block_timestamp >  to_date(sysdate())
-)
-, agg as (
+),
+perps_mat as(
     SELECT
-        block_timestamp,
-        tx_id,
+        date,
         PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,582+1,16))/1e6 as size_usd, -- position size fee
-        PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,680+1,16))/1e6 as fee_usd, --open fee
+        PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,680+1,16))/1e6 as fees_usd, --open fee
         PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,454+1,64)) as owner, -- owner/trader address
-        PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,243,64)) as mint, 
-        PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,630+1,16))/1e6 as price,
-        hex_data
+        tx_id
     FROM hex_cte
     WHERE SUBSTRING(hex_data,17,16) = 'f5715534d6bb9984' -- IncreasePosition
 
     UNION ALL
 
     SELECT
-        block_timestamp,
-        tx_id,
+        date,
         PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data, 584+1,16))/1e6 as size_usd, --open fee
         case when substring(hex_data,1+651,1) = 1
             then PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,1+668,16)) / 1e6
             else PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,1+652,16)) / 1e6
             end as fee_usd, --close fee, has an optional param priceSlippage before it so we need this case when.
         PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,456+1,64)) as owner, -- owner/trader address
-        PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,243,64)) as mint, 
-        PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,635,16))/1e6 as price,
-        hex_data
+        tx_id
     FROM hex_cte
     WHERE substring(hex_data,1+16,16) = '409c2b4a6d83107f' -- DecreasePosition
 
@@ -59,26 +54,18 @@ with hex_cte as (
 
 
     SELECT
-        block_timestamp,
-        tx_id,
+        date,
         PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,1+354,16)) / 1e6 as size_usd, --close fee
         PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,1+564,16)) / 1e6 as fee_usd, --close fee
         PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,1+388,64)) as owner, -- owner/trader address
-        PC_DBT_DB.PROD.HEX_TO_BASE58(SUBSTRING(hex_data,291,64)) as mint, 
-        PC_DBT_DB.PROD.BIG_ENDIAN_HEX_TO_DECIMAL(SUBSTRING(hex_data,549,16))/1e6 as price,
-        hex_data
+        tx_id
     FROM hex_cte
     WHERE substring(hex_data,1+16,16) IN ('68452084d423bf2f', '806547a880485654') --LiquidatePosition, LiquidateFullPosition
 )
 SELECT
-    block_timestamp,
-    tx_id,
-    'solana' as chain,
-    'jupiter' as app,
-    size_usd,
-    fee_usd,
-    owner,
-    mint,
-    price,
-    hex_data
-FROM agg
+    date
+    , sum(size_usd) as volume
+    , sum(fees_usd) as fees
+    , count(distinct owner) as traders
+    , count(distinct tx_id) as txns
+FROM perps_mat group by 1 order by 1 desc
