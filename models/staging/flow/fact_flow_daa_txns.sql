8e1591f500b64fe54a89be2e7e959ba495767f6b
Tang Bo Hao <tech@btang.cn>
2024-10-30 13:53:36 +0800

    feat: update flow_daa_txns

--- models/staging/flow/fact_flow_daa_txns.sql
+++ models/staging/flow/fact_flow_daa_txns.sql
@@ -1,19 +1,21 @@
 {{ config(materialized="view") }}
-WITH ez_actors_data AS (
+WITH
+ez_actors_data AS (
     SELECT
-        date_trunc('day', block_timestamp) AS date,
-        COUNT(DISTINCT actors) AS daa,
-        COUNT(DISTINCT tx_id) AS txns
-    FROM flow_flipside.core.ez_transaction_actors
-    WHERE tx_succeeded
+        date_trunc('day', b.block_timestamp) AS date,
+        COUNT(DISTINCT CAST(value AS VARCHAR)) AS daa,  -- Explicitly casting to VARCHAR
+        COUNT(DISTINCT b.tx_id) AS txns,
+    FROM
+        flow_flipside.core.ez_transaction_actors AS b,
+        LATERAL FLATTEN(INPUT => b.actors) AS a  -- Flattening the actors array
     GROUP BY date
-), evm_data AS (
+),
+evm_data AS (
     SELECT
         date_trunc('day', block_timestamp) AS date,
         COUNT(DISTINCT from_address) AS daa,
         COUNT(DISTINCT tx_hash) AS txns
     FROM flow_flipside.core_evm.fact_transactions
-    WHERE tx_succeeded
     GROUP BY date
 )
 SELECT
@@ -26,4 +28,5 @@ FROM (
     UNION ALL
     SELECT * FROM evm_data
 ) AS combined_data
-GROUP BY date
+GROUP BY date 
+ORDER by date desc
