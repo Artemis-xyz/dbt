7382e58a3fe84871125e9f46c4a6a9809923e9e7
Son Do <34179343+Sunny77D@users.noreply.github.com>
2024-11-01 14:26:09 -0400

    Fixed Osmosis Fees (#585)

--- models/staging/osmosis/fact_osmosis_daa_txns.sql
+++ models/staging/osmosis/fact_osmosis_daa_txns.sql
@@ -1,8 +1,11 @@
-{{ config(materialized="view") }}
+{{ config(materialized="incremental", unique_key="date") }}
 select
     block_timestamp::date as date,
     count(distinct tx_from) as daa,
     count(*) as txns,
     'osmosis' as chain
 from osmosis_flipside.core.fact_transactions
+{% if is_incremental() %}
+    where block_timestamp::date >= (select dateadd('day', -5, max(date)) from {{ this }})
+{% endif %}
 group by date
--- models/staging/osmosis/fact_osmosis_gas_gas_usd_fees_revenue.sql
+++ models/staging/osmosis/fact_osmosis_gas_gas_usd_fees_revenue.sql
@@ -1,4 +1,4 @@
-{{ config(materialized="view") }}
+{{ config(materialized="incremental", unique_key="date") }}
 
 select
     t1.date,
@@ -10,3 +10,7 @@ select
 from {{ ref("fact_osmosis_gas_gas_usd") }} t1
 left join {{ ref("fact_osmosis_trading_fees") }} t2 on t1.date = t2.date
 where t1.date < to_date(sysdate())
+{% if is_incremental() %}
+    and t1.date >= (select dateadd('day', -5, max(date)) from {{ this }})
+{% endif %}
+and gas_usd is not null
