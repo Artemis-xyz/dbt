47a42bbe71783d82ddc157b2d72b34f52086f113
Alex Weseley <pampawes@gmail.com>
2024-11-05 00:10:39 -0500

    remove cte

--- models/staging/maple/fact_maple_deposits.sql
+++ models/staging/maple/fact_maple_deposits.sql
@@ -5,19 +5,16 @@
     )
 }}
 
-WITH deposits AS (
-    SELECT
-        d.block_timestamp AS date,
-        d.tx_hash,
-        d.block,
-        p.pool_name,
-        p.asset,
-        d.owner_ AS lender_id,
-        d.shares_ / POWER(10, p.precision) AS shares,
-        d.assets_ / POWER(10, p.precision) AS amount,
-        'Deposit' AS description
-    FROM {{ ref('fact_maple_v2_Pool_Deposit') }} d
-    LEFT JOIN {{ ref('dim_maple_pools') }} p ON p.pool_id = d.contract_address
-)
 
-SELECT * FROM deposits
+SELECT
+    d.block_timestamp AS date,
+    d.tx_hash,
+    d.block,
+    p.pool_name,
+    p.asset,
+    d.owner_ AS lender_id,
+    d.shares_ / POWER(10, p.precision) AS shares,
+    d.assets_ / POWER(10, p.precision) AS amount,
+    'Deposit' AS description
+FROM {{ ref('fact_maple_v2_Pool_Deposit') }} d
+LEFT JOIN {{ ref('dim_maple_pools') }} p ON p.pool_id = d.contract_address
