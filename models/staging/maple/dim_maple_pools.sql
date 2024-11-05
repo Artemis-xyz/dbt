18259431eb0b5da403976a503a218295ee73ab73
Alex Weseley <pampawes@gmail.com>
2024-11-05 00:10:30 -0500

    adding source for pools

--- models/staging/maple/dim_maple_pools.sql
+++ models/staging/maple/dim_maple_pools.sql
@@ -9,6 +9,8 @@
 -- Creates a table consisting of each Maple Pool with its ID, name, activation block, Loan Managers, etc.
 -- For Pools that were migrated from V1->V2, also includes the V1 contract address so we can merge all historical data for those Pools
 -- Each Pool is denominated in a single asset, and we store that asset's on-chain decimal precision here (e.g. WETH is 18, USDC is 6)
+
+-- Source: https://dune.com/queries/2957151
 SELECT 
 * 
 FROM 
