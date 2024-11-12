48a6cd638b3e8a650c7dade43f0168e16a596cc2
Alex Weseley <pampawes@gmail.com>
2024-11-12 15:13:44 -0500

    initial bitcoin etf commit

---
+++ models/staging/bitcoin/fact_bitcoin_etf_addresses.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='table',
+        snowflake_warehouse='BITCOIN'
+    )
+}}
+
+with
+    max_extraction as (
+        select max(extraction_date) as max_date
+        from {{ source("PROD_LANDING", "raw_bitcoin_etf_addresses") }}
+    )
+select
+    date(to_timestamp(value:date::number / 1000)) as date,
+    value:addresses::int as addresses,
+    value as source,
+    'bitcoin' as chain
+from
+    {{ source("PROD_LANDING", "raw_bitcoin_etf_addresses") }},
+    lateral flatten(input => parse_json(source_json))
+where extraction_date = (select max_date from max_extraction)
