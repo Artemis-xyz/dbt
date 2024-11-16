77c7cdd80f6ad0d6304aaac36808e37303b4e89f
Alex Weseley <pampawes@gmail.com>
2024-11-15 19:02:40 -0500

    fix incremental and clean up code

--- models/staging/metaplex/fact_metaplex_transaction_counts.sql
+++ models/staging/metaplex/fact_metaplex_transaction_counts.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized="incremental",
-    snowflake_warehouse="METAPLEX"
+    snowflake_warehouse="METAPLEX",
+    unique_key= ["date", "program_id"]
 ) }}
 
 with all_metaplex_transactions AS (
@@ -10,6 +11,11 @@ with all_metaplex_transactions AS (
         program_id
     FROM
         {{ ref('fact_filtered_metaplex_solana_events') }}
+        {% if is_incremental() %}
+            WHERE block_timestamp > (SELECT MAX(date) FROM {{ this }})
+        {% else %}
+            WHERE block_timestamp >= date('2021-08-01')
+        {% endif %}
 
     UNION
 
@@ -82,57 +88,16 @@ cumulative_transactions AS (
         ) AS cumulative_signed_transactions
     FROM
         daily_transaction_counts_full dtcf
-),
-
-pivoted_transactions AS (
-    SELECT
-        date,
-        SUM(daily_signed_transactions) AS txns,
-        MAX(CASE WHEN program_name = 'Token Metadata' THEN cumulative_signed_transactions END) AS token_metadata,
-        MAX(CASE WHEN program_name = 'Bubblegum' THEN cumulative_signed_transactions END) AS bubblegum,
-        MAX(CASE WHEN program_name = 'Core' THEN cumulative_signed_transactions END) AS core,
-        MAX(CASE WHEN program_name = 'Candy Machine v3' THEN cumulative_signed_transactions END) AS candy_machine_v3,
-        MAX(CASE WHEN program_name = 'Candy Machine v2' THEN cumulative_signed_transactions END) AS candy_machine_v2,
-        MAX(CASE WHEN program_name = 'Candy Machine v1' THEN cumulative_signed_transactions END) AS candy_machine_v1,
-        MAX(CASE WHEN program_name = 'Auction House' THEN cumulative_signed_transactions END) AS auction_house,
-        MAX(CASE WHEN program_name = 'Auctioneer' THEN cumulative_signed_transactions END) AS auctioneer,
-        MAX(CASE WHEN program_name = 'Candy Guard' THEN cumulative_signed_transactions END) AS candy_guard,
-        MAX(CASE WHEN program_name = 'Core Candy Guard' THEN cumulative_signed_transactions END) AS core_candy_guard,
-        MAX(CASE WHEN program_name = 'Core Candy Machine' THEN cumulative_signed_transactions END) AS core_candy_machine,
-        MAX(CASE WHEN program_name = 'Gumdrop' THEN cumulative_signed_transactions END) AS gumdrop,
-        MAX(CASE WHEN program_name = 'Hydra' THEN cumulative_signed_transactions END) AS hydra,
-        MAX(CASE WHEN program_name = 'Inscriptions' THEN cumulative_signed_transactions END) AS inscriptions,
-        MAX(CASE WHEN program_name = 'MPL-Hybrid' THEN cumulative_signed_transactions END) AS mpl_hybrid,
-        MAX(CASE WHEN program_name = 'Token Auth Rules' THEN cumulative_signed_transactions END) AS token_auth_rules,
-        SUM(cumulative_signed_transactions) AS cumulative_all_programs
-    FROM
-        cumulative_transactions
-    GROUP BY
-        date
 )
 
 SELECT 
-    TO_CHAR(date, 'YYYY-MM-DD') AS date,
-    txns,
-    token_metadata,
-    bubblegum,
-    core,
-    candy_machine_v3,
-    candy_machine_v2,
-    candy_machine_v1,
-    auction_house,
-    auctioneer,
-    candy_guard,
-    core_candy_guard,
-    core_candy_machine,
-    gumdrop,
-    hydra,
-    inscriptions,
-    mpl_hybrid,
-    token_auth_rules,
-    cumulative_all_programs
+    date(date) AS date,
+    program_id,
+    program_name,
+    daily_signed_transactions,
+    cumulative_signed_transactions
 FROM 
-    pivoted_transactions
+    cumulative_transactions
 WHERE date < to_date(sysdate())
 ORDER BY 
     date DESC
