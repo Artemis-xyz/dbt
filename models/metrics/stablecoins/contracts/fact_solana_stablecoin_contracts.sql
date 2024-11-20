d823542cb00c4e04eeff610b2df6f800efc1304b
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-11-20 16:15:49 -0500

    Stablecoins: USDS (#629)

--- models/metrics/stablecoins/contracts/fact_solana_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_solana_stablecoin_contracts.sql
@@ -12,5 +12,6 @@ from
             ),
             ('USDT', 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB', 6, 'tether', 0),
             ('EURC', 'HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr', 6, 'euro-coin', 0),
-            ('PYUSD', '2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo', 6, 'paypal-usd', 0)
+            ('PYUSD', '2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo', 6, 'paypal-usd', 0),
+            ('USDS', 'USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA', 6, 'usds', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
