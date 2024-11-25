1aafb24b5cf5a4926a5b750e5a3c0d318f67af0c
Alex Weseley <pampawes@gmail.com>
2024-11-25 15:21:48 -0500

    add grouping

--- macros/rwa/ez_rwa_by_chain.sql
+++ macros/rwa/ez_rwa_by_chain.sql
@@ -3,12 +3,14 @@
 select
     tvl.date,
     tvl.issuer,
+    tvl.chain,
     tvl.symbol,
-    tvl.tokenized_supply_change,
-    tvl.tokenized_mcap_change,
-    tvl.tokenized_supply,
-    tvl.tokenized_mcap,
+    sum(tvl.tokenized_supply_change) as tokenized_supply_change,
+    sum(tvl.tokenized_mcap_change) as tokenized_mcap_change,
+    sum(tvl.tokenized_supply) as tokenized_supply,
+    sum(tvl.tokenized_mcap) as tokenized_mcap,
 from {{ ref('fact_' ~ issuer ~ '_tvl_by_chain') }} tvl
 where date < to_date(sysdate())
+group by 1, 2, 3, 4
 
 {% endmacro %}
--- macros/rwa/ez_rwa_by_product.sql
+++ macros/rwa/ez_rwa_by_product.sql
@@ -4,11 +4,11 @@ select
     tvl.date,
     tvl.issuer,
     tvl.symbol,
-    tvl.tokenized_supply_change,
-    tvl.tokenized_mcap_change,
-    tvl.tokenized_supply,
-    tvl.tokenized_mcap,
+    sum(tvl.tokenized_supply_change) as tokenized_supply_change,
+    sum(tvl.tokenized_mcap_change) as tokenized_mcap_change,
+    sum(tvl.tokenized_supply) as tokenized_supply,
+    sum(tvl.tokenized_mcap) as tokenized_mcap,
 from {{ ref('fact_' ~ issuer ~ '_tvl_by_product') }} tvl
 where date < to_date(sysdate())
-
+group by 1, 2, 3
 {% endmacro %}
