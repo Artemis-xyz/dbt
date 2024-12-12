79e6e736076e70c7dd4ab0f2e0f756409acad896
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-12-12 14:31:55 -0500

    Celo: adding ez transfers (#672)

---
+++ models/projects/celo/raw/ez_celo_token_transfers.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        materialized="view",
+        snowflake_warehouse="CELO",
+        database="celo",
+        schema="raw",
+        alias="ez_token_transfers",
+    )
+}}
+
+select *
+from {{ ref('fact_celo_token_transfers') }}
