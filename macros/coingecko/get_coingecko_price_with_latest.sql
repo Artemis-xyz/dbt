4712530d359c948fbd85ce1362427aea2ee19329
Sebastian Melendez <sebm@seas.upenn.edu>
2024-12-11 22:02:07 -0500

    updated fact celo token transfers

--- macros/coingecko/get_coingecko_price_with_latest.sql
+++ macros/coingecko/get_coingecko_price_with_latest.sql
@@ -9,3 +9,21 @@
     from {{ ref("fact_coingecko_token_realtime_data") }}
     where token_id = '{{ coingecko_id }}'
 {% endmacro %}
+
+
+
+{% macro get_multiple_coingecko_price_with_latest(chain) %}
+    select date as date, contract_address, decimals, symbol, shifted_token_price_usd as price
+    from {{ ref("fact_coingecko_token_date_adjusted_gold") }}
+    inner join {{ ref('dim_coingecko_token_map')}}
+        on coingecko_id = coingecko_token_id
+    where
+        chain = '{{ chain }}'
+        and date < dateadd(day, -1, to_date(sysdate()))
+    union
+    select dateadd('day', -1, to_date(sysdate())) as date, contract_address, decimals, symbol, token_current_price as price
+    from {{ ref("fact_coingecko_token_realtime_data") }}
+    inner join {{ ref('dim_coingecko_token_map')}}
+        on token_id = coingecko_token_id
+    where chain = '{{ chain }}'
+{% endmacro %}
