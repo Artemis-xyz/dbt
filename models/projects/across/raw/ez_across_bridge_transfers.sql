95c10913aabb38a7c18b593e49d47240902bb5d3
Sebastian Melendez <sebm@seas.upenn.edu>
2024-12-14 09:52:54 -0500

    updateing art share models

--- models/projects/across/raw/ez_across_bridge_transfers.sql
+++ models/projects/across/raw/ez_across_bridge_transfers.sql
@@ -9,5 +9,22 @@
 }}
 
 
-select *
-from {{ref('fact_across_transfers')}}
+
+select 
+    version
+    , contract_address
+    , block_timestamp
+    , event_index
+    , t3.chain as source_chain
+    , depositor as source_address
+    , tx_hash as destination_tx_hash
+    
+    , t2.chain as destination_chain
+    , recipient as destination_address
+    , destination_token_symbol
+    , destination_token
+    , amount
+from {{ref('fact_across_transfers')}} t1
+left join {{ref('chain_ids')}} t2 on t1.destination_chain_id = t2.id
+left join {{ref('chain_ids')}} t3 on t1.origin_chain_id = t3.id
+limit 10
