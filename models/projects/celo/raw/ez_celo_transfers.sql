c84ca01bfb2c329490690ebb933cd770aedebfa7
Sebastian Melendez <sebm@seas.upenn.edu>
2024-12-15 20:24:33 -0500

    adding celo native and complete token transfers

---
+++ models/projects/celo/raw/ez_celo_transfers.sql
@@ -0,0 +1,47 @@
+{{
+    config(
+        materialized="incremental",
+        snowflake_warehouse="CELO",
+        database="celo",
+        schema="raw",
+        alias="ez_transfers",
+    )
+}}
+
+select
+    block_timestamp,
+    block_number,
+    transaction_hash,
+    index::string as event_index,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    from_address,
+    to_address,
+    amount,
+    amount_adjusted,
+    amount_usd,
+    tx_status
+from {{ref('fact_celo_native_token_transfers')}}
+{% if is_incremental() %}
+    where block_timestamp >= (select max(block_timestamp) from {{ this }})
+{% endif %}
+union all
+select
+    block_timestamp,
+    block_number,
+    transaction_hash,
+    event_index::string as event_index,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    from_address,
+    to_address,
+    amount,
+    amount_adjusted,
+    amount_usd,
+    tx_status
+from {{ref('fact_celo_token_transfers')}}
+{% if is_incremental() %}
+    where block_timestamp >= (select max(block_timestamp) from {{ this }})
+{% endif %}
