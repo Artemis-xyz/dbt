e838dd4f0995530cd0d0b8829c3cac129723cebd
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-12-16 17:25:32 -0500

    Adding FDUSD and USDe (#685)

--- models/metrics/stablecoins/contracts/fact_ethereum_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_ethereum_stablecoin_contracts.sql
@@ -24,5 +24,6 @@ from
             ('EURC', '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c', 6, 'euro-coin', 0),
             ('USDP', '0x8e870d67f660d95d5be530380d0ec0bd388289e1', 18, 'paxos-standard', 0),
             ('AUSD', '0x00000000eFE302BEAA2b3e6e1b18d08D69a9012a', 6, 'agora-dollar', 0),
-            ('USDS', '0xdC035D45d973E3EC169d2276DDab16f1e407384F', 18, 'usds', 0)
+            ('USDS', '0xdC035D45d973E3EC169d2276DDab16f1e407384F', 18, 'usds', 0),
+            ('FDUSD', '0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409', 18, 'first-digital-usd', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
