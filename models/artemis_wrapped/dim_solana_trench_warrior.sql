60c45eb7a861ab8297cfbfb25d9fa7fda01dae40
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-12-19 14:53:57 -0500

    Updating Art Wrapped Models  (#690)

--- models/artemis_wrapped/dim_solana_trench_warrior.sql
+++ models/artemis_wrapped/dim_solana_trench_warrior.sql
@@ -1,7 +1,8 @@
 {{config(materialized='table')}}
 
 
-select lower(address) as address, 'SOLANA_TRENCH_WARRIOR' as category, array_agg(app) as reason
+select lower(address) as address, 'SOLANA_TRENCH_WARRIOR' as category, sum(interactions) as reason
 from {{ ref('agg_solana_app_interactions') }}
-where interactions > 300 and app in ('pumpdotfun', 'raydium')
+where app in ('pumpdotfun', 'raydium')
 group by 1
+having reason > 300
--- models/artemis_wrapped/metrics/agg_artemis_wrapped_metrics.sql
+++ models/artemis_wrapped/metrics/agg_artemis_wrapped_metrics.sql
@@ -3,23 +3,17 @@
 select 
     address
     , total_txns
-    , total_txns_percent_rank
     , total_gas_paid
-    , total_gas_paid_percent_rank
     , days_onchain
-    , days_onchain_percent_rank
     , apps_used
-    , apps_used_percent_rank
-from {{ ref('agg_base_artemis_wrapped_metrics_with_percent') }}
+from {{ ref('agg_base_artemis_wrapped_metrics') }}
+
 union all
+
 select 
-    address
+    address::string as address
     , total_txns
-    , total_txns_percent_rank
     , total_gas_paid
-    , total_gas_paid_percent_rank
     , days_onchain
-    , days_onchain_percent_rank
     , apps_used
-    , apps_used_percent_rank
-from {{ ref('agg_solana_artemis_wrapped_metrics_with_percent') }}
+from {{ ref('agg_solana_artemis_wrapped_metrics') }}
