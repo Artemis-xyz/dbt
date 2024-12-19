c69336aae3090da9ad8b3ae86c3d61c16e3f22af
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2024-12-18 22:57:46 -0500

    Updating wrapped models (#689)

--- models/artemis_wrapped/dim_artemis_wrapped.sql
+++ models/artemis_wrapped/dim_artemis_wrapped.sql
@@ -33,13 +33,13 @@ categories as (
             , ARRAY_TO_STRING(boomer.reason, ', ')
         ) as reason
     from {{ref('dim_blockbuster')}} block_buster
-    full outer join {{ref('dim_bob_the_builder')}} bob_the_builder using (address)
-    full outer join {{ref('dim_botimus_prime')}} botimus_prime using (address)
-    full outer join {{ref('dim_terminally_based')}} terminally_based using (address)
-    full outer join {{ref('dim_wolf_of_wallstreet')}} wolf_of_wallstreet using (address)
-    full outer join {{ref('dim_dora_the_explorer')}} dora_the_explorer using (address)
-    full outer join {{ref('dim_old_mcdonald')}} old_mcdonald using (address)
-    full outer join {{ref('dim_boomer')}} boomer using (address)
+    full outer join {{ref('dim_bob_the_builder')}} bob_the_builder on lower(block_buster.address) = lower(bob_the_builder.address)
+    full outer join {{ref('dim_botimus_prime')}} botimus_prime on lower(block_buster.address) = lower(botimus_prime.address)
+    full outer join {{ref('dim_terminally_based')}} terminally_based on lower(block_buster.address) = lower(terminally_based.address)
+    full outer join {{ref('dim_wolf_of_wallstreet')}} wolf_of_wallstreet on lower(block_buster.address) = lower(wolf_of_wallstreet.address)
+    full outer join {{ref('dim_dora_the_explorer')}} dora_the_explorer on lower(block_buster.address) = lower(dora_the_explorer.address)
+    full outer join {{ref('dim_old_mcdonald')}} old_mcdonald on lower(block_buster.address) = lower(old_mcdonald.address)
+    full outer join {{ref('dim_boomer')}} boomer on lower(block_buster.address) = lower(boomer.address)
 )
 
 select 
@@ -54,5 +54,5 @@ select
     , days_onchain_percent_rank as days_onchain_percent_rank
     , apps_used
     , apps_used_percent_rank as apps_used_percent_rank
-from {{ref('agg_artemis_wrapped_metrics')}}
-left join categories using (address)
+from {{ref('agg_artemis_wrapped_metrics')}} wrapped
+left join categories on lower(wrapped.address) = lower(categories.address)
--- models/artemis_wrapped/metrics/agg_artemis_wrapped_metrics.sql
+++ models/artemis_wrapped/metrics/agg_artemis_wrapped_metrics.sql
@@ -10,4 +10,16 @@ select
     , days_onchain_percent_rank
     , apps_used
     , apps_used_percent_rank
-from {{ ref('agg_base_artemis_wrapped_metrics') }}
+from {{ ref('agg_base_artemis_wrapped_metrics_with_percent') }}
+union all
+select 
+    address
+    , total_txns
+    , total_txns_percent_rank
+    , total_gas_paid
+    , total_gas_paid_percent_rank
+    , days_onchain
+    , days_onchain_percent_rank
+    , apps_used
+    , apps_used_percent_rank
+from {{ ref('agg_solana_artemis_wrapped_metrics_with_percent') }}
