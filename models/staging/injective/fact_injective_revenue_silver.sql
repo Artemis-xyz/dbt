1dcae71767772d99e3d2e3f9be9cb5efdf950494
Alex Weseley <pampawes@gmail.com>
2025-01-10 09:44:23 -0500

    inj unlocks and mints, revenue

--- models/staging/injective/fact_injective_revenue_silver.sql
+++ models/staging/injective/fact_injective_revenue_silver.sql
@@ -1,4 +1,4 @@
-select r.date, r.revenue_native * p.price as revenue, 'injective' as chain
+select r.date, r.revenue_native * p.price as revenue, r.revenue_native as revenue_native, 'injective' as chain
 from {{ ref("fact_injective_revenue_native_silver")}} r
 left join
     ({{ get_coingecko_price_with_latest("injective-protocol") }}) p on p.date = r.date
