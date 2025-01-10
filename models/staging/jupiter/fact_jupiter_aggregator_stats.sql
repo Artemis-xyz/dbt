8ac59e829903f130572a36019a15e8a30990f3e7
Alex Weseley <pampawes@gmail.com>
2025-01-10 11:50:41 -0500

    add jupiter agg trader count

--- models/staging/jupiter/fact_jupiter_aggregator_stats.sql
+++ models/staging/jupiter/fact_jupiter_aggregator_stats.sql
@@ -5,16 +5,10 @@
     )
 }}
 
-with
-    max_extraction as (
-        select max(extraction_date) as max_date
-        from {{ source("PROD_LANDING", "raw_jupiter_aggregator_metrics") }}
-    )
-select
-    value:date::date as date,
-    value:overall::number as aggregator_multi_hop_volume,
-    value:single::number as aggregator_single_hop_volume
-from
-    {{ source("PROD_LANDING", "raw_jupiter_aggregator_metrics") }},
-    lateral flatten(input => parse_json(source_json))
-where extraction_date = (select max_date from max_extraction)
+SELECT
+    date,
+    aggregator_multi_hop_volume,
+    aggregator_single_hop_volume,
+    unique_aggregator_traders
+FROM {{ ref("fact_jupiter_aggregator_volume") }}
+FULL OUTER JOIN {{ ref("fact_jupiter_aggregator_unique_traders") }} USING (date)
