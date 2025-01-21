dc5f86e6f072c4fda895e25b55e57d078898588e
limzhengjie <limzhengjie01@gmail.com>
2025-01-21 16:03:09 -0500

    Hyperliquid trades and fees

--- models/projects/hyperliquid/core/ez_hyperliquid_metrics_by_chain.sql
+++ models/projects/hyperliquid/core/ez_hyperliquid_metrics_by_chain.sql
@@ -16,6 +16,14 @@ with
     unique_traders_data as (
         select date, unique_traders, chain
         from {{ ref("fact_hyperliquid_unique_traders") }}
+    ),
+    daily_transactions_data as (
+        select date, trades, chain
+        from {{ ref("fact_hyperliquid_daily_transactions") }}
+    ),
+    fees_data as (
+        select date, fees/1e9 as fees, spot_fees/1e9 as spot_fees, chain
+        from {{ ref("fact_hyperliquid_fees") }}
     )
 select
     date,
@@ -23,7 +31,12 @@ select
     'DeFi' as category,
     chain,
     trading_volume,
-    unique_traders
+    unique_traders,
+    trades,
+    fees,
+    spot_fees
 from unique_traders_data
 left join trading_volume_data using(date, chain)
+left join daily_transactions_data using(date, chain)
+left join fees_data using(date, chain)
 where date < to_date(sysdate())
