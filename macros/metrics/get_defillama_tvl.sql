e69c28de2259f098477487c6c77f77d876ecb013
Alex Weseley <pampawes@gmail.com>
2025-01-22 16:44:35 -0500

    add dl tvl for several protocols across chains

---
+++ macros/metrics/get_defillama_tvl.sql
@@ -0,0 +1,12 @@
+{% macro get_defillama_protocol_tvl(defillama_name) %}
+
+select 
+    t.date, 
+    tvl
+from pc_dbt_db.prod.fact_defillama_protocol_tvls t
+join pc_dbt_db.prod.fact_defillama_protocols p 
+    on p.id = t.defillama_protocol_id 
+    and p.name ilike '%{{ defillama_name }}%'
+order by t.date desc
+
+{% endmacro %}
---
+++ models/projects/veax/core/ez_veax_metrics_by_chain.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='table',
+        snowflake_warehouse='VEAX',
+        database='VEAX',
+        schema='core',
+        alias='ez_metrics_by_chain'
+    )
+}}
+
+with veax_tvl as (
+    {{ get_defillama_protocol_tvl('veax') }}
+)
+
+select
+    veax_tvl.date,
+    'Defillama' as source,
+    'near' as chain,
+    veax_tvl.tvl
+from veax_tvl
+where veax_tvl.date < to_date(sysdate())
