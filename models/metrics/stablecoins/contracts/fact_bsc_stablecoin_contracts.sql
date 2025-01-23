b5c639f10b9eb43b5623d2b78fcddff460c96de5
Irwyn Liong <106728017+irwynliong@users.noreply.github.com>
2025-01-23 18:19:24 -0500

    Tusd asset (#730)

--- models/metrics/stablecoins/contracts/fact_bsc_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_bsc_stablecoin_contracts.sql
@@ -8,5 +8,6 @@ from
             ('DAI', '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'dai', 0),
             ('BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'binance-usd', 0),
             ('FDUSD', '0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409', 18, 'first-digital-usd', 0),
-            ('USDX', '0xf3527ef8dE265eAa3716FB312c12847bFBA66Cef', 18, 'usdx-money-usdx', 0)
+            ('USDX', '0xf3527ef8dE265eAa3716FB312c12847bFBA66Cef', 18, 'usdx-money-usdx', 0),
+            ('TUSD', '0x40af3827F39D0EAcBF4A168f8D4ee67c121D11c9', 18, 'true-usd', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
