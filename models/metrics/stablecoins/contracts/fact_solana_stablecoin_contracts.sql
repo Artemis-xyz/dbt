7eb8b872523cbf400baf3ab4ddb713c6f70d7847
Irwyn <irwynliong01@gmail.com>
2025-01-24 12:22:34 -0500

    removing usd* temporarily

--- models/metrics/stablecoins/contracts/fact_solana_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_solana_stablecoin_contracts.sql
@@ -16,6 +16,6 @@ from
             ('USDS', 'USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA', 6, 'usds', 0),
             ('USDe', 'DEkqHyPN7GMRJ5cArtQFAWefqbZb33Hyf6s5iCwjEonT', 9, 'ethena-usde', 0),
             ('FDUSD', '9zNQRsGLjNKwCUU5Gq5LR8beUCPzQMVMqKAi3SSZh54u', 6, 'first-digital-usd', 0),
-            ('USDY', 'A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6', 6, 'ondo-us-dollar-yield', 0),
-            ('USD*', '7MCXdudmztA6VwqsaWsNxFn8f7dVPxZorAHYbwwT2oTb', 9, 'perena-usd', 0) -- coingecko id is a placeholder until updated
+            ('USDY', 'A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6', 6, 'ondo-us-dollar-yield', 0)
+            -- ('USD*', '7MCXdudmztA6VwqsaWsNxFn8f7dVPxZorAHYbwwT2oTb', 9, 'perena-usd', 0) -- coingecko id is a placeholder until updated
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
