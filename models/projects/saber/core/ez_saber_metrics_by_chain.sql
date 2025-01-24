850346ed5ccddd6049365150d0811654ea4750d6
alexwes <pampawes@gmail.com>
2025-01-23 19:22:55 -0500

    Wes/dl dexes (#728)

---
+++ models/projects/saber/core/ez_saber_metrics_by_chain.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table',
+        snowflake_warehouse='SABER',
+        database='SABER',
+        schema='core',
+        alias='ez_metrics_by_chain'
+    )
+}}
+
+with saber_tvl as (
+    {{ get_defillama_protocol_tvl('saber') }}
+)
+select
+    saber_tvl.date,
+    'Defillama' as source,
+    'solana' as chain,
+    saber_tvl.tvl
+from saber_tvl
+where saber_tvl.date < to_date(sysdate())
