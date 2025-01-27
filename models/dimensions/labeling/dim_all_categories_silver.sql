d090239f80a79dd71b3c18876f0f0a6ac10295b7
Horace Liu <65971326+horaceliuHL@users.noreply.github.com>
2025-01-27 11:46:50 -0500

    Add categories to labeling pipeline (#731)

--- models/dimensions/labeling/dim_all_categories_silver.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized="table"
-    )
-}}
-
-with temp as (
-select distinct category, sub_category from dim_apps_gold where category is not null)
-select category, ARRAY_AGG(sub_category) as sub_category from temp group by category;
