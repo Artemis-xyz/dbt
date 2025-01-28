a81dd3a6b4103f72b53f44bfcf0c85fe5b2cb24d
Ben Stamm <benstamm11@gmail.com>
2025-01-28 18:10:33 -0500

    adding balancer v1 sql queries

--- models/projects/balancer/core/ez_balancer_metrics_by_chain.sql
+++ models/projects/balancer/core/ez_balancer_metrics_by_chain.sql
@@ -7,16 +7,26 @@
         alias='ez_metrics_by_chain'
     )
 }}
-
-with all_tvl_by_chain as (
+/*
+    with all_tvl_by_chain as (
+        SELECT
+            date,
+            chain,
+            sum(tvl_native) as tvl_native,
+            sum(tvl_usd) as tvl_usd
+        FROM {{ ref('fact_balancer_tvl_by_chain_and_token') }}
+        group by 1,2
+),
+*/
+with tvl_balancer_v1 as (
     SELECT
         date,
-        chain,
-        sum(tvl_native) as tvl_native,
-        sum(tvl_usd) as tvl_usd
-    FROM {{ ref('fact_balancer_tvl_by_chain_and_token') }}
+        'ethereum' as chain,
+        SUM(tvl_token_adjusted) as tvl_usd
+    FROM {{ ref('fact_balancer_liquidity') }}
     group by 1,2
 )
+
 , treasury_by_chain as (
     SELECT
         date,
@@ -51,24 +61,63 @@ with all_tvl_by_chain as (
     FROM {{ ref('dim_date_spine') }}
     CROSS JOIN (SELECT distinct chain from treasury_by_chain
         UNION
-        SELECT distinct chain from all_tvl_by_chain
-        UNION
+        --SELECT distinct chain from all_tvl_by_chain
+        --UNION
         SELECT distinct chain from treasury_native
         UNION
         SELECT distinct chain from net_treasury
     )
     where date between '2020-03-01' and to_date(sysdate())
 )
+,   trading_metrics_by_chain AS (
+        SELECT 
+            block_date AS date,
+            blockchain AS chain,
+            version,
+            COUNT(*) AS swap_count,
+            SUM(swap_fee_usd) AS trading_fees,
+            SUM(swap_fee_usd) AS fees, --total fees == trading fees 
+            SUM(swap_fee_usd) AS primary_supply_side_revenue,
+            0 AS secondary_supply_side_revenue,
+            SUM(swap_fee_usd) AS total_supply_side_revenue,
+            0 AS protocol_revenue,
+            0 AS operating_expenses,
+            0 AS token_incentives,        -- to verify
+            0 AS protocol_earnings,       -- to verify
+            SUM(token_sold_amount_usd) AS trading_volume,
+            COUNT(DISTINCT taker) AS unique_traders,
+            'TBD' AS gas_cost_native,
+            'TBD' AS gas_cost_usd
+        FROM {{ ref('fact_balancer_trades') }}
+        WHERE NOT (token_sold_amount_raw > 9E25 AND token_sold_amount_usd > 10000000000)
+        GROUP BY block_date, blockchain, version
+)
 
 select
     date_chain_spine.date,
     date_chain_spine.chain,
-    all_tvl_by_chain.tvl_usd as tvl,
+    trading_metrics_by_chain.version,
+    trading_metrics_by_chain.swap_count,
+    trading_metrics_by_chain.trading_fees,
+    trading_metrics_by_chain.fees,
+    trading_metrics_by_chain.primary_supply_side_revenue,
+    trading_metrics_by_chain.secondary_supply_side_revenue,
+    trading_metrics_by_chain.total_supply_side_revenue,
+    trading_metrics_by_chain.protocol_revenue,
+    trading_metrics_by_chain.operating_expenses,
+    trading_metrics_by_chain.token_incentives,
+    trading_metrics_by_chain.protocol_earnings,
+    --all_tvl_by_chain.tvl_usd as tvl,
+    tvl_balancer_v1.tvl_usd,
     treasury_by_chain.usd_balance as treasury_value,
+    treasury_native.treasury_native as treasury_native,
     net_treasury.net_treasury_usd as net_treasury_value,
-    treasury_native.treasury_native as treasury_native
+    trading_metrics_by_chain.trading_volume,
+    trading_metrics_by_chain.unique_traders
 from date_chain_spine
-left join all_tvl_by_chain using (date, chain)
+--left join all_tvl_by_chain using (date, chain)
 left join treasury_by_chain using (date, chain)
 left join treasury_native using (date, chain)
 left join net_treasury using (date, chain)
+left join trading_metrics_by_chain using (date, chain)
+left join tvl_balancer_v1 using (date, chain)
--- models/projects/balancer/core/ez_balancer_metrics_by_token.sql
+++ models/projects/balancer/core/ez_balancer_metrics_by_token.sql
@@ -17,7 +17,17 @@ with all_tvl_by_token as (
     FROM {{ ref('fact_balancer_tvl_by_chain_and_token') }}
     where tvl_usd > 0
     group by 1,2
+),
+tvl_balancer_v1 as (
+    SELECT
+        date,
+        token_address,
+        token_symbol as token,
+        SUM(tvl_token_adjusted) as tvl_usd
+    FROM {{ ref('fact_balancer_liquidity') }}
+    group by 1,2,3
 )
+
 
 , treasury_by_token as (
     SELECT
@@ -48,6 +58,31 @@ with all_tvl_by_token as (
     and native_balance > 0
     group by 1,2
 )
+,   trading_metrics_by_token_sold AS (
+        SELECT 
+            block_date AS date,
+            blockchain AS chain,
+            version,
+            token_sold_address,
+            token_sold_symbol as token,
+            blockchain,
+            COUNT(*) AS swap_count,
+            SUM(swap_fee_usd) AS trading_fees,
+            SUM(swap_fee_usd) AS fees, --total fees == trading fees 
+            SUM(swap_fee_usd) AS primary_supply_side_revenue,
+            0 AS secondary_supply_side_revenue,
+            SUM(swap_fee_usd) AS total_supply_side_revenue,
+            0 AS protocol_revenue,
+            0 AS operating_expenses,
+            0 AS token_incentives,        -- to verify
+            0 AS token_incentives_native,
+            0 AS protocol_earnings,       -- to verify
+            SUM(token_sold_amount_usd) AS trading_volume,
+            COUNT(DISTINCT taker) AS unique_traders,
+        FROM {{ ref('fact_balancer_trades') }}
+        WHERE NOT (token_sold_amount_raw > 9E25 AND token_sold_amount_usd > 10000000000)
+        GROUP BY block_date, token_sold_address, token_sold_symbol, blockchain, version
+) 
 ,date_token_spine as (
     SELECT
         distinct
@@ -61,19 +96,29 @@ with all_tvl_by_token as (
                 SELECT distinct token from net_treasury
                 UNION
                 SELECT distinct token from treasury_native
+                UNION
+                SELECT distinct token from trading_metrics_by_token_sold
+                UNION
+                SELECT distinct token from tvl_balancer_v1
                 )
     where date between '2020-03-01' and to_date(sysdate())
 )
-
 select
     date_token_spine.date,
-    token,
-    all_tvl_by_token.tvl_usd as tvl,
+    trading_metrics_by_token_sold.token,
+    trading_metrics_by_token_sold.blockchain,
+    trading_metrics_by_token_sold.trading_fees,
+    trading_metrics_by_token_sold.token_incentives_native,
     treasury_by_token.usd_balance as treasury_value,
     net_treasury.net_treasury_usd as net_treasury_value,
-    treasury_native.treasury_native as treasury_native
+    treasury_native.treasury_native as treasury_native,
+    trading_metrics_by_token_sold.trading_volume as trading_volume,
+    tvl_balancer_v1.tvl_usd
+    --all_tvl_by_token.tvl_usd as tvl
 from date_token_spine
-full outer join all_tvl_by_token using (date, token)
+--full outer join all_tvl_by_token using (date, token)
 full outer join treasury_by_token using (date, token)
 full outer join net_treasury using (date, token)
 full outer join treasury_native using (date, token)
+left join trading_metrics_by_token_sold using (date, token)
+left join tvl_balancer_v1 using (date, token)
