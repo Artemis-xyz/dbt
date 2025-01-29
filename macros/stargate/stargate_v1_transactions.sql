131a4843ec4e7df752fcab2a30478b5c5f24eee1
HPX <henry@artemisanalytics.xyz>
2025-01-29 01:33:28 -0800

    Update jinja, add fallback to no pools

--- macros/stargate/stargate_v1_transactions.sql
+++ macros/stargate/stargate_v1_transactions.sql
@@ -14,25 +14,25 @@ with
             ('0xF39B7Be294cB36dE8c510e267B82bb588705d977'),
             ('0x600E576F9d853c95d58029093A16EE49646F3ca5')
             
-            {% if chain == 'avalanche' %}
+            {% elif chain == 'avalanche' %}
             ('0x1205f31718499dBf1fCa446663B532Ef87481fe1'),
             ('0x29e38769f23701A2e4A8Ef0492e19dA4604Be62c'),
             ('0x1c272232Df0bb6225dA87f4dEcD9d37c32f63Eea'),
             ('0x8736f92646B2542B3e5F3c63590cA7Fe313e283B'),
             ('0xEAe5c2F6B25933deB62f754f239111413A0A25ef')
 
-            {% if chain == 'base' %}
+            {% elif chain == 'base' %}
             ('0x28fc411f9e1c480AD312b3d9C60c22b965015c6B'),
             ('0x4c80E24119CFB836cdF0a6b53dc23F04F7e652CA')
 
-            {% if chain == 'bsc' %}
+            {% elif chain == 'bsc' %}
             ('0x9aA83081AA06AF7208Dcc7A4cB72C94d057D2cda'),
             ('0x98a5737749490856b401DB5Dc27F522fC314A4e1'),
             ('0x4e145a589e4c03cBe3d28520e4BF3089834289Df'),
             ('0x7BfD7f2498C4796f10b6C611D9db393D3052510C'),
             ('0x68C6c27fB0e02285829e69240BE16f32C5f8bEFe')
 
-            {% if chain == 'ethereum' %}
+            {% elif chain == 'ethereum' %}
             ('0x101816545F6bd2b1076434B54383a1E633390A2E'),
             ('0xdf0770dF86a8034b3EFEf0A1Bb3c889B8332FF56'),
             ('0x38EA452219524Bb87e18dE1C24D3bB59510BD783'),
@@ -46,7 +46,7 @@ with
             ('0x430Ebff5E3E80A6C58E7e6ADA1d90F5c28AA116d'),
             ('0xa572d137666dcbadfa47c3fc41f15e90134c618c')
 
-            {% if chain == 'optimism' %}
+            {% elif chain == 'optimism' %}
             ('0xd22363e3762cA7339569F3d33EADe20127D5F98C'),
             ('0xDecC0c09c3B5f6e92EF4184125D5648a66E35298'),
             ('0x165137624F1f692e69659f944BF69DE02874ee27'),
@@ -55,6 +55,10 @@ with
             ('0x3533F5e279bDBf550272a199a223dA798D9eff78'),
             ('0x5421FA1A48f9FF81e4580557E86C7C0D24C1803')
 
+            {% else %}
+            (NULL) -- Fallback
+
+            {% endif %}
         ) AS addresses(address)
     ),
 
