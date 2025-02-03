296017bec5af604ea40fd7391cf00481e424e219
alexwes <pampawes@gmail.com>
2025-02-02 20:27:54 -0500

    use bigquery instead of zettablocks (#761)

--- models/projects/sui/core/ez_sui_metrics.sql
+++ models/projects/sui/core/ez_sui_metrics.sql
@@ -12,13 +12,7 @@ with
     fundamental_data as (select * EXCLUDE date, TO_TIMESTAMP_NTZ(date) AS date from {{ source('PROD_LANDING', 'ez_sui_metrics') }}),
     price_data as ({{ get_coingecko_metrics("sui") }}),
     defillama_data as ({{ get_defillama_metrics("sui") }}),
-    github_data as ({{ get_github_metrics("sui") }}),
-    epoch_data as (
-        select
-            date,
-            stake_subsidy_amount as mints_native
-        from {{ ref('fact_sui_epoch_staking_data') }}
-    )
+    github_data as ({{ get_github_metrics("sui") }})
 select
     fundamental_data.date,
     'sui' as chain,
@@ -44,7 +38,6 @@ select
     weekly_developers_core_ecosystem,
     weekly_developers_sub_ecosystem
 from fundamental_data
-left join epoch_data on fundamental_data.date = epoch_data.date
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
 left join github_data on fundamental_data.date = github_data.date
