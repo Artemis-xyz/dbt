298bf81874a9cd0ee6c4a3ac013664c55859a636
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-02-05 18:39:56 -0500

    Son/add parity ez tables (#775)

---
+++ models/projects/moonbeam/core/ez_moonbeam_metrics.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="MOONBEAM",
+        database="moonbeam",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+with
+    fundamental_data as (
+        select
+            date, 
+            txns,
+            daa, 
+            fees_native, 
+            fees_usd
+        from {{ ref("fact_moonbeam_fundamental_metrics") }}
+    )
+select
+    date
+    , txns
+    , daa
+    , coalesce(fees_native, 0) as fees_native
+    , coalesce(fees_usd, 0) as fees_usd
+from fundamental_data
+where fundamental_data.date < to_date(sysdate())
---
+++ models/staging/mythos/fact_mythos_transactions.sql
@@ -0,0 +1,31 @@
+with
+    transactions as (
+        {{ parse_parity_parquets("mythos", "transactions") }}
+    ),
+    fees as (
+        {{ parse_parity_parquets("mythos", "fees") }}
+    ),
+    decimals as (
+        select
+            chain
+            , unit
+            , decimals
+        from {{ source("MANUAL_STATIC_TABLES", "polkadot_token_decimals") }}
+        where chain = 'mythos'
+    ),
+    prices as ({{ get_coingecko_price_with_latest("mythos") }})
+    select
+        coalesce(t.timestamp::date, f.timestamp::date) as date
+        , coalesce(t.timestamp, f.timestamp) as timestamp
+        , coalesce(t.number, f.number) as number
+        , coalesce(t.hash, f.hash) as hash
+        , success
+        , signer_id
+        , coalesce(t.relay_chain, f.relay_chain) as relay_chain
+        , coalesce(f.fees, 0) / POW(10, decimals) as fees_native
+        , coalesce(f.fees, 0) / POW(10, decimals) * price as fees_usd
+    FROM transactions as t
+    full join fees as f on t.hash = f.hash
+    left join prices on prices.date = coalesce(t.timestamp::date, f.timestamp::date)
+    where
+        coalesce(t.timestamp, f.timestamp) < date(sysdate())
