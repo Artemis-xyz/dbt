6bdaae8586bda207e1ab39b1c495bf2f718ca437
Son Do <sondo077@gmail.com>
2025-02-05 18:21:53 -0500

    Added Moonbeam evm transactions

--- models/staging/moonbeam/fact_moonbeam_evm_transactions.sql
+++ models/staging/moonbeam/fact_moonbeam_evm_transactions.sql
@@ -10,5 +10,5 @@ select
     , parquet_raw:"relay_chain"::string as relay_chain
     , parquet_raw:"signer"::string as signer 
     , to_timestamp(RIGHT(parquet_raw:"timestamp", 3)::integer) as timestamp
-    , parquet_raw:"timestamp"
+    , parquet_raw:"timestamp" as timestamp_raw
 from {{ source("PROD_LANDING", "raw_moonbeam_evm_transactions_parquet") }} 
