8dd420d5a2fe2646a46ef1e8d78cdd1a59a7efeb
Irwyn Liong <106728017+irwynliong@users.noreply.github.com>
2025-02-08 09:16:35 -0500

    Adding USDz, USDtb and crvUSD (#785)

--- models/metrics/stablecoins/contracts/fact_arbitrum_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_arbitrum_stablecoin_contracts.sql
@@ -19,5 +19,6 @@ from
             ('TUSD', '0x4D15a3A2286D883AF0AA1B3f21367843FAc63E07', 18, 'true-usd', 0),
             ('FRAX', '0x17fc002b466eec40dae837fc4be5c67993ddbd6f', 18, 'frax', 0),
             ('GHO', '0x7dfF72693f6A4149b17e7C6314655f6A9F7c8B33', 18, 'gho', 0),
-            ('DOLA', '0x6a7661795c374c0bfc635934efaddff3a7ee23b6', 18, 'dola-usd', 0)
+            ('DOLA', '0x6a7661795c374c0bfc635934efaddff3a7ee23b6', 18, 'dola-usd', 0),
+            ('crvUSD', '0x498bf2b1e120fed3ad3d42ea2165e9b73f99c1e5', 18, 'crvusd', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
