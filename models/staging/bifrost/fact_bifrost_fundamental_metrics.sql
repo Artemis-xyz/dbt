9c38d71bb4f1dddb648bd7a15345e826bfe91b48
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-02-10 14:23:43 -0800

    add parity evm transactions (#788)

--- models/staging/bifrost/fact_bifrost_fundamental_metrics.sql
+++ models/staging/bifrost/fact_bifrost_fundamental_metrics.sql
@@ -5,13 +5,31 @@
     )
 }}
 
+with evm_transactions as (
+    select
+        date(timestamp) as date
+        , count(distinct receiver) as daa
+        , count(*) as txns
+    from {{ ref("fact_bifrost_evm_transactions") }}
+    group by date
+),
+parity_transactions as (
+    select
+        date
+        , count(*) as txns
+        , count(distinct signer_id) as daa
+        , sum(fees_native) as fees_native
+        , sum(fees_usd) as fees_usd
+    from {{ ref("fact_bifrost_transactions") }} as t
+    where signer_id is not null
+    group by date
+)
 select
-    date, 
-    count(*) as txns,
-    count(distinct signer_id) as daa, 
-    sum(fees_native) as fees_native, 
-    sum(fees_usd) as fees_usd
-from {{ ref("fact_bifrost_transactions") }}
-where signer_id is not null
-    and date < to_date(sysdate())
-GROUP BY date
+    coalesce(t.date, e.date) as date
+    , t.txns + e.txns as txns
+    , t.daa + e.daa as daa
+    , t.fees_native as fees_native
+    , t.fees_usd as fees_usd
+from parity_transactions as t
+left join evm_transactions as e on t.date = e.date
+    and coalesce(t.date, e.date) < to_date(sysdate())
