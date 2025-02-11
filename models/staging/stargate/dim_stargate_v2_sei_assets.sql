a5c224cb1dd40a24743f6d052d1d5569536a1e32
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2025-02-11 16:22:03 -0500

    Stargate: Index Berachain, mantle, sei (#794)

---
+++ models/staging/stargate/dim_stargate_v2_sei_assets.sql
@@ -0,0 +1,9 @@
+{{config(materialized="table", snowflake_warehouse='BAM_TRENDING_WAREHOUSE_LG')}}
+{{
+    stargate_asset_map(
+        'sei'
+        , '0x1502FA4be69d526124D453619276FacCab275d3D'
+        , '0x5c386D85b1B82FD9Db681b9176C8a4248bb6345B'
+        , '0x160345fC359604fC6e70E3c5fAcbdE5F7A9342d8'
+    )
+}}
