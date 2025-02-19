8ddd9c8e4fe441d8bacc335c08453a7f487b3f56
Sebastian Melendez <sebm@seas.upenn.edu>
2025-02-19 14:13:02 -0500

    updating incremental

--- models/staging/standard_8d737a18/fact_celo_traces_standard_8d737a18.sql
+++ models/staging/standard_8d737a18/fact_celo_traces_standard_8d737a18.sql
@@ -1,3 +1,3 @@
-{{config(materialized="incremental", unique_key=["tx_hash", "event_index"])}}
+{{config(materialized="incremental", unique_key=["tx_hash", "trace_id"])}}
 
 {{standard_8d737a18_traces("celo")}}
