e5b9f04fd942601a44658639be70838c51829e9a
Alex Weseley <pampawes@gmail.com>
2025-02-18 23:19:57 -0500

    remove dups from account to mint table

--- models/staging/solana/fact_solana_token_account_to_mint.sql
+++ models/staging/solana/fact_solana_token_account_to_mint.sql
@@ -17,6 +17,7 @@ select
 from
     solana_flipside.core.fact_token_balances
 where 1=1
+    and owner is not null -- some duplicates in source table where owner is null
     {% if is_incremental() %}
         and block_timestamp
         >= (select dateadd('day', -3, max(block_timestamp)) from solana_flipside.core.fact_token_balances) -- 3 day lookback on source table
