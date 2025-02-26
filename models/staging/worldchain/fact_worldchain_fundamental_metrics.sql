1892aeb32627dcf1ca230e66b175c7760723a651
Son Do <sondo077@gmail.com>
2025-02-26 11:32:49 -0500

    unichain, worldchain revs

--- models/staging/worldchain/fact_worldchain_fundamental_metrics.sql
+++ models/staging/worldchain/fact_worldchain_fundamental_metrics.sql
@@ -1,6 +1,17 @@
-{{ config(materialized="incremental", snowflake_warehouse="WORLDCHAIN", unique_key=["date"]) }}
+{{ config(materialized="incremental", snowflake_warehouse="UNICHAIN", unique_key=["date"]) }}
 
-with fundamental_data as (
+with 
+eth_l2_cost as (
+    {{ get_eth_l2_cost(
+        addresses=[
+            '0xff00000000000000000000000000000000000480',
+            '0x069c4c579671f8c120b1327a73217D01Ea2EC5ea'
+        ],
+        start_date = '2024-11-04'
+        )
+    }}
+),
+fundamental_data as (
     {{ get_goldsky_chain_fundamental_metrics("worldchain") }}
 ),
 price as (
@@ -12,7 +23,13 @@ SELECT
     fd.daa,
     fd.txns,
     fd.fees_native,
-    fees_native * price as fees
+    fees_native * price as fees,
+    coalesce(c.gas, 0) as cost_native,
+    coalesce(c.gas, 0) * price as cost,
+    fees - coalesce(c.gas * price, 0) as revenue,
+    fees_native - coalesce(c.gas, 0) as revenue_native
 FROM fundamental_data fd
 LEFT JOIN price p
 ON fd.date = p.date
+LEFT JOIN eth_l2_cost as c
+on fd.date = c.date
