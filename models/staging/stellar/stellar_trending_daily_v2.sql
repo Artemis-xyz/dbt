c48fd4db82f1934116f95dd676374bb7c7340dd2
horaceliuHL <horaceliu.hl@gmail.com>
2025-03-03 17:29:56 -0500

    Change tables to v2 in trending dbt models

--- models/staging/stellar/stellar_trending_daily_v2.sql
+++ models/staging/stellar/stellar_trending_daily_v2.sql
@@ -21,7 +21,7 @@ WITH contracts AS (
         metrics.gas_usd,
         metrics.friendly_name,
         metrics.category
-    FROM {{ ref('ez_stellar_metrics_by_application') }} metrics
+    FROM {{ ref('ez_stellar_metrics_by_application_v2') }} metrics
     LEFT join contracts 
         ON metrics.app = contracts.app
     where 
@@ -36,7 +36,7 @@ WITH contracts AS (
         metrics.dau,
         metrics.gas,
         metrics.gas_usd
-    FROM {{ ref('ez_stellar_metrics_by_application') }} metrics
+    FROM {{ ref('ez_stellar_metrics_by_application_v2') }} metrics
     LEFT join contracts 
         ON metrics.app = contracts.app
     WHERE
