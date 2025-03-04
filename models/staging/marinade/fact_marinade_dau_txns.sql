9f77632d3e456fe21fde94f907bb511760574fd9
limzhengjie <limzhengjie01@gmail.com>
2025-03-04 16:40:36 -0500

    added signers index

--- models/staging/marinade/fact_marinade_dau_txns.sql
+++ models/staging/marinade/fact_marinade_dau_txns.sql
@@ -5,7 +5,7 @@
 
 select 
     date(block_timestamp) as date, 
-    count(distinct signers) as dau,
+    count(distinct signers[0]) as dau,
     count(*) as txns
 from solana_flipside.core.fact_decoded_instructions
 where program_id = 'MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD'
