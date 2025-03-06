38187a200d1be507632add8c969626eded27bb17
limzhengjie <86870373+limzhengjie@users.noreply.github.com>
2025-03-06 08:04:54 +0800

    veSTG top 100 holders (#890)

---
+++ models/staging/stargate/fact_stargate_top100veSTGholders.sql
@@ -0,0 +1,31 @@
+{{config(materialized="table", snowflake_warehouse='STARGATE')}}
+
+with 
+combined_events as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                ref("dim_stargate_arbitrum_veSTGholders"),
+                ref("dim_stargate_avalanche_veSTGholders"),
+                ref("dim_stargate_bsc_veSTGholders"),
+                ref("dim_stargate_ethereum_veSTGholders"),
+                ref("dim_stargate_optimism_veSTGholders"),
+                ref("dim_stargate_polygon_veSTGholders"),
+            ],
+        )
+    }}
+)
+select
+    from_address,
+    stg_balance,
+    veSTG_balance,
+    remaining_days,
+    remaining_staking_period,
+    percentage_of_circulating_supply,
+    number_of_votes_cast,
+    last_voted_timestamp,
+    last_change_timestamp,
+    last_action_type,
+    fees_received,
+    chain
+from combined_events
