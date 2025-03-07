a94480be7bfa12958bb4ace3e5e290d91ba99f10
horaceliuHL <horaceliu.hl@gmail.com>
2025-03-07 11:47:01 -0500

    Make dim events silver unique based on source

--- models/dimensions/events/dim_events_silver.sql
+++ models/dimensions/events/dim_events_silver.sql
@@ -35,3 +35,4 @@ select
     source
 from event_signatures
 where event_id = 1 and event_name not in ('AuthorizationCanceled', 'ValidatorEcdsaPublicKeyUpdated', 'ValidatorBlsPublicKeyUpdated', 'AuthorizationUsed', 'TransferComment') -- These events emit types currently not supported by the decode_evm_event_log function
+QUALIFY ROW_NUMBER() OVER (PARTITION BY topic_zero ORDER BY source = 'artemis' DESC) = 1
