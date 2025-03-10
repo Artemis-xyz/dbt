d4a9a9b69bfddf5eea3a950d69945621b3d02a92
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-03-10 11:34:14 -0400

    Fixed Across Null data (#913)

--- models/staging/across/fact_across_v3_transfers.sql
+++ models/staging/across/fact_across_v3_transfers.sql
@@ -8,10 +8,10 @@
 
 select 
     src_messaging_contract_address as contract_address
-    , src_block_timestamp as block_timestamp
-    , src_tx_hash as tx_hash
-    , src_event_index as event_index
-    , dst_amount as amount
+    , coalesce(src_block_timestamp, dst_block_timestamp) as block_timestamp
+    , coalesce(src_tx_hash, dst_tx_hash) as tx_hash
+    , coalesce(src_event_index, dst_event_index) as event_index
+    , coalesce(dst_amount, src_amount) as amount
     , depositor
     , recipient
     , destination_chain_id
@@ -21,3 +21,4 @@ select
     , origin_token as input_token
     , null as destination_token_symbol
 from {{ref('fact_across_v3_complete_transfers')}}
+where coalesce(src_block_timestamp, dst_block_timestamp) is not null
