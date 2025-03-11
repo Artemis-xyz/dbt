3905ccff53706a9c7dc9c21b8fd63012d5c9c537
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-03-11 19:44:34 -0400

    Decode wormhole, icp update, and op super chain updates (#923)

--- models/staging/across/fact_across_v2_transfers.sql
+++ models/staging/across/fact_across_v2_transfers.sql
@@ -8,7 +8,7 @@
 
 SELECT
     src_messaging_contract_address as contract_address
-    , src_block_timestamp as block_timestamp
+    , coalesce(src_block_timestamp, dst_block_timestamp) as block_timestamp
     , src_tx_hash as tx_hash
     , src_event_index as event_index
     , coalesce(dst_amount, src_amount) as amount
@@ -21,3 +21,4 @@ SELECT
     , dst_relayer_fee_pct as relayer_fee_pct
     , null as destination_token_symbol
 from {{ref('fact_across_v2_complete_transfers')}}
+where coalesce(src_block_timestamp, dst_block_timestamp) is not null
