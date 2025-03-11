c9b1b440f3b195ecaec744afa2e581cd92225579
Alex Weseley <pampawes@gmail.com>
2025-03-11 16:24:47 -0400

    jitosol tvl and fees

--- models/projects/jito/core/ez_jito_metrics.sql
+++ models/projects/jito/core/ez_jito_metrics.sql
@@ -7,12 +7,39 @@
         alias='ez_metrics'
     )
 }}
+with 
+    jito_mgmt_withdraw_fees as (
+        SELECT 
+            day as date
+            , fees
+        FROM {{ ref('fact_jito_mgmt_withdraw_fees') }}
+    )
+    , jito_dau_txns_fees as ( -- Tips
+        SELECT 
+            day as date
+            , fees
+            , revenue
+            , supply_side_fees
+            , txns
+            , dau
+        FROM {{ ref('fact_jito_dau_txns_fees') }}
+    )
+    , date_spine as (
+        SELECT
+            date
+        FROM {{ ref('dim_date_spine') }}
+        WHERE date between (select min(date) from jito_dau_txns_fees) and (to_date(sysdate()))
+    )
 
 SELECT 
-    day as date
-    , fees
-    , revenue
-    , supply_side_fees
-    , txns
-    , dau
-FROM {{ ref('fact_jito_dau_txns_fees') }}
+    date
+    , withdraw_management_fees
+    , tip_fees
+    , withdraw_management_fees + tip_fees as fees
+    , tip_revenue + tip_fees as revenue
+    , tip_supply_side_fees as supply_side_fees
+    , tip_txns as txns
+    , tip_dau as dau
+FROM date_spine ds
+LEFT JOIN jito_mgmt_withdraw_fees using (date)
+LEFT JOIN jito_dau_txns_fees using (date)
---
+++ models/staging/jito/fact_jito_mgmt_withdraw_fees.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized="incremental",
+        snowflake_warehouse="MEDIUM"
+    )
+}}
+
+SELECT
+    block_timestamp::date as date,
+    mint,
+    sum(amount * price) as withdraw_management_fees
+FROM {{ source('SOLANA_FLIPSIDE', 'fact_transfers') }} t
+LEFT JOIN solana_flipside.price.ez_prices_hourly p on p.token_address = t.mint and p.hour = t.block_timestamp::date
+WHERE 1=1
+    AND tx_to = '5eosrve6LktMZgVNszYzebgmmC7BjLK8NoWyRQtcmGTF'
+    AND mint = 'J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn'
+    {% if is_incremental() %}
+        AND block_timestamp > (select dateadd(day, -3, max(date)) from {{ this }})
+    {% endif %}
+    AND amount * price < 1e5
+GROUP BY 1, 2
+ORDER BY 1 desc
