a8a89b37899622c1eeed5d8fb3e9a67e95080847
Alex Weseley <pampawes@gmail.com>
2025-03-11 19:37:56 -0400

    coalesce and use new macro

--- models/staging/jito/fact_jitosol_tvl.sql
+++ models/staging/jito/fact_jitosol_tvl.sql
@@ -5,16 +5,4 @@
     )
 }}
 
-{% set stake_accounts_query %}
-  select distinct stake_account from {{ ref('fact_jitosol_stake_accounts') }}
-{% endset %}
-
-{% set stake_accounts_results = run_query(stake_accounts_query) %}
-
-{% if execute %}
-    {% set stake_accounts_list = stake_accounts_results.columns[0].values() %}
-{% else %}
-    {% set stake_accounts_list = [] %}
-{% endif %}
-
-{{ get_treasury_balance(chain='solana', addresses = stake_accounts_list, earliest_date='2022-10-24') }}
+{{get_entity_historical_balance(chain='solana', table_name='fact_jitosol_stake_accounts', address_column='stake_account', earliest_date='2022-10-24')}}
