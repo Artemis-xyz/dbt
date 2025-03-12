701b966df80fe93b7e17b68c8cae9cab2ef6d963
Horace Liu <65971326+horaceliuHL@users.noreply.github.com>
2025-03-11 23:42:04 -0400

    Fix celo decode events macro (#924)

--- macros/decode/decode_events.sql
+++ macros/decode/decode_events.sql
@@ -18,7 +18,7 @@
     inner join {{ ref("dim_events_silver") }} t2 on t1.topic_zero = t2.topic_zero
     where tx_status = 1
     {% if is_incremental() %}
-        where
+        and
             block_timestamp
             >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
     {% endif %}
