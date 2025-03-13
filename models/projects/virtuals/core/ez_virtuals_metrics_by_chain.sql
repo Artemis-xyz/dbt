751ba6b4459db9bf8b7cbe04c6d93f0ef88a42dc
limzhengjie <limzhengjie01@gmail.com>
2025-03-13 15:44:43 -0400

    ez virtuals metrics by chain for trading_volume

---
+++ models/projects/virtuals/core/ez_virtuals_metrics_by_chain.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse = 'VIRTUALS',
+        database = 'VIRTUALS',
+        schema = 'core',
+        alias = 'ez_metrics_by_chain'
+    )
+}}
+
+select
+    date,
+    'base' as chain,
+    daily_agents,
+    dau,
+    volume_native,
+    volume_usd,
+    fee_fun_native,
+    fee_fun_usd,
+    tax_usd,
+    fees
+from {{ ref('ez_virtuals_metrics') }}
