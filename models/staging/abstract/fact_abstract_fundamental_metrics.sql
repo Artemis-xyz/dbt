a1bf7935ea12caf03d98abba33125a4682885134
Son Do <sondo077@gmail.com>
2025-03-13 18:07:25 -0400

    Fix Flows and added Abstract Data

---
+++ models/staging/abstract/fact_abstract_fundamental_metrics.sql
@@ -0,0 +1,34 @@
+{{ config(materialized="incremental", snowflake_warehouse="ABSTRACT", unique_key=["date"]) }}
+
+with 
+eth_l2_cost as (
+    {{ get_eth_l2_cost(
+        addresses=[
+            '0x5D8ba173Dc6C3c90C8f7C04C9288BeF5FDbAd06E',
+        ],
+        start_date = '2024-10-25'
+        )
+    }}
+),
+fundamental_data as (
+    {{ get_goldsky_chain_fundamental_metrics("abstract") }}
+),
+price as (
+    {{ get_coingecko_price_with_latest('ethereum') }}
+)
+
+SELECT 
+    fd.date,
+    fd.daa,
+    fd.txns,
+    fd.fees_native,
+    fees_native * price as fees,
+    coalesce(c.gas, 0) as cost_native,
+    coalesce(c.gas, 0) * price as cost,
+    fees - coalesce(c.gas * price, 0) as revenue,
+    fees_native - coalesce(c.gas, 0) as revenue_native
+FROM fundamental_data fd
+LEFT JOIN price p
+ON fd.date = p.date
+LEFT JOIN eth_l2_cost as c
+on fd.date = c.date
