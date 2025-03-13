96646734f382bc6bc0620c3b1084aa32bf9623f6
Irwyn Liong <106728017+irwynliong@users.noreply.github.com>
2025-03-13 14:27:05 -0400

    C911: Add supportStablecoins for sui and mantle (#939)

--- models/projects/sui/core/ez_sui_metrics.sql
+++ models/projects/sui/core/ez_sui_metrics.sql
@@ -12,6 +12,7 @@ with
     fundamental_data as (select * EXCLUDE date, TO_TIMESTAMP_NTZ(date) AS date from {{ source('PROD_LANDING', 'ez_sui_metrics') }}),
     price_data as ({{ get_coingecko_metrics("sui") }}),
     defillama_data as ({{ get_defillama_metrics("sui") }}),
+    stablecoin_data as ({{ get_stablecoin_metrics("sui") }}),
     github_data as ({{ get_github_metrics("sui") }})
 select
     fundamental_data.date,
@@ -36,9 +37,25 @@ select
     weekly_commits_core_ecosystem,
     weekly_commits_sub_ecosystem,
     weekly_developers_core_ecosystem,
-    weekly_developers_sub_ecosystem
+    weekly_developers_sub_ecosystem,
+    stablecoin_total_supply,
+    stablecoin_txns,
+    stablecoin_dau,
+    stablecoin_mau,
+    stablecoin_transfer_volume,
+    artemis_stablecoin_txns,
+    artemis_stablecoin_dau,
+    artemis_stablecoin_mau,
+    artemis_stablecoin_transfer_volume,
+    p2p_stablecoin_txns,
+    p2p_stablecoin_dau,
+    p2p_stablecoin_mau,
+    stablecoin_data.p2p_stablecoin_transfer_volume,
+    stablecoin_tokenholder_count,
+    p2p_stablecoin_tokenholder_count
 from fundamental_data
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
+left join stablecoin_data on fundamental_data.date = stablecoin_data.date
 left join github_data on fundamental_data.date = github_data.date
 where fundamental_data.date < to_date(sysdate())
