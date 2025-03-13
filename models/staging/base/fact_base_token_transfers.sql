e63e2052c8e565af3473b88fdf00e7fe5a22d8b3
Sebastian Melendez <sebm@seas.upenn.edu>
2025-03-13 18:12:09 -0400

    updating incremental

--- models/staging/base/fact_base_token_transfers.sql
+++ models/staging/base/fact_base_token_transfers.sql
@@ -1,3 +1,2 @@
-
-{{ config(snowflake_warehouse="BASE", materialized="incremental") }}
+{{ config(snowflake_warehouse="BASE", materialized="incremental", unique_key=["transaction_hash", "event_index"]) }}
 {{ token_transfer_events('base') }}
