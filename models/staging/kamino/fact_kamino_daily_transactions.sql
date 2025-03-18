e5f98dc56b2b35ccd85955f799f8577397e5ef60
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-03-18 12:12:06 -0400

    Added transactions and daus to kamino

---
+++ models/staging/kamino/fact_kamino_daily_transactions.sql
@@ -0,0 +1,10 @@
+{{ config(materialized="table", snowflake_warehouse="KAMINO") }}
+
+select
+    date_trunc('day', block_timestamp) as date, 
+    count(distinct tx_id) as tx_count, 
+from solana_flipside.core.ez_events_decoded
+where program_id = 'KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD' and date > '2025-03-10'
+group by date
+order by date desc
+
