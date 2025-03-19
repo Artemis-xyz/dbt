4efc6d9309b2c277ab0cd324d812d905f1421da8
limzhengjie <limzhengjie01@gmail.com>
2025-03-19 16:04:05 -0400

    sonic supply side emissions data

--- models/projects/sonic/core/ez_sonic_metrics.sql
+++ models/projects/sonic/core/ez_sonic_metrics.sql
@@ -21,7 +21,15 @@ with
             dau
         FROM {{ ref("fact_sonic_fundamental_metrics") }}
     )
-    , price_data as ({{ get_coingecko_metrics("sonic") }})
+    , supply_data as (
+        select
+            date,
+            emissions_native,
+            premine_unlocks_native,
+            circulating_supply_native
+        from {{ ref("fact_sonic_supply_data") }}
+    )
+    , price_data as ({{ get_coingecko_metrics("sonic-3") }})
 select
     fundamentals.date
     , fundamentals.fees
@@ -29,7 +37,11 @@ select
     , fundamentals.dau
     , sonic_dex_volumes.dex_volumes
     , price_data.price
+    , supply_data.emissions_native
+    , supply_data.premine_unlocks_native
+    , supply_data.circulating_supply_native
 from fundamentals
 left join sonic_dex_volumes on fundamentals.date = sonic_dex_volumes.date
 left join price_data on fundamentals.date = price_data.date
+left join supply_data on fundamentals.date = supply_data.date
 where fundamentals.date < to_date(sysdate())
