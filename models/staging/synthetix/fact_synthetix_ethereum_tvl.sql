3c24346c718c6c8455d994493949316f15334f25
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-03-18 20:27:20 -0400

    Edited the fact sheets to show each token individually, edited some spacing, and added a fact sheet for tvl_by_chain_and_token

--- models/staging/synthetix/fact_synthetix_ethereum_tvl.sql
+++ models/staging/synthetix/fact_synthetix_ethereum_tvl.sql
@@ -1,34 +1,34 @@
 {{ config(materialized="table", snowflake_warehouse="SYNTHETIX") }}
 
 with recursive
-  partitioned_transactions as (
+partitioned_transactions as (
     select 
-      date_trunc('hour', block_timestamp) as hourly_timestamp,
-      contract_address,
-      balance_token,
-      row_number() over (
+        date_trunc('hour', block_timestamp) as hourly_timestamp,
+        contract_address,
+        balance_token,
+        row_number() over (
         partition by date_trunc('day', block_timestamp), contract_address
         order by block_timestamp desc
-      ) as rn
+        ) as rn
     from ethereum.prod_raw.ez_address_balances_by_token
     where address ILIKE '0xffffffaeff0b96ea8e4f94b2253f31abdd875847'
-  ),
-  daily_tvl as (
+),
+daily_tvl as (
     select 
-      date_trunc('day', hourly_timestamp) as date,
-      sum(coalesce(balance_token/pow(10,coalesce(decimals,0)), 0) * eph.price) as tvl_usd
+        date_trunc('day', hourly_timestamp) as date,
+        eph.symbol as token, 
+        coalesce(balance_token/pow(10,coalesce(decimals,0)), 0) * eph.price as balance_usd
     from partitioned_transactions pt
     inner join ethereum_flipside.price.ez_prices_hourly eph
-      on pt.contract_address = eph.token_address
-     and pt.hourly_timestamp = eph.hour
+        on pt.contract_address = eph.token_address
+        and pt.hourly_timestamp = eph.hour
     where rn = 1
-    group by 1
-  ),
-  date_bounds as (
+),
+date_bounds as (
     select min(date) as min_date, max(date) as max_date
     from daily_tvl
-  ),
-  date_series as (
+),
+date_series as (
     -- base case: start at the minimum date
     select min_date as date
     from date_bounds
@@ -37,17 +37,20 @@ with recursive
     select date + interval '1 day'
     from date_series, date_bounds
     where date < max_date
-  ),
-  daily_with_series as (
-    select ds.date, dt.tvl_usd
+),
+daily_with_series as (
+    select ds.date, dt.token, dt.balance_usd
     from date_series ds
     left join daily_tvl dt on ds.date = dt.date
-  )
+)
 select 
-  date,
-  /* this window function carries forward the last non-null tvl_usd */
-  last_value(tvl_usd ignore nulls) over (
-    order by date rows between unbounded preceding and current row
-  ) as tvl_usd
+    date,
+    last_value(token ignore nulls) over (
+        order by date rows between unbounded preceding and current row
+    ) as token, 
+    /* this window function carries forward the last non-null tvl_usd */
+    last_value(balance_usd ignore nulls) over (
+        order by date rows between unbounded preceding and current row
+    ) as tvl_usd
 from daily_with_series
 order by date desc
---
+++ models/staging/synthetix/fact_synthetix_tvl_by_chain_and_token.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='table',
+        snowflake_warehouse='SYNTHETIX',
+        database='SYNTHETIX',
+        schema='raw',
+        alias='fact_synthetix_tvl_by_chain_and_token'
+    )
+}}
+
+with all_tvl_by_chain_and_token as (
+    SELECT * FROM {{ ref('fact_synthetix_ethereum_tvl') }}
+    UNION ALL
+    SELECT * FROM {{ ref('fact_synthetix_optimism_tvl') }}
+    UNION ALL
+    SELECT * FROM {{ ref('fact_synthetix_base_tvl') }}
+)
+
+select
+    date,
+    token,
+    tvl_usd
+from all_tvl_by_chain_and_token
