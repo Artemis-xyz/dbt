30b662c2579b3a6405419755198ac8679fc42c36
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-03-19 11:08:32 -0400

    Removed another date constraint in daily transactions

--- models/staging/kamino/fact_kamino_daily_transactions.sql
+++ models/staging/kamino/fact_kamino_daily_transactions.sql
@@ -4,7 +4,7 @@ select
     date_trunc('day', block_timestamp) as date, 
     count(distinct tx_id) as tx_count, 
 from solana_flipside.core.ez_events_decoded
-where program_id = 'KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD' and date > '2025-03-10'
+where program_id = 'KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD'
 group by date
 order by date desc
 
