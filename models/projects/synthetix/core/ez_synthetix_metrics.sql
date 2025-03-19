d04895193a61f6844293b5d5101e9312c49360f6
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-03-18 23:10:16 -0400

    Added native and net treasury numbers through the available macro

--- models/projects/synthetix/core/ez_synthetix_metrics.sql
+++ models/projects/synthetix/core/ez_synthetix_metrics.sql
@@ -49,6 +49,22 @@ with
             daily_expenses as expenses
         from {{ ref('fact_synthetix_expenses') }}
     ),
+    treasury_native as (
+        SELECT
+            date,
+            sum(native_balance) as treasury_native
+        FROM {{ ref('fact_synthetix_treasury_by_token') }}
+        where token = 'SNX'
+        group by 1
+    ),
+    net_treasury as (
+        SELECT
+            date,
+            sum(usd_balance) as net_treasury_usd
+        FROM {{ ref('fact_synthetix_treasury_by_token') }}
+        where token <> 'SNX'
+        group by 1
+    ),
     market_data as (
         {{ get_coingecko_metrics('synthetix') }}
     )
@@ -56,13 +72,15 @@ select
     date,
     'synthetix' as app,
     'DeFi' as category,
-    trading_volume,
-    unique_traders,
-    tvl_usd,
-    net_deposits,
-    fees,
-    fees as revenue, 
-    expenses,
+    coalesce(trading_volume, 0) as trading_volume,
+    coalesce(unique_traders, 0) as unique_traders,
+    coalesce(tvl_usd, 0) as tvl_usd,
+    coalesce(net_deposits, 0) as net_deposits,
+    coalesce(fees, 0) as fees,
+    coalesce(fees, 0) as revenue, 
+    coalesce(expenses, 0) as expenses,
+    coalesce(treasury_native, 0) as treasury_native,
+    coalesce(net_treasury_usd, 0) as net_treasury_usd,
     coalesce(market_data.price, 0) as price,
     coalesce(market_data.market_cap, 0) as market_cap,
     coalesce(market_data.fdmc, 0) as fdmc,
@@ -76,6 +94,8 @@ left join tvl using(date)
 left join net_deposits using(date)
 left join fees using(date)
 left join expenses using(date)
+left join treasury_native using(date)
+left join net_treasury using(date)
 left join market_data using(date)
 left join token_holders using(date)
 where date < to_date(sysdate())
