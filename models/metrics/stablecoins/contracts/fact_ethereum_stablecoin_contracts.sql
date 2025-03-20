e30464b73d0fa1a782577075855243f7b5af212d
Irwyn Liong <106728017+irwynliong@users.noreply.github.com>
2025-03-20 16:01:25 -0400

    adding EURA and EURS (#983)

--- models/metrics/stablecoins/contracts/fact_ethereum_stablecoin_contracts.sql
+++ models/metrics/stablecoins/contracts/fact_ethereum_stablecoin_contracts.sql
@@ -54,5 +54,7 @@ from
             ('EURT', '0xc581b735a1688071a1746c968e0798d642ede491', 6, 'tether-eurt', 0),
             ('AEUR', '0xA40640458FBc27b6EefEdeA1E9C9E17d4ceE7a21', 18, 'anchored-coins-eur', 0),
             ('LUSD', '0x5f98805A4E8be255a32880FDeC7F6728C6568bA0', 18, 'liquity-usd', 0),
-            ('BOLD', '0xb01dd87B29d187F3E3a4Bf6cdAebfb97F3D9aB98', 18, 'liquity-bold', 0)
+            ('BOLD', '0xb01dd87B29d187F3E3a4Bf6cdAebfb97F3D9aB98', 18, 'liquity-bold', 0),
+            ('EURS', '0xdB25f211AB05b1c97D595516F45794528a807ad8', 2, 'stasis-eurs', 0),
+            ('EURA', '0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8', 18, 'ageur', 0)
     ) as results(symbol, contract_address, num_decimals, coingecko_id, initial_supply)
