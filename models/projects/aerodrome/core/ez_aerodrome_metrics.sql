44741d8060e7b1bbc0c92a10e3399a7d6a13ebd1
Alex Weseley <pampawes@gmail.com>
2025-03-27 13:04:53 -0400

    abstract and aerodrome standardized metrics

--- models/projects/aerodrome/core/ez_aerodrome_metrics.sql
+++ models/projects/aerodrome/core/ez_aerodrome_metrics.sql
@@ -25,6 +25,9 @@ with swap_metrics as (
     FROM {{ ref('fact_aerodrome_tvl') }}
     GROUP BY date
 )
+, market_metrics as (
+    {{get_coingecko_metrics('aerodrome-finance')}}
+)
 , date_spine as (
     SELECT
         ds.date
@@ -35,6 +38,8 @@ with swap_metrics as (
                         select min(date) as min_date from swap_metrics
                         UNION ALL
                         select min(date) as min_date from tvl_metrics
+                        UNION ALL
+                        select min(date) as min_date from market_metrics
                     )
                 )
         and to_date(sysdate())
@@ -46,6 +51,22 @@ SELECT
     sm.daily_volume_usd as trading_volume,
     sm.daily_fees_usd as trading_fees,
     tm.tvl_usd as tvl
+
+    -- Standardized Metrics
+    sm.unique_traders as spot_dau,
+    sm.total_swaps as spot_txns,
+    sm.daily_volume_usd as spot_volume,
+    sm.daily_fees_usd as ecosystem_revenue,
+    sm.daily_fees_usd as participating_token_revenue
+
+    -- Market Metrics
+    , mm.price as price
+    , mm.token_volume as token_volume
+    , mm.market_cap as market_cap
+    , mm.fdmc as fdmc
+    , mm.token_turnover_circulating as token_turnover_circulating
+    , mm.token_turnover_fdv as token_turnover_fdv
 FROM date_spine ds
 LEFT JOIN swap_metrics sm using (date)
 LEFT JOIN tvl_metrics tm using (date)
+LEFT JOIN market_metrics mm using (date)
--- models/projects/aerodrome/core/ez_aerodrome_metrics_by_chain.sql
+++ models/projects/aerodrome/core/ez_aerodrome_metrics_by_chain.sql
@@ -47,6 +47,13 @@ SELECT
     sm.daily_volume_usd as trading_volume,
     sm.daily_fees_usd as trading_fees,
     tm.tvl_usd as tvl
+
+    -- Standardized Metrics
+    sm.unique_traders as spot_dau,
+    sm.total_swaps as spot_txns,
+    sm.daily_volume_usd as spot_volume,
+    sm.daily_fees_usd as ecosystem_revenue,
+    sm.daily_fees_usd as participating_token_revenue
 FROM date_spine ds
 LEFT JOIN swap_metrics sm using (date)
 LEFT JOIN tvl_metrics tm using (date)
