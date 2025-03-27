c7a5b982fec2f72a77c68c93c4c5dccebc34d72a
Son Do <sondo077@gmail.com>
2025-03-27 09:59:59 -0400

    Fix Debridge Volume

--- models/staging/debridge/fact_debridge_fundamental_metrics.sql
+++ models/staging/debridge/fact_debridge_fundamental_metrics.sql
@@ -1,7 +1,7 @@
 {{ config(snowflake_warehouse="DEBRIDGE", materialized="table") }}
 select 
     date_trunc('day', src_timestamp) as date
-    , sum(amount_sent) as bridge_volume
+    , sum(case when date < '2025-01-01' then coalesce(amount_sent, 0) else amount_sent end) as bridge_volume
     , sum(coalesce(percentage_fee,0) + coalesce(fix_fee,0)) as ecosystem_revenue
     , count(*) as bridge_txns
 from {{ ref("fact_debridge_transfers_with_prices") }}
