95901cf1483db9bc87867d4a4e40188200c6006a
alexwes <pampawes@gmail.com>
2025-03-31 10:14:57 -0400

    Wes/grass decimals (#1008)

--- models/staging/grass/fact_grass_data_scraped.sql
+++ models/staging/grass/fact_grass_data_scraped.sql
@@ -7,17 +7,17 @@
 
 WITH max_extraction AS (
     SELECT MAX(extraction_date) AS max_date
-    FROM LANDING_DATABASE.PROD_LANDING.raw_grass_data_scraped
+    FROM {{ source('PROD_LANDING', 'raw_grass_data_scraped') }}
 ),
 latest_data AS (
     SELECT PARSE_JSON(source_json):result:data AS data_array
-    FROM LANDING_DATABASE.PROD_LANDING.raw_grass_data_scraped
+    FROM {{ source('PROD_LANDING', 'raw_grass_data_scraped') }}
     WHERE extraction_date = (SELECT max_date FROM max_extraction)
 )
 , flattened_data AS (
     SELECT
         f.value:date::date AS date,
-        f.value:dailyDataCollected::number AS data_collected_tb
+        f.value:dailyDataCollected::number/1e3 AS data_collected_tb
     FROM latest_data,
     LATERAL FLATTEN(input => latest_data.data_array) AS f
     )
