8b983f4d985c9fd6bdbadf2dd52eb866330aa052
limzhengjie <limzhengjie01@gmail.com>
2025-04-01 16:07:56 -0400

    morpho demand side metrics standardized

--- models/projects/morpho/ez_morpho_metrics.sql
+++ models/projects/morpho/ez_morpho_metrics.sql
@@ -8,39 +8,53 @@
     )
  }}
 
- with deposits as (
-     select
-         date
-         , sum(borrow_amount_usd) as borrow_amount_usd
-         , sum(supply_amount_usd) as supply_amount_usd
-         , sum(supply_amount_usd) + sum(collat_amount_usd) as deposit_amount_usd
-     from {{ ref("fact_morpho_deposits_loans") }}
-     group by 1
- ),
- fees as (
-     select
-         date
-         , sum(interest_usd) as fees
-     from {{ ref("fact_morpho_fees") }}
-     group by 1
- ),
- cumulative_metrics as (
-     select
-         d.date
-         , sum(d.borrow_amount_usd) over (order by d.date rows between unbounded preceding and current row) as borrows
-         , sum(d.supply_amount_usd) over (order by d.date rows between unbounded preceding and current row) as supplies
-         , sum(d.deposit_amount_usd) over (order by d.date rows between unbounded preceding and current row) as deposits
-         , fees
-         , sum(f.fees) over (order by d.date rows between unbounded preceding and current row) as fees_cumulative
-         , deposits - borrows as tvl
-     from deposits d
-     left join fees f on d.date = f.date
+with morpho_data as (
+    select
+        date
+        , sum(borrow_amount_usd) as borrow_amount_usd
+        , sum(supply_amount_usd) as supply_amount_usd
+        , sum(supply_amount_usd) + sum(collat_amount_usd) as deposit_amount_usd
+        , sum(fees_usd) as fees
+    from {{ ref("fact_morpho_data") }}
+    group by 1
+)
+
+, cumulative_metrics as (
+    select
+        d.date
+        , sum(d.borrow_amount_usd) over (order by d.date rows between unbounded preceding and current row) as borrows
+        , sum(d.supply_amount_usd) over (order by d.date rows between unbounded preceding and current row) as supplies
+        , sum(d.deposit_amount_usd) over (order by d.date rows between unbounded preceding and current row) as deposits
+        , fees
+        , sum(fees) over (order by d.date rows between unbounded preceding and current row) as fees_cumulative
+        , deposits - borrows as tvl
+    from morpho_data d
  )
- select
-     date
-     , borrows as daily_borrows_usd
-     , supplies as total_available_supply
-     , deposits as daily_supply_usd
-     , tvl
-     , fees
- from cumulative_metrics 
+
+, morpho_market_data as (
+    {{ get_coingecko_metrics('morpho') }}
+)
+
+select
+    date
+    , borrows as daily_borrows_usd
+    , supplies as total_available_supply
+    , deposits as daily_supply_usd
+    , fees
+
+    -- Standardized metrics
+    , tvl
+    , fees as gross_protocol_revenue
+    , borrows as lending_borrows
+    , supplies as lending_total_available_borrows
+    , deposits as lending_deposits
+
+    -- Market Metrics
+    , mdd.price
+    , mdd.market_cap
+    , mdd.fdmc
+    , mdd.token_turnover_circulating
+    , mdd.token_turnover_fdv
+    , mdd.token_volume
+from cumulative_metrics
+left join morpho_market_data mdd using (date)
--- models/projects/morpho/ez_morpho_metrics_by_chain.sql
+++ models/projects/morpho/ez_morpho_metrics_by_chain.sql
@@ -14,35 +14,34 @@ with deposits as (
         , borrow_amount_usd
         , supply_amount_usd
         , supply_amount_usd + collat_amount_usd as deposit_amount_usd
+        , fees_usd
         , chain
-    from {{ ref("fact_morpho_deposits_loans") }}
-),
-fees as (
-    select
-        date
-        , interest_usd as fees
-        , chain
-    from {{ ref("fact_morpho_fees") }}
+    from {{ ref("fact_morpho_data") }}
 ),
 cumulative_metrics as (
     select
-        d.date,
-        sum(d.borrow_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as borrows,
-        sum(d.supply_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as supplies,
-        sum(d.deposit_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as deposits,
-        f.fees,
-        sum(f.fees) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as fees_cumulative,
-        sum(d.deposit_amount_usd - d.borrow_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as tvl,
-        d.chain
+        d.date
+        , sum(d.borrow_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as borrows
+        , sum(d.supply_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as supplies
+        , sum(d.deposit_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as deposits
+        , d.fees_usd as fees
+        , sum(d.fees_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as fees_cumulative
+        , sum(d.deposit_amount_usd - d.borrow_amount_usd) over (partition by d.chain order by d.date rows between unbounded preceding and current row) as tvl
+        , d.chain
     from deposits d
-    left join fees f on d.date = f.date and d.chain = f.chain
 )
 select
     date
     , borrows as daily_borrows_usd
     , supplies as total_available_supply
     , deposits as daily_supply_usd
-    , tvl
     , fees
     , chain
+
+    -- Standardized metrics
+    , tvl
+    , fees as gross_protocol_revenue
+    , borrows as lending_borrows
+    , supplies as lending_total_available_borrows
+    , deposits as lending_deposits
 from cumulative_metrics 
