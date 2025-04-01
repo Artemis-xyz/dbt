c591e23bd19258256023c04327c0d43f9ce980c8
limzhengjie <limzhengjie01@gmail.com>
2025-04-01 11:18:21 -0400

    updated bitflow models

--- models/projects/bitflow/core/ez_bitflow_metrics.sql
+++ models/projects/bitflow/core/ez_bitflow_metrics.sql
@@ -12,10 +12,6 @@ with bitflow_tvl as (
     {{ get_defillama_protocol_tvl('bitflow') }}
 )
 
-, bitflow_market_data as (
-    {{get_coingecko_metrics('bitflow')}}
-)
-
 select
     bitflow_tvl.date
     , 'Defillama' as source
@@ -23,13 +19,5 @@ select
     -- Standardized Metrics
     , bitflow_tvl.tvl
 
-    -- Market Metrics
-    , bmm.price
-    , bmm.market_cap
-    , bmm.fdmc
-    , bmm.token_turnover_circulating
-    , bmm.token_turnover_fdv
-    , bmm.token_volume
 from bitflow_tvl
-left join bitflow_market_data bmm using (date)
 where bitflow_tvl.date < to_date(sysdate())
