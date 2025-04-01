b1c01a617ac3d14b99af13d31be5ebc9604b8297
Alex Weseley <pampawes@gmail.com>
2025-04-01 09:49:18 -0400

    remove prefix for tvl

--- models/projects/etherfi/core/ez_etherfi_metrics.sql
+++ models/projects/etherfi/core/ez_etherfi_metrics.sql
@@ -12,10 +12,10 @@ SELECT
     date,
     'etherfi' as app,
     'DeFi' as category,
-    sum(num_restaked_eth) as lrt_tvl_native,
-    sum(amount_restaked_usd) as lrt_tvl,
-    sum(num_restaked_eth_net_change) as lrt_tvl_native_net_change,
-    sum(amount_restaked_usd_net_change) as lrt_tvl_net_change
+    sum(num_restaked_eth) as tvl_native,
+    sum(amount_restaked_usd) as tvl,
+    sum(num_restaked_eth_net_change) as tvl_native_net_change,
+    sum(amount_restaked_usd_net_change) as tvl_net_change
 from {{ ref('ez_etherfi_metrics_by_chain') }}
 where date < to_date(sysdate())
 group by 1, 2, 3
