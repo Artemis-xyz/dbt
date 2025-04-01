ca00b6b5930e1d6a12ee6683e10340af2b748b86
Alex Weseley <pampawes@gmail.com>
2025-04-01 00:49:26 -0400

    use prefix

--- models/projects/etherfi/core/ez_etherfi_metrics_by_chain.sql
+++ models/projects/etherfi/core/ez_etherfi_metrics_by_chain.sql
@@ -29,9 +29,9 @@ select
     restaked_eth_metrics.num_restaked_eth_net_change,
     restaked_eth_metrics.amount_restaked_usd_net_change
     --Standardized Metrics
-    , num_restaked_eth as tvl_native
-    , amount_restaked_usd as tvl
-    , num_restaked_eth_net_change as tvl_native_net_change
-    , amount_restaked_usd_net_change as tvl_net_change
+    , num_restaked_eth as lrt_tvl_native
+    , amount_restaked_usd as lrt_tvl
+    , num_restaked_eth_net_change as lrt_tvl_native_net_change
+    , amount_restaked_usd_net_change as lrt_tvl_net_change
 from restaked_eth_metrics
 where restaked_eth_metrics.date < to_date(sysdate())
