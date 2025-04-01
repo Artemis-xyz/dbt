fa30ced0917b59fe2dae4d07dba14fc94fec7820
limzhengjie <limzhengjie01@gmail.com>
2025-03-31 22:23:25 -0400

    renamed ez_metrics_by_chain column

--- models/projects/morpho/ez_morpho_metrics_by_chain.sql
+++ models/projects/morpho/ez_morpho_metrics_by_chain.sql
@@ -39,9 +39,9 @@ cumulative_metrics as (
 )
 select
     date
-    , borrows
-    , supplies
-    , deposits
+    , borrows as daily_borrows_usd
+    , supplies as total_available_supply
+    , deposits as daily_supply_usd
     , tvl
     , fees
     , chain
