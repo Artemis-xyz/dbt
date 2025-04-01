29e462fa0275db807443548e2ecfabef975b9a1c
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-01 13:36:36 -0400

    Added market data metrics using coingecko

--- models/projects/bananagun/core/ez_bananagun_metrics.sql
+++ models/projects/bananagun/core/ez_bananagun_metrics.sql
@@ -18,12 +18,14 @@ WITH metrics AS (
     FROM {{ ref('fact_bananagun_all_metrics') }}
     GROUP BY date
 )
-
 , burns AS (
     SELECT
         date
         , burns_usd AS revenue
     FROM {{ ref('fact_bananagun_coin_metrics') }}
+)
+, market_data as (
+    {{ get_coingecko_metrics('bananagun') }}
 )
 
 SELECT
@@ -34,6 +36,13 @@ SELECT
     , metrics.fees_usd AS fees
     , metrics.fees_usd * 0.6 AS supply_side_fees
     , metrics.fees_usd * 0.4 + burns.revenue AS revenue
+    , coalesce(market_data.price, 0) as price
+    , coalesce(market_data.market_cap, 0) as market_cap
+    , coalesce(market_data.fdmc, 0) as fdmc
+    , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
+    , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
+    , coalesce(market_data.token_volume, 0) as token_volume
 FROM metrics
-LEFT JOIN burns ON metrics.date = burns.date
+LEFT JOIN burns using (date)
+LEFT JOIN market_data using (date)
 ORDER BY metrics.date DESC
