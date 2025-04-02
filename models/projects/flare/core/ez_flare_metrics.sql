2f0dd402bef72f969a8334e24ae0feafbbe91d8b
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-02 14:33:36 -0400

    flare standardization

--- models/projects/flare/core/ez_flare_metrics.sql
+++ models/projects/flare/core/ez_flare_metrics.sql
@@ -32,13 +32,19 @@ txns as (
         daily_volume as dex_volumes
     from {{ref("fact_flare_daily_dex_volumes")}}
 )
-
 select
-    coalesce(fees.date, txns.date, daus.date) as date,
-    dau,
-    txns,
-    fees_usd as fees,
-    dex_volumes
+    coalesce(fees.date, txns.date, daus.date) as date
+    , dau
+    , txns
+    , fees_usd as fees
+    -- Standardized Metrics
+    -- Usage Metrics
+    , txns AS chain_txns
+    , dau AS chain_dau
+    -- Cashflow metrics
+    , fees_usd AS gross_protocol_revenue
+    -- Chain Metrics
+    , dex_volumes
 from fees
 left join txns on fees.date = txns.date
 left join daus on fees.date = daus.date 
