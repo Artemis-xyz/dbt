ec66a5df12b53dc5bb631272545b7c5a824b3aed
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-02 17:13:47 -0400

    ink standardization

--- models/projects/ink/core/ez_ink_metrics.sql
+++ models/projects/ink/core/ez_ink_metrics.sql
@@ -20,6 +20,15 @@ select
     , fees_native
     , fees
     , ink_dex_volumes.dex_volumes
+    -- Standardized Metrics
+    -- Usage Metrics
+    , txns AS chain_txns
+    , daa AS chain_dau
+    -- Cashflow Metrics
+    , fees AS gross_protocol_revenue
+    , fees_native AS gross_protocol_revenue_native
+    -- Chain Metrics
+    , ink_dex_volumes.dex_volumes AS chain_dex_volumes
 from {{ ref("fact_ink_fundamental_metrics") }}
 left join ink_dex_volumes using (date)
 where date < to_date(sysdate())
