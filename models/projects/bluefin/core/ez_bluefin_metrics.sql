050a68cbf7e34c70309fb78b0ae16efd2849a59a
Son Do <sondo077@gmail.com>
2025-04-02 11:01:28 -0400

    Added Avantis, bluefin, and holdstation

--- models/projects/bluefin/core/ez_bluefin_metrics.sql
+++ models/projects/bluefin/core/ez_bluefin_metrics.sql
@@ -1,8 +1,8 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="AEVO",
-        database="aevo",
+        snowflake_warehouse="BLUEFIN",
+        database="bluefin",
         schema="core",
         alias="ez_metrics",
     )
@@ -11,15 +11,14 @@
 with
     trading_volume_data as (
         select date, sum(trading_volume) as trading_volume
-        from {{ ref("fact_aevo_trading_volume") }}
+        from {{ ref("fact_bluefin_trading_volume_silver") }}
         group by date
     )
-    , price as ({{ get_coingecko_metrics("aevo-exchange") }})
+    , price as ({{ get_coingecko_metrics("bluefin") }})
 select
     date
-    , 'aevo' as app
+    , 'bluefin' as app
     , 'DeFi' as category
-    , trading_volume
     -- standardize metrics
     , trading_volume as perp_volume
     -- Market Data
--- models/projects/holdstation/core/ez_holdstation_metrics.sql
+++ models/projects/holdstation/core/ez_holdstation_metrics.sql
@@ -1,8 +1,8 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="AEVO",
-        database="aevo",
+        snowflake_warehouse="HOLDSTATION",
+        database="holdstation",
         schema="core",
         alias="ez_metrics",
     )
@@ -11,17 +11,22 @@
 with
     trading_volume_data as (
         select date, sum(trading_volume) as trading_volume
-        from {{ ref("fact_aevo_trading_volume") }}
+        from {{ ref("fact_holdstation_trading_volume") }}
         group by date
     )
-    , price as ({{ get_coingecko_metrics("aevo-exchange") }})
+    , unique_traders_data as (
+        select date, sum(unique_traders) as unique_traders
+        from {{ ref("fact_holdstation_unique_traders") }}
+        group by date
+    )
+    , price as ({{ get_coingecko_metrics("holdstation") }})
 select
     date
-    , 'aevo' as app
+    , 'holdstation' as app
     , 'DeFi' as category
-    , trading_volume
     -- standardize metrics
     , trading_volume as perp_volume
+    , unique_traders as perp_dau
     -- Market Data
     , price
     , market_cap
@@ -30,5 +35,6 @@ select
     , token_turnover_fdv
     , token_volume
 from trading_volume_data
+left join unique_traders_data using(date)
 left join price using(date)
 where date < to_date(sysdate())
