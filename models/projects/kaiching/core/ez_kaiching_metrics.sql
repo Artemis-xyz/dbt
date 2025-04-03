1f02ed9df93d469f9e4f9bd8afa22484a1ca0244
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-03 17:55:50 -0400

    Changed trailing commas to leading

--- models/projects/kaiching/core/ez_kaiching_metrics.sql
+++ models/projects/kaiching/core/ez_kaiching_metrics.sql
@@ -13,16 +13,16 @@ with
     fundamental_data as ({{get_bam_data_for_application("kaiching", ["near"])}}),
     rolling_wau_mau as ({{get_rolling_active_address_metrics_by_app("kaiching", "near")}})
 SELECT 
-    fd.date,
-    fd.app,
-    fd.friendly_name,
-    fd.gas_usd,
-    fd.txns,
-    fd.daa,
-    fd.new_users,
-    fd.returning_users,
-    rwa.mau,
-    rwa.wau
+    fd.date
+    , fd.app
+    , fd.friendly_name
+    , fd.gas_usd
+    , fd.txns
+    , fd.daa
+    , fd.new_users
+    , fd.returning_users
+    , rwa.mau
+    , rwa.wau
 FROM 
     fundamental_data as fd
 left join rolling_wau_mau as rwa on fd.date = rwa.date
