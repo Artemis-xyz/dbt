412f347c0babbc64e49ef2e38b95fa88e3ef993a
limzhengjie <limzhengjie01@gmail.com>
2025-04-03 19:25:32 -0400

    usual standardized metrics

--- models/projects/usual/core/ez_usual_metrics.sql
+++ models/projects/usual/core/ez_usual_metrics.sql
@@ -15,16 +15,16 @@ with usd0_metrics as (
         stablecoin_dau,
         stablecoin_total_supply as usd0_tvl
     from {{ ref('ez_usd0_metrics') }}
-), 
+)
 
-usd0pp_metrics as (
+, usd0pp_metrics as (
     select
         date,
         usd0pp_tvl as usd0pp_tvl
     from {{ ref('fact_usd0pp_tvl') }}
-), 
+)
 
-usual_fees as (
+, usual_fees as (
     select 
         date,
         daily_treasury_revenue,
@@ -32,9 +32,9 @@ usual_fees as (
         fees, 
         cumulative_fees 
     from {{ ref('fact_usual_fees') }}
-), 
+) 
 
-usual_burn_mint as (
+, usual_burn_mint as (
     select 
         date, 
         daily_supply, 
@@ -42,23 +42,51 @@ usual_burn_mint as (
         daily_treasury, 
         cumulative_treasury, 
         daily_burned, 
-        cumulative_burned 
+        cumulative_burned,
+        current_circulating_supply_native,
+        cumulative_supply
     from {{ ref('fact_usual_burn_mint') }}
 )
+
+, market_metrics as (
+    select * from ({{ get_coingecko_price_with_latest("usual") }}) 
+)
+
 select 
-    usd0.date,
-    usd0.stablecoin_txns as stablecoin_txns,
-    usd0.stablecoin_dau as stablecoin_dau,
-    usd0.usd0_tvl as usd0_tvl,
-    usd0pp.usd0pp_tvl as usd0pp_tvl,
-    usual.fees as fees,
-    usual.daily_treasury_revenue as treasury_revenue,
-    ubm.daily_supply as daily_supply,
-    ubm.daily_treasury as daily_treasury,
-    ubm.daily_burned as daily_burned,
+    usd0.date
+    , usd0.stablecoin_txns
+    , usd0.stablecoin_dau
+    , usd0.usd0_tvl
+    , usd0pp.usd0pp_tvl
+    , usual.fees
+    , usual.daily_treasury_revenue as treasury_revenue
+    , ubm.daily_supply
+    , ubm.daily_treasury
+    , ubm.daily_burned
     -- revenue is the sum of treasury revenue, daily burned, and fees
-    usual.fees + usual.daily_treasury_revenue + ubm.daily_burned as revenue
+    , usual.fees + (usual.daily_treasury_revenue * mm.price) + (ubm.daily_burned * mm.price) as revenue
+
+    -- Standardized Metrics
+    , usd0.stablecoin_txns
+    , usd0.stablecoin_dau
+
+    -- Revenue Metrics
+    , usual.fees + (usual.daily_treasury_revenue * mm.price) + (ubm.daily_burned * mm.price) as gross_protocol_revenue
+    , usual.daily_treasury * mm.price as treasury_cash_flow
+    , ubm.daily_burned as burned_cash_flow_native
+
+    , ubm.current_circulating_supply_native
+
+    -- Market Metrics
+    , mm.price
+    , mm.token_volume
+    , mm.market_cap
+    , mm.fdmc
+    , mm.token_turnover_circulating
+    , mm.token_turnover_fdv
+
 from usd0_metrics usd0 
 left join usd0pp_metrics usd0pp on usd0.date = usd0pp.date
 left join usual_fees usual on usd0.date = usual.date
 left join usual_burn_mint ubm on usd0.date = ubm.date
+left join market_metrics mm on usd0.date = mm.date
