cf2372576792b4b30279b76a1889cc9216f84b08
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-03 16:11:35 -0400

    Ton standardization

--- models/projects/ton/core/ez_ton_metrics.sql
+++ models/projects/ton/core/ez_ton_metrics.sql
@@ -30,41 +30,57 @@ with
     github_data as ({{ get_github_metrics("ton") }}),
     rolling_metrics as ({{ get_rolling_active_address_metrics("ton") }})
 select
-    ton.date,
-    'ton' as chain,
-    transaction_nodes,
-    stablecoin_total_supply,
-    stablecoin_txns,
-    stablecoin_dau,
-    stablecoin_mau,
-    stablecoin_transfer_volume,
-    artemis_stablecoin_txns,
-    artemis_stablecoin_dau,
-    artemis_stablecoin_mau,
-    artemis_stablecoin_transfer_volume,
-    p2p_stablecoin_txns,
-    p2p_stablecoin_dau,
-    p2p_stablecoin_mau,
-    p2p_stablecoin_transfer_volume,
-    stablecoin_tokenholder_count,
-    p2p_stablecoin_tokenholder_count,
-    price,
-    market_cap,
-    fdmc,
-    tvl,
-    weekly_commits_core_ecosystem,
-    weekly_commits_sub_ecosystem,
-    weekly_developers_core_ecosystem,
-    weekly_developers_sub_ecosystem,
-    dau,
-    wau,
-    mau,
-    txns,
-    fees_native,
-    fees_native * price as fees,
-    fees_native / 2 as revenue_native,
-    (fees_native / 2) * price as revenue,
-    avg_txn_fee_native * price as avg_txn_fee
+    ton.date
+    , 'ton' as chain
+    , dau
+    , wau
+    , mau
+    , txns
+    , fees_native
+    , fees_native * price AS fees
+    , fees_native /2 AS revenue_native
+    , (fees_native / 2) * price AS revenue
+    , avg_txn_fee_native * price AS avg_txn_fee
+    -- Bespoke Metrics
+    , transaction_nodes
+    -- Standardized Metrics
+    -- Market Data Metrics
+    , price
+    , market_cap
+    , fdmc
+    , tvl
+    -- Chain Usage Metrics
+    , dau AS chain_dau
+    , wau AS chain_wau
+    , mau AS chain_mau
+    , avg_txn_fee_native * price AS chain_avg_txn_fee
+    , txns AS chain_txns
+    -- Cash Flow Metrics
+    , fees_native AS protocol_revenue_native
+    , fees * price AS protocol_revenue
+    , fees_native / 2 AS burned_cash_flow_native
+    , (fees_native / 2) * price AS burned_cash_flow
+    -- Developer Metrics
+    , weekly_commits_core_ecosystem
+    , weekly_commits_sub_ecosystem
+    , weekly_developers_core_ecosystem
+    , weekly_developers_sub_ecosystem
+    -- Stablecoin Metrics
+    , stablecoin_total_supply
+    , stablecoin_txns
+    , stablecoin_dau
+    , stablecoin_mau
+    , stablecoin_transfer_volume
+    , stablecoin_tokenholder_count
+    , artemis_stablecoin_txns
+    , artemis_stablecoin_dau
+    , artemis_stablecoin_mau
+    , artemis_stablecoin_transfer_volume
+    , p2p_stablecoin_txns
+    , p2p_stablecoin_dau
+    , p2p_stablecoin_mau
+    , p2p_stablecoin_transfer_volume
+    , p2p_stablecoin_tokenholder_count
 from ton_apps_fundamental_data as ton
 left join price_data on ton.date = price_data.date
 left join defillama_data on ton.date = defillama_data.date
