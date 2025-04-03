2bb1b0e367e4fa3ed6c105469fdfd935203dcd90
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-03 17:19:21 -0400

    Aptos Standardization

--- models/projects/aptos/core/ez_aptos_metrics.sql
+++ models/projects/aptos/core/ez_aptos_metrics.sql
@@ -27,26 +27,40 @@ with
     github_data as ({{ get_github_metrics("aptos") }}),
     rolling_metrics as ({{ get_rolling_active_address_metrics("aptos") }})
 select
-    fundamental_data.date,
-    fundamental_data.chain,
-    txns,
-    dau,
-    wau,
-    mau,
-    fees_native,
-    fees,
-    avg_txn_fee,
-    revenue_native,
-    revenue,
-    price,
-    market_cap,
-    fdmc,
-    tvl,
-    dex_volumes,
-    weekly_commits_core_ecosystem,
-    weekly_commits_sub_ecosystem,
-    weekly_developers_core_ecosystem,
-    weekly_developers_sub_ecosystem
+    fundamental_data.date
+    , fundamental_data.chain
+    , txns
+    , dau
+    , wau
+    , mau
+    , fees_native
+    , fees
+    , avg_txn_fee
+    , revenue_native
+    , revenue
+    -- Standardized Metrics
+    -- Market Data Metrics
+    , price
+    , market_cap
+    , fdmc
+    , tvl
+    -- Chain Usage Metrics
+    , txns AS chain_txns
+    , dau AS chain_dau
+    , wau AS chain_wau
+    , mau AS chain_mau
+    , dex_volumes
+    , avg_txn_fee AS chain_avg_txn_fee
+    -- Cashflow Metrics
+    , fees AS gross_protocol_revenue
+    , fees_native AS gross_protocol_revenue_native
+    , revenue AS token_cash_flow
+    , revenue_native AS token_cash_flow_native
+    -- Developer Metrics
+    , weekly_commits_core_ecosystem
+    , weekly_commits_sub_ecosystem
+    , weekly_developers_core_ecosystem
+    , weekly_developers_sub_ecosystem
 from fundamental_data
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
