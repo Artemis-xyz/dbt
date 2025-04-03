53b75e82d1984f2aa7444881619bfdd151584ac2
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-03 11:14:16 -0400

    Added standardized metrics to centrifuge EZM table

--- models/projects/centrifuge/core/ez_centrifuge_metrics.sql
+++ models/projects/centrifuge/core/ez_centrifuge_metrics.sql
@@ -17,11 +17,34 @@ with
             fees_usd
         from {{ ref("fact_centrifuge_fundamental_metrics") }}
     )
+    , market_data as (
+        {{ get_coingecko_metrics("centrifuge") }}
+    )
 select
     date
     , txns
     , daa as dau
     , coalesce(fees_native, 0) as fees_native
     , coalesce(fees_usd, 0) as fees
+
+    -- Standardized Metrics
+
+    -- Chain Metrics
+    , daa as chain_dau
+    , txns as chain_txns
+    , coalesce(fees_usd, 0) as chain_revenue
+
+    -- Cash Flow Metrics
+    , coalesce(fees_usd, 0) as gross_protocol_revenue
+    , coalesce(fees_native, 0) as gross_protocol_revenue_native
+
+    -- Token Metrics
+    , coalesce(market_data.price, 0) as price
+    , coalesce(market_data.market_cap, 0) as market_cap
+    , coalesce(market_data.fdmc, 0) as fdmc
+    , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
+    , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
+    , coalesce(market_data.token_volume, 0) as token_volume 
 from fundamental_data
+left join market_data using (date)
 where fundamental_data.date < to_date(sysdate())
