066d67b2378954ce73bfa595e55942090532aed8
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-03 18:05:56 -0400

    Reordered columns

--- models/projects/centrifuge/core/ez_centrifuge_metrics.sql
+++ models/projects/centrifuge/core/ez_centrifuge_metrics.sql
@@ -29,6 +29,12 @@ select
 
     -- Standardized Metrics
 
+    -- Token Metrics
+    , coalesce(market_data.price, 0) as price
+    , coalesce(market_data.market_cap, 0) as market_cap
+    , coalesce(market_data.fdmc, 0) as fdmc
+    , coalesce(market_data.token_volume, 0) as token_volume 
+
     -- Chain Metrics
     , daa as chain_dau
     , txns as chain_txns
@@ -38,13 +44,9 @@ select
     , coalesce(fees_usd, 0) as gross_protocol_revenue
     , coalesce(fees_native, 0) as gross_protocol_revenue_native
 
-    -- Token Metrics
-    , coalesce(market_data.price, 0) as price
-    , coalesce(market_data.market_cap, 0) as market_cap
-    , coalesce(market_data.fdmc, 0) as fdmc
+    -- Turnover Metrics
     , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
     , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
-    , coalesce(market_data.token_volume, 0) as token_volume 
 from fundamental_data
 left join market_data using (date)
 where fundamental_data.date < to_date(sysdate())
