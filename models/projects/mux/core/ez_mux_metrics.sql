23ab528125f091442c117c8b05bd96667b9c8458
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-04-03 13:57:00 -0400

    Standardization perps (#1040)

--- models/projects/mux/core/ez_mux_metrics.sql
+++ models/projects/mux/core/ez_mux_metrics.sql
@@ -4,26 +4,35 @@
         snowflake_warehouse="MUX",
         database="mux",
         schema="core",
-        alias="ez_metrics_by_chain",
+        alias="ez_metrics",
     )
 }}
 
-with 
+with
     mux_data as (
-        select date, trading_volume, unique_traders, chain
+        select 
+            date
+            , sum(trading_volume) as trading_volume
+            , sum(unique_traders) as unique_traders
         from {{ ref("fact_mux_trading_volume_unique_traders") }}
         where chain is not null
+        group by 1
     )
-
+    , price as ({{ get_coingecko_metrics("mcdex") }})
 select
     date
     , 'mux' as app
     , 'DeFi' as category
-    , chain
-    , trading_volume
-    , unique_traders
     -- standardize metrics
     , trading_volume as perp_volume
     , unique_traders as perp_dau
+    -- Market Data
+    , price
+    , market_cap
+    , fdmc
+    , token_turnover_circulating
+    , token_turnover_fdv
+    , token_volume
 from mux_data
+left join price using(date)
 where date < to_date(sysdate())
