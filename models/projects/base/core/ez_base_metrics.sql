e148bd5512e97ff925e5d5925bdb70862ac722df
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-04 09:29:44 -0400

    fix warehouse config

--- models/projects/base/core/ez_base_metrics.sql
+++ models/projects/base/core/ez_base_metrics.sql
@@ -2,7 +2,7 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="ANALYTICS_XL",
+        snowflake_warehouse="BASE",
         database="base",
         schema="core",
         alias="ez_metrics",
