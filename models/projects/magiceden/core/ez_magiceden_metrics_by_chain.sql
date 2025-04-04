7da02c2e2ce87748441392c37ff954c95f8a00bc
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-03 22:40:06 -0400

    Added nft metrics to global schema, generated magic eden schema, edited EZM tables with standardized metrics

--- models/projects/magiceden/core/ez_magiceden_metrics_by_chain.sql
+++ models/projects/magiceden/core/ez_magiceden_metrics_by_chain.sql
@@ -8,7 +8,55 @@
     )
 }}
 
-select 
-    * 
-from
-    {{ ref('fact_magiceden_metrics_by_chain') }}
+WITH date_spine AS (
+    SELECT * 
+    FROM {{ ref('dim_date_spine') }}
+    WHERE date BETWEEN '2020-03-16' AND TO_DATE(SYSDATE())
+)
+, magiceden_metrics AS (
+    SELECT
+        date
+        , chain
+        , SUM(daily_trading_volume) AS daily_trading_volume
+        , SUM(active_wallets) AS dau 
+        , SUM(collections_transacted) AS collections_transacted
+        , SUM(total_trades) AS txns
+        , SUM(total_platform_fees) AS revenue
+        , SUM(total_creator_fees) AS supply_side_fees
+        , SUM(total_fees_usd) AS fees
+    FROM
+        {{ ref('fact_magiceden_metrics_by_chain') }}
+    GROUP BY
+        date, 
+        chain
+)
+
+SELECT 
+    date_spine.date
+    , chain
+    , COALESCE(daily_trading_volume, 0) AS daily_trading_volume
+    , COALESCE(dau, 0) AS dau 
+    , COALESCE(collections_transacted, 0) AS collections_transacted
+    , COALESCE(txns, 0) AS txns
+    , COALESCE(revenue, 0) AS revenue
+    , COALESCE(supply_side_fees, 0) AS supply_side_fees
+    , COALESCE(fees, 0) AS fees
+
+    -- Standardized Metrics
+
+    -- NFT Metrics
+    , COALESCE(dau, 0) AS nft_dau
+    , COALESCE(txns, 0) AS nft_txns
+    , COALESCE(collections_transacted, 0) AS nft_collections_transacted
+    , COALESCE(supply_side_fees, 0) AS nft_royalties
+    , COALESCE(supply_side_fees, 0) AS nft_fees
+    , COALESCE(daily_trading_volume, 0) AS nft_volume
+
+    -- Cash Flow Metrics
+    , COALESCE(revenue, 0) AS gross_protocol_revenue
+    , COALESCE(revenue, 0) AS foundation_cash_flow
+    , COALESCE(supply_side_fees, 0) AS service_cash_flow  
+FROM date_spine
+LEFT JOIN magiceden_metrics m ON date_spine.date = m.date
+WHERE date_spine.date < to_date(SYSDATE())
+ORDER BY date_spine.date, chain
