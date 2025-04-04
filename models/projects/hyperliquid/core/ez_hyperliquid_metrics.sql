30f69bec443143a32a8bf3ca519c0261fa2b2e2c
limzhengjie <limzhengjie01@gmail.com>
2025-04-04 12:52:01 -0400

    hyperliquid standardization

--- models/projects/hyperliquid/core/ez_hyperliquid_metrics.sql
+++ models/projects/hyperliquid/core/ez_hyperliquid_metrics.sql
@@ -8,20 +8,78 @@
     )
 }}
 
-SELECT
-    date,
-    sum(trading_volume) as trading_volume,
-    sum(unique_traders) as unique_traders,
-    sum(txns) as txns,
-    sum(fees) as fees,
-    sum(spot_fees) as spot_fees,
-    sum(perp_fees) as perp_fees,
-    sum(auction_fees) as auction_fees,
-    sum(daily_burn) as daily_burn,
-    avg(price) as price,
+with
+    trading_volume_data as (
+        select date, trading_volume, chain
+        from {{ ref("fact_hyperliquid_trading_volume") }}
+    )
+    , unique_traders_data as (
+        select date, unique_traders, chain
+        from {{ ref("fact_hyperliquid_unique_traders") }}
+    )
+    , daily_transactions_data as (
+        select date, trades, chain
+        from {{ ref("fact_hyperliquid_daily_transactions") }}
+    )
+    , fees_data as (
+        select date, chain, trading_fees, spot_fees, perp_fees
+        from {{ ref("fact_hyperliquid_fees") }}
+    )
+    , auction_fees_data as (
+        select date, chain, sum(auction_fees) as auction_fees
+        from {{ ref("fact_hyperliquid_auction_fees") }}
+        group by 1, 2
+    )
+    , daily_burn_data as (
+        select date, daily_burn, chain
+        from {{ ref("fact_hyperliquid_daily_burn") }}
+    )
+    , market_metrics as (
+        ({{ get_coingecko_metrics("hyperliquid") }}) 
+    )
+select
+    date
+    , 'hyperliquid' as app
+    , 'DeFi' as category
+    , trading_volume
+    , unique_traders::string as unique_traders
+    , trades as txns
+    , trading_fees as fees
+    , spot_fees
+    , perp_fees
+    , auction_fees
+    , daily_burn
     -- protocol’s revenue split between HLP (supplier) and AF (holder) at a ratio of 46%:54%
-    sum(primary_supply_side_revenue) as primary_supply_side_revenue,
+    , COALESCE(trading_fees * 0.46, 0) as primary_supply_side_revenue
     -- add daily burn back to the revenue
-    sum(revenue) as revenue
-FROM {{ ref("ez_hyperliquid_metrics_by_chain") }}
-GROUP BY 1
+    , COALESCE(trading_fees * 0.54, 0) + COALESCE(daily_burn, 0) * mm.price as revenue
+
+    -- Standardized Metrics
+    , unique_traders::string as perp_dau
+    , trading_volume as perp_volume
+    , trades as perp_txns
+
+    -- Revenue Metrics (trading fees includes spot and perp fees + auction fees)
+    , trading_fees as gross_protocol_revenue
+    -- (allegedly) protocol’s revenue split between Hyperliquid Liquidity Provider and Assistance Fund (buybacks) at a ratio of 46%:54%
+    , trading_fees * 0.46 as service_cash_flow
+    , trading_fees * 0.54 as buybacks
+    , daily_burn as burned_cash_flow_native
+    , daily_burn * mm.price as burned_cash_flow
+
+    -- Market metrics
+    , mm.price as price
+    , mm.token_volume as token_volume
+    , mm.market_cap as market_cap
+    , mm.fdmc as fdmc
+    , mm.token_turnover_circulating as token_turnover_circulating
+    , mm.token_turnover_fdv as token_turnover_fdv
+
+from unique_traders_data
+left join trading_volume_data using(date, chain)
+left join daily_transactions_data using(date, chain)
+left join fees_data using(date, chain)
+left join auction_fees_data using(date, chain)
+left join daily_burn_data using(date, chain)
+left join market_metrics mm using(date)
+where date < to_date(sysdate())
--- models/projects/hyperliquid/core/ez_hyperliquid_metrics_by_chain.sql
+++ models/projects/hyperliquid/core/ez_hyperliquid_metrics_by_chain.sql
@@ -12,54 +12,73 @@ with
     trading_volume_data as (
         select date, trading_volume, chain
         from {{ ref("fact_hyperliquid_trading_volume") }}
-    ),
-    unique_traders_data as (
+    )
+    , unique_traders_data as (
         select date, unique_traders, chain
         from {{ ref("fact_hyperliquid_unique_traders") }}
-    ),
-    daily_transactions_data as (
+    )
+    , daily_transactions_data as (
         select date, trades, chain
         from {{ ref("fact_hyperliquid_daily_transactions") }}
-    ),
-    fees_data as (
-        SELECT date, chain, trading_fees, spot_fees, perp_fees
-        FROM {{ ref("fact_hyperliquid_fees") }}
-    ),
-    auction_fees_data as (
+    )
+    , fees_data as (
+        select date, chain, trading_fees, spot_fees, perp_fees
+        from {{ ref("fact_hyperliquid_fees") }}
+    )
+    , auction_fees_data as (
         select date, chain, sum(auction_fees) as auction_fees
         from {{ ref("fact_hyperliquid_auction_fees") }}
         group by 1, 2
-    ),
-    daily_burn_data as (
+    )
+    , daily_burn_data as (
         select date, daily_burn, chain
         from {{ ref("fact_hyperliquid_daily_burn") }}
-    ),
-    price as (
-        select * from ({{ get_coingecko_price_with_latest("hyperliquid") }}) 
+    )
+    , market_metrics as (
+        ({{ get_coingecko_metrics("hyperliquid") }}) 
     )
 select
-    date,
-    'hyperliquid' as app,
-    'DeFi' as category,
-    chain,
-    trading_volume,
-    unique_traders::string as unique_traders,
-    trades as txns,
-    trading_fees as fees,
-    spot_fees,
-    perp_fees,
-    auction_fees,
-    daily_burn,
-    price,
+    date
+    , 'hyperliquid' as app
+    , 'DeFi' as category
+    , chain
+    , trading_volume
+    , unique_traders::string as unique_traders
+    , trades as txns
+    , trading_fees as fees
+    , spot_fees
+    , perp_fees
+    , auction_fees
+    , daily_burn
     -- protocol’s revenue split between HLP (supplier) and AF (holder) at a ratio of 46%:54%
-    COALESCE(trading_fees * 0.46, 0) as primary_supply_side_revenue,
+    , COALESCE(trading_fees * 0.46, 0) as primary_supply_side_revenue
     -- add daily burn back to the revenue
-    COALESCE(trading_fees * 0.54, 0) + COALESCE(daily_burn, 0) * p.price as revenue
+    , COALESCE(trading_fees * 0.54, 0) + COALESCE(daily_burn, 0) * mm.price as revenue
+
+    -- Standardized Metrics
+    , unique_traders::string as perp_dau
+    , trading_volume as perp_volume
+    , trades as perp_txns
+
+    -- Revenue Metrics (trading fees includes spot and perp fees + auction fees)
+    , trading_fees as gross_protocol_revenue
+    -- (allegedly) protocol’s revenue split between Hyperliquid Liquidity Provider and Assistance Fund (buybacks) at a ratio of 46%:54%
+    , trading_fees * 0.46 as service_cash_flow
+    , trading_fees * 0.54 as buybacks
+    , daily_burn as burned_cash_flow_native
+    , daily_burn * mm.price as burned_cash_flow
+
+    -- Market metrics
+    , mm.price as price
+    , mm.token_volume as token_volume
+    , mm.market_cap as market_cap
+    , mm.fdmc as fdmc
+    , mm.token_turnover_circulating as token_turnover_circulating
 from unique_traders_data
 left join trading_volume_data using(date, chain)
 left join daily_transactions_data using(date, chain)
 left join fees_data using(date, chain)
 left join auction_fees_data using(date, chain)
 left join daily_burn_data using(date, chain)
-left join price p using(date)
+left join market_metrics mm using(date)
 where date < to_date(sysdate())
