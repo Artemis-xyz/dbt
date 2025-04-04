d9f6f2448f628f8f98e8d73e5e48fc0edd9c6e12
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-04 12:58:23 -0400

    Edited EZM table and added EZM by chain to fit standardized metrics

--- models/projects/usual/core/ez_usual_metrics_by_chain.sql
+++ models/projects/usual/core/ez_usual_metrics_by_chain.sql
@@ -4,7 +4,7 @@
         snowflake_warehouse='USUAL',
         database='usual',
         schema='core',
-        alias='ez_metrics'
+        alias='ez_metrics_by_chain'
     )
 }}
 
@@ -45,12 +45,11 @@ with usd0_metrics as (
         , cumulative_burned 
     from {{ ref('fact_usual_burn_mint') }}
 )
-
-, market_data as (
-    {{ get_coingecko_metrics('usual') }}
-)
 select 
-    date
+    usd0.date
+    , chain as 'base'
+    , usd0.stablecoin_txns as stablecoin_txns
+    , usd0.stablecoin_dau as stablecoin_dau
     , usd0.usd0_tvl as usd0_tvl
     , usd0pp.usd0pp_tvl as usd0pp_tvl
     , usual.fees as fees
@@ -63,16 +62,11 @@ select
 
     -- Standardized Metrics
 
-    -- Token Metrics
-    , coalesce(market_data.price, 0) as price
-    , coalesce(market_data.market_cap, 0) as market_cap
-    , coalesce(market_data.fdmc, 0) as fdmc
-    , coalesce(market_data.token_volume, 0) as token_volume
-
     -- Stablecoin Metrics
-    , coalesce(usd0.stablecoin_dau, 0) as stablecoin_dau
-    , coalesce(usd0.stablecoin_txns, 0) as stablecoin_txns
+    , coalesce(market_data.stablecoin_dau, 0) as stablecoin_dau
+    , coalesce(market_data.stablecoin_txns, 0) as stablecoin_txns
     , coalesce(usd0.usd0_tvl, 0) as stablecoin_total_supply
+    , coalesce(market_data.stablecoin_volume, 0) as stablecoin_volume
 
     -- Crypto metrics
     , coalesce(usd0pp.usd0pp_tvl, 0) as tvl
@@ -88,12 +82,7 @@ select
 
     -- Supply Metrics
     , coalesce(ubm.daily_supply, 0) as mints_native
-
-    -- Turnover Metrics
-    , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
-    , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
 from usd0_metrics usd0 
-left join usd0pp_metrics usd0pp using (date)
-left join usual_fees usual using (date)
-left join usual_burn_mint ubm using (date)
-left join market_data using (date)
+left join usd0pp_metrics usd0pp on usd0.date = usd0pp.date
+left join usual_fees usual on usd0.date = usual.date
+left join usual_burn_mint ubm on usd0.date = ubm.date
