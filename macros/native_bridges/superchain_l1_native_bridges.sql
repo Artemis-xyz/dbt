2e28c3b854e32b2eca588d296d4ec7852d3de60d
Son Do <sondo077@gmail.com>
2025-04-04 19:17:42 -0400

    Fixed Data Quality Issue

--- macros/native_bridges/superchain_l1_native_bridges.sql
+++ macros/native_bridges/superchain_l1_native_bridges.sql
@@ -1,111 +1,48 @@
-{% macro get_superchain_l1_native_bridges(contract_address) %}
-    eth_deposits as (
-        select
-            block_timestamp
-            , tx_hash
-            , event_index
-            , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
-            , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
-            , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
-            , null as fee
-            , '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' as token_address
-            , 'ethereum' as source_chain
-            , 'optimism' as destination_chain
-        from ethereum_flipside.core.ez_decoded_event_logs
-        where
-            contract_address = lower('{{ contract_address }}')
-            and event_name = 'ETHDepositInitiated'
-            {% if is_incremental() %}
+{% macro get_superchain_l1_native_bridges(contract_address, chain) %}
 
-                and block_timestamp
-                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
+select
+    block_timestamp
+    , tx_hash
+    , event_index
+    , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
+    , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
+    , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
+    , null as fee
+    , case 
+        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"l1Token", decoded_log:"localToken")  
+        when event_name = 'ETHDepositInitiated' then  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"l2Token", decoded_log:"remoteToken")
+        when event_name = 'ETHWithdrawalFinalized' then '0x4200000000000000000000000000000000000006'
+    end as src_token_address
+    , case 
+        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"remoteToken", decoded_log:"l2Token")
+        when event_name = 'ETHDepositInitiated' then '0x4200000000000000000000000000000000000006'
+        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"localToken", decoded_log:"l1Token")
+        when event_name = 'ETHWithdrawalFinalized' then '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    end as dst_token_address
+    , case 
+        when event_name = 'ERC20DepositInitiated' then '{{chain}}'
+        when event_name = 'ETHDepositInitiated' then '{{chain}}'
+        when event_name = 'ERC20WithdrawalFinalized' then 'ethereum' 
+        when event_name = 'ETHWithdrawalFinalized' then 'ethereum'
+    end as source_chain
+    , case 
+        when event_name = 'ERC20DepositInitiated' then 'ethereum'
+        when event_name = 'ETHDepositInitiated' then 'ethereum'
+        when event_name = 'ERC20WithdrawalFinalized' then '{{chain}}' 
+        when event_name = 'ETHWithdrawalFinalized' then '{{chain}}'
+    end as destination_chain
+    , decoded_log
+    , event_name
+    , contract_address
+from ethereum_flipside.core.fact_decoded_event_logs
+where
+    contract_address = lower('{{ contract_address }}')
+    and event_name in ('ETHDepositInitiated', 'ETHWithdrawalFinalized', 'ERC20DepositInitiated', 'ERC20WithdrawalFinalized')
+    {% if is_incremental() %}
 
-            {% endif %}
-    ),
+        and block_timestamp
+        >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
 
-    eth_withdraws as (
-        select
-            block_timestamp
-            , tx_hash
-            , event_index
-            , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
-            , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
-            , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
-            , null as fee
-            , '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' as token_address
-            , 'optimism' as source_chain
-            , 'ethereum' as destination_chain
-        from ethereum_flipside.core.fact_decoded_event_logs
-        where
-            contract_address = lower('{{ contract_address }}')
-            and event_name = 'ETHWithdrawalFinalized'
-            {% if is_incremental() %}
-
-                and block_timestamp
-                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
-
-            {% endif %}
-    ),
-
-    erc20_deposits as (
-        select
-            block_timestamp
-            , tx_hash
-            , event_index
-            , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
-            , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
-            , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
-            , null as fee
-            , decoded_log:"l1Token" as token_address
-            , 'ethereum' as source_chain
-            , 'optimism' as destination_chain
-        from ethereum_flipside.core.fact_decoded_event_logs
-        where
-            contract_address = lower('{{ contract_address }}')
-            and event_name = 'ERC20DepositInitiated'
-            {% if is_incremental() %}
-
-                and block_timestamp
-                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
-
-            {% endif %}
-    ),
-
-    erc20_withdraws as (
-        select
-            block_timestamp,
-            , tx_hash
-            , event_index
-            , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
-            , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
-            , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
-            , null as fee
-            , decoded_log:"l1Token" as token_address
-            , 'optimism' as source_chain
-            , 'ethereum' as destination_chain
-        from ethereum_flipside.core.fact_decoded_event_logs
-        where
-            contract_address = lower('{{ contract_address }}')
-            and event_name = 'ERC20WithdrawalFinalized'
-            {% if is_incremental() %}
-
-                and block_timestamp
-                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
-
-            {% endif %}
-    )
-
-select *
-from eth_deposits
-union
-select *
-from eth_withdraws
-union
-select *
-from erc20_deposits
-union
-select *
-from erc20_withdraws
-
-
+    {% endif %}
 {% endmacro %}
