1b885b11d8cb4d24c822bc2a4a380687084c9a0b
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-04 06:30:02 -0700

    Soneium Standardization (#1058)

--- models/projects/soneium/core/ez_soneium_metrics.sql
+++ models/projects/soneium/core/ez_soneium_metrics.sql
@@ -14,5 +14,12 @@ select
     , daa as dau
     , fees_native
     , fees
+    -- Standardized metrics
+    -- Chain Usage Metrics
+    , dau as chain_dau
+    , txns as chain_txns
+    -- Cashflow metrics
+    , fees_native AS gross_protocol_revenue_native
+    , fees AS gross_protocol_revenue
 from {{ ref("fact_soneium_fundamental_metrics") }}
 where date < to_date(sysdate())
