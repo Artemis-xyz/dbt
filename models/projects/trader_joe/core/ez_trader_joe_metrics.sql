d4100469ab823f1077be500fe9014f4ea3dedf23
Alex Weseley <pampawes@gmail.com>
2025-04-04 10:52:21 -0400

    trader joe - all ez materialize and retl succeeds

--- models/projects/trader_joe/core/ez_trader_joe_metrics.sql
+++ models/projects/trader_joe/core/ez_trader_joe_metrics.sql
@@ -9,14 +9,23 @@
 }}
 
 SELECT
-    date,
-    app,
-    category,
-    sum(tvl) as tvl,
-    sum(trading_volume) as trading_volume,
-    sum(trading_fees) as trading_fees,
-    sum(unique_traders) as unique_traders,
-    sum(number_of_swaps) as number_of_swaps,
-    sum(gas_cost_native) as gas_cost_native
+    date
+    , app
+    , category
+    , sum(trading_volume) as trading_volume
+    , sum(trading_fees) as trading_fees
+    , sum(unique_traders) as unique_traders
+    , sum(number_of_swaps) as number_of_swaps
+    , sum(gas_cost_usd) as gas_cost_usd
+
+    -- Standardized Metrics
+    , sum(spot_dau) as spot_dau
+    , sum(spot_txns) as spot_txns
+    , sum(spot_volume) as spot_volume
+    , sum(tvl) as tvl
+    , sum(trading_fees) as trading_fees
+    , sum(gross_protocol_revenue) as gross_protocol_revenue
+    , sum(gas_cost_native) as gas_cost_native
+    , sum(gas_cost) as gas_cost
 FROM {{ ref("ez_trader_joe_metrics_by_chain") }}
 GROUP BY 1, 2, 3
