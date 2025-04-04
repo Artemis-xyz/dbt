215a6ae58ad469e0d0995cd3e6eb1746a822a74b
limzhengjie <limzhengjie01@gmail.com>
2025-04-04 18:39:22 -0400

    Scroll Standardization

--- models/projects/scroll/core/ez_scroll_metrics.sql
+++ models/projects/scroll/core/ez_scroll_metrics.sql
@@ -34,34 +34,60 @@ with
         select date, daily_volume as dex_volumes
         from {{ ref("fact_scroll_daily_dex_volumes") }}
     )
-
+    , price_data as ({{ get_coingecko_metrics("scroll") }})
 select
-    fundamental_data.date
+    fd.date
     , 'scroll' as chain
-    , txns
-    , dau
-    , wau
-    , mau
-    , fees
-    , fees / txns as avg_txn_fee
-    , median_txn_fee
-    , fees_native
-    , revenue
-    , revenue_native
-    , l1_data_cost
-    , l1_data_cost_native
-    , tvl
-    , weekly_commits_core_ecosystem
-    , weekly_commits_sub_ecosystem
-    , weekly_developers_core_ecosystem
-    , weekly_developers_sub_ecosystem
-    , weekly_contracts_deployed
-    , weekly_contract_deployers
-    , dune_dex_volumes_scroll.dex_volumes
-from fundamental_data
-left join github_data using (date)
-left join contract_data using (date)
-left join defillama_data using (date)
-left join rolling_metrics using (date)
-left join scroll_dex_volumes as dune_dex_volumes_scroll on fundamental_data.date = dune_dex_volumes_scroll.date
-where fundamental_data.date < to_date(sysdate())
+    , fd.txns
+    , fd.dau
+    , rm.wau
+    , rm.mau
+    , fd.fees
+    , fd.fees / fd.txns as avg_txn_fee
+    , fd.median_txn_fee
+    , fd.fees_native
+    , fd.revenue
+    , fd.revenue_native
+    , dsv.dex_volumes
+
+    -- Standardized Metrics
+    -- Market Data
+    , pd.price
+    , pd.market_cap
+    , pd.fdmc
+    , pd.token_volume
+    , pd.token_turnover_circulating
+
+    -- Chain Metrics
+    , fd.dau as chain_dau
+    , rm.wau as chain_wau
+    , rm.mau as chain_mau
+    , fd.txns as chain_txns
+    , dsv.dex_volumes as chain_dex_volumes
+
+    -- Cashflow Metrics
+    , fd.fees as gross_protocol_revenue
+    , fd.fees_native as gross_protocol_revenue_native
+    , avg_txn_fee as chain_avg_txn_fee
+    , fd.median_txn_fee as chain_median_txn_fee
+    , fd.l1_data_cost as l1_data_cost
+    , fd.l1_data_cost_native as l1_data_cost_native
+    -- All the fees goes into (L2ScrollFeeVault - L1 Data Cost) = Scroll Labs (Equity Cash Flow)
+    , coalesce(fd.fees, 0) - coalesce(fd.l1_data_cost, 0) as equity_cash_flow
+    
+    -- Developer Metrics
+    , gd.weekly_commits_core_ecosystem
+    , gd.weekly_commits_sub_ecosystem
+    , gd.weekly_developers_core_ecosystem
+    , gd.weekly_developers_sub_ecosystem
+    , cd.weekly_contracts_deployed
+    , cd.weekly_contract_deployers
+
+from fundamental_data fd
+left join github_data gd on fd.date = gd.date
+left join contract_data cd on fd.date = cd.date
+left join defillama_data dd on fd.date = dd.date
+left join rolling_metrics rm on fd.date = rm.date
+left join scroll_dex_volumes dsv on fd.date = dsv.date
+left join price_data pd on fd.date = pd.date
+where fd.date < to_date(sysdate())
