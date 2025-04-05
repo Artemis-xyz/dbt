9d18327568fa50b5c5b126f1d67afe6b3166e689
Son Do <sondo077@gmail.com>
2025-04-04 23:05:01 -0400

    Superchain bridge transfers

--- macros/native_bridges/superchain_l1_native_bridges.sql
+++ macros/native_bridges/superchain_l1_native_bridges.sql
@@ -6,18 +6,18 @@ select
     , event_index
     , coalesce(decoded_log:"from", decoded_log:"_from") as depositor
     , coalesce(decoded_log:"to", decoded_log:"_to") as recipient
-    , coalesce(decoded_log:"amount", decoded_log:"_amount") as amount
+    , TO_NUMERIC(coalesce(decoded_log:"amount", decoded_log:"_amount")) as amount_native
     , null as fee
     , case 
-        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"l1Token", decoded_log:"localToken")  
+        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"l1Token", decoded_log:"localToken")::string
         when event_name = 'ETHDepositInitiated' then  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"l2Token", decoded_log:"remoteToken")
+        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"l2Token", decoded_log:"remoteToken")::string
         when event_name = 'ETHWithdrawalFinalized' then '0x4200000000000000000000000000000000000006'
     end as src_token_address
     , case 
-        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"remoteToken", decoded_log:"l2Token")
+        when event_name = 'ERC20DepositInitiated' then coalesce(decoded_log:"remoteToken", decoded_log:"l2Token")::string
         when event_name = 'ETHDepositInitiated' then '0x4200000000000000000000000000000000000006'
-        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"localToken", decoded_log:"l1Token")
+        when event_name = 'ERC20WithdrawalFinalized' then coalesce(decoded_log:"localToken", decoded_log:"l1Token")::string
         when event_name = 'ETHWithdrawalFinalized' then '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
     end as dst_token_address
     , case 
