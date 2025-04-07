0f5a8f4b66f60c3f6a8c58f24602bd76387c00d0
Anish Toomu <anishtoomu@gmail.com>
2025-04-07 11:48:58 -0400

    litecoin transactions ez table

---
+++ models/projects/litecoin/core/ez_litecoin_transactions.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="LITECOIN",
+        database="litecoin",
+        schema="core",
+        alias="ez_transactions",
+    )
+}}
+
+select 
+    hash,
+    size,
+    virtual_size,
+    version,
+    lock_time,
+    block_hash,
+    block_number,
+    block_timestamp,
+    block_timestamp_month,
+    input_count,
+    output_count,
+    input_value / 100000000 as input_value_ltc,
+    output_value / 100000000 as output_value_ltc,
+    is_coinbase,
+    fee / 100000000 as fee_ltc,
+    inputs,
+    outputs,
+    unique_id
+from {{ ref('fact_litecoin_transfers') }}
+where block_timestamp < to_date(sysdate()) 
