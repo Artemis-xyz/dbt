5d7a645ba86798119eb2ca43b3b07568192f6dd0
Sebastian Melendez <sebm@seas.upenn.edu>
2025-04-06 20:57:33 -0400

    Standardizing bridging protocols

--- models/projects/debridge/core/ez_debridge_metrics.sql
+++ models/projects/debridge/core/ez_debridge_metrics.sql
@@ -19,12 +19,23 @@ with bridge_volume_fees as (
     from {{ ref("fact_debridge_fundamental_metrics") }}
 )
 
+, price_data as ({{ get_coingecko_metrics("debridge") }})
+
 select
     date
-    , bridge_volume
     , ecosystem_revenue
-    , bridge_txns
     , txns
     , fees
+    -- Standardized Metrics
+    , bridge_volume
+    , bridge_txns
+    , ecosystem_revenue as gross_protocol_revenue
+    , price_data.price as price
+    , price_data.market_cap as market_cap
+    , price_data.fdmc as fdmc
+    , price_data.token_turnover_circulating as token_turnover_circulating
+    , price_data.token_turnover_fdv as token_turnover_fdv
+    , price_data.token_volume as token_volume
 from bridge_volume_fees
+left join price_data on bridge_volume_fees.date = price_data.date
 where date < to_date(sysdate())
