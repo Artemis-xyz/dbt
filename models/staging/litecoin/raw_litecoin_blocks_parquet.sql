4486a14722af6326aae0f104803162041a5ab470
Anish Toomu <anishtoomu@gmail.com>
2025-04-09 15:12:38 -0400

    removed unneccessary raw files

--- models/staging/litecoin/raw_litecoin_blocks_parquet.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table') }}
-
-create or replace table LANDING_DATABASE.PROD_LANDING.RAW_LITECOIN_BLOCKS_PARQUET (
-    PARQUET_RAW VARIANT
-); 
--- models/staging/litecoin/raw_litecoin_inputs_parquet.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table') }}
-
-create or replace table LANDING_DATABASE.PROD_LANDING.RAW_LITECOIN_INPUTS_PARQUET (
-    PARQUET_RAW VARIANT
-); 
