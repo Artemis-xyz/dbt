3f0b47593e6223de08b16bfc70d98e5d2063235e
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-10 18:27:12 -0400

    revert bsc changes

--- models/staging/bsc/fact_bsc_transactions_v2.sql
+++ models/staging/bsc/fact_bsc_transactions_v2.sql
@@ -39,50 +39,49 @@ with
                 -- this filter will only be applied on an incremental run 
                 and date >= (select dateadd('day', -5, max(raw_date)) from {{ this }})
             {% endif %}
-    ), ez_transactions_base AS (
-
-    select
-        tx_hash,
-        coalesce(to_address, t.from_address) as contract_address,
-        block_timestamp,
-        date_trunc('day', block_timestamp) raw_date,
-        t.from_address,
-        tx_fee,
-        (tx_fee * price) gas_usd,
-        'bsc' as chain,
-        new_contracts.name,
-        new_contracts.app,
-        new_contracts.friendly_name,
-        new_contracts.sub_category,
-        case
-            when t.input_data = '0x' and t.value > 0
-            then 'EOA'
-            when new_contracts.category is not null
-            then new_contracts.category
-            else null
-        end as category,
-        sybil.user_type,
-        sybil.address_life_span,
-        sybil.cur_total_txns,
-        sybil.cur_distinct_to_address_count,
-        sybil.probability,
-        sybil.engagement_type,
-        bal.balance_usd,
-        bal.native_token_balance,
-        bal.stablecoin_balance,
-        CAST(current_timestamp() AS TIMESTAMP_NTZ) AS last_updated_timestamp
-    from bsc_flipside.core.fact_transactions as t
-    left join new_contracts on lower(t.to_address) = lower(new_contracts.address)
-    left join collapsed_prices on raw_date = collapsed_prices.date
-    left join
-        {{ ref("dim_bsc_sybil_address") }} as sybil on t.from_address = sybil.from_address
-    left join balances as bal on t.from_address = bal.address and raw_date = bal.date
-    {% if is_incremental() %}
-        -- this filter will only be applied on an incremental run 
-        where
-            block_timestamp
-            >= (select dateadd('day', -5, max(block_timestamp)) from {{ this }})
-            or 
-            new_contracts.last_updated
-                >= (select dateadd('day', -5, max(last_updated_timestamp)) from {{ this }})
-    {% endif %}
+    )
+select
+    tx_hash,
+    coalesce(to_address, t.from_address) as contract_address,
+    block_timestamp,
+    date_trunc('day', block_timestamp) raw_date,
+    t.from_address,
+    tx_fee,
+    (tx_fee * price) gas_usd,
+    'bsc' as chain,
+    new_contracts.name,
+    new_contracts.app,
+    new_contracts.friendly_name,
+    new_contracts.sub_category,
+    case
+        when t.input_data = '0x' and t.value > 0
+        then 'EOA'
+        when new_contracts.category is not null
+        then new_contracts.category
+        else null
+    end as category,
+    sybil.user_type,
+    sybil.address_life_span,
+    sybil.cur_total_txns,
+    sybil.cur_distinct_to_address_count,
+    sybil.probability,
+    sybil.engagement_type,
+    bal.balance_usd,
+    bal.native_token_balance,
+    bal.stablecoin_balance,
+    CAST(current_timestamp() AS TIMESTAMP_NTZ) AS last_updated_timestamp
+from bsc_flipside.core.fact_transactions as t
+left join new_contracts on lower(t.to_address) = lower(new_contracts.address)
+left join collapsed_prices on raw_date = collapsed_prices.date
+left join
+    {{ ref("dim_bsc_sybil_address") }} as sybil on t.from_address = sybil.from_address
+left join balances as bal on t.from_address = bal.address and raw_date = bal.date
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run 
+    where
+        block_timestamp
+        >= (select dateadd('day', -5, max(block_timestamp)) from {{ this }})
+        or 
+        new_contracts.last_updated
+            >= (select dateadd('day', -5, max(last_updated_timestamp)) from {{ this }})
+{% endif %}
