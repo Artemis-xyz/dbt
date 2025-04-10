6e256cefc5c39fccf9e42adedbd48eff09f7f3a1
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-09 21:07:10 -0400

    Cleaned up and consolidated files, improved fees to be by token and chain

--- models/staging/synthetix/fact_synthetix_token_incentives_by_chain.sql
+++ models/staging/synthetix/fact_synthetix_token_incentives_by_chain.sql
@@ -1,19 +1,21 @@
-{{
-    config(
-        materialized='table',
-        snowflake_warehouse='SYNTHETIX',
-        database='SYNTHETIX',
-        schema='raw',
-        alias='fact_synthetix_token_incentives_by_chain'
-    )
-}}
+{{ config(materialized="table", snowflake_warehouse="SYNTHETIX") }}
 
-with all_token_incentives_by_chain as (
-    SELECT * FROM {{ ref('fact_synthetix_ethereum_token_incentives') }}
+
+with ethereum_token_incentives as (
+    select
+        date(block_timestamp) as date,
+        'Ethereum' as chain,
+        sum(
+            case
+                when amount_usd is not null then amount_usd
+                when contract_address ILIKE '0xc011a72400e58ecd99ee497cf89e3775d4bd732f' then (raw_amount/1e18)*eph.price
+                else 0 end
+        ) as token_incentives
+    from ethereum_flipside.core.ez_token_transfers as tt
+    join ethereum_flipside.price.ez_prices_hourly as eph
+        on eph.symbol ILIKE 'SNX' and eph.hour = date_trunc('hour', tt.block_timestamp)
+    where lower(from_address) in (lower('0x29C295B046a73Cde593f21f63091B072d407e3F2'), lower('0xFfA72Fd80d8A84032d855bfb67036BAF45949009'))
+    group by date
 )
 
-select
-    date,
-    chain,
-    token_incentives
-from all_token_incentives_by_chain
+select * from ethereum_token_incentives
