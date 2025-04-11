57c7b0d54b54364832264a5cf408a0b3fa3a7d31
Alex Weseley <pampawes@gmail.com>
2025-04-10 20:35:30 -0400

    icp definitions

--- models/projects/internet_computer/core/ez_internet_computer_metrics.sql
+++ models/projects/internet_computer/core/ez_internet_computer_metrics.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="ANALYTICS_XL",
+        snowflake_warehouse="internet_computer",
         database="internet_computer",
         schema="core",
         alias="ez_metrics",
