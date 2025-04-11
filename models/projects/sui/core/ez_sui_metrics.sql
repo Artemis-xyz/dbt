d4861d3d6e0fd01226f4610f1b973a7e8f618392
Alex Weseley <pampawes@gmail.com>
2025-04-11 11:17:16 -0400

    warehouse

--- models/projects/sui/core/ez_sui_metrics.sql
+++ models/projects/sui/core/ez_sui_metrics.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="ANALYTICS_XL",
+        snowflake_warehouse="SUI",
         database="sui",
         schema="core",
         alias="ez_metrics",
