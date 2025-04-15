3dcb5c320d07c0e6a561417c8e01a935c35794cd
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-15 15:39:10 -0400

    Renamed supply data fact sheet and added to EZM table

--- models/projects/aerodrome/core/ez_aerodrome_metrics.sql
+++ models/projects/aerodrome/core/ez_aerodrome_metrics.sql
@@ -28,6 +28,16 @@ with swap_metrics as (
 , market_metrics as (
     {{get_coingecko_metrics('aerodrome-finance')}}
 )
+, supply_metrics as (
+    SELECT
+        date,
+        pre_mine_unlocks,
+        emissions_native,
+        locked_supply,
+        total_supply,
+        circulating_supply
+    FROM {{ ref('fact_aerodrome_supply_data') }}
+)
 , date_spine as (
     SELECT
         ds.date
@@ -40,6 +50,8 @@ with swap_metrics as (
                         select min(date) as min_date from tvl_metrics
                         UNION ALL
                         select min(date) as min_date from market_metrics
+                        UNION ALL
+                        select min(date) as min_date from supply_metrics
                     )
                 )
         and to_date(sysdate())
@@ -48,34 +60,45 @@ SELECT
     ds.date
 
     -- Old metrics needed for compatibility
-    , sm.unique_traders
-    , sm.total_swaps as number_of_swaps
-    , sm.daily_volume_usd as trading_volume
-    , sm.daily_fees_usd as trading_fees
+    , coalesce(sm.unique_traders, 0) as unique_traders
+    , coalesce(sm.total_swaps, 0) as number_of_swaps
+    , coalesce(sm.daily_volume_usd, 0) as trading_volume
+    , coalesce(sm.daily_fees_usd, 0) as trading_fees
 
     -- Standardized Metrics
     -- Market Metrics
-    , mm.price as price
-    , mm.market_cap as market_cap
-    , mm.fdmc as fdmc
-    , mm.token_volume as token_volume
+    , coalesce(mm.price, 0) as price
+    , coalesce(mm.market_cap, 0) as market_cap
+    , coalesce(mm.fdmc, 0) as fdmc
+    , coalesce(mm.token_volume, 0) as token_volume
 
     -- Usage/Sector Metrics
-    , sm.unique_traders as spot_dau
-    , sm.total_swaps as spot_txns
-    , sm.daily_volume_usd as spot_volume
-    , tm.tvl_usd as tvl
+    , coalesce(sm.unique_traders, 0) as spot_dau
+    , coalesce(sm.total_swaps, 0) as spot_txns
+    , coalesce(sm.daily_volume_usd, 0) as spot_volume
+    , coalesce(tm.tvl_usd, 0) as tvl
 
     -- Money Metrics
-    , sm.daily_fees_usd as spot_fees
-    , sm.daily_fees_usd as gross_protocol_revenue
-    , sm.daily_fees_usd as fee_sharing_token_cash_flow
+    , coalesce(sm.daily_fees_usd, 0) as spot_fees
+    , coalesce(sm.daily_fees_usd, 0) as gross_protocol_revenue
+    , coalesce(sm.daily_fees_usd, 0) as fee_sharing_token_cash_flow
+
+    -- Supply Metrics
+    , coalesce(sp.pre_mine_unlocks, 0) + coalesce(sp.emissions_native, 0) as mints_native
+    , (coalesce(sp.pre_mine_unlocks, 0) + coalesce(sp.emissions_native, 0)) * coalesce(mm.price, 0) as mints
+    , coalesce(sp.emissions_native, 0) as emissions_native
+    , coalesce(sp.pre_mine_unlocks, 0) as premine_unlocks_native
+    , coalesce(sp.emissions_native, 0) as net_supply_change_native
+    , coalesce(sp.circulating_supply, 0) as circulating_supply_native
+    , coalesce(sp.locked_supply, 0) as locked_supply
+    , coalesce(sp.total_supply, 0) as total_supply
 
     -- Other Metrics
-    , mm.token_turnover_circulating as token_turnover_circulating
-    , mm.token_turnover_fdv as token_turnover_fdv
+    , coalesce(mm.token_turnover_circulating, 0) as token_turnover_circulating
+    , coalesce(mm.token_turnover_fdv, 0) as token_turnover_fdv
 FROM date_spine ds
 LEFT JOIN swap_metrics sm using (date)
 LEFT JOIN tvl_metrics tm using (date)
 LEFT JOIN market_metrics mm using (date)
+LEFT JOIN supply_metrics sp using (date)
 WHERE ds.date < to_date(sysdate())
--- models/projects/aerodrome/raw/fact_aerodrome_supply_data.sql
+++ models/projects/aerodrome/raw/fact_aerodrome_supply_data.sql
@@ -4,7 +4,7 @@
         snowflake_warehouse='AERODROME',
         database='aerodrome',
         schema='raw',
-        alias='fact_aerodrome_supply_side'
+        alias='fact_aerodrome_supply_data'
     )
 }}
 
@@ -12,7 +12,7 @@ with locked_supply_balance as (
     select 
         date(block_timestamp) as date, 
         balance_token / 1e18 as ve_aero_balance, 
-        row_number() over (partition by date(block_timestamp) order by block_timestamp) as rn, 
+        row_number() over (partition by date(block_timestamp) order by block_timestamp) as rn
     from base.prod_raw.ez_address_balances_by_token
     where lower(contract_address) = lower('0x940181a94A35A4569E4529A3CDfB74e38FD98631')
         and lower(address) = lower('0xeBf418Fe2512e7E6bd9b87a8F0f294aCDC67e6B4')
@@ -21,7 +21,7 @@ with locked_supply_balance as (
 emissions as (
     select 
         date(block_timestamp) as date, 
-        sum(amount) as emissions, 
+        sum(amount) as emissions
     from base_flipside.core.ez_token_transfers
     where lower(contract_address) = lower('0x940181a94A35A4569E4529A3CDfB74e38FD98631')
         and lower(from_address) = lower('0x0000000000000000000000000000000000000000')
@@ -30,15 +30,19 @@ emissions as (
 
 select 
     coalesce(em.date, lsb.date) as date, 
-    coalesce(em.emissions, 0) as emissions, 
+    case when em.date = '2023-08-28' then coalesce(em.emissions, 0) else 0 end as pre_mine_unlocks, 
+    case when em.date <> '2023-08-28' then coalesce(em.emissions, 0) else 0 end as emissions_native, 
     coalesce(lsb.ve_aero_balance, 0) as locked_supply, 
-    sum(emissions) over (
+    sum(coalesce(em.emissions, 0)) over (
         order by em.date
         rows between unbounded preceding and current row
-    ) as total_supply, 
-    coalesce(total_supply,0) - coalesce(locked_supply,0) as circulating_supply
+    ) as total_supply,
+    sum(coalesce(em.emissions, 0)) over (
+        order by em.date
+        rows between unbounded preceding and current row
+    ) - coalesce(lsb.ve_aero_balance, 0) as circulating_supply
 from emissions as em
 full join locked_supply_balance as lsb
     on em.date = lsb.date
 where lsb.rn = 1
-order by date
+order by date asc
