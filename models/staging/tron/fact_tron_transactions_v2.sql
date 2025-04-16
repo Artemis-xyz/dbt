78eaa773374c3f658f69a7a42bde567f085e675e
horaceliuHL <horaceliu.hl@gmail.com>
2025-04-16 17:31:26 -0400

    Fix fact tron transactions

--- models/staging/tron/fact_tron_transactions_v2.sql
+++ models/staging/tron/fact_tron_transactions_v2.sql
@@ -2,7 +2,7 @@
     config(
         materialized="incremental",
         unique_key="tx_hash",
-        snowflake_warehouse="BAM_TRANSACTION_XLG",
+        snowflake_warehouse="BAM_TRANSACTION_2XLG",
     )
 }}
 
@@ -18,7 +18,7 @@ with
             transfer_type = 'fees'
             {% if is_incremental() %}
                 and block_timestamp
-                >= (select dateadd('day', -5, max(block_timestamp)) from {{ this }})
+                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
             {% endif %}
         group by transaction_hash
     ),
@@ -35,51 +35,106 @@ with
         from {{ ref("dim_all_addresses_labeled_silver") }} as contract
         where chain = 'tron'
     ),
-    balances as (
-        select address, date, balance_usd, native_token_balance, stablecoin_balance
-        from {{ ref("fact_tron_daily_balances") }}
+    transactions_with_contracts as (
+        select
+            hash as tx_hash,
+            coalesce(to_address, t.from_address) as contract_address,
+            block_timestamp,
+            date_trunc('day', block_timestamp) raw_date,
+            t.from_address,
+            'tron' as chain,
+            new_contracts.name,
+            new_contracts.app,
+            new_contracts.friendly_name,
+            new_contracts.sub_category,
+            case
+                when t.input = '0x' and t.value::double > 0
+                then 'EOA'
+                when new_contracts.category is not null
+                then new_contracts.category
+                else null
+            end as category,
+            CAST(current_timestamp() AS TIMESTAMP_NTZ) AS last_updated_timestamp,
+            fees.gas::double as tx_fee,
+            fees.gas_usd::double as gas_usd
+        from tron_allium.raw.transactions as t
+        left join tron_fees as fees on t.hash = fees.tx_hash
+        left join new_contracts on lower(t.to_address) = lower(new_contracts.address)
         {% if is_incremental() %}
             -- this filter will only be applied on an incremental run 
-            where date >= (select dateadd('day', -5, max(raw_date)) from {{ this }})
+            where
+                block_timestamp
+                >= (select dateadd('day', -3, max(block_timestamp)) from {{ this }})
+        {% endif %}
+    ),
+
+    {% if is_incremental() %}
+    new_contracts_incremental as (
+        select * from new_contracts 
+        where last_updated
+                >= (select dateadd('day', -3, max(last_updated_timestamp)) from {{ this }})
+    ), 
+    updated_contract_transactions as (
+        select
+            hash as tx_hash,
+            coalesce(to_address, t.from_address) as contract_address,
+            block_timestamp,
+            date_trunc('day', block_timestamp) raw_date,
+            t.from_address,
+            'tron' as chain,
+            new_contracts_incremental.name,
+            new_contracts_incremental.app,
+            new_contracts_incremental.friendly_name,
+            new_contracts_incremental.sub_category,
+            case
+                when t.input = '0x' and t.value::double > 0
+                then 'EOA'
+                when new_contracts_incremental.category is not null
+                then new_contracts_incremental.category
+                else null
+            end as category,
+            CAST(current_timestamp() AS TIMESTAMP_NTZ) AS last_updated_timestamp
+        from tron_allium.raw.transactions as t
+        inner join new_contracts_incremental on lower(t.to_address) = lower(new_contracts_incremental.address)
+    ),
+    updated_contract_tron_fees as (
+        select
+            concat('0x', t.transaction_hash) as tx_hash,
+            max(t.amount) as gas,
+            max(t.usd_amount) as gas_usd,
+            max(t.usd_exchange_rate) as price
+        from tron_allium.assets.trx_token_transfers t
+        where
+            transfer_type = 'fees'
+            and concat('0x', transaction_hash) in (
+                select tx_hash from updated_contract_transactions
+            )
+        group by transaction_hash
+    ),
+    final_updated_contract_transactions as (
+        select 
+            t.*,
+            fees.gas::double as tx_fee,
+            fees.gas_usd::double as gas_usd
+        from updated_contract_transactions as t
+        left join updated_contract_tron_fees as fees on t.tx_hash = fees.tx_hash
+    ),
+    {% endif %}
+
+    final_table as (
+        select * from transactions_with_contracts
+        {% if is_incremental() %}
+            union
+            select * from final_updated_contract_transactions
         {% endif %}
     )
-select
-    hash as tx_hash,
-    coalesce(to_address, t.from_address) as contract_address,
-    block_timestamp,
-    date_trunc('day', block_timestamp) raw_date,
-    t.from_address,
-    fees.gas::double as tx_fee,
-    fees.gas_usd::double as gas_usd,
-    'tron' as chain,
-    new_contracts.name,
-    new_contracts.app,
-    new_contracts.friendly_name,
-    new_contracts.sub_category,
-    case
-        when t.input = '0x' and t.value::double > 0
-        then 'EOA'
-        when new_contracts.category is not null
-        then new_contracts.category
-        else null
-    end as category,
-    bal.balance_usd::double as balance_usd,
-    bal.native_token_balance::double as native_token_balance,
-    bal.stablecoin_balance::double as stablecoin_balance,
+
+select 
+    *,
+    null as balance_usd, -- got from balances table for sybil model
+    null as native_token_balance, -- got from balances table for sybil model
+    null as stablecoin_balance, -- got from balances table for sybil model
     null as user_type,
     null as probability,
-    null as engagement_type,
-    CAST(current_timestamp() AS TIMESTAMP_NTZ) AS last_updated_timestamp
-from tron_allium.raw.transactions as t
-left join tron_fees as fees on t.hash = fees.tx_hash
-left join new_contracts on lower(t.to_address) = lower(new_contracts.address)
-left join balances as bal on t.from_address = bal.address and raw_date = bal.date
-{% if is_incremental() %}
-    -- this filter will only be applied on an incremental run 
-    where
-        block_timestamp
-        >= (select dateadd('day', -5, max(block_timestamp)) from {{ this }})
-        or 
-        new_contracts.last_updated
-        >= (select dateadd('day', -5, max(last_updated_timestamp)) from {{ this }})
-{% endif %}
+    null as engagement_type
+from final_table
