cf245335d895592ba58c689ef303bf9e337450ea
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-16 11:29:04 -0400

    Redid the max_extraction logic

--- models/staging/bitcoin/fact_bitcoin_dex_volumes.sql
+++ models/staging/bitcoin/fact_bitcoin_dex_volumes.sql
@@ -13,12 +13,17 @@ with
         where extraction_date = (select max_date from max_extraction)
     ),
     lnexchange as (
+        with max_extraction as (
+            select max(extraction_date) as max_date
+            from {{ source('PROD_LANDING', 'raw_lnexchange_daily_volume') }}
+        )
         select
             value:date::date as date,
             value:volume_usd::number as lnexchange_volume
         from
             {{ source('PROD_LANDING', 'raw_lnexchange_daily_volume') }},
             lateral flatten(input => parse_json(source_json))
+        where extraction_date = (select max_date from max_extraction)
     )
 select
     coalesce(bisq.date, lnexchange.date) as date,
