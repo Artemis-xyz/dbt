ab50c53c1a55f09fb61876f9640e0e96ba0e6614
Anish Toomu <34148007+av2mu@users.noreply.github.com>
2025-04-17 16:45:20 -0400

    Litecoin protocol addition from BigQuery Share (#1122)
    Co-authored-by: Alex Kan <29241719+akan72@users.noreply.github.com>

---
+++ models/staging/litecoin/fact_litecoin_blocks.sql
@@ -0,0 +1,38 @@
+{{ config(
+    materialized='table',
+    unique_key='unique_id',
+    snowflake_warehouse='LITECOIN'
+) }}
+with source_data as (
+    select 
+        PARQUET_RAW:hash::string as hash,
+        PARQUET_RAW:size::number as size,
+        PARQUET_RAW:stripped_size::number as stripped_size,
+        PARQUET_RAW:weight::number as weight,
+        PARQUET_RAW:number::number as number,
+        PARQUET_RAW:version::number as version,
+        PARQUET_RAW:merkle_root::string as merkle_root,
+        PARQUET_RAW:timestamp::timestamp as timestamp,
+        PARQUET_RAW:timestamp_month::string as timestamp_month,
+        PARQUET_RAW:nonce::string as nonce,
+        PARQUET_RAW:bits::string as bits,
+        PARQUET_RAW:coinbase_param::string as coinbase_param,
+        PARQUET_RAW:transaction_count::number as transaction_count
+    from {{ source('PROD_LANDING', 'raw_litecoin_blocks_parquet') }}
+)
+
+select 
+    hash,
+    size,
+    stripped_size,
+    weight,
+    number,
+    version,
+    merkle_root,
+    timestamp,
+    timestamp_month,
+    nonce,
+    bits,
+    coinbase_param,
+    transaction_count
+from source_data
