cd9e7bd9a5df5fe13e2d0ba0acc8cd2872726320
Son Do <sondo077@gmail.com>
2025-04-18 11:33:35 -0400

    Added Datasets

--- models/staging/across/fact_across_complete_transfers.sql
+++ models/staging/across/fact_across_complete_transfers.sql
@@ -55,46 +55,86 @@ across_transfers as (
         , 'across' as bridge_message_app
         , '2' as version
     from {{ ref('fact_across_v2_complete_transfers') }}
-)
+),
 
+transfer_with_price as  (
+    SELECT 
+        src_messaging_contract_address
+        , src_block_timestamp
+        , src_tx_hash
+        , src_event_index
+        , src_prices.symbol as src_symbol
+        , src_prices.decimals as src_decimals
+        , src_prices.price as src_token_price
+        , src_amount/POW(10, src_prices.decimals) * src_prices.price as amount_sent
+        , src_amount as amount_sent_native
+        , src_amount/POW(10, src_prices.decimals) as amount_sent_adjusted
+        , src_chain
+        , origin_chain_id
+        , origin_token
+        , dst_messaging_contract_address
+        , dst_block_timestamp
+        , dst_tx_hash
+        , dst_event_index
+        , dst_amount
+        , dst_prices.symbol as dst_symbol
+        , dst_prices.decimals as dst_decimals
+        , dst_prices.price as dst_token_price
+        , dst_amount/POW(10, dst_prices.decimals) * dst_prices.price as amount_received
+        , dst_amount as amount_received_native
+        , dst_amount/POW(10, dst_prices.decimals) as amount_received_adjusted
+        , depositor
+        , recipient
+        , destination_chain_id
+        , destination_token
+        , dst_message
+        , dst_chain
+        , deposit_id
+        , protocol_fee
+        , bridge_message_app
+        , version
+    FROM
+        across_transfers
+    LEFT JOIN prices as src_prices
+        ON lower(origin_token) = lower(src_prices.contract_address) and date_trunc('day', src_block_timestamp) = src_prices.date and src_chain = src_prices.chain
+    LEFT JOIN prices as dst_prices
+        ON lower(destination_token) = lower(dst_prices.contract_address) and date_trunc('day', dst_block_timestamp) = dst_prices.date and dst_chain = dst_prices.chain
+)
 SELECT 
-    src_messaging_contract_address
-    , src_block_timestamp
-    , src_tx_hash
-    , src_event_index
-    , src_prices.symbol as src_symbol
-    , src_prices.decimals as src_decimals
-    , src_prices.price as src_token_price
-    , src_amount/POW(10, src_prices.decimals) * src_prices.price as amount_sent
-    , src_amount as amount_sent_native
-    , src_amount/POW(10, src_prices.decimals) as amount_sent_adjusted
-    , src_chain
+    max(src_messaging_contract_address) as src_messaging_contract_address
+    , max(src_block_timestamp) as src_block_timestamp
+    , max(src_tx_hash) as src_tx_hash
+    , max(src_event_index) as src_event_index
+    , max(src_symbol) as src_symbol
+    , max(src_decimals) as src_decimals
+    , max(src_token_price) as src_token_price
+    , max(amount_sent) as amount_sent
+    , max(amount_sent_native) as amount_sent_native
+    , max(amount_sent_adjusted) as amount_sent_adjusted
+    , max(src_chain) as src_chain
     , origin_chain_id
-    , origin_token
-    , dst_messaging_contract_address
-    , dst_block_timestamp
-    , dst_tx_hash
-    , dst_event_index
-    , dst_amount
-    , dst_prices.symbol as dst_symbol
-    , dst_prices.decimals as dst_decimals
-    , dst_prices.price as dst_token_price
-    , dst_amount/POW(10, dst_prices.decimals) * dst_prices.price as amount_received
-    , dst_amount as amount_received_native
-    , dst_amount/POW(10, dst_prices.decimals) as amount_received_adjusted
-    , depositor
-    , recipient
-    , destination_chain_id
-    , destination_token
-    , dst_message
-    , dst_chain
+    , max(origin_token) as origin_token
+    , max(dst_messaging_contract_address) as dst_messaging_contract_address
+    , max(dst_block_timestamp) as dst_block_timestamp
+    , max(dst_tx_hash) as dst_tx_hash
+    , max(dst_event_index) as dst_event_index
+    , max(dst_amount) as dst_amount
+    , max(dst_symbol) as dst_symbol
+    , max(dst_decimals) as dst_decimals
+    , max(dst_token_price) as dst_token_price
+    , max(amount_received) as amount_received
+    , max(amount_received_native) as amount_received_native
+    , max(amount_received_adjusted) as amount_received_adjusted
+    , max(depositor) as depositor
+    , max(recipient) as recipient
+    , max(destination_chain_id) as destination_chain_id
+    , max(destination_token) as destination_token
+    , max(dst_message) as dst_message
+    , max(dst_chain) as dst_chain
     , deposit_id
-    , protocol_fee
-    , bridge_message_app
-    , version
+    , max(protocol_fee) as protocol_fee
+    , max(bridge_message_app) as bridge_message_app
+    , max(version) as version
 FROM
-    across_transfers
-LEFT JOIN prices as src_prices
-    ON lower(origin_token) = lower(src_prices.contract_address) and date_trunc('day', src_block_timestamp) = src_prices.date
-LEFT JOIN prices as dst_prices
-    ON lower(destination_token) = lower(dst_prices.contract_address) and date_trunc('day', dst_block_timestamp) = dst_prices.date
+    transfer_with_price
+GROUP BY deposit_id, origin_chain_id
