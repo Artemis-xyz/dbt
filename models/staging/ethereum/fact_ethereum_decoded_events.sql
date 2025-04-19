8ed41b8ee32d589893e1f62dc2d1884c840c568a
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2025-04-19 16:30:00 -0400

    Update incremental key for decoding pipeline (#1174)

--- models/staging/ethereum/fact_ethereum_decoded_events.sql
+++ models/staging/ethereum/fact_ethereum_decoded_events.sql
@@ -1,3 +1,3 @@
-{{ config(snowflake_warehouse="ETHEREUM_LG", materialized="incremental") }}
+{{ config(snowflake_warehouse="ETHEREUM_LG", materialized="incremental", unique_key=["transaction_hash", "event_index"]) }}
 
 {{ decode_artemis_events('ethereum') }}
