914668b1a5aa738ba8ab93e0540a2e742e6a6ed8
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-04-24 14:04:30 -0400

    Added a few standardized metrics and metrics to match the sheet

--- models/projects/ethena/core/ez_ethena_metrics.sql
+++ models/projects/ethena/core/ez_ethena_metrics.sql
@@ -38,8 +38,14 @@ select
     usde_metrics.stablecoin_dau as stablecoin_dau,
     usde_metrics.stablecoin_txns as stablecoin_txns,
     coalesce(ena_metrics.fees, 0) as fees,
-    coalesce(ena_metrics.fees, 0) as gross_protocol_revenue,
+    coalesce(ena_metrics.fees, 0) as gross_protocol_revenue,--20% of fees supports Ethena's reserve fund
+    0.2 * coalesce(ena_metrics.fees, 0) as foundation_cash_flow, --20% of fees supports Ethena's reserve fund
+    0.8 * coalesce(ena_metrics.fees, 0) as service_cash_flow, --80% to sUSDe holders--80% of fees supports Ethena's ecosystem fund
+    coalesce(ena_metrics.fees, 0) * 0.2 as susde_fees, 
+    0 as ena_fees, 
     tvl.stablecoin_total_supply as tvl,
+    tvl.stablecoin_total_supply as usde_supply,
+    tvl.stablecoin_total_supply - lag(tvl.stablecoin_total_supply) over (order by date) as net_usde_supply_change,
     {{ daily_pct_change('tvl.stablecoin_total_supply') }} as tvl_growth, 
     supply_data.circulating_supply_native as circulating_supply_native,
     supply_data.circulating_supply_native - lag(supply_data.circulating_supply_native) over (order by date) as net_supply_change_native,
