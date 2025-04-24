9fed6e12c8289cfaa991bed2ab207540628762b3
Akhil Vajjhala <119370506+0xbeamish@users.noreply.github.com>
2025-04-24 04:21:20 -0400

    Akhil/blur supply data (#1166)

--- models/projects/blur/core/ez_blur_metrics.sql
+++ models/projects/blur/core/ez_blur_metrics.sql
@@ -21,15 +21,19 @@ with
         select *
         from {{ ref("fact_blur_daily_txns") }}
     )
+    , blur_daily_supply as (
+        select *
+        from {{ ref("fact_blur_daily_supply") }}
+    )
     , market_data as (
         {{ get_coingecko_metrics("blur") }}
     )
 
 select
     blur_daus.date
-    , blur_daus.dau
-    , blur_daily_txns.daily_txns as txns
-    , blur_fees.fees
+    , coalesce(blur_daus.dau, 0) as dau
+    , coalesce(blur_daily_txns.daily_txns, 0) as txns
+    , coalesce(blur_fees.fees, 0) as fees
 
     -- Token Metrics
     , coalesce(market_data.price, 0) as price
@@ -38,13 +42,18 @@ select
     , coalesce(market_data.token_volume, 0) as token_volume 
 
     -- NFT Metrics
-    , blur_daus.dau as nft_dau
-    , blur_daily_txns.daily_txns as nft_txns
-    , blur_fees.fees as nft_fees
+    , coalesce(blur_daus.dau, 0) as nft_dau
+    , coalesce(blur_daily_txns.daily_txns, 0) as nft_txns
+    , coalesce(blur_fees.fees, 0) as nft_fees
 
     -- Cash Flow Metrics
-    , blur_fees.fees as gross_protocol_revenue
-    , blur_fees.fees as service_cash_flow
+    , coalesce(blur_fees.fees, 0) as gross_protocol_revenue
+    , coalesce(blur_fees.fees, 0) as service_cash_flow
+
+    -- Supply Metrics
+    , coalesce(blur_daily_supply.premine_unlocks_native, 0) as premine_unlocks_native
+    , coalesce(blur_daily_supply.circulating_supply_native, 0) as circulating_supply_native
+    , coalesce(blur_daily_supply.net_supply_change_native, 0) as net_supply_change_native
 
     -- Turnover Metrics
     , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
@@ -53,5 +62,6 @@ select
 from blur_daus
 left join blur_daily_txns using (date)
 left join blur_fees using (date)
+left join blur_daily_supply using (date)
 left join market_data using (date)
 order by date desc
