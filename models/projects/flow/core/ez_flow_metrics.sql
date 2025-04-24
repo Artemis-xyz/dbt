9e9843b21b367df76f28730efe1421fe52bc9925
Ben Stamm <benstamm11@gmail.com>
2025-04-23 20:15:21 -0400

    April Supply Data Refresh - Flow:wq

--- models/projects/flow/core/ez_flow_metrics.sql
+++ models/projects/flow/core/ez_flow_metrics.sql
@@ -9,52 +9,95 @@
     )
 }}
 
-with
-    fees_revenue_data as (
-        select date, total_fees_usd as fees, fees_burned_usd as revenue
-        from {{ ref("fact_flow_fees_revs") }}
-    ),
-    dau_txn_data as (
-        select date, chain, daa as dau, txns from {{ ref("fact_flow_daa_txns") }}
-    ),
-    price_data as ({{ get_coingecko_metrics("flow") }}),
-    defillama_data as ({{ get_defillama_metrics("flow") }}),
-    github_data as ({{ get_github_metrics("flow") }}),
-    nft_metrics as ({{ get_nft_metrics("flow") }})
+with fees_revenue_data as (
+    select 
+        date, 
+        total_fees_usd as fees, 
+        fees_burned_usd as revenue
+    from {{ ref("fact_flow_fees_revs") }}
+)
+,    dau_txn_data as (
+    select 
+        date, 
+        chain, 
+        daa as dau, 
+        txns 
+    from {{ ref("fact_flow_daa_txns") }}
+)
+, daily_supply_data as (
+    select
+        date,
+        emissions_native,
+        premine_unlocks_native,
+        burns_native,
+        net_supply_change_native,
+        circulating_supply
+    from {{ ref('fact_flow_daily_supply_data') }}
+)
+, date_spine as (
+    select
+        ds.date
+    from {{ ref('dim_date_spine') }} ds
+    where ds.date between (select min(date) from dau_txn_data) and to_date(sysdate())
+)
+,    market_metrics as ({{ get_coingecko_metrics("flow") }})
+,    defillama_data as ({{ get_defillama_metrics("flow") }})
+,    github_data as ({{ get_github_metrics("flow") }})
+,    nft_metrics as ({{ get_nft_metrics("flow") }})
+
 select
-    dau_txn_data.date
-    , txns
-    , dau
-    , fees
-    , fees / txns as avg_txn_fee
-    , revenue
+    date_spine.date
+
+    --Old metrics needed for compatibility
+    , dau_txn_data.txns
+    , dau_txn_data.dau
+    , fees_revenue_data.fees
+    , fees_revenue_data.fees / dau_txn_data.txns as avg_txn_fee
+    , fees_revenue_data.revenue
     , dau_txn_data.chain
-    , dex_volumes
+    , defillama_data.dex_volumes as dex_volumes
+    , defillama_data.tvl as tvl
+
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price
-    , market_cap
-    , fdmc
-    , tvl
-    -- Chain Usage Metrics
-    , txns AS chain_txns
-    , dau AS chain_dau
-    , fees / txns AS chain_avg_txn_fee
-    , dex_volumes AS chain_spot_volume
-    , nft_trading_volume
+
+    -- Market Metrics
+    , market_metrics.price
+    , market_metrics.market_cap
+    , market_metrics.fdmc
+    , market_metrics.tvl
+
+    -- Usage Metrics
+    , dau_txn_data.txns as chain_txns
+    , dau_txn_data.dau as chain_dau
+    , fees_revenue_data.fees / dau_txn_data.txns as chain_avg_txn_fee
+    , defillama_data.dex_volumes as chain_spot_volume
+    , defillama_data.tvl as chain_tvl
+    , nft_metrics.nft_trading_volume as chain_nft_trading_volume
+
     -- Cashflow metrics
-    , fees as chain_fees
-    , fees AS gross_protocol_revenue
-    , revenue AS burned_cash_flow
+    , fees_revenue_data.fees AS chain_fees
+    , fees_revenue_data.fees AS gross_protocol_revenue
+    , fees_revenue_data.revenue AS burned_cash_flow
+
     -- Developer Metrics
-    , weekly_commits_core_ecosystem
-    , weekly_commits_sub_ecosystem
-    , weekly_developers_core_ecosystem
-    , weekly_developers_sub_ecosystem
-from dau_txn_data
-left join fees_revenue_data on dau_txn_data.date = fees_revenue_data.date
-left join price_data on dau_txn_data.date = price_data.date
-left join defillama_data on dau_txn_data.date = defillama_data.date
-left join github_data on dau_txn_data.date = github_data.date
-left join nft_metrics on dau_txn_data.date = nft_metrics.date
-where dau_txn_data.date < to_date(sysdate())
+    , github_data.weekly_commits_core_ecosystem
+    , github_data.weekly_commits_sub_ecosystem
+    , github_data.weekly_developers_core_ecosystem
+    , github_data.weekly_developers_sub_ecosystem
+
+    --FLOW Token Supply Data
+    , daily_supply_data.emissions_native
+    , daily_supply_data.premine_unlocks_native
+    , daily_supply_data.burns_native
+    , daily_supply_data.net_supply_change_native
+    , daily_supply_data.circulating_supply
+
+from date_spine
+left join dau_txn_data on date_spine.date = dau_txn_data.date
+left join fees_revenue_data on date_spine.date = fees_revenue_data.date
+left join market_metrics on date_spine.date = market_metrics.date
+left join daily_supply_data on date_spine.date = daily_supply_data.date
+left join defillama_data on date_spine.date = defillama_data.date
+left join github_data on date_spine.date = github_data.date
+left join nft_metrics on date_spine.date = nft_metrics.date
+where date_spine.date < to_date(sysdate())
