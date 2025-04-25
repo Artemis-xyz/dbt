9313ea9887f04376dcbe0259545039ab768c568b
limzhengjie <86870373+limzhengjie@users.noreply.github.com>
2025-04-26 00:34:53 +0800

    Hyperliquid data fix (#1235)

--- models/projects/hyperliquid/core/ez_hyperliquid_metrics.sql
+++ models/projects/hyperliquid/core/ez_hyperliquid_metrics.sql
@@ -9,63 +9,59 @@
 }}
 
 with trading_volume_data as (
-    select 
-        date, 
-        chain,
-        trading_volume
+    select date, trading_volume as perp_volume, chain
     from {{ ref("fact_hyperliquid_trading_volume") }}
 )
 , unique_traders_data as (
-    select 
-        date, 
-        chain,
-        unique_traders
+    select date, unique_traders, chain
     from {{ ref("fact_hyperliquid_unique_traders") }}
 )
 , daily_transactions_data as (
-    select 
-        date, 
-        chain,
-        trades
+    select date, trades, chain
     from {{ ref("fact_hyperliquid_daily_transactions") }}
 )
 , fees_data as (
-    select 
-        date, 
-        chain, 
-        trading_fees, 
-        spot_fees, 
-        perp_fees
+    select date, chain, trading_fees, spot_fees, perp_fees
     from {{ ref("fact_hyperliquid_fees") }}
 )
 , auction_fees_data as (
-    select 
-        date, 
-        chain, 
-        sum(auction_fees) as auction_fees
+    select date, chain, sum(auction_fees) as auction_fees
     from {{ ref("fact_hyperliquid_auction_fees") }}
     group by 1, 2
 )
 , daily_burn_data as (
-    select 
-        date, 
-        chain, 
-        daily_burn
+    select date, daily_burn, chain
     from {{ ref("fact_hyperliquid_daily_burn") }}
 )
+, daily_assistance_fund_data as (
+    select date, daily_balance as daily_buybacks_native, balance as assistance_fund_balance, chain
+    from {{ ref("fact_hyperliquid_assistance_fund_balance") }}
+)
+, hype_staked_data as (
+    -- snapshot data only starts as early as 2024-01-06
+    select date, chain, staked_hype, num_stakers
+    from {{ ref("fact_hyperliquid_hype_staked") }}
+)
+, spot_trading_volume_data as (
+    select date, spot_trading_volume, chain
+    from {{ ref("fact_hyperliquid_spot_trading_volume") }}
+)
 , daily_supply_data as (
-    select
-        date,
-        emissions_native,
-        premine_unlocks_native,
-    from {{ref('fact_hyperliquid_daily_supply_data')}}
-)
-, date_spine as (
-    select * from {{ ref('dim_date_spine') }}
-    where date between '2022-12-20' and to_date(sysdate())
-)
+     select
+         date,
+         emissions_native,
+         premine_unlocks_native,
+     from {{ref('fact_hyperliquid_daily_supply_data')}}
+ )
 , market_metrics as (
     ({{ get_coingecko_metrics("hyperliquid") }}) 
+)
+, date_spine as (
+    SELECT
+        date,
+        'hyperliquid' as chain
+    FROM {{ref("dim_date_spine")}}
+    WHERE date between '2023-06-13' and to_date(sysdate())
 )
     
 select
@@ -74,42 +70,45 @@ select
     , 'DeFi' as category
 
     --Old metrics needed for compatibility
-    , trading_volume
+    , perp_volume + spot_trading_volume as trading_volume
     , unique_traders::string as unique_traders
     , trades as txns
     , trading_fees as fees
     , auction_fees
     , daily_burn
-    , COALESCE(trading_fees * 0.46, 0) as primary_supply_side_revenue -- protocol’s revenue split between HLP (supplier) and AF (holder) at a ratio of 46%:54%
-    , COALESCE(trading_fees * 0.54, 0) + COALESCE(daily_burn, 0) * market_metrics.price as revenue -- add daily burn back to the revenue
+    , trading_fees * 0.03 as primary_supply_side_revenue
+    -- add daily burn back to the revenue
+     , (daily_buybacks_native * mm.price) + (daily_burn * mm.price) as revenue
+     , daily_buybacks_native
+     , num_stakers
+     , staked_hype
 
     -- Standardized Metrics
 
     -- Market Metrics
-    , market_metrics.price as price
-    , market_metrics.token_volume as token_volume
-    , market_metrics.market_cap as market_cap
-    , market_metrics.fdmc as fdmc
+    , price
+    , token_volume
+    , market_cap
+    , fdmc
+    , token_turnover_circulating
+    , token_turnover_fdv
 
     -- Usage Metrics
-    , unique_traders_data.unique_traders::string as perp_dau
-    , trading_volume_data.trading_volume as perp_volume
-    , daily_transactions_data.trades as perp_txns
+    , unique_traders::string as perp_dau
+    , perp_volume as perp_volume
+    , trades as perp_txns
 
     -- Cash Flow Metrics
-    , fees_data.perp_fees
-    , fees_data.spot_fees
-    , fees_data.perp_fees + fees_data.spot_fees as trading_fees
-    , daily_burn_data.daily_burn * market_metrics.price as chain_fees
-    , trading_fees + chain_fees as gross_protocol_revenue
-    , trading_fees * 0.46 as service_cash_flow
-    , trading_fees * 0.54 as buybacks_cash_flow
-    , daily_burn_data.daily_burn as burned_cash_flow_native
-    , chain_fees as burned_cash_flow
-
-    -- Token Turnover Metrics
-    , market_metrics.token_turnover_circulating as token_turnover_circulating
-    , market_metrics.token_turnover_fdv as token_turnover_fdv
+    , perp_fees
+    , spot_fees
+    -- all l1 fees are burned
+     , daily_burn * mm.price as chain_fees
+     , trading_fees + (daily_burn * mm.price) as gross_protocol_revenue
+     , trading_fees * 0.03 as service_cash_flow
+     , (daily_buybacks_native * mm.price) as buyback_cash_flow
+     , daily_buybacks_native as buybacks_native
+     , daily_burn as burned_cash_flow_native
+     , daily_burn * mm.price as burned_cash_flow
 
     --HYPE Token Supply Data
     , coalesce(daily_supply_data.emissions_native, 0) as emissions_native
@@ -119,7 +118,7 @@ select
     , sum(coalesce(daily_supply_data.emissions_native, 0) + coalesce(daily_supply_data.premine_unlocks_native, 0) - coalesce(daily_burn_data.daily_burn, 0)) over (order by daily_supply_data.date) as circulating_supply_native
 
 from date_spine
-left join market_metrics using(date)
+left join market_metrics mm using(date)
 left join unique_traders_data using(date)
 left join trading_volume_data using(date)
 left join daily_transactions_data using(date)
@@ -127,4 +126,7 @@ left join fees_data using(date)
 left join daily_burn_data using(date)
 left join daily_supply_data using(date)
 left join auction_fees_data using(date)
+left join hype_staked_data using(date)
+left join spot_trading_volume_data using(date)
+left join daily_assistance_fund_data using(date)
 where date < to_date(sysdate())
