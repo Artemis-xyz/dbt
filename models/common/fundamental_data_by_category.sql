83ef63da00b310db7d89aa9328cfac23f1c082ca
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-04-25 11:23:36 -0700

    Add Views Atop BAM Data to Common Schema (#1236)

---
+++ models/common/fundamental_data_by_category.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        snowflake_warehouse="COMMON",
+        database="common",
+        schema="core",
+        materialized='view'
+    )
+}}
+
+SELECT 
+    *
+FROM {{ ref("all_chains_gas_dau_txns_by_category_v2") }}
