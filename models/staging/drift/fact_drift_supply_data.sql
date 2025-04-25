c52716102ea8bfed8e6685d75e41e84a61f46f4a
Alex Weseley <pampawes@gmail.com>
2025-04-25 02:46:03 -0400

    fix supply

--- models/staging/drift/fact_drift_supply_data.sql
+++ models/staging/drift/fact_drift_supply_data.sql
@@ -9,6 +9,6 @@ SELECT
     date,
     ecosystem_trading_rewards as gross_emissions,
     airdrop + protocol_development + strategic_partnerships as premine_unlocks,
-    gross_emissions + premine_unlocks as net_supply_change,
+    airdrop + protocol_development + strategic_partnerships + ecosystem_trading_rewards as net_supply_change,
     sum(net_supply_change) over (order by date) as circulating_supply
 FROM {{ source('MANUAL_STATIC_TABLES', 'drift_daily_supply_data')}}
