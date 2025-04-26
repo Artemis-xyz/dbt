7e6b13abad49309b85d9dec25e3581f618beada7
limzhengjie <limzhengjie01@gmail.com>
2025-04-26 14:45:36 -0400

    Add new asset - Safe and outerlands refresh

---
+++ models/projects/safe/ez_safe_metrics.sql
@@ -0,0 +1,62 @@
+{{
+    config(
+        materialized="table",
+        database = 'SAFE',
+        schema = 'core',
+        snowflake_warehouse = 'SAFE',
+        alias = 'ez_metrics'
+    )
+}}
+
+with txns as (
+    select
+        date
+        , txns
+    from {{ ref("fact_safe_txns") }}
+)
+, safes_created as (
+    select
+        date
+        , safes_created
+    from {{ ref("fact_safe_daily_safes_created") }}
+)
+, tvl as (
+    select
+        date
+        , sum(tvl) as tvl
+    from {{ ref("fact_safe_tvl_by_chain") }}
+    group by date
+)
+, supply_data as (
+    select
+        date
+        , gross_emissions_native
+        , premine_unlocks_native
+        , burns_native
+        , net_supply_change_native
+        , circulating_supply_native
+    from {{ ref("fact_safe_supply_data") }}
+)
+
+, market_data as (
+    {{ get_coingecko_metrics('safe') }}
+)
+
+select
+    txns.date
+    , txns
+    , safes_created
+    , tvl
+
+    -- Standardized Metrics
+    , price
+    , market_cap
+    , fdmc
+    , token_volume
+    , token_turnover_circulating
+    , token_turnover_fdv
+    
+from txns
+left join safes_created using (date)
+left join tvl using (date)
+left join market_data using (date)
