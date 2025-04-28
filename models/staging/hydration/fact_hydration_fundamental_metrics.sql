45bffc89f8e2b7231fb66a1e598e7be27663a417
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-04-28 11:24:31 -0400

    Fixed Centrifuge and hydration (#1252)

--- models/staging/hydration/fact_hydration_fundamental_metrics.sql
+++ models/staging/hydration/fact_hydration_fundamental_metrics.sql
@@ -30,8 +30,8 @@ select
     coalesce(t.date, e.date) as date
     , coalesce(t.txns,0) + coalesce(e.txns,0) as txns
     , coalesce(t.daa,0) + coalesce(e.daa, 0) as daa
-    , coalesce(t.fees_native,0) as fees_native
-    , coalesce(t.fees_usd,0) as fees_usd
+    , coalesce(t.fees_native,0) + coalesce(e.fees_native, 0) as fees_native
+    , coalesce(t.fees_usd,0) + coalesce(e.fees, 0) as fees_usd
 from parity_transactions as t
 full join evm_transactions as e on t.date = e.date
     and coalesce(t.date, e.date) < to_date(sysdate())
