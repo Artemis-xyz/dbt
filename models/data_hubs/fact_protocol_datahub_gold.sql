ae99f20ad19745991ec80cdf5e7c4b1f4e803b42
Alex Weseley <pampawes@gmail.com>
2025-04-30 20:01:55 -0400

    initial datahub commit

---
+++ models/data_hubs/fact_protocol_datahub_gold.sql
@@ -0,0 +1,13 @@
+{{ config(materialized="view") }}
+
+SELECT 
+    date,
+    dh.artemis_id,
+    cat."VALUES" as sectors,
+    dau::number as dau,
+    txns::number as txns,
+    gross_protocol_revenue::number as gross_protocol_revenue,
+    tvl::number as tvl
+FROM {{ source('PC_DBT_DB', 'fact_datahub_silver') }} dh
+LEFT JOIN {{ source('POSTGRES_REPLICATED', 'core_asset') }} ca ON ca.artemis_id = dh.artemis_id 
+LEFT JOIN {{ source('POSTGRES_REPLICATED', 'core_assettag') }} cat on cat.asset_id = ca.id AND cat."KEY" = 'groups'
