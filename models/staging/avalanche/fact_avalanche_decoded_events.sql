c71a1f99914b626a1e0cfe18c1c40ea15f0cca51
alexwes <pampawes@gmail.com>
2025-05-01 18:04:53 -0400

    Wes/euler (#1286)

--- models/staging/avalanche/fact_avalanche_decoded_events.sql
+++ models/staging/avalanche/fact_avalanche_decoded_events.sql
@@ -1,3 +1,3 @@
 {{ config(snowflake_warehouse="AVALANCHE_LG", materialized="incremental", unique_key=["transaction_hash", "event_index"]) }}
 
-{{ decode_artemis_events('arbitrum') }}
+{{ decode_artemis_events('avalanche') }}
