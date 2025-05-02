cf0b7cfa03121b600c02212a4c67cec3e2b108e2
horaceliuHL <horaceliu.hl@gmail.com>
2025-05-02 10:08:46 -0400

    Preliminary Sui changes

--- models/projects/sui/core/ez_sui_metrics_by_contract_v2.sql
+++ models/projects/sui/core/ez_sui_metrics_by_contract_v2.sql
@@ -7,7 +7,36 @@
         alias="ez_metrics_by_contract_v2",
     )
 }}
-SELECT
-    * EXCLUDE date,
-    TO_TIMESTAMP_NTZ(date) AS date
-FROM {{ source('PROD_LANDING', 'ez_sui_metrics_by_contract_v2') }}
+-- Based on the `get_fundamental_data_for_chain_by_contractn` macro in the primary
+-- repo
+with
+    contract_data as (
+        select
+            package as contract_address,
+            raw_date as date,
+            max(name) name,
+            max(chain) as chain,
+            max(app) as app,
+            max(friendly_name) as friendly_name,
+            ROUND(sum(tx_fee), 3) gas,
+            sum(gas_usd) gas_usd,
+            count(*) txns,
+            count(distinct from_address) dau,
+            max(category) category
+        from {{ source("PROD_LANDING", "fact_sui_transactions_silver_bigquery_v2") }}
+        group by date, contract_address
+    )
+select
+    TO_TIMESTAMP_NTZ(contract_data.date) as date,
+    contract_data.contract_address,
+    chain,
+    name,
+    app,
+    friendly_name,
+    category,
+    gas,
+    gas_usd,
+    txns,
+    dau
+from contract_data
+where contract_data.date < current_date()
--- models/projects/sui/core/ez_sui_metrics_by_subcategory.sql
+++ models/projects/sui/core/ez_sui_metrics_by_subcategory.sql
@@ -7,7 +7,51 @@
         alias="ez_metrics_by_subcategory",
     )
 }}
-SELECT
-    * EXCLUDE date,
-    TO_TIMESTAMP_NTZ(date) AS date
-FROM {{ source('PROD_LANDING', 'ez_sui_metrics_by_subcategory') }}
+-- Based on the `get_fundamental_data_for_chain_by_category` macro in the primary
+-- repo
+with
+    min_date as (
+        select min(raw_date) as start_date, from_address, category, sub_category
+        from {{ source("PROD_LANDING", "fact_sui_transactions_silver_bigquery_v2") }}
+        group by sub_category, category, from_address
+    ),
+    new_users as (
+        select count(distinct from_address) as new_users, category, sub_category, start_date
+        from min_date
+        group by start_date, category, sub_category
+    ),
+    agg_data as (
+        select
+            raw_date,
+            category,
+            sub_category,
+            max(chain) as chain,
+            sum(tx_fee) gas,
+            sum(gas_usd) gas_usd,
+            count(*) txns,
+            count(distinct from_address) dau
+        from {{ source("PROD_LANDING", "fact_sui_transactions_silver_bigquery_v2") }}
+        group by raw_date, category, sub_category
+    )
+select
+    TO_TIMESTAMP_NTZ(agg_data.raw_date) as date,
+    if(agg_data.category is null, 'Unlabeled', cast(agg_data.category as string)) as category,
+    if(agg_data.sub_category is null, 'Unlabeled', cast(agg_data.sub_category as string)) as sub_category,
+    chain,
+    ROUND(gas, 3) AS gas,
+    gas_usd,
+    txns,
+    dau,
+    (dau - new_users.new_users) as returning_users,
+    new_users.new_users,
+    null as low_sleep_users,
+    null as high_sleep_users,
+    null as sybil_users,
+    null as non_sybil_users
+from agg_data
+left join
+    new_users
+    on cast(agg_data.category as string) = cast(new_users.category as string)
+    and cast(agg_data.sub_category as string) = cast(new_users.sub_category as string)
+    and agg_data.raw_date = new_users.start_date
+where agg_data.raw_date < current_date()
