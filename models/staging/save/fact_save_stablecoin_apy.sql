668d6bd5b5e4434a547efc071353117a05587d90
Irwyn Liong <106728017+irwynliong@users.noreply.github.com>
2025-05-05 19:16:18 -0400

    Adding save apy (#1304)

---
+++ models/staging/save/fact_save_stablecoin_apy.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized="table"
+) }}
+
+with latest as (
+  select
+    id,
+    max(extraction_timestamp) as max_timestamp
+  from {{ ref("fact_save_apy") }}
+  group by id
+)
+
+select
+    f.extraction_timestamp as timestamp,
+    f.id,
+    f.name,
+    f.apy,
+    f.tvl,
+    f.symbol,
+    f.protocol,
+    f.type,
+    p.link
+from {{ ref("fact_save_apy") }} f
+join latest l
+on f.id = l.id
+    and f.extraction_timestamp = l.max_timestamp
+join {{ ref("save_stablecoin_lending_ids") }} p
+on f.id = p.id
