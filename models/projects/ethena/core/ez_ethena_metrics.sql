d2eab8d380314b4c509642a304e7c2d0f7372fc2
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-05 08:47:49 -0400

    Added another ena_cashflow for the cashflow to separate from ena_metrics

--- models/projects/ethena/core/ez_ethena_metrics.sql
+++ models/projects/ethena/core/ez_ethena_metrics.sql
@@ -18,12 +18,17 @@ with usde_metrics as (
 , ena_metrics as (
     SELECT
         date,
-        yield_fees.service_cash_flow,
-        yield_fees.foundation_cash_flow, 
         sum(coalesce(collateral_fees.collateral_fee, 0) + coalesce(yield_fees.fees, 0)) as fees
     FROM  {{ ref('fact_ethena_yield_fees') }} yield_fees
     left join  {{ ref('fact_ethena_collateral_fees') }} collateral_fees using(date)
-    group by 1, 2, 3
+    group by 1
+)
+, ena_cashflow as (
+    SELECT
+        date,
+        service_cash_flow,
+        foundation_cash_flow
+    FROM {{ ref('fact_ethena_yield_fees') }}
 )
 , tvl as (
     SELECT
@@ -41,9 +46,9 @@ select
     usde_metrics.stablecoin_txns as stablecoin_txns,
     coalesce(ena_metrics.fees, 0) as fees,
     coalesce(ena_metrics.fees, 0) as gross_protocol_revenue,
-    coalesce(ena_metrics.foundation_cash_flow, 0) as foundation_cash_flow, --20% of fees supports Ethena's reserve fund
-    coalesce(ena_metrics.service_cash_flow, 0) as service_cash_flow, --80% of fees supports Ethena's ecosystem fund
-    coalesce(ena_metrics.service_cash_flow, 0) as susde_fees, 
+    coalesce(ena_cashflow.foundation_cash_flow, 0) as foundation_cash_flow, --20% of fees supports Ethena's reserve fund
+    coalesce(ena_cashflow.service_cash_flow, 0) as service_cash_flow, --80% of fees supports Ethena's ecosystem fund
+    coalesce(ena_cashflow.service_cash_flow, 0) as susde_fees, 
     tvl.stablecoin_total_supply as tvl,
     tvl.stablecoin_total_supply as usde_supply,
     tvl.stablecoin_total_supply - lag(tvl.stablecoin_total_supply) over (order by date) as net_usde_supply_change,
@@ -52,6 +57,7 @@ select
     supply_data.circulating_supply_native - lag(supply_data.circulating_supply_native) over (order by date) as net_supply_change_native,
 from usde_metrics
 left join ena_metrics using(date)
+left join ena_cashflow using(date)
 left join tvl using(date)
 left join supply_data using(date)
 where usde_metrics.date < to_date(sysdate())
