da7ee31f0c95d5e7cd3792610945286b528f8d4a
Irwyn <irwynliong01@gmail.com>
2025-05-06 19:05:08 -0400

    simplify the logic

--- models/staging/federal_funds/fact_fedfunds_rates.sql
+++ models/staging/federal_funds/fact_fedfunds_rates.sql
@@ -2,40 +2,25 @@
     materialized="table"
 ) }}
 
-with base as (
-  select
-    parse_json(source_json) as json,
-    extraction_date
-  from {{ source("PROD_LANDING", "raw_fedfunds") }}
-),
-
-flattened as (
+with flattened as (
   select
     value:"date"::string as date,
     value:"value"::string as value,
     base.extraction_date
-  from base,
-    lateral flatten(input => base.json:observations)
-),
-
-latest_date as (
-    select
-        max(date) as max_date,
-        max(extraction_date) as max_extraction_date
-    from flattened
+  from {{ source("PROD_LANDING", "raw_fedfunds") }} as base,
+    lateral flatten(input => parse_json(base.source_json):observations) as value
 )
 
 select
-    date as timestamp,
-    extraction_date,
-    'Fed Funds' as name,
-    value / 100 as apy,
-    null as tvl,
-    [] as symbol,
-    'fedfunds' as protocol,
-    'Fed Fund Rates' as type,
-    null as link
+  date as timestamp,
+  extraction_date,
+  'Fed Funds' as name,
+  value / 100 as apy,
+  null as tvl,
+  [] as symbol,
+  'fedfunds' as protocol,
+  'Fed Fund Rates' as type,
+  null as link
 from flattened
-join latest_date
-on flattened.date = latest_date.max_date
-    and flattened.extraction_date = latest_date.max_extraction_date
+where extraction_date = (select max(extraction_date) from flattened)
+  and date = (select max(date) from flattened where extraction_date = (select max(extraction_date) from flattened))
