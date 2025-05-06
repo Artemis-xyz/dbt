e356778043ff5a8ab00e4f7479f05b51f549cd3d
Anish Toomu <anishtoomu@gmail.com>
2025-05-06 16:28:03 -0400

    added daa fact and correct transactions

--- models/staging/cardano/fact_cardano_daa.sql
+++ models/staging/cardano/fact_cardano_daa.sql
@@ -1,16 +1,64 @@
-{{ config(materialized="view") }}
-WITH cardano_data AS (
-    SELECT
-        parse_json(source_json) AS data
-    FROM
-        {{ source("PROD_LANDING", "raw_cardano_daa") }}
-)
-SELECT
-    date(value[0]) as date,
-    F.value[1]::INT AS daa,
-    F.value AS source,
-    'cardano' AS chain
-FROM
-    cardano_data,
-    LATERAL FLATTEN(input => data:data:values) AS F
-ORDER BY DATE DESC
+{{ config(
+    materialized='table',
+    snowflake_warehouse='CARDANO'
+) }}
+
+with 
+    -- Get transaction timestamps
+    tx_times as (
+        select
+            tx_hash,
+            block_time
+        from {{ ref('fact_cardano_transactions') }}
+    ),
+    -- Get input addresses from tx_in_out
+    input_addresses as (
+        select
+            date_trunc('day', t.block_time) as date,
+            f.value:address::string as address
+        from {{ ref('fact_cardano_tx_in_out') }} txio
+        inner join tx_times t 
+            on txio.epoch_no = t.epoch_no 
+            and txio.slot_no = t.slot_no 
+            and txio.txidx = t.txidx,
+        lateral flatten(input => txio.inputs) f
+        where f.value:address is not null
+    ),
+    -- Get output addresses from tx_in_out
+    output_addresses as (
+        select
+            date_trunc('day', t.block_time) as date,
+            f.value:address::string as address
+        from {{ ref('fact_cardano_tx_in_out') }} txio
+        inner join tx_times t 
+            on txio.epoch_no = t.epoch_no 
+            and txio.slot_no = t.slot_no 
+            and txio.txidx = t.txidx,
+        lateral flatten(input => txio.outputs) f
+        where f.value:address is not null
+    ),
+    -- Combine unique addresses for each day
+    unique_daily_addresses as (
+        select date, address
+        from input_addresses
+        union distinct
+        select date, address
+        from output_addresses
+    ),
+    -- Calculate daily active addresses (DAA)
+    daily_active_addresses as (
+        select
+            date,
+            count(distinct address) as daa,
+            'cardano' as chain
+        from unique_daily_addresses
+        group by 1
+    )
+
+select 
+    date,
+    daa,
+    chain
+from daily_active_addresses
+where date < current_date()
+order by date desc
