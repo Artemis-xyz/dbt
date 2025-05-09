af8cbb65ab3542185858254cc30c9e604b20ea55
Son Do <34179343+Sunny77D@users.noreply.github.com>
2025-05-09 11:00:02 -0400

    Fix dml and unique test (#1329)

--- macros/coingecko/get_coingecko_price_with_latest.sql
+++ macros/coingecko/get_coingecko_price_with_latest.sql
@@ -64,37 +64,49 @@
 {% endmacro %}
 
 {% macro get_coingecko_prices_on_chains(chains) %}
-    select date as date, chain, contract_address, decimals, symbol, shifted_token_price_usd as price
-    from {{ ref("fact_coingecko_token_date_adjusted_gold") }}
-    inner join {{ ref('dim_coingecko_token_map')}}
-        on coingecko_id = coingecko_token_id
-    where
-        chain in  (
-            {% for chain in chains %}
-                '{{chain}}' {% if not loop.last %},{% endif %}
-            {% endfor %}
-        )
-        and date < dateadd(day, -1, to_date(sysdate()))
-    union
-    select dateadd('day', -1, to_date(sysdate())) as date, chain, contract_address, decimals, symbol, token_current_price as price
-    from {{ ref("fact_coingecko_token_realtime_data") }}
-    inner join {{ ref('dim_coingecko_token_map')}}
-        on token_id = coingecko_token_id
-    where
-        chain in  (
-            {% for chain in chains %}
-                '{{chain}}' {% if not loop.last %},{% endif %}
-            {% endfor %}
-        )
-    union
-    select to_date(sysdate()) as date, chain, contract_address, decimals, symbol, token_current_price as price
-    from {{ ref("fact_coingecko_token_realtime_data") }}
-    inner join {{ ref('dim_coingecko_token_map')}}
-        on token_id = coingecko_token_id
-    where
-        chain in  (
-            {% for chain in chains %}
-                '{{chain}}' {% if not loop.last %},{% endif %}
-            {% endfor %}
-        )
+    with 
+        prices_by_chain as (
+            select date as date, chain, contract_address, decimals, symbol, shifted_token_price_usd as price
+            from {{ ref("fact_coingecko_token_date_adjusted_gold") }}
+            inner join {{ ref('dim_coingecko_token_map')}}
+                on coingecko_id = coingecko_token_id
+            where
+                chain in  (
+                    {% for chain in chains %}
+                        '{{chain}}' {% if not loop.last %},{% endif %}
+                    {% endfor %}
+                )
+                and date < dateadd(day, -1, to_date(sysdate()))
+            union
+            select dateadd('day', -1, to_date(sysdate())) as date, chain, contract_address, decimals, symbol, token_current_price as price
+            from {{ ref("fact_coingecko_token_realtime_data") }}
+            inner join {{ ref('dim_coingecko_token_map')}}
+                on token_id = coingecko_token_id
+            where
+                chain in  (
+                    {% for chain in chains %}
+                        '{{chain}}' {% if not loop.last %},{% endif %}
+                    {% endfor %}
+                )
+            union
+            select to_date(sysdate()) as date, chain, contract_address, decimals, symbol, token_current_price as price
+            from {{ ref("fact_coingecko_token_realtime_data") }}
+            inner join {{ ref('dim_coingecko_token_map')}}
+                on token_id = coingecko_token_id
+            where
+                chain in  (
+                    {% for chain in chains %}
+                        '{{chain}}' {% if not loop.last %},{% endif %}
+                    {% endfor %}
+                )
+    )
+    select 
+        date,
+        chain,
+        contract_address,
+        max(decimals) as decimals,
+        max(symbol) as symbol,
+        max(price) as price
+    from prices_by_chain
+    group by date, chain, contract_address
 {% endmacro %}
