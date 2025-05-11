a8fe618e42e674f22da8a1f17a1fc6bd5c7fc6b8
Son Do <sondo077@gmail.com>
2025-05-10 22:02:57 -0400

    Filled columns for optimism

--- models/staging/bridge_flows/fact_superchain_bridge_transfers.sql
+++ models/staging/bridge_flows/fact_superchain_bridge_transfers.sql
@@ -12,6 +12,38 @@ with
         }}
     )
 select
-    *
+    null as src_messaging_contract_address -- need to add
+    , src_block_timestamp -- need to add
+    , src_transaction_hash as src_tx_hash
+    , src_event_index -- need to add
+    , amount_native as src_amount
+    , amount_native as amount_sent_native
+    , amount_adjusted as amount_sent_adjusted
+    , amount_usd as amount_sent
+    , decimals as src_decimals
+    , source_token_symbol as src_symbol
+    , source_chain as src_chain
+    , null as origin_chain_id
+    , origin_token
+    , null as dst_messaging_contract_address -- need to add
+    , dst_block_timestamp -- need to add
+    , dst_transaction_hash as dst_tx_hash
+    , dst_event_index -- need to add
+    , amount_native as dst_amount
+    , amount_native as amount_received_native
+    , amount_adjusted as amount_received_adjusted
+    , amount_usd as amount_received
+    , decimals as dst_decimals
+    , destination_token_symbol as dst_symbol
+    , depositor
+    , recipient
+    , null as destination_chain_id
+    , destination_token
+    , destination_chain as dst_chain
+    , null as token_address
+    , null as token_chain
+    , null as protocol_fee
+    , null as bridge_message_app
+    , null as version
     , 'superchain_bridge' as app
 from flows_by_super_chain
