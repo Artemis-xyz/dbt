df77786bd40a80b10a9d866386e792c277e9ea8d
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2025-05-11 17:37:56 -0400

    Add aptos and Kaia to final stablecoin table (#1350)

---
+++ models/staging/standard_8d737a18/fact_kaia_stablecoin_transfers_standard_8d737a18.sql
@@ -0,0 +1,4 @@
+{{config(materialized="incremental", snowflake_warehouse="STANDARD_8D737A18", unique_key=["transaction_hash", "transfer_index"])}}
+
+
+{{standard_8d737a18_stablecoin_transfers("kaia")}}
