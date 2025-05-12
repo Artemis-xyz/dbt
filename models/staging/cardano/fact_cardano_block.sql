b2ead34c7522a0d6fa588a44035a10c286e382df
Anish Toomu <anishtoomu@gmail.com>
2025-05-12 17:21:53 -0400

    renamed tables

--- models/staging/cardano/fact_cardano_block.sql
+++ models/staging/cardano/fact_cardano_block.sql
@@ -8,7 +8,7 @@ with source_data as (
     select 
         PARQUET_RAW:epoch_no::integer as epoch_no,
         PARQUET_RAW:slot_no::integer as slot_no,
-        PARQUET_RAW:block_time::timestamp as block_time,
+        to_timestamp(PARQUET_RAW:block_time::number / 1000000) as block_time,
         PARQUET_RAW:block_size::integer as block_size,
         PARQUET_RAW:tx_count::integer as tx_count,
         PARQUET_RAW:sum_tx_fee::integer as sum_tx_fee,
@@ -18,7 +18,7 @@ with source_data as (
         PARQUET_RAW:block_hash::string as block_hash,
         -- Generate a unique ID for each record
         md5(concat(block_hash)) as unique_id
-    from {{ source('PROD_LANDING', 'raw_cardano_blocks_parquet') }}
+    from {{ source('PROD_LANDING', 'raw_cardano_block_parquet') }}
 )
 
 select 
--- models/staging/cardano/fact_cardano_transactions.sql
+++ models/staging/cardano/fact_cardano_transactions.sql
@@ -8,22 +8,20 @@ with source_data as (
     select 
         PARQUET_RAW:epoch_no::integer as epoch_no,
         PARQUET_RAW:tx_hash::string as tx_hash,
-        PARQUET_RAW:block_time::timestamp as block_time,
+        to_timestamp(PARQUET_RAW:block_time::integer / 1000000) as block_time,
         PARQUET_RAW:slot_no::integer as slot_no,
         PARQUET_RAW:txidx::integer as txidx,
         PARQUET_RAW:out_sum::numeric as out_sum,
         PARQUET_RAW:fee::numeric as fee,
         PARQUET_RAW:deposit::numeric as deposit,
         PARQUET_RAW:size::integer as size,
-        PARQUET_RAW:invalid_before::numeric as invalid_before,
-        PARQUET_RAW:invalid_after::numeric as invalid_after,
         PARQUET_RAW:valid_script::boolean as valid_script,
         PARQUET_RAW:script_size::integer as script_size,
         PARQUET_RAW:count_inputs::integer as count_inputs,
         PARQUET_RAW:count_outputs::integer as count_outputs,
         -- Generate a unique ID for each record
         md5(concat(tx_hash)) as unique_id
-    from {{ source('PROD_LANDING', 'raw_cardano_transactions_parquet') }}
+    from {{ source('PROD_LANDING', '`raw_cardano_tx_parquet`') }}
 )
 
 select 
