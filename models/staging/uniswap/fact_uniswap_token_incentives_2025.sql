6f5c7d62d9eee6424b23cfc3b6d5273b5171d1ee
Benjamin Stamm <benjaminstamm@mac.mynetworksettings.com>
2025-05-12 15:01:12 -0400

    comments for clarity

--- models/staging/uniswap/fact_uniswap_token_incentives_2025.sql
+++ models/staging/uniswap/fact_uniswap_token_incentives_2025.sql
@@ -6,7 +6,7 @@ SELECT
     sum(amount) as amount_native,
     sum(amount_usd) as amount
 FROM ethereum_flipside.core.ez_token_transfers
-WHERE from_address = lower('0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae')
-AND contract_address = lower('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984')
-AND date > '2025-04-01'
+WHERE from_address = lower('0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae') --MERKL Angle Distributor Contract
+AND contract_address = lower('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984') --UNI Token
+AND block_timestamp::date > '2025-04-01'
 GROUP BY 1
