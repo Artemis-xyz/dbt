72c42b19db4b892045e98fe3a942179ef27220af
alexwes <pampawes@gmail.com>
2025-05-12 07:45:47 -0400

    forward fill tvl (#1315)
    Co-authored-by: Alex Kan <29241719+akan72@users.noreply.github.com>

--- models/projects/liquity/core/ez_liquity_metrics_by_token.sql
+++ models/projects/liquity/core/ez_liquity_metrics_by_token.sql
@@ -79,7 +79,17 @@ with  fees_and_revs as (
                 union
                 select distinct token from outstanding_supply
                 union
-                select distinct token from fees_and_revs)
+                select distinct token from fees_and_revs
+                union
+                select distinct token from token_incentives
+                union
+                select distinct token from treasury_by_token
+                union
+                select distinct token from net_treasury
+                union
+                select distinct token from treasury_native
+                union
+                select distinct token from tvl)
     where date between '2021-04-05' and to_date(sysdate())
 )
 
