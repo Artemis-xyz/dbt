95cf9e6f142f3c2314e0fe52b7f22ce762cd939a
alexwes <pampawes@gmail.com>
2025-05-12 07:36:34 -0400

    Update models/projects/liquity/raw/fact_liquity_v1_tvl.sql
    Co-authored-by: Alex Kan <29241719+akan72@users.noreply.github.com>

--- models/projects/liquity/raw/fact_liquity_v1_tvl.sql
+++ models/projects/liquity/raw/fact_liquity_v1_tvl.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='table',
-        snowflake_warehouse='ANALYTICS_XL',
+        snowflake_warehouse='LIQUIDTY',
         database='LIQUITY',
         schema='raw',
         alias='fact_liquity_v1_tvl'
