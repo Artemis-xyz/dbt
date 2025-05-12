e743f65a7f1ac420d803ed8778b9693d411a34ec
Alex Weseley <pampawes@gmail.com>
2025-05-12 19:23:08 -0400

    small changes to datahub

--- models/data_hubs/fact_protocol_datahub_gold.sql
+++ models/data_hubs/fact_protocol_datahub_gold.sql
@@ -8,12 +8,13 @@ SELECT
     cat."VALUES" as sectors,
     dau::number as dau,
     txns::number as txns,
+    volume::number as volume,
     gross_protocol_revenue::number as gross_protocol_revenue,
     tvl::number as tvl,
     price::number as price,
     market_cap::number as market_cap,
     fdmc::number as fdmc,
     token_volume::number as token_volume
-FROM {{ source('PC_DBT_DB', 'fact_datahub_silver') }} dh
+FROM {{ source('PC_DBT_DB', 'fact_datahub_metrics_silver') }} dh
 LEFT JOIN {{ source('POSTGRES_REPLICATED', 'core_asset') }} ca ON REPLACE(ca.artemis_id, '-', '_') = dh.artemis_id 
 LEFT JOIN {{ source('POSTGRES_REPLICATED', 'core_assettag') }} cat on cat.asset_id = ca.id AND cat."KEY" = 'groups'
