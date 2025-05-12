2915289026373ffef700cc6d3f49c9c4ec6d2665
Alex Weseley <pampawes@gmail.com>
2025-05-12 19:49:57 -0400

    add datahub by chain models

---
+++ models/common/ez_protocol_datahub_by_chain.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        materialized="view",
+        database="common",
+        schema="core",
+        snowflake_warehouse="COMMON",
+    )
+}}
+
+select 
+    date,
+    artemis_id,
+    sectors,
+    dau,
+    txns,
+    volume,
+    gross_protocol_revenue,
+    tvl,
+    price,
+    market_cap,
+    fdmc,
+    token_volume
+from {{ ref("fact_protocol_datahub_by_chain_gold") }}
+where date < to_date(sysdate())
+order by date desc, artemis_id asc
