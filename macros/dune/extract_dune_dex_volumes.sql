4e9fd7b8e26dc38d131f2f3ed76c670619890920
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-13 16:02:56 -0400

    Edited macro to filter on price magnitude and changed base dex volumes to match

--- macros/dune/extract_dune_dex_volumes.sql
+++ macros/dune/extract_dune_dex_volumes.sql
@@ -1,9 +1,24 @@
 {% macro extract_dune_dex_volumes(chain) %}
-    select 
-        block_date::date as date,
-        sum(amount_usd) as daily_volume
-    from {{ source("DUNE_DEX_VOLUMES", "trades")}}
-    where blockchain = '{{chain}}'
-    group by date
-    order by date asc
+    WITH coingecko_latest_prices AS (
+        {{ get_multiple_coingecko_price_with_latest(chain) }}
+    )
+
+    SELECT 
+        block_date::date AS date,
+        sum(amount_usd) AS daily_volume
+    FROM {{ source("DUNE_DEX_VOLUMES", "trades")}}
+    LEFT JOIN coingecko_latest_prices AS token_bought_price
+        ON block_date::date = token_bought_price.date
+        AND lower(token_bought_address_hex) = lower(token_bought_price.contract_address)
+    LEFT JOIN coingecko_latest_prices AS token_sold_price
+        ON block_date::date = token_sold_price.date
+        AND lower(token_sold_address_hex) = lower(token_sold_price.contract_address)
+    WHERE blockchain = '{{ chain }}'
+        AND token_sold_price.price IS NOT NULL
+        AND token_bought_price.price IS NOT NULL 
+        AND token_sold_price.price * token_sold_amount != 0
+        AND token_bought_price.price * token_bought_amount != 0
+        AND (token_bought_price.price * token_bought_amount) / (token_sold_price.price * token_sold_amount) > 0.2
+        AND (token_bought_price.price * token_bought_amount) / (token_sold_price.price * token_sold_amount) < 4
+    GROUP BY 1
 {% endmacro %}
--- models/staging/base/fact_base_daily_dex_volumes.sql
+++ models/staging/base/fact_base_daily_dex_volumes.sql
@@ -5,26 +5,4 @@
     )
 }}
 
-select 
-    block_date::date as date,
-    sum(amount_usd) as daily_volume
-from {{ source("DUNE_DEX_VOLUMES", "trades") }}
-where blockchain = 'base'
-  and not (
-    (date = '2025-04-25' and lower(tx_from_hex) in (
-        lower('0x80205B17056BE5F99A9EB028E811C20DDF896F2A'), 
-        lower('0xE3223F7E3343C2C8079F261D59EE1E513086C7C3')
-    ))
-    or
-    (date = '2025-04-29' and lower(tx_from_hex) in (
-        lower('0x80205B17056BE5F99A9EB028E811C20DDF896F2A'), 
-        lower('0xE3223F7E3343C2C8079F261D59EE1E513086C7C3')
-    ))
-    or
-    (date = '2025-05-07' and lower(tx_from_hex) in (
-        lower('0xf14149bde6f7e2573f38aceda6220d7dfff66592')
-    ))
-)
-group by date
-order by date asc
-
+{{ extract_dune_dex_volumes("base") }}
