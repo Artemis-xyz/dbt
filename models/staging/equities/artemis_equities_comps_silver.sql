5ccd574bfe4618ddee06ca97969381f9ad545105
horaceliuHL <horaceliu.hl@gmail.com>
2025-05-13 14:06:47 -0400

    Fix errors

--- models/staging/equities/artemis_equities_comps_silver.sql
+++ models/staging/equities/artemis_equities_comps_silver.sql
@@ -8,23 +8,22 @@ with
     {% endif %}
     data as (
         select 
-            value:"fiscal_year"::VARCHAR AS fiscal_year,
-            value:"fiscal_period"::VARCHAR AS fiscal_period,
-            value:"date"::VARCHAR AS date,
-            value:"metric"::VARCHAR AS metric,
-            value:"metric_friendly_name"::VARCHAR AS metric_friendly_name,
-            value:"factor"::VARCHAR AS factor,
-            value:"balance"::VARCHAR AS balance,
-            value:"unit"::VARCHAR AS unit,
-            value:"value"::VARCHAR AS value,
-            value:"ticker"::VARCHAR AS ticker,
-            value:"company_name"::VARCHAR AS company_name,
-            value:"company_stock_exchange"::VARCHAR AS company_stock_exchange,
-            value:"company_cik"::VARCHAR AS company_cik,
+            source_json:"fiscal_year"::VARCHAR AS fiscal_year,
+            source_json:"fiscal_period"::VARCHAR AS fiscal_period,
+            source_json:"date"::VARCHAR AS date,
+            source_json:"metric"::VARCHAR AS metric,
+            source_json:"metric_friendly_name"::VARCHAR AS metric_friendly_name,
+            source_json:"factor"::VARCHAR AS factor,
+            source_json:"balance"::VARCHAR AS balance,
+            source_json:"unit"::VARCHAR AS unit,
+            source_json:"value"::VARCHAR AS value,
+            source_json:"ticker"::VARCHAR AS ticker,
+            source_json:"company_name"::VARCHAR AS company_name,
+            source_json:"company_stock_exchange"::VARCHAR AS company_stock_exchange,
+            source_json:"company_cik"::VARCHAR AS company_cik,
             extraction_date
         from
-            {{ source("PROD_LANDING", "artemis_equities_comps") }},
-            lateral flatten(input => parse_json(source_json))
+            {{ source("PROD_LANDING", "artemis_equities_comps") }}
         {% if is_incremental() %}
         where extraction_date = (select max_date from max_extraction)
         {% endif %}
