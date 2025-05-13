c88f7856c9c921578722703fb8872d6f0df48bcb
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-13 18:40:02 -0400

    Added a partition to get only one price per date

--- macros/dune/extract_dune_dex_volumes.sql
+++ macros/dune/extract_dune_dex_volumes.sql
@@ -1,18 +1,32 @@
 {% macro extract_dune_dex_volumes(chain) %}
+    {% set coingecko_chain = 'avalanche' if chain == 'avalanche_c' 
+        else ('arbitrum-nova' if chain == 'nova' 
+        else ('bsc' if chain == 'bnb'
+        else ('polygon-zkevm' if chain == 'zkevm'
+        else chain))) %}
+    
     WITH coingecko_latest_prices AS (
-        {{ get_multiple_coingecko_price_with_latest(chain) }}
+        {{ get_multiple_coingecko_price_with_latest(coingecko_chain) }}
+    ), 
+
+    partitioned_coingecko_prices AS (
+        SELECT *, 
+            ROW_NUMBER() OVER (PARTITION BY date, contract_address ORDER BY price DESC) AS rn
+        FROM coingecko_latest_prices
     )
 
     SELECT 
         block_date::date AS date,
         sum(amount_usd) AS daily_volume
     FROM {{ source("DUNE_DEX_VOLUMES", "trades")}}
-    LEFT JOIN coingecko_latest_prices AS token_bought_price
+    LEFT JOIN partitioned_coingecko_prices AS token_bought_price
         ON block_date::date = token_bought_price.date
         AND lower(token_bought_address_hex) = lower(token_bought_price.contract_address)
-    LEFT JOIN coingecko_latest_prices AS token_sold_price
+        AND token_bought_price.rn = 1
+    LEFT JOIN partitioned_coingecko_prices AS token_sold_price
         ON block_date::date = token_sold_price.date
         AND lower(token_sold_address_hex) = lower(token_sold_price.contract_address)
+        AND token_sold_price.rn = 1
     WHERE blockchain = '{{ chain }}'
         AND token_sold_price.price IS NOT NULL
         AND token_bought_price.price IS NOT NULL 
