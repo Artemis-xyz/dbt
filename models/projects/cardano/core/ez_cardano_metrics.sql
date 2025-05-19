996ddfbf8e76ca8e9d725a7ef873ca9861d7bdf6
Anish Toomu <anishtoomu@gmail.com>
2025-05-19 17:04:22 -0400

    added nfts to ez_metrics

--- models/projects/cardano/core/ez_cardano_metrics.sql
+++ models/projects/cardano/core/ez_cardano_metrics.sql
@@ -32,44 +32,47 @@ with
     ),
     price_data as ({{ get_coingecko_metrics("cardano") }}),
     defillama_data as ({{ get_defillama_metrics("cardano") }}),
-    github_data as ({{ get_github_metrics("cardano") }})
+    github_data as ({{ get_github_metrics("cardano") }}),
+    nft_data as ({{ get_nft_metrics("cardano") }})  
 
 select
-    f.date,
-    f.chain,
-    txns,
-    dau,
-    fees_native,
-    fees,
-    fees / txns as avg_txn_fee,
-    dex_volumes,
+    f.date
+    , f.chain
+    , txns
+    , dau
+    , fees_native
+    , fees
+    , fees / txns as avg_txn_fee
+    , dex_volumes
+    , nft_trading_volume
     -- Standardized Metrics
     -- Market Data
-    price,
-    market_cap,
-    fdmc,
-    token_volume,
+    , price
+    , market_cap
+    , fdmc
+    , token_volume
     -- Chain Metrics
-    txns as chain_txns,
-    dau as chain_dau,
-    avg_txn_fee as chain_avg_txn_fee,
-    dex_volumes as chain_spot_volume,
+    , txns as chain_txns
+    , dau as chain_dau
+    , avg_txn_fee as chain_avg_txn_fee
+    , dex_volumes as chain_spot_volume
+    , nft_trading_volume as chain_nft_trading_volume
     -- Cash Flow Metrics
-    fees as ecosystem_revenue,
-    fees as chain_fees,
-    fees as gross_protocol_revenue,
-    fees_native as gross_protocol_revenue_native,
+    , fees as chain_fees
+    , fees as ecosystem_revenue
+    , fees_native as ecosystem_revenue_native
     -- Crypto Metrics
-    tvl,
+    , tvl
     -- Developer Metrics
-    weekly_commits_core_ecosystem,
-    weekly_commits_sub_ecosystem,
-    weekly_developers_core_ecosystem,
-    weekly_developers_sub_ecosystem,
-    token_turnover_circulating,
-    token_turnover_fdv
+    , weekly_commits_core_ecosystem
+    , weekly_commits_sub_ecosystem
+    , weekly_developers_core_ecosystem
+    , weekly_developers_sub_ecosystem
+    , token_turnover_circulating
+    , token_turnover_fdv
 from fundamental_data f
 left join price_data on f.date = price_data.date
 left join defillama_data on f.date = defillama_data.date
 left join github_data on f.date = github_data.date
+left join nft_metrics on f.date = nft_metrics.date
 where f.date < to_date(sysdate())
