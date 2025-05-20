e1fcd2e65427fc3b4c79b11137fc22a6e9a05c28
Ben Stamm <benstamm11@gmail.com>
2025-05-20 19:41:55 +0100

    feature/eigenlayer avs rewards & token incentives

--- models/projects/eigenlayer/core/ez_eigenlayer_metrics.sql
+++ models/projects/eigenlayer/core/ez_eigenlayer_metrics.sql
@@ -9,18 +9,25 @@
 }}
 
 -- Simplified ez metrics table that aggregates data for eigenlayer
-WITH 
-    eigenlayer_aggregated AS (
-        SELECT 
+WITH date_spine AS (
+        SELECT
             date
-            , protocol as app
-            , category
-            , SUM(num_restaked_eth) AS num_restaked_eth
-            , SUM(amount_restaked_usd) AS amount_restaked_usd
-        FROM {{ref('fact_eigenlayer_restaked_assets')}}
-        GROUP BY date, protocol, category
-    )
-    , eigenlayer_supply_data AS (
+        FROM {{ ref('dim_date_spine') }}
+        WHERE date BETWEEN 
+            (SELECT MIN(date) FROM {{ ref('fact_eigenlayer_restaked_assets') }}) 
+            AND (SELECT MAX(date) FROM {{ ref('fact_eigenlayer_restaked_assets') }})
+)
+, eigenlayer_aggregated AS (
+    SELECT 
+        date
+        , protocol as app
+        , category
+        , SUM(num_restaked_eth) AS num_restaked_eth
+        , SUM(amount_restaked_usd) AS amount_restaked_usd
+    FROM {{ref('fact_eigenlayer_restaked_assets')}}
+    GROUP BY date, protocol, category
+)
+, eigenlayer_supply_data AS (
         SELECT
             date
             , emissions_native
@@ -28,13 +35,45 @@ WITH
             , net_supply_change_native
             , circulating_supply
         FROM {{ ref('fact_eigenlayer_supply_data') }}
-    )
-    , market_data as (
-        {{ get_coingecko_metrics('eigenlayer') }}
-    )
+)
+, avs_rewards_submitted AS (
+    SELECT 
+        date
+        , SUM(amount_usd) AS avs_rewards_submitted
+    FROM {{ ref('fact_eigenlayer_avs_rewards_submitted') }}
+    WHERE event_name ilike '%AVS%'
+    GROUP BY date
+)
+, avs_rewards_claimed AS (
+    SELECT 
+        date
+        , SUM(amount_usd) AS avs_rewards_claimed
+    FROM {{ ref('fact_eigenlayer_avs_rewards_claimed') }}
+    GROUP BY date
+)
+, avs_and_operator_counts AS (
+    SELECT
+        date
+        , SUM(active_operators) AS active_operators
+        , SUM(active_avs) AS active_avs
+    FROM {{ ref('fact_eigenlayer_avs_and_operator_counts') }}
+    GROUP BY date
+)
+, market_data as (
+    {{ get_coingecko_metrics('eigenlayer') }}
+)
+, token_incentives as (
+    SELECT
+        date
+        , SUM(amount_aduj) AS token_incentives_native
+        , SUM(amount_usd) AS token_incentives
+    FROM {{ ref('fact_eigenlayer_avs_rewards_submitted') }}
+    WHERE event_name not ilike '%AVS%'
+    GROUP BY date
+)
 
 SELECT 
-    date
+    d.date
     , app
     , category
 
@@ -47,6 +86,12 @@ SELECT
     , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
 
     -- Crypto Metrics
+    , coalesce(avs_rewards_submitted.avs_rewards_submitted, 0) as avs_rewards_submitted
+    , coalesce(avs_rewards_claimed.avs_rewards_claimed, 0) as avs_rewards_claimed
+    , coalesce(avs_and_operator_counts.active_operators, 0) as active_operators
+    , coalesce(avs_and_operator_counts.active_avs, 0) as active_avs
+    , coalesce(token_incentives.token_incentives, 0) as token_incentives
+    , coalesce(token_incentives.token_incentives_native, 0) as token_incentives_native
     , amount_restaked_usd as tvl
     , amount_restaked_usd - LAG(amount_restaked_usd) 
         OVER (ORDER BY date) AS tvl_net_change
@@ -58,12 +103,18 @@ SELECT
     , circulating_supply
     , emissions_native as emissions_native
     , net_supply_change_native
-    , premine_unlocks_native as premine_unlocks_native
+    , premine_unlocks_native as premine_unlocks_native_native
 
     -- Turnover Metrics
     , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
     , coalesce(market_data.token_volume, 0) as token_volume
-FROM eigenlayer_aggregated
+FROM date_spine d
+LEFT JOIN eigenlayer_aggregated using (date)
 LEFT JOIN eigenlayer_supply_data using (date)
+LEFT JOIN token_incentives using (date)
+LEFT JOIN avs_rewards_claimed using (date)
+LEFT JOIN avs_and_operator_counts using (date)
+LEFT JOIN avs_rewards_submitted using (date)
 LEFT JOIN market_data using (date)
-ORDER BY date
+WHERE d.date < CURRENT_DATE()
+ORDER BY d.date
