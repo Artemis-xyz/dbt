d54bae037523d11f78ed27d8d02a59f27f18140d
limzhengjie <86870373+limzhengjie@users.noreply.github.com>
2025-05-21 03:53:20 +0800

    add Believe fees (#1412)

--- models/projects/believe/core/ez_believe_metrics_by_chain.sql
+++ models/projects/believe/core/ez_believe_metrics_by_chain.sql
@@ -26,6 +26,15 @@ with
         from {{ ref('fact_believe_coins_minted') }}
         group by 1
     )
+    
+    , believe_fees as (
+        select
+            date
+            , sum(amount_native) as fees_native
+            , sum(amount_usd) as ecosystem_revenue
+        from {{ ref('fact_believe_fees') }}
+        group by 1
+    )
 
     , market_metrics as (
         {{  get_coingecko_metrics('ben-pasternak')  }}
@@ -50,9 +59,15 @@ select
     , bst.txns as launchpad_txns
     , bst.unique_traders as launchpad_dau
     , bcm.coins_minted
+    , bf.ecosystem_revenue
+    , bf.fees_native
+    , bf.ecosystem_revenue * 0.5 as foundation_cash_flow
+    , bf.ecosystem_revenue * 0.5 as service_cash_flow
 
 from believe_swap_trades bst
 left join believe_coins_minted bcm
     on bst.date = bcm.date
 left join market_metrics mm
     on bst.date = mm.date
+left join believe_fees bf
+    on bst.date = bf.date
