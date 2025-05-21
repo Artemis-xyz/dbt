66420ed9e3fd96e59221bbdb32bbe6c30137f246
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-21 14:20:10 -0400

    Added preliminary tvl code

---
+++ models/staging/bluefin/fact_bluefin_spot_tvl.sql
@@ -0,0 +1,24 @@
+{{config(
+    materialized = 'table',
+    database = 'bluefin'
+)}}
+
+WITH vault_balances AS (
+    SELECT
+        date,
+        pool_address,
+        (COALESCE(vault_a_amount, 0) * COALESCE(price_a, 0)) AS vault_a_tvl,
+        (COALESCE(vault_b_amount, 0) * COALESCE(price_b, 0)) AS vault_b_tvl,
+        (COALESCE(vault_a_amount, 0) * COALESCE(price_a, 0)) + (COALESCE(vault_b_amount, 0) * COALESCE(price_b, 0)) AS pool_tvl
+    FROM {{ ref('fact_bluefin_dex_swaps') }}
+    GROUP BY 1, 2
+), 
+
+partitioned_vault_balances AS (
+    SELECT
+        date,
+        ROW_NUMBER() OVER (PARTITION BY pool_address ORDER BY date DESC) AS rn,
+    FROM vault_balances
+)
+
+SELECT * FROM partitioned_vault_balances
