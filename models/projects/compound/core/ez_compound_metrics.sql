8ae754c3d334e9864b1f468db77ee11f721bf3fc
Kaviish Sethi <kaviishvsethi@gmail.com>
2025-05-21 13:45:22 +0530

    feature/compound token incentives (#1417)

--- models/projects/compound/core/ez_compound_metrics.sql
+++ models/projects/compound/core/ez_compound_metrics.sql
@@ -30,6 +30,12 @@ with
         group by 1
     )
     , price_data as ({{ get_coingecko_metrics("compound-governance-token") }})
+    , token_incentives as (
+        select
+            day as date,
+            total_usd_value as token_incentives
+        from {{ref('fact_compound_token_incentives')}}
+    )
 
 select
     compound_metrics.date
@@ -41,7 +47,10 @@ select
     , price_data.price
     , price_data.market_cap
     , price_data.fdmc
+    , coalesce(token_incentives.token_incentives, 0) as token_incentives
 from compound_metrics
 left join price_data
     on compound_metrics.date = price_data.date
+left join token_incentives
+    on compound_metrics.date = token_incentives.date
 where compound_metrics.date < to_date(sysdate())
