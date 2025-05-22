5ab074a40545fac659ce571241d8ba46d2aad4f7
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-05-22 17:56:26 -0400

    Fix ez_vertex_metrics_by_chain

--- models/projects/vertex/core/ez_vertex_metrics_by_chain.sql
+++ models/projects/vertex/core/ez_vertex_metrics_by_chain.sql
@@ -18,6 +18,14 @@ with
         select date, unique_traders, chain
         from {{ ref("fact_vertex_unique_traders") }}
     )
+    , token_incentives as (
+        select 
+            date,
+            sum(amount) as token_incentives_native,
+            sum(amount_usd) as token_incentives
+        from {{ ref("fact_vertex_token_incentives") }}
+        group by date
+    )
 select
     date
     , 'vertex' as app
@@ -28,6 +36,9 @@ select
     -- standardize metrics
     , trading_volume as perp_volume
     , unique_traders as perp_dau
+
+    , token_incentives
 from unique_traders_data
 left join trading_volume_data using(date, chain)
+left join token_incentives using(date)
 where date < to_date(sysdate())
