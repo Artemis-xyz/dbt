0356d12194f44ff4baace828123839540cd4ca3c
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-05-27 16:27:06 -0400

    Remove dau_over_100 columns from ez_tron_metrics (#1456)

--- models/projects/tron/core/ez_tron_metrics.sql
+++ models/projects/tron/core/ez_tron_metrics.sql
@@ -36,7 +36,6 @@ select
     , fees AS revenue
     , avg_txn_fee
     , median_txn_fee
-    , dau_over_100
     , dex_volumes
     -- Standardized Metrics
     -- Market Data Metrics
@@ -53,7 +52,6 @@ select
     , median_txn_fee AS chain_median_txn_fee
     , returning_users
     , new_users
-    , dau_over_100 AS dau_over_100_balance
     , p2p_native_transfer_volume
     , p2p_token_transfer_volume
     , p2p_transfer_volume
