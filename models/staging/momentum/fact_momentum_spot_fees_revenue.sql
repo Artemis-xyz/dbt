05b8ab5cb69ebd3ffe3d9a5b5ba8ec7189bd3cc3
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-28 11:34:10 -0400

    Added revenue and spot_tvl

--- models/staging/momentum/fact_momentum_spot_fees_revenue.sql
+++ models/staging/momentum/fact_momentum_spot_fees_revenue.sql
@@ -11,5 +11,9 @@ SELECT
     symbol_b,
     SUM(fee_amount_native) AS fees_native, 
     SUM(fee_amount_usd) AS fees_usd, 
+    SUM(protocol_fee_amount_native) AS foundation_cash_flow_native, 
+    SUM(protocol_fee_amount_usd) AS foundation_cash_flow,
+    SUM(fee_amount_native) - SUM(protocol_fee_amount_native) AS service_cash_flow_native,
+    SUM(fee_amount_usd) - SUM(protocol_fee_amount_usd) AS service_cash_flow
 FROM {{ ref('fact_raw_momentum_spot_swaps') }}
 GROUP BY 1, 2, 3, 4, 5
