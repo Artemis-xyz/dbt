c8725881417c288a769dd667537b439dd52aaee5
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-05-29 16:06:49 -0400

    Added flowx spot volumes

---
+++ models/staging/flowx/fact_flowx_spot_volumes.sql
@@ -0,0 +1,16 @@
+{{config(
+    materialized = 'table'
+)}}
+
+SELECT
+    date, 
+    pool_address,
+    symbol_a,
+    symbol_b, 
+    SUM(amount_a_swapped_native) AS amount_a_swapped_native,
+    SUM(amount_b_swapped_native) AS amount_b_swapped_native,
+    SUM(amount_a_swapped_usd) AS amount_a_swapped_usd,
+    SUM(amount_b_swapped_usd) AS amount_b_swapped_usd,
+    SUM(GREATEST(COALESCE(amount_a_swapped_usd, 0), COALESCE(amount_b_swapped_usd, 0))) AS volume_usd
+FROM {{ ref('fact_raw_flowx_spot_swaps') }}
+GROUP BY 1, 2, 3, 4
