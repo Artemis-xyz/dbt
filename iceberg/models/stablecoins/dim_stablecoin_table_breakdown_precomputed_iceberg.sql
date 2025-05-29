7c72ffcb01d0164d0ce545afda722b517a2c79bd
horaceliuHL <horaceliu.hl@gmail.com>
2025-05-29 18:08:53 -0400

    Precompute large dim_stablecoin_table_breakdown table

--- iceberg/models/stablecoins/dim_stablecoin_table_breakdown_precomputed_iceberg.sql
+++ iceberg/models/stablecoins/dim_stablecoin_table_breakdown_precomputed_iceberg.sql
@@ -5,12 +5,10 @@
         database="ARTEMIS_ICEBERG",
         schema="STABLECOINS",
         external_volume="ICEBERG_EXTERNAL_VOLUME_INTERNAL",
-        alias="dim_stablecoin_table_breakdown",
+        alias="dim_stablecoin_table_breakdown_precomputed",
         post_hook = "{{ merge_tags_dict({
             'duckdb': 'true',
-            'order_by': 'chain, symbol',
-            'partitioned_order_by': 'stablecoin_supply',
-            'partitioned_order_by_breakdown': 'chunk'
+            'order_by': 'stablecoin_supply, chain, symbol'
         }) }}"
     )
 }}
@@ -19,5 +17,5 @@ SELECT
     * EXCLUDE(name, historical_l_30_stablecoin_supply),
     to_json(name) AS name,
     to_json(historical_l_30_stablecoin_supply) AS historical_l_30_stablecoin_supply
-FROM PC_DBT_DB.PROD.dim_stablecoin_table_breakdown
+FROM PC_DBT_DB.PROD.dim_stablecoin_table_breakdown_precomputed
 ORDER BY stablecoin_supply, chain, symbol
