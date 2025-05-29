b1457b4554f573b68369d365eff0053726597886
Akhil Vajjhala <119370506+0xbeamish@users.noreply.github.com>
2025-05-29 10:35:47 -0400

    Akhil/sui momentum (#1460)

---
+++ models/projects/momentum/core/ez_momentum_metrics.sql
@@ -0,0 +1,64 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="MOMENTUM",
+        database="momentum",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+
+WITH
+    date_spine AS(
+        SELECT
+            date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date < to_date(sysdate())
+            AND date >= (SELECT MIN(date) FROM {{ ref("fact_raw_momentum_spot_swaps") }})
+    )
+    , spot_trading_volume AS (
+        SELECT date, SUM(volume_usd) AS spot_dex_volumes
+        FROM {{ ref("fact_momentum_spot_volume") }}
+        GROUP BY 1
+    )
+    , spot_dau_txns AS (
+        SELECT date, SUM(dau) AS dau, SUM(txns) AS txns
+        FROM {{ ref("fact_momentum_spot_dau_txns") }}
+        GROUP BY 1
+    )
+    , spot_fees_revenue AS (
+        SELECT date, SUM(fees_usd) AS fees_usd, SUM(service_cash_flow) AS service_cash_flow, SUM(foundation_cash_flow) AS foundation_cash_flow
+        FROM {{ ref("fact_momentum_spot_fees_revenue") }}
+        GROUP BY 1
+    )
+    , tvl AS (
+        SELECT date, SUM(tvl) AS tvl
+        FROM {{ ref("fact_momentum_spot_tvl") }}
+        GROUP BY 1
+    )
+select
+    date
+    , 'momentum' AS app
+    , 'DeFi' AS category
+    -- Standardized Metrics
+
+    --Cashflow Metrics
+    , COALESCE(spot_fees_revenue.fees_usd, 0) AS ecosystem_revenue
+    , COALESCE(spot_fees_revenue.foundation_cash_flow, 0) AS foundation_cash_flow
+    , COALESCE((spot_fees_revenue.service_cash_flow), 0) AS service_cash_flow
+    
+    -- Spot DEX Metrics
+    , COALESCE(spot_dau_txns.dau, 0) AS spot_dau
+    , COALESCE(spot_dau_txns.txns, 0) AS spot_txns
+    , COALESCE(spot_fees_revenue.fees_usd, 0) AS spot_fees
+    , COALESCE(spot_trading_volume.spot_dex_volumes, 0) AS spot_volume
+
+    -- Crypto Metrics
+    , COALESCE(tvl.tvl, 0) AS tvl
+    , COALESCE(tvl.tvl, 0) - COALESCE(LAG(tvl.tvl) OVER (ORDER BY date), 0) AS tvl_net_change
+
+FROM date_spine
+LEFT JOIN spot_trading_volume USING(date)
+LEFT JOIN spot_dau_txns USING(date)
+LEFT JOIN spot_fees_revenue USING(date)
+LEFT JOIN tvl USING(date)
---
+++ models/staging/momentum/fact_raw_momentum_spot_swaps.sql
@@ -0,0 +1,44 @@
+{{config(
+    materialized = 'table',
+    database = 'momentum'
+)}}
+
+WITH coingecko_prices AS (
+    {{get_multiple_coingecko_price_with_latest('sui')}}
+)
+
+
+SELECT
+    parquet_raw:date::date AS date
+    , parquet_raw:timestamp_ms::timestamp AS timestamp
+    , parquet_raw:transaction_digest::string AS transaction_digest
+    , parquet_raw:pool::string AS pool_address
+    , parquet_raw:sender::string AS sender
+
+    , coingecko_prices_fee.symbol AS fee_symbol
+    , parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) AS fee_amount_native
+    , parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) AS protocol_fee_amount_native
+    , parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_fee.price AS fee_amount_usd
+    , parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_fee.price AS protocol_fee_amount_usd
+
+    , coingecko_prices_a.symbol AS symbol_a
+    , parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) AS amount_a_swapped_native
+    , parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price AS amount_a_swapped_usd
+    , parquet_raw:vault_a_amount_raw::float / POW(10, coingecko_prices_a.decimals) AS vault_a_amount_native
+    , parquet_raw:vault_a_amount_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price AS vault_a_amount_usd
+
+    , coingecko_prices_b.symbol AS symbol_b
+    , parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) AS amount_b_swapped_native
+    , parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_b.price AS amount_b_swapped_usd
+    , parquet_raw:vault_b_amount_raw::float / POW(10, coingecko_prices_b.decimals) AS vault_b_amount_native
+    , parquet_raw:vault_b_amount_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_b.price AS vault_b_amount_usd
+FROM {{ source('PROD_LANDING', 'raw_sui_fact_momentum_dex_swaps_parquet') }}
+LEFT JOIN coingecko_prices AS coingecko_prices_a
+    ON coingecko_prices_a.date = parquet_raw:date::date
+    AND lower(coingecko_prices_a.contract_address) = lower(parquet_raw:token_address_a::string)
+LEFT JOIN coingecko_prices AS coingecko_prices_fee
+    ON coingecko_prices_fee.date = parquet_raw:date::date
+    AND lower(coingecko_prices_fee.contract_address) = lower(parquet_raw:fee_address::string)
+LEFT JOIN coingecko_prices AS coingecko_prices_b
+    ON coingecko_prices_b.date = parquet_raw:date::date
+    AND lower(coingecko_prices_b.contract_address) = lower(parquet_raw:token_address_b::string)
