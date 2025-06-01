7716ca23b43f0d0c63deec60452625ed29b0a857
Son Do <sondo077@gmail.com>
2025-06-01 10:04:44 -0400

    Fix Plume DBT Models

--- models/staging/plume/fact_plume_address_debits.sql
+++ models/staging/plume/fact_plume_address_debits.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized="incremental",
-        unique_key=["tx_hash", "event_index", "trace_index"],
+        unique_key=["transaction_hash", "event_index", "trace_index"],
         snowflake_warehouse="BALANCES_MD",
     )
 }}
