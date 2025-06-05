5da367e2cbf1cd7b2fa7ab3877334b6574f31775
Irwyn <irwynliong01@gmail.com>
2025-06-05 15:03:13 -0400

    Reverting macro

--- macros/stablecoins/stablecoin_breakdown.sql
+++ macros/stablecoins/stablecoin_breakdown.sql
@@ -1,48 +1,4 @@
 {% macro stablecoin_breakdown(breakdowns=[], granularity='day') %}
-
-{% if granularity != 'day' %}
-    with base as (
-        select *
-        from {{ ref("agg_daily_stablecoin_breakdown_with_labels_silver") }}
-        {% if is_incremental() %}
-            where date >= (select dateadd('{{granularity}}', -3, max(date_trunc('{{granularity}}', date))) from {{ this }})
-        {% endif %}
-        {% if 'application' in breakdowns %}
-            {% if not is_incremental() %}
-                where application is not null
-            {% else %}
-                and application is not null
-            {% endif %}
-        {% endif %}
-    ),
-    last_date_per_group as (
-        select
-            date_trunc('{{ granularity }}', date) as date_granularity
-            {% for breakdown in breakdowns %}
-                {% if breakdown in ('application') %}
-                    , coalesce(application, 'Unlabeled') as {{ breakdown }}
-                {% elif breakdown == 'category' %}
-                    , coalesce(artemis_category_id, 'Unlabeled') as {{ breakdown }}
-                {% else %}
-                    , {{ breakdown }}
-                {% endif %}
-            {% endfor %}
-            , max(date) as last_date
-        from base
-        group by date_trunc('{{ granularity }}', date)
-        {% for breakdown in breakdowns %}, {{ breakdown }}{% endfor %}
-    ),
-    joined as (
-        select b.*, l.last_date
-        from base b
-        left join last_date_per_group l
-            on date_trunc('{{ granularity }}', b.date) = l.date_granularity
-            {% for breakdown in breakdowns %}
-                and b.{{ breakdown }} = l.{{ breakdown }}
-            {% endfor %}
-    )
-{% endif %}
-
 select
     date_trunc('{{granularity}}', date) as date_granularity
     {% for breakdown in breakdowns %}
