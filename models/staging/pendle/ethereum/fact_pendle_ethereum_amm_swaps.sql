20797f0b9dc1e8b223bb0c5aa08ec14becd514fd
Alex Weseley <pampawes@gmail.com>
2025-06-05 01:46:59 -0400

    modify amm logic

---
+++ models/staging/pendle/ethereum/fact_pendle_ethereum_amm_swaps.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        materialized="incremental",
+        snowflake_warehouse="ANALYTICS_XL",
+        unique_key = ["tx_hash", "event_index"]
+    )
+}}
+
+{{ get_pendle_amm_swaps_for_chain('ethereum') }}
