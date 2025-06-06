b1f9ccdedebc5efb74aad344c2d7c72ad5766e16
Son Do <sondo077@gmail.com>
2025-06-06 18:05:22 -0400

    Added Bridge Flows

--- models/projects/bridge_flows/core/ez_bridge_flows.sql
+++ models/projects/bridge_flows/core/ez_bridge_flows.sql
@@ -15,6 +15,9 @@ SELECT
     , src_tx_hash
     , src_event_index
     , amount_sent_native as src_amount
+    , amount_sent as src_amount_usd
+    , amount_sent_native as src_amount_native
+    , amount_sent_adjusted as src_amount_adjusted
     , amount_sent_native
     , amount_sent_adjusted
     , amount_sent
@@ -22,12 +25,17 @@ SELECT
     , src_symbol
     , src_chain
     , src_ids.id as origin_chain_id
+    , src_ids.id as src_chain_id
     , src_token_address as origin_token
+    , src_token_address
     , dst_messaging_contract_address
     , dst_block_timestamp
     , dst_tx_hash
     , dst_event_index
     , amount_received_native as dst_amount
+    , amount_received as dst_amount_usd
+    , amount_received_native as dst_amount_native
+    , amount_received_adjusted as dst_amount_adjusted
     , amount_received_native
     , amount_received_adjusted
     , amount_received
@@ -36,7 +44,9 @@ SELECT
     , src_address as depositor
     , dst_address as recipient
     , dst_ids.id as destination_chain_id
+    , dst_ids.id as dst_chain_id
     , dst_token_address as destination_token
+    , dst_token_address
     , dst_chain
     , null as token_address
     , null as token_chain
@@ -57,6 +67,9 @@ SELECT
     , src_tx_hash
     , src_event_index
     , amount_sent_native as src_amount
+    , amount_sent as src_amount_usd
+    , amount_sent_native as src_amount_native
+    , amount_sent_adjusted as src_amount_adjusted
     , amount_sent_native
     , amount_sent_adjusted
     , amount_sent
@@ -64,12 +77,17 @@ SELECT
     , src_symbol
     , src_chain
     , origin_chain_id
+    , origin_chain_id as src_chain_id
     , origin_token
+    , origin_token as src_token_address
     , dst_messaging_contract_address
     , dst_block_timestamp
     , dst_tx_hash
     , dst_event_index
     , amount_received_native as dst_amount
+    , amount_received as dst_amount_usd
+    , amount_received_native as dst_amount_native
+    , amount_received_adjusted as dst_amount_adjusted
     , amount_received_native
     , amount_received_adjusted
     , amount_received
@@ -78,7 +96,9 @@ SELECT
     , depositor
     , recipient
     , destination_chain_id
+    , destination_chain_id as dst_chain_id
     , destination_token
+    , destination_token as dst_token_address
     , dst_chain
     , null as token_address
     , null as token_chain
@@ -100,19 +120,27 @@ SELECT
     , src_tx_hash
     , null as src_event_index
     , amount_native as src_amount
+    , amount as src_amount_usd
+    , amount_native as src_amount_native
+    , amount_adjusted as src_amount_adjusted
     , amount_native as amount_sent_native
     , amount_adjusted as amount_sent_adjusted
     , amount as amount_sent
     , decimals as src_decimals
     , symbol as src_symbol
     , source_chain as src_chain
-    , null as origin_chain_id
+    , src_chain_id as origin_chain_id
+    , src_chain_id
     , null as origin_token
+    , null as src_token_address
     , null as dst_messaging_contract_address
     , dst_timestamp as dst_block_timestamp
     , dst_tx_hash
     , null as dst_event_index
     , amount_native as dst_amount
+    , amount as dst_amount_usd
+    , amount_native as dst_amount_native
+    , amount_adjusted as dst_amount_adjusted
     , amount_native as amount_received_native
     , amount_adjusted as amount_received_adjusted
     , amount as amount_received
@@ -120,8 +148,10 @@ SELECT
     , symbol as dst_symbol
     , from_address as depositor
     , to_address as recipient
-    , null as destination_chain_id
+    , dst_chain_id as destination_chain_id
+    , dst_chain_id
     , null as destination_token
+    , null as dst_token_address
     , destination_chain as dst_chain
     , token_address
     , token_chain
@@ -142,19 +172,27 @@ SELECT
     , source_tx_hash as src_tx_hash
     , null as src_event_index
     , amount_sent_native as src_amount
+    , amount_sent_adjusted as src_amount_usd
+    , amount_sent_native as src_amount_native
+    , amount_sent_adjusted as src_amount_adjusted
     , amount_sent_native
     , amount_sent_adjusted
     , amount_sent
     , source_token_decimals as src_decimals
     , source_token_symbol as src_symbol
     , source_chain as src_chain
-    , null as origin_chain_id
+    , src_chain_id as origin_chain_id
+    , src_chain_id
     , source_token_address as origin_token
+    , source_token_address as src_token_address
     , null as dst_messaging_contract_address
     , dst_block_timestamp
     , null as dst_tx_hash
     , null as dst_event_index
     , amount_received_native as dst_amount
+    , amount_received as dst_amount_usd
+    , amount_received_native as dst_amount_native
+    , amount_received_adjusted as dst_amount_adjusted
     , amount_received_native
     , amount_received_adjusted
     , amount_received
@@ -163,7 +201,9 @@ SELECT
     , depositor
     , recipient
     , null as destination_chain_id
+    , dst_chain_id
     , destination_token_address as destination_token
+    , destination_token_address as dst_token_address
     , destination_chain as dst_chain
     , null as token_address
     , null as token_chain
@@ -178,13 +218,14 @@ WHERE
     and (source_chain is not null and destination_chain is not null)
 union all
 select
--- unique ids 
     src_messaging_contract_address
     , src_block_timestamp
     , src_tx_hash
     , src_event_index
-    , src_amount
--- amounts
+    , amount_sent_native as src_amount
+    , amount_sent as src_amount_usd
+    , amount_sent_native as src_amount_native
+    , amount_sent_adjusted as src_amount_adjusted
     , amount_sent_native
     , amount_sent_adjusted
     , amount_sent
@@ -192,23 +233,28 @@ select
     , src_symbol
     , src_chain
     , origin_chain_id
+    , origin_chain_id as src_chain_id
     , origin_token
+    , origin_token as src_token_address
     , dst_messaging_contract_address
     , dst_block_timestamp
     , dst_tx_hash
     , dst_event_index
--- amounts
-    , dst_amount
+    , amount_received_native as dst_amount
+    , amount_received as dst_amount_usd
+    , amount_received_native as dst_amount_native
+    , amount_received_adjusted as dst_amount_adjusted
     , amount_received_native
     , amount_received_adjusted
     , amount_received
     , dst_decimals
     , dst_symbol
--- src / dst amount
     , depositor
     , recipient
     , destination_chain_id
+    , destination_chain_id as dst_chain_id
     , destination_token
+    , destination_token as dst_token_address
     , dst_chain
     , token_address
     , token_chain
