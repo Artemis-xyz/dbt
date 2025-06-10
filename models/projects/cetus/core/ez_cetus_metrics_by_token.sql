74357cc2516adac98b0df4a0ea8de0517121fc23
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-10 19:29:05 -0400

    Changed Cetus EZMs with changes to fact tables

--- models/projects/cetus/core/ez_cetus_metrics_by_token.sql
+++ models/projects/cetus/core/ez_cetus_metrics_by_token.sql
@@ -25,13 +25,13 @@ WITH
     )
 
     , spot_dau_txns AS (
-        SELECT date, token_sold AS token, SUM(dau) AS dau, SUM(txns) AS txns
+        SELECT date, token_sold AS token, SUM(pool_dau) AS dau, SUM(pool_txns) AS txns
         FROM {{ ref("fact_cetus_spot_dau_txns") }}
         GROUP BY 1, 2
 
         UNION ALL
 
-        SELECT date, token_bought AS token, SUM(dau) AS dau, SUM(txns) AS txns
+        SELECT date, token_bought AS token, SUM(pool_dau) AS dau, SUM(pool_txns) AS txns
         FROM {{ ref("fact_cetus_spot_dau_txns") }}
         GROUP BY 1, 2
 
@@ -42,7 +42,7 @@ WITH
             date, 
             symbol_a AS token, 
             SUM(fees_native) AS fees_native, 
-            SUM(fees_usd) AS fees_usd, 
+            SUM(fees) AS fees, 
             SUM(service_cash_flow_native) AS service_cash_flow_native, 
             SUM(service_cash_flow) AS service_cash_flow, 
             SUM(foundation_cash_flow_native) AS foundation_cash_flow_native, 
@@ -56,7 +56,7 @@ WITH
             date, 
             symbol_b AS token, 
             SUM(fees_native) AS fees_native, 
-            SUM(fees_usd) AS fees_usd, 
+            SUM(fees) AS fees, 
             SUM(service_cash_flow_native) AS service_cash_flow_native, 
             SUM(service_cash_flow) AS service_cash_flow, 
             SUM(foundation_cash_flow_native) AS foundation_cash_flow_native, 
@@ -85,7 +85,7 @@ WITH
         SUM(spot_dau_txns.dau) AS spot_dau, 
         SUM(spot_dau_txns.txns) AS spot_txns, 
         SUM(spot_fees_revenue.fees_native) AS spot_fees_native, 
-        SUM(spot_fees_revenue.fees_usd) AS spot_fees, 
+        SUM(spot_fees_revenue.fees) AS spot_fees, 
         SUM(spot_fees_revenue.service_cash_flow) AS spot_service_cash_flow, 
         SUM(spot_fees_revenue.foundation_cash_flow) AS spot_foundation_cash_flow, 
         SUM(tvl.tvl_native) AS spot_tvl_native, 
