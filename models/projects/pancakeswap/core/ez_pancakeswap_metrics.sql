310fdc4122f78b3c772ed06ee59d377e5e8f83e0
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-10 14:37:04 -0400

    Added pancakeswap revenue and burns metrics

--- models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
+++ models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
@@ -60,9 +60,41 @@ with trading_volume_pool as (
 )
 , token_incentives as (
     select
-        date,
-        sum(amount_usd) as token_incentives_usd
+        date
+        , sum(amount_usd) as token_incentives_usd
     from {{ ref('fact_pancakeswap_token_incentives') }}
+    group by date
+)
+, fees_revenue as (
+    select
+        date
+        , sum(trading_fees) as fees
+    -- This distribution is outlined here https://docs.pancakeswap.finance/products/pancakeswap-exchange/pancakeswap-pools
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.67
+                when fee_percent = 0.0005 then trading_fees * 0.66
+                when fee_percent = 0.0025 then trading_fees * 0.68
+                when fee_percent = 0.01 then trading_fees * 0.68
+            end
+        ) as service_cash_flow 
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.15
+                when fee_percent = 0.0005 then trading_fees * 0.15
+                when fee_percent = 0.0025 then trading_fees * 0.23
+                when fee_percent = 0.01 then trading_fees * 0.23 
+            end
+        ) as burned_cash_flow 
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.18
+                when fee_percent = 0.0005 then trading_fees * 0.19
+                when fee_percent = 0.0025 then trading_fees * 0.09
+                when fee_percent = 0.01 then trading_fees * 0.09
+            end
+        ) as treasury_cash_flow
+    from {{ ref('ez_pancakeswap_dex_swaps') }}
     group by date
 )
 select
@@ -74,10 +106,12 @@ select
     , trading_volume.trading_volume as spot_volume
     , trading_volume.unique_traders as spot_dau
     
-    , trading_volume.trading_fees as spot_fees
-    , trading_volume.trading_fees as gross_fees
+    , fees_revenue.fees as spot_fees
+    , fees_revenue.fees as fees
     -- About 68% of fees go to LPs
-    , trading_volume.trading_fees * .68 as service_cash_flow
+    , fees_revenue.service_cash_flow as service_cash_flow
+    , fees_revenue.burned_cash_flow as burned_cash_flow
+    , fees_revenue.treasury_cash_flow as treasury_cash_flow
     -- TODO: the remaining 32% of fees are distributed differently depending on the fee tier of the pool. We currently have the fee tier in
     -- pancakeswap's ez_dex_swap. This needs to be pulled forward to the correct tables.
     -- The remaining fees are distributed among CAKE burns, Treasury, and Fixed Term CAKE Stakers
@@ -89,4 +123,5 @@ select
 from tvl
 left join trading_volume using(date)
 left join token_incentives using(date)
+left join fees_revenue using(date)
 where tvl.date < to_date(sysdate())
