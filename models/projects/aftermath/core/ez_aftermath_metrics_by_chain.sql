f4381040902b13cfe31d425fd47c6be37039d2e6
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-10 11:36:42 -0400

    Added some changes to aftermath to avoid pricing issues

--- models/projects/aftermath/core/ez_aftermath_metrics_by_chain.sql
+++ models/projects/aftermath/core/ez_aftermath_metrics_by_chain.sql
@@ -8,39 +8,60 @@
     )
 }}
 
-WITH 
-    defillama_tvl AS (
-        SELECT
-            date, 
-            SUM(tvl) AS tvl
-        FROM {{ ref("fact_defillama_protocol_tvls") }}
-        WHERE defillama_protocol_id = 3636 OR defillama_protocol_id = 3259 OR defillama_protocol_id = 3981
-            -- This includes Aftermath Liquid Staking, Aftermath AMM, and Aftermath Aggregator
-        GROUP BY 1
-    )
 
-    , date_spine AS (
-        SELECT date
+WITH
+    date_spine AS(
+        SELECT
+            date, 'sui' AS chain
         FROM {{ ref("dim_date_spine") }}
-        WHERE date >= (SELECT MIN(date) FROM defillama_tvl)
-          AND date < to_date(sysdate())
+        WHERE date < to_date(sysdate())
+            AND date >= (SELECT MIN(date) FROM {{ ref("fact_raw_aftermath_spot_swaps") }})
     )
-
-    , defillama_tvl_forwardfill AS (
-        SELECT
-            d.date,
-            LAST_VALUE(t.tvl IGNORE NULLS) OVER (
-                ORDER BY d.date
-                ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-            ) AS tvl
-        FROM date_spine d
-        LEFT JOIN defillama_tvl t ON d.date = t.date
+    , spot_trading_volume AS (
+        SELECT date, 'sui' AS chain, SUM(volume_usd) AS spot_dex_volumes
+        FROM {{ ref("fact_aftermath_spot_volumes") }}
+        GROUP BY 1, 2
+    )
+    , spot_dau_txns AS (
+        SELECT date, 'sui' AS chain, SUM(dau) AS dau, SUM(txns) AS txns
+        FROM {{ ref("fact_aftermath_spot_dau_txns") }}
+        GROUP BY 1, 2
+    )
+    , spot_fees_revenue AS (
+        SELECT date, 'sui' AS chain, SUM(fees) AS fees, SUM(service_cash_flow) AS service_cash_flow, SUM(foundation_cash_flow) AS foundation_cash_flow
+        FROM {{ ref("fact_aftermath_spot_fees_revenues") }}
+        GROUP BY 1, 2
+    )
+    , tvl AS (
+        SELECT date, 'sui' AS chain, SUM(tvl) AS tvl
+        FROM {{ ref("fact_aftermath_spot_tvl") }}
+        GROUP BY 1, 2
     )
 
 SELECT
-    d.date, 
-    'sui' AS chain,
-    'aftermath' AS app,
-    'DeFi' AS category,
-    COALESCE(d.tvl, 0) AS tvl
-FROM defillama_tvl_forwardfill d
+    date
+    , 'aftermath' as app
+    , 'DeFi' as category
+    , chain
+
+    -- Standardized Metrics
+
+    --Cashflow Metrics
+    , COALESCE(spot_fees_revenue.fees, 0) AS fees   
+    , COALESCE(spot_fees_revenue.foundation_cash_flow, 0) AS foundation_cash_flow
+    , COALESCE(spot_fees_revenue.service_cash_flow, 0) AS service_cash_flow
+    
+    -- Spot DEX Metrics
+    , COALESCE(spot_dau_txns.dau, 0) AS spot_dau
+    , COALESCE(spot_dau_txns.txns, 0) AS spot_txns
+    , COALESCE(spot_fees_revenue.fees, 0) AS spot_fees
+    , COALESCE(spot_trading_volume.spot_dex_volumes, 0) AS spot_volume
+
+    -- Crypto Metrics
+    , COALESCE(tvl.tvl, 0) AS tvl
+    , COALESCE(tvl.tvl, 0) - COALESCE(LAG(tvl.tvl) OVER (ORDER BY date), 0) AS tvl_net_change
+FROM date_spine
+LEFT JOIN spot_trading_volume USING(date, chain)
+LEFT JOIN spot_dau_txns USING(date, chain)
+LEFT JOIN spot_fees_revenue USING(date, chain)
+LEFT JOIN tvl USING(date, chain)
--- models/staging/aftermath/fact_raw_aftermath_spot_swaps.sql
+++ models/staging/aftermath/fact_raw_aftermath_spot_swaps.sql
@@ -34,40 +34,51 @@ SELECT
     , coingecko_prices_fee.symbol AS fee_symbol
     , parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) AS fee_amount_native
     , CASE
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_b::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_a::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_b::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_a::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_a::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_a.price > 1.2 THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_b::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_b.price > 1.2 THEN parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
         ELSE parquet_raw:fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_fee.price 
     END AS fee_amount_usd
     , parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) AS protocol_fee_share_amount_native
     , CASE  
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_b::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_a::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_b::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_a::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_a::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_a.price > 1.2 THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_b::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_b.price > 1.2 THEN parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_a.price
         ELSE parquet_raw:protocol_fee_amount_raw::float / POW(10, coingecko_prices_fee.decimals) * coingecko_prices_fee.price 
     END AS protocol_fee_share_amount_usd
     , coingecko_prices_a.symbol AS symbol_a
     , coingecko_prices_a.price AS price_a
     , coingecko_prices_a.contract_address AS coingecko_token_address_a
     , parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) AS amount_a_swapped_native
-    , parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price AS amount_a_swapped_usd
+    , CASE
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_a::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_a::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_a.price > 1.2 THEN parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_b.price
+        ELSE parquet_raw:amount_a_swapped_raw::float / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price 
+    END AS amount_a_swapped_usd
     , (parquet_raw:vault_a_amount_raw::float / 1e18) / POW(10, coingecko_prices_a.decimals) AS vault_a_amount_native
-    , (parquet_raw:vault_a_amount_raw::float / 1e18) / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price AS vault_a_amount_usd
-
+    , CASE
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_a::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN (parquet_raw:vault_a_amount_raw::float / 1e18) / POW(10, coingecko_prices_a.decimals) * coingecko_prices_b.price
+        WHEN LOWER(parquet_raw:token_address_a::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_a.price > 1.2 THEN (parquet_raw:vault_a_amount_raw::float / 1e18) / POW(10, coingecko_prices_a.decimals) * coingecko_prices_b.price
+        ELSE (parquet_raw:vault_a_amount_raw::float / 1e18) / POW(10, coingecko_prices_a.decimals) * coingecko_prices_a.price 
+    END AS vault_a_amount_usd
     , coingecko_prices_b.symbol AS symbol_b
-    , CASE 
-        WHEN parquet_raw:date::date = '2024-11-12' THEN coingecko_prices_a.price
-        ELSE coingecko_prices_b.price 
-    END AS price_b
+    , coingecko_prices_b.price AS price_b
     , coingecko_prices_b.contract_address AS coingecko_token_address_b
     , parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) AS amount_b_swapped_native
     , CASE
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_b::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_b::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
+        WHEN LOWER(parquet_raw:token_address_b::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_b.price > 1.2 THEN parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
         ELSE parquet_raw:amount_b_swapped_raw::float / POW(10, coingecko_prices_b.decimals) * coingecko_prices_b.price 
     END AS amount_b_swapped_usd
     , (parquet_raw:vault_b_amount_raw::float / 1e18) / POW(10, coingecko_prices_b.decimals) AS vault_b_amount_native
     , CASE
-        WHEN parquet_raw:date::date = '2024-11-12' AND parquet_raw:token_address_b::string = '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI' THEN (parquet_raw:vault_b_amount_raw::float / 1e18) / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
+        WHEN parquet_raw:date::date = '2024-11-12' AND LOWER(parquet_raw:token_address_b::string) = LOWER('0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI') THEN (parquet_raw:vault_b_amount_raw::float / 1e18) / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
+        WHEN LOWER(parquet_raw:token_address_b::string) = LOWER('0x909cba62ce96d54de25bec9502de5ca7b4f28901747bbf96b76c2e63ec5f1cba::coin::COIN') AND coingecko_prices_b.price > 1.2 THEN (parquet_raw:vault_b_amount_raw::float / 1e18) / POW(10, coingecko_prices_b.decimals) * coingecko_prices_a.price
         ELSE (parquet_raw:vault_b_amount_raw::float / 1e18) / POW(10, coingecko_prices_b.decimals) * coingecko_prices_b.price 
-    END AS vault_b_amount_usd
+    END AS vault_b_amount_usd, 
+    ROW_NUMBER() OVER (PARTITION BY parquet_raw:transaction_digest::string ORDER BY coingecko_prices_a.price DESC, coingecko_prices_b.price DESC) AS rn
 FROM {{ source('PROD_LANDING', 'raw_sui_fact_aftermath_dex_swaps_gold_parquet') }}
 LEFT JOIN coingecko_prices AS coingecko_prices_a
     ON coingecko_prices_a.date = parquet_raw:date::date
@@ -78,3 +89,4 @@ LEFT JOIN coingecko_prices AS coingecko_prices_fee
 LEFT JOIN coingecko_prices AS coingecko_prices_b
     ON coingecko_prices_b.date = parquet_raw:date::date
     AND lower(coingecko_prices_b.contract_address) = lower(parquet_raw:token_address_b::string)
+QUALIFY rn = 1
