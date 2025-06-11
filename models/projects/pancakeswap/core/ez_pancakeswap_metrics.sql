839fe3e3e70b260c7aa554e4517bf762c2f21154
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-11 08:36:11 -0400

    Had to change the metric names

--- models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
+++ models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
@@ -109,9 +109,9 @@ select
     , fees_revenue.fees as spot_fees
     , fees_revenue.fees as fees
     -- About 68% of fees go to LPs
-    , fees_revenue.service_cash_flow as service_fee_allocation
-    , fees_revenue.burned_cash_flow as burned_fee_allocation
-    , fees_revenue.treasury_cash_flow as treasury_fee_allocation
+    , fees_revenue.service_cash_flow as service_cash_flow
+    , fees_revenue.burned_cash_flow as burned_cash_flow
+    , fees_revenue.treasury_cash_flow as treasury_cash_flow
     -- TODO: the remaining 32% of fees are distributed differently depending on the fee tier of the pool. We currently have the fee tier in
     -- pancakeswap's ez_dex_swap. This needs to be pulled forward to the correct tables.
     -- The remaining fees are distributed among CAKE burns, Treasury, and Fixed Term CAKE Stakers
