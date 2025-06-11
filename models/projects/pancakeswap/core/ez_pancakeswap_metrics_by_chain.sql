7f4dc7f3106f685a32d44b61d87efc64d3de0df7
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-11 09:09:23 -0400

    Updated pancakeswap EZM by pool and EZM by chain

--- models/projects/pancakeswap/core/ez_pancakeswap_metrics_by_chain.sql
+++ models/projects/pancakeswap/core/ez_pancakeswap_metrics_by_chain.sql
@@ -69,6 +69,39 @@ with trading_volume_by_pool as (
     where chain = 'bsc'
     group by date
 )
+, fees_revenue as (
+    select
+        block_timestamp::date as date
+        , chain
+        , sum(trading_fees) as fees
+    -- This distribution is outlined here https://docs.pancakeswap.finance/products/pancakeswap-exchange/pancakeswap-pools
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.67
+                when fee_percent = 0.0005 then trading_fees * 0.66
+                when fee_percent = 0.0025 then trading_fees * 0.68
+                when fee_percent = 0.01 then trading_fees * 0.68
+            end
+        ) as service_cash_flow 
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.15
+                when fee_percent = 0.0005 then trading_fees * 0.15
+                when fee_percent = 0.0025 then trading_fees * 0.23
+                when fee_percent = 0.01 then trading_fees * 0.23 
+            end
+        ) as burned_cash_flow 
+        , sum(
+            case
+                when fee_percent = 0.0001 then trading_fees * 0.18
+                when fee_percent = 0.0005 then trading_fees * 0.19
+                when fee_percent = 0.0025 then trading_fees * 0.09
+                when fee_percent = 0.01 then trading_fees * 0.09
+            end
+        ) as treasury_cash_flow
+    from {{ ref('ez_pancakeswap_dex_swaps') }}
+    group by 1, 2
+)
 select
     tvl_by_chain.date
     , 'pancakeswap' as app
@@ -89,12 +122,15 @@ select
     , trading_volume_by_chain.gas_cost_native
 
     -- Cashflow Metrics
-    , trading_volume_by_chain.trading_fees as spot_fees
-    , trading_volume_by_chain.trading_fees as ecosystem_revenue
-    , trading_volume_by_chain.trading_fees * .68 as service_fee_allocation
+    , fees_revenue.fees as spot_fees
+    , fees_revenue.fees as fees
+    , fees_revenue.service_cash_flow as service_cash_flow
+    , fees_revenue.burned_cash_flow as burned_cash_flow
+    , fees_revenue.treasury_cash_flow as treasury_cash_flow
     , token_incentives.token_incentives_usd as token_incentives
 
 from tvl_by_chain
 left join trading_volume_by_chain using(date, chain)
 left join token_incentives using(date, chain)
+left join fees_revenue using(date, chain)
 where tvl_by_chain.date < to_date(sysdate())
