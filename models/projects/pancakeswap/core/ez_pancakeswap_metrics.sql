02d299bba09b0fcd70ea80aaea9c4d1386eb2206
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-11 17:30:57 -0400

    Removed the extra trading fees metric in EZ pancake swaps and renamed metrics

--- models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
+++ models/projects/pancakeswap/core/ez_pancakeswap_metrics.sql
@@ -77,7 +77,7 @@ with trading_volume_pool as (
                 when fee_percent = 0.0025 then trading_fees * 0.68
                 when fee_percent = 0.01 then trading_fees * 0.68
             end
-        ) as service_cash_flow 
+        ) as service_fee_allocation 
         , sum(
             case
                 when fee_percent = 0.0001 then trading_fees * 0.15
@@ -85,7 +85,7 @@ with trading_volume_pool as (
                 when fee_percent = 0.0025 then trading_fees * 0.23
                 when fee_percent = 0.01 then trading_fees * 0.23 
             end
-        ) as burned_cash_flow 
+        ) as burned_fee_allocation 
         , sum(
             case
                 when fee_percent = 0.0001 then trading_fees * 0.18
@@ -93,7 +93,7 @@ with trading_volume_pool as (
                 when fee_percent = 0.0025 then trading_fees * 0.09
                 when fee_percent = 0.01 then trading_fees * 0.09
             end
-        ) as treasury_cash_flow
+        ) as treasury_fee_allocation
     from {{ ref('ez_pancakeswap_dex_swaps') }}
     group by date
 )
@@ -106,14 +106,13 @@ select
     , trading_volume.trading_volume as spot_volume
     , trading_volume.unique_traders as spot_dau
     
-    , trading_volume.trading_fees as trading_fees
     , fees_revenue.fees as spot_fees
     , fees_revenue.fees as fees
     -- About 68% of fees go to LPs
-    , fees_revenue.service_cash_flow as service_cash_flow
-    , fees_revenue.burned_cash_flow as burned_cash_flow
-    , fees_revenue.treasury_cash_flow as treasury_cash_flow
-    , burned_cash_flow as revenue
+    , fees_revenue.service_fee_allocation
+    , fees_revenue.burned_fee_allocation
+    , fees_revenue.treasury_fee_allocation
+    , fees_revenue.burned_fee_allocation as revenue
     -- TODO: the remaining 32% of fees are distributed differently depending on the fee tier of the pool. We currently have the fee tier in
     -- pancakeswap's ez_dex_swap. This needs to be pulled forward to the correct tables.
     -- The remaining fees are distributed among CAKE burns, Treasury, and Fixed Term CAKE Stakers
