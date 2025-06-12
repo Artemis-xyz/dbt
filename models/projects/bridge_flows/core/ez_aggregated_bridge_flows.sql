888f4051646a97a9ce77642f4f66e2e517860c93
Son Do <sondo077@gmail.com>
2025-06-12 14:18:14 -0400

    Added ez_aggregated_bridge_flows - simplified flows data

---
+++ models/projects/bridge_flows/core/ez_aggregated_bridge_flows.sql
@@ -0,0 +1,18 @@
+{{
+    config (
+        materialized="table",
+        snowflake_warehouse="BRIDGE_FLOWS",
+        database="bridge_flows",
+        schema="core",
+        alias="ez_flows",
+    )
+}}
+select 
+    date
+    , source_chain
+    , destination_chain
+    , app 
+    , category -- token category type
+    , amount_usd
+from {{ ref('agg_daily_bridge_flows_metrics') }} 
+order by date desc 
