8efbaa60bcd76b627f901df54c3e0a8d410b953a
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-06-13 14:28:50 -0400

    Add Coingecko Historical Circulating Supply to Snowflake (#1537)

--- models/staging/coingecko/fact_coingecko_token_date_adjusted_gold.sql
+++ models/staging/coingecko/fact_coingecko_token_date_adjusted_gold.sql
@@ -4,5 +4,6 @@ select
     coingecko_id,
     shifted_token_price_usd,
     shifted_token_market_cap,
-    shifted_token_h24_volume_usd
+    shifted_token_h24_volume_usd,
+    shifted_token_circulating_supply
 from {{ ref("fact_coingecko_token_date_adjusted") }}
