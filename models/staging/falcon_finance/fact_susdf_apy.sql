a2e748a6c5dc4b39054507fe64df201ffe43a2df
Irwyn <irwynliong01@gmail.com>
2025-06-13 14:32:01 -0400

    fix logic

--- models/staging/falcon_finance/fact_susdf_apy.sql
+++ models/staging/falcon_finance/fact_susdf_apy.sql
@@ -3,8 +3,7 @@
 with base as (
   select
     source_json,
-    extraction_date,
-    source_url
+    extraction_date
   from {{ source("PROD_LANDING", "raw_susdf_apy") }}
 ),
 
@@ -17,11 +16,10 @@ flattened as (
 ),
 
 base_tvl as (
-    select
-        source_json,
-        extraction_date,
-        source_url
-    from {{ source("PROD_LANDING", "raw_susdf_tvl") }}
+  select
+    source_json,
+    extraction_date
+  from {{ source("PROD_LANDING", "raw_susdf_tvl") }}
 ),
 
 flattened_tvl as (
@@ -34,17 +32,39 @@ flattened_tvl as (
 
 extracted as (
   select
-    pool:date::timestamp as date,
-    pool:value::float as ratio,
-    lag(pool:value::float) over (order by pool:date::timestamp) as previous_ratio,
-  from flattened
+    date,
+    ratio,
+    lag(ratio) over (order by date) as previous_ratio
+  from (
+    select
+      pool:date::timestamp as date,
+      pool:value::float as ratio,
+      extraction_date,
+      row_number() over (
+        partition by pool:date::timestamp
+        order by extraction_date desc
+      ) as rnk
+    from flattened
+  )
+  where rnk = 1
 ),
 
 extracted_tvl as (
   select
-    pool:date::timestamp as date,
-    pool:value::float as tvl,
-  from flattened_tvl
+    date,
+    tvl
+  from (
+    select
+      pool:date::timestamp as date,
+      pool:value::float as tvl,
+      extraction_date,
+      row_number() over (
+        partition by pool:date::timestamp
+        order by extraction_date desc
+      ) as rnk
+    from flattened_tvl
+  )
+  where rnk = 1
 ),
 
 joined as (
@@ -54,21 +74,19 @@ joined as (
       when e.previous_ratio is null then 0
       else (power(e.ratio / e.previous_ratio, 365) - 1)
     end as apy,
-    et.tvl,
-    row_number() over (partition by e.date order by e.date desc) as rnk
+    et.tvl
   from extracted e
   join extracted_tvl et on e.date = et.date
 )
 
 select
-    'Staked USDf Vault' as name,
-    apy,
-    tvl,
-    array_construct('USDf') as symbol,
-    'USDf' as protocol,
-    'Vault' as type,
-    'ethereum' as chain,
-    'https://app.falcon.finance/earn/classic' as link,
-    date as extraction_timestamp
+  'Staked USDf Vault' as name,
+  apy,
+  tvl,
+  array_construct('USDf') as symbol,
+  'USDf' as protocol,
+  'Vault' as type,
+  'ethereum' as chain,
+  'https://app.falcon.finance/earn/classic' as link,
+  date as extraction_timestamp
 from joined
-where rnk = 1
