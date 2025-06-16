1f015662bfa4daac485fda303717ed0fe674f8be
Irwyn <irwynliong01@gmail.com>
2025-06-16 17:57:52 -0400

    staging heh

--- models/staging/ripple/fact_ripple_address_balances.sql
+++ models/staging/ripple/fact_ripple_address_balances.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized="incremental",
-        unique_key=["block_timestamp", "block_number", "transaction_index", "trust_line_index", "contract_address", "address", "balance_raw"],
+        unique_key=["block_timestamp", "block_number", "transaction_index", "trust_line_index", "contract_address", "address"],
         snowflake_warehouse="RIPPLE",
     )
 }}
@@ -13,7 +13,8 @@ select
     trust_line_index,
     token_address as contract_address,
     account_address as address,
-    balance as balance_raw
+    source_balance as balance_raw,
+    balance as balance_native,
 from {{ source("SONARX_XRP_BALANCES", "token_balances") }}
 {% if is_incremental() %}
     where block_timestamp >= (select dateadd(day, -3, max(block_timestamp)) from {{ this }})
