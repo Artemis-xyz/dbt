59f6601d89418ba7571bba7ccbb794a68be26f0e
horaceliuHL <horaceliu.hl@gmail.com>
2025-06-17 14:45:02 -0400

    Fix bug in iceberg model

--- iceberg/models/labels/dim_all_apps_changelog_iceberg.sql
+++ iceberg/models/labels/dim_all_apps_changelog_iceberg.sql
@@ -6,10 +6,9 @@
         schema="LABELS",
         external_volume="ICEBERG_EXTERNAL_VOLUME_INTERNAL",
         alias="dim_all_apps_changelog",
-        partition_by=["chain"],
         post_hook = "{{ merge_tags_dict({
             'duckdb': 'true',
-            'order_by': 'chain, artemis_application_id'
+            'order_by': 'artemis_application_id'
         }) }}"
     )
 }}
@@ -18,4 +17,4 @@ SELECT
     * EXCLUDE (last_updated_timestamp),
     last_updated_timestamp::TIMESTAMP_NTZ(6) AS last_updated_timestamp
 FROM PC_DBT_DB.PROD.DIM_ALL_APPS_CHANGELOG
-ORDER BY chain, artemis_application_id
+ORDER BY artemis_application_id
