4cc31785f7bb7e079f6d7f4d246b2edae29fdc96
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-06-23 16:08:58 -0400

    Added leo revenue metrics

---
+++ models/projects/leo/core/ez_leo_metrics.sql
@@ -0,0 +1,48 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="LEO",
+        database="leo",
+        schema="core",
+        alias="ez_metrics",
+    )
+}}
+
+with 
+    date_spine as (
+        SELECT * 
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date < to_date(sysdate()) AND date >= (SELECT MIN(date) FROM {{ ref("fact_leo_revenue") }})
+        
+    )
+
+    , leo_revenue as (
+        SELECT 
+            date, 
+            SUM(leo_burn_amount) AS revenue_native
+        FROM {{ ref("fact_leo_revenue") }}
+        GROUP BY 1
+    )
+
+    , market_data as (
+        {{ get_coingecko_metrics('leo-token') }}
+    )
+
+    select 
+        date
+        , revenue_native
+        , revenue_native * price as revenue_usd
+        , revenue_native AS burned_fee_allocation_native
+        , revenue_native * price as burned_fee_allocation
+        , revenue_native AS buybacks_native
+        , revenue_native * price AS buybacks
+        , revenue_native AS buyback_fee_allocation
+        , price
+        , market_cap
+        , fdmc
+        , token_turnover_circulating
+        , token_turnover_fdv
+        , token_volume
+    from date_spine
+    left join leo_revenue using (date)
+    left join market_data using (date)
---
+++ models/staging/leo/fact_leo_revenue.sql
@@ -0,0 +1,22 @@
+{{config(
+    materialized = 'table',
+    database = 'leo', 
+    snowflake_warehouse = 'leo'
+)}}
+
+with
+    max_extraction as (
+        select max(extraction_date) as max_date
+        from {{ source("PROD_LANDING", "raw_leo_burn_history") }}
+    )
+
+select
+    value:date::date as date,
+    value:timestamp_ms::timestamp as timestamp_ms,
+    value:chain::string as chain,
+    value:tx_hash::string as tx_hash,
+    value:leo_burn_amount::number as leo_burn_amount
+from
+    {{ source("PROD_LANDING", "raw_leo_burn_history") }},
+    lateral flatten(input => parse_json(source_json))
+where extraction_date = (select max_date from max_extraction)
