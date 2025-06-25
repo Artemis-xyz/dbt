931ec3d7a068c3a49b023fdb6c20587f3e56153c
Alex Weseley <pampawes@gmail.com>
2025-06-25 17:31:08 -0400

    refactor ez solana metrics + tweaks to sol supply models

--- models/projects/solana/core/ez_solana_metrics.sql
+++ models/projects/solana/core/ez_solana_metrics.sql
@@ -24,87 +24,6 @@ with
     nft_metrics as ({{ get_nft_metrics("solana") }}),
     p2p_metrics as ({{ get_p2p_metrics("solana") }}),
     rolling_metrics as ({{ get_rolling_active_address_metrics("solana") }}),
-    {% if not is_incremental() %}
-        unrefreshed_data as (
-            select
-                date_trunc('day', block_timestamp) as date,
-                sum(case when index = 0 then fee / pow(10, 9) else 0 end) gas,
-                median(case when index = 0 then fee / pow(10, 9) end) as median_txn_fee_native,
-                sum(
-                    case
-                        when index = 0 then (array_size(signers) * (5000 / 1e9)) else 0
-                    end
-                ) as base_fee_native,
-                count_if(index = 0) as txns,
-                count(
-                    distinct(case when succeeded = 'TRUE' then value else null end)
-                ) dau,
-                null as returning_users,
-                null as new_users
-            from
-                solana_flipside.core.fact_transactions,
-                lateral flatten(input => signers)
-            where
-                date_trunc('day', block_timestamp)
-                < (select min(raw_date) from {{ ref('fact_solana_transactions_v2') }})
-            group by date
-        ),
-        unrefreshed_data_with_price as (
-            select
-                unrefreshed_data.date,
-                gas,
-                gas * price as gas_usd,
-                base_fee_native,
-                txns,
-                dau,
-                returning_users,
-                new_users,
-                median_txn_fee_native * price as median_txn_fee
-            from unrefreshed_data
-            left join price on unrefreshed_data.date = price.date
-        ),
-    {% endif %}
-    min_date as (
-        select min(raw_date) as start_date, value as signer
-        from {{ ref('fact_solana_transactions_v2') }}, lateral flatten(input => signers)
-        where succeeded = 'TRUE'
-        group by signer
-    ),
-    new_users as (
-        select count(distinct signer) as new_users, start_date
-        from min_date
-        group by start_date
-    ),
-    voting_fees as (
-        select
-            date_trunc('day', block_timestamp) as date,
-            sum(num_votes * 5000) / pow(10, 9) as vote_tx_fee_native
-        from solana_flipside.gov.fact_votes_agg_block
-        {% if is_incremental() %}
-            where
-                date_trunc('day', block_timestamp)
-                > (select dateadd('day', -3, max(date)) from {{ this }})
-        {% endif %}
-        group by date
-    ),
-    agg_data as (
-        select
-            raw_date,
-            max(chain) as chain,
-            sum(case when index = 0 then tx_fee else 0 end) gas,
-            sum(case when index = 0 then gas_usd else 0 end) gas_usd,
-            median(case when index = 0 then gas_usd end) as median_txn_fee,
-            sum(
-                case when index = 0 then (array_size(signers) * (5000 / 1e9)) else 0 end
-            ) as base_fee_native,
-            count_if(index = 0) as txns,
-            count(distinct(case when succeeded = 'TRUE' then value else null end)) dau
-        from {{ ref('fact_solana_transactions_v2') }}, lateral flatten(input => signers)
-        {% if is_incremental() %}
-            where raw_date > (select dateadd('day', -3, max(date)) from {{ this }})
-        {% endif %}
-        group by raw_date
-    ),
     fundamental_usage as (
         select
             agg_data.raw_date as date,
@@ -115,23 +34,9 @@ with
             txns,
             dau,
             (dau - new_users) as returning_users,
-            new_users
-        from agg_data
-        left join new_users on date = new_users.start_date
-        {% if not is_incremental() %}
-            union
-            select
-                date,
-                gas,
-                gas_usd,
-                median_txn_fee,
-                base_fee_native,
-                txns,
-                dau,
-                returning_users,
-                new_users
-            from unrefreshed_data_with_price
-        {% endif %}
+            new_users,
+            vote_tx_fee_native
+        from {{ ref('fact_solana_fundamental_data') }}
     ), 
     solana_dex_volumes as (
         select date, daily_volume_usd as dex_volumes
@@ -230,7 +135,6 @@ select
 from fundamental_usage
 left join defillama_data on fundamental_usage.date = defillama_data.date
 left join stablecoin_data on fundamental_usage.date = stablecoin_data.date
-left join voting_fees on fundamental_usage.date = voting_fees.date
 left join price on fundamental_usage.date = price.date
 left join github_data on fundamental_usage.date = github_data.date
 left join contract_data on fundamental_usage.date = contract_data.date
--- models/staging/solana/fact_solana_supply_data.sql
+++ models/staging/solana/fact_solana_supply_data.sql
@@ -10,7 +10,7 @@ with base as (
         v.date
         , sum(amount_unlocked) as amount_unlocked
         , sum(cumulative_unlocked) as cumulative_unlocked
-    FROM fact_solana_vesting_schedule v
+    FROM {{ ref('fact_solana_vesting_schedule') }} v
     GROUP BY 1
     
 )
@@ -22,11 +22,11 @@ SELECT
     , issuance
     , 500e6 as initial_supply
     , sum(issuance) OVER (ORDER BY date ASC) as cumulative_issuance
-    , sum(burned_fee_allocation_native) over (order by base.date ASC) as burns_cumulative
+    , sum(IFF(fundamental_usage.date < '2025-02-13', fees_native * .5, (base_fee_native + vote_tx_fee_native) * .5)) over (order by base.date ASC) as burns_cumulative
     , 500e6 + coalesce(cumulative_issuance,0) - coalesce(burns_cumulative,0) as total_supply
     , 52300000 as estimated_foundation_holdings
     , 500e6 + coalesce(cumulative_issuance,0) - 52300000 - coalesce(burns_cumulative,0) as issued_supply
     , coalesce(cumulative_unlocked,0) + coalesce(cumulative_issuance,0) - coalesce(burns_cumulative,0) - 52300000 as circulating_supply
 FROM base 
-LEFT JOIN fact_solana_issuance_silver USING(date)
-LEFT JOIN solana.prod_core.ez_metrics USING(date)
+LEFT JOIN {{ ref('fact_solana_issuance_silver') }} USING(date)
+LEFT JOIN {{ ref('ez_solana_metrics') }} USING(date)
