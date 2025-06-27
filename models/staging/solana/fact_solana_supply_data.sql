48c731c4b0874dc0ca1342f1913a5176654b0200
Alex Weseley <pampawes@gmail.com>
2025-06-27 17:18:14 -0400

    add sol one time burn

--- models/staging/solana/fact_solana_supply_data.sql
+++ models/staging/solana/fact_solana_supply_data.sql
@@ -15,6 +15,11 @@ with base as (
     GROUP BY 1
     
 )
+, one_time_burn as (
+    SELECT
+        '2020-05-31' as date,
+        11365067 as sol_one_time_burn
+)
 SELECT
     b.date
     , b.amount_unlocked
@@ -23,11 +28,14 @@ SELECT
     , i.issuance
     , 500e6 as initial_supply
     , sum(i.issuance) OVER (ORDER BY b.date ASC) as cumulative_issuance
-    , sum(IFF(f.date < '2025-02-13', (f.gas + f.vote_tx_fee_native) * .5, (f.base_fee_native + f.vote_tx_fee_native) * .5)) over (order by b.date ASC) as burns_cumulative
+    , sum(IFF(f.date < '2025-02-13', (f.gas + f.vote_tx_fee_native) * .5, (f.base_fee_native + f.vote_tx_fee_native) * .5) ) over (order by b.date ASC)  as burns_cumulative
+    , SUM(coalesce(otb.sol_one_time_burn, 0) ) OVER (ORDER BY b.date ASC) as one_time_burns_cumulative
     , 500e6 + coalesce(cumulative_issuance,0) - coalesce(burns_cumulative,0) as total_supply
     , 52300000 as estimated_foundation_holdings
-    , 500e6 + coalesce(cumulative_issuance,0) - 52300000 - coalesce(burns_cumulative,0) as issued_supply
-    , 500e6 - coalesce(unvested_tokens,0) + coalesce(cumulative_issuance,0) - coalesce(burns_cumulative,0) - 52300000 as circulating_supply
+    , 500e6 + coalesce(cumulative_issuance,0) - 52300000 - coalesce(burns_cumulative,0) - coalesce(one_time_burns_cumulative, 0) as issued_supply
+    , 447650000.00000000048 - cumulative_unlocked as unvested_insider_tokens
+    , initial_supply - unvested_insider_tokens + coalesce(cumulative_issuance,0) - coalesce(burns_cumulative,0) - 52300000 - coalesce(one_time_burns_cumulative, 0) as circulating_supply
 FROM base b
-LEFT JOIN {{ ref('fact_solana_issuance_silver') }}  i USING(date)      
-LEFT JOIN {{ ref('fact_solana_fundamental_data') }} f USING(date)
+LEFT JOIN  fact_solana_issuance_silver   i USING(date)      
+LEFT JOIN  fact_solana_fundamental_data f USING(date)
+LEFT JOIN one_time_burn otb USING(date)
