7aade76db85d0545f85d78a5aed62d4c4c28a207
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-06-30 16:40:49 -0400

    Clean up macro logic and add comments

--- macros/developer/latest_developer_data_date.sql
+++ macros/developer/latest_developer_data_date.sql
@@ -1,6 +1,7 @@
 {% macro latest_developer_data_date() -%}
+    -- Get the date of the most recent Sunday and then subtract 14 days
     DATEADD('day', 
-        -7 - ((DAYOFWEEKISO(CURRENT_DATE()) % 7) + 7), 
+        -14 - ((DAYOFWEEKISO(CURRENT_DATE()) % 7)), 
         CURRENT_DATE()
     )
 {%- endmacro %}
