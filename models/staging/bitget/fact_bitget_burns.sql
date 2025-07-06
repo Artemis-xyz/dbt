2ea9c4b52ca08c8b18faa539a104960fe065e6a6
kevin123lhr <kevinli00@yahoo.com>
2025-07-05 20:17:49 -0400

    bgb issued/float supply and rev

---
+++ models/staging/bitget/fact_bitget_burns.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized="table",
+        snowflake_warehouse="BITGET"
+    )
+}}
+
+select
+    t.block_timestamp::date as date,
+    sum(t.raw_amount_precise) / 1e18 as burns_native,
+    sum(t.raw_amount_precise * p.price) / 1e18 as revenue
+FROM 
+    ethereum_flipside.core.ez_token_transfers t
+LEFT JOIN  ethereum_flipside.price.ez_prices_hourly p
+ON t.block_timestamp::date = p.hour
+AND lower(p.token_address) = lower('0x54D2252757e1672EEaD234D27B1270728fF90581')
+WHERE 1=1
+AND lower(t.contract_address) = lower('0x54D2252757e1672EEaD234D27B1270728fF90581')
+AND t.to_address = '0x000000000000000000000000000000000000dead'
+GROUP BY 1
+ORDER BY 1 DESC
