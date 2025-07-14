fdc2512b76a3852103f8885197caf793d5ba60c2
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-14 15:33:52 -0400

    Fixed Polygon revenue pricing

--- models/metrics/revenue/agg_daily_polygon_revenue.sql
+++ models/metrics/revenue/agg_daily_polygon_revenue.sql
@@ -1,7 +1,8 @@
 {{ config(materialized="table") }}
 
 with
-    prices as ({{ get_coingecko_price_with_latest("matic-network") }}),
+    matic_prices as ({{ get_coingecko_price_with_latest("matic-network") }}),
+    pol_prices as ({{ get_coingecko_price_with_latest("polygon-ecosystem-token") }}),
     chain_rev as (
         select
             date_trunc('day', block_timestamp) date,
@@ -13,7 +14,11 @@ select
     'polygon' as chain,
     chain_rev.date as date,
     coalesce(native_token_burn, 0) as native_token_burn,
-    coalesce(native_token_burn * price, 0) as revenue
+    CASE
+        WHEN chain_rev.date < '2024-09-04' THEN coalesce(native_token_burn * matic_prices.price, 0) 
+        WHEN chain_rev.date >= '2024-09-04' THEN coalesce(native_token_burn * pol_prices.price, 0)
+    END as revenue
 from chain_rev
-left join prices on chain_rev.date = prices.date
+left join matic_prices on chain_rev.date = matic_prices.date
+left join pol_prices on chain_rev.date = pol_prices.date
 order by date desc
