e7e49c88b3351f665dbf9d6c9c586bd7cfa6fa7c
Alex Weseley <pampawes@gmail.com>
2025-07-16 00:54:58 -0400

    updates to seed, ez metrics, and add token to treasury table

--- models/projects/pendle/raw/fact_pendle_treasury.sql
+++ models/projects/pendle/raw/fact_pendle_treasury.sql
@@ -1,11 +1,43 @@
 {{
     config(
         materialized="table",
-        snowflake_warehouse="PENDLE",
+        snowflake_warehouse="ANALYTICS_XL",
         database="pendle",
         schema="raw",
         alias="fact_pendle_treasury",
     )
 }}
 
-{{ get_treasury_balance('ethereum', '0x8270400d528c34e1596EF367eeDEc99080A1b592', '2021-06-21') }}
+with treasury_distributor as (
+    {{ forward_filled_balance_for_address('ethereum', '0x399be606db281a054e359eb709df9f21e922ec9a') }}
+)
+, treasury_vester as (
+     {{ forward_filled_balance_for_address('ethereum', '0xc21A74c7150fed22C7CA0Bf9A15BBe0DdB4977CC') }}
+)
+SELECT
+    date
+    , 'PENDLE' as token
+    , contract_address
+    , address
+    , balance_raw
+    , balance_native
+    , price
+    , balance
+    , chain
+    , unique_id
+FROM treasury_distributor
+WHERE contract_address = '0x808507121b80c02388fad14726482e061b8da827'
+UNION ALL
+SELECT
+    date
+    , 'PENDLE' as token
+    , contract_address
+    , address
+    , balance_raw
+    , balance_native
+    , price
+    , balance
+    , chain
+    , unique_id
+FROM treasury_vester
+WHERE contract_address = '0x808507121b80c02388fad14726482e061b8da827'
--- models/staging/pendle/fact_pendle_supply_data.sql
+++ models/staging/pendle/fact_pendle_supply_data.sql
@@ -8,9 +8,9 @@
 with emissions as (
     SELECT
         date,
-        sum(token_incentives_native) as emissions_native
+        sum(amt_pendle) as emissions
     FROM
-        {{ ref('fact_pendle_token_incentives_by_chain')}}
+        {{ ref('fact_pendle_token_incentives_by_chain_silver')}}
     GROUP BY 1
 )
 , ve_pendle_locked as (
@@ -21,22 +21,47 @@ with emissions as (
     WHERE contract_address = '0x808507121b80c02388fad14726482e061b8da827'
     GROUP BY 1
 )
+, total_supply as (
+    SELECT
+        block_timestamp::date as date,
+        sum(amount) as minted_amount
+    FROM
+        ethereum_flipside.core.ez_token_transfers
+    WHERE 
+        FROM_ADDRESS = lower('0x0000000000000000000000000000000000000000')
+        AND contract_address = lower('0x808507121B80c02388fAd14726482e061B8da827')
+    GROUP BY 1
+)
+, ecosystem_fund_holdings as (
+    SELECT
+        date,
+        SUM(balance_native) as ecosystem_fund_balance
+    FROM {{ ref('fact_pendle_treasury') }}
+    WHERE contract_address = '0x808507121b80c02388fad14726482e061b8da827'
+    GROUP BY 1
+)
 , date_spine as (
     SELECT
         date
     FROM
         {{ ref('dim_date_spine')}}
-    WHERE date between '2021-04-27' and to_date(sysdate())
+    WHERE date between '2021-04-27' and to_date(sysdate()) -- TGE to present
 )
 SELECT
-    ds.date,
-    coalesce(advisors + liquidity_boostrapping + investors + ecosystem_fund + team, 0) as unlocks_native,
-    sum(unlocks_native) over (order by date asc) as unlocks_native_cumulative,
-    coalesce(emissions_native, 0) as emissions_native,
-    sum(emissions_native) over (order by date asc) as emissions_native_cumulative,
-    coalesce(ve_pendle_balance, 0) as pendle_locked,
-    unlocks_native_cumulative + emissions_native_cumulative - pendle_locked as circulating_supply
+    ds.date
+    , coalesce(ecosystem_fund_balance, 0) as foundation_balance
+    , coalesce(advisors + liquidity_boostrapping + investors + team, 0) as unlocks_native
+    , 111.5 * 1e6  as total_unlock_allocation
+    , sum(unlocks_native) over (order by date asc) as unlocks_native_cumulative
+    , coalesce(emissions, 0) as emissions_native
+    , sum(emissions_native) over (order by date asc) as emissions_native_cumulative
+    , coalesce(ve_pendle_balance, 0) as pendle_locked
+    , sum(minted_amount) over (order by date asc) as total_supply_native
+    , total_unlock_allocation + emissions_native_cumulative - pendle_locked - foundation_balance as issued_supply_native
+    , unlocks_native_cumulative + emissions_native_cumulative - pendle_locked - foundation_balance as circulating_supply_native
 FROM date_spine ds
 LEFT JOIN {{ source("MANUAL_STATIC_TABLES", "pendle_unlocks_seed") }} using(date)
 LEFT JOIN emissions using(date)
 LEFT JOIN ve_pendle_locked using(date)
+LEFT JOIN total_supply using(date)
+LEFT JOIN ecosystem_fund_holdings using(date)
