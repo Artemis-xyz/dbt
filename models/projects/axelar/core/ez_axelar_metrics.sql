441c039d8f61a3c8b6f27f6f9b7ebf6b80019bc4
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-22 16:21:27 -0400

    Edited bridge metrics fact table to account for the API sending different data post-April, and standardized EZM

--- models/projects/axelar/core/ez_axelar_metrics.sql
+++ models/projects/axelar/core/ez_axelar_metrics.sql
@@ -38,60 +38,60 @@ with
         from {{ ref("fact_axelar_validator_fees") }}
     )
     , github_data as ({{ get_github_metrics("Axelar Network") }})
-    , price_data as ({{ get_coingecko_metrics("axelar") }})
+    , market_data as ({{ get_coingecko_metrics("axelar") }})
     , supply_data as (
         select * 
         from {{ ref("fact_axelar_supply") }}
     )
 select 
     crosschain_data.date
-    , 'axelar' as chain
-    
-    , axelar_chain_data.dau
-    , axelar_chain_data.txns
-    , crosschain_data.bridge_daa
-
-    , crosschain_data.fees
-    , crosschain_data.fees / crosschain_data.bridge_txns as avg_txn_fee
-    , validator_fees_data.validator_fees
-    
 
     -- Standardized Metrics
+
+    -- Market Data
+    , market_data.price as price
+    , market_data.market_cap as market_cap
+    , market_data.fdmc as fdmc
+    , market_data.token_volume as token_volume
+
+    -- Usage Data 
     , crosschain_data.bridge_daa as bridge_dau
-    , crosschain_data.bridge_volume as bridge_volume
-    , crosschain_data.bridge_txns
-
     , axelar_chain_data.dau as chain_dau
+    , coalesce(crosschain_data.bridge_daa, 0) + coalesce(axelar_chain_data.dau, 0) as dau
+    , crosschain_data.bridge_txns
     , axelar_chain_data.txns as chain_txns
+    , coalesce(crosschain_data.bridge_txns, 0) + coalesce(axelar_chain_data.txns, 0) as txns
+    , crosschain_data.bridge_volume as bridge_volume
+    , crosschain_data.bridge_volume as volume
 
+    -- Fee Data
     , crosschain_data.fees as bridge_fees
     , validator_fees_data.validator_fees as chain_fees
-    , crosschain_data.fees as ecosystem_revenue
-    , crosschain_data.fees / crosschain_data.bridge_txns as chain_avg_txn_fee
-    , validator_fees_data.validator_fees as validator_fee_allocation
+    , coalesce(crosschain_data.fees, 0) + coalesce(validator_fees_data.validator_fees, 0) as fees
+    , coalesce(supply_data.totalBurned, 0) * market_data.price as burned_fee_allocation
+
+    -- Financial Statements
+    , coalesce(supply_data.totalBurned, 0) * market_data.price as revenue
+
+    -- Supply Data
     , mints_data.mints as gross_emissions_native
-    , coalesce(supply_data.totalBurned, 0) as burned_fee_allocation_native
-    , coalesce(supply_data.totalBurned, 0) * price_data.price as burned_fee_allocation
-
-
-    , price_data.price as price
-    , price_data.market_cap as market_cap
-    , price_data.fdmc as fdmc
-    , price_data.token_turnover_circulating as token_turnover_circulating
-    , price_data.token_turnover_fdv as token_turnover_fdv
-    , price_data.token_volume as token_volume
-
     , coalesce(supply_data.circulatingSupply, 0) - lag(coalesce(supply_data.circulatingSupply, 0)) over (order by crosschain_data.date) as net_supply_change_native
     , coalesce(supply_data.circulatingSupply, 0) as circulating_supply_native
 
+    -- Developer Data
     , github_data.weekly_commits_core_ecosystem
     , github_data.weekly_commits_sub_ecosystem
     , github_data.weekly_developers_core_ecosystem
     , github_data.weekly_developers_sub_ecosystem
+
+    -- Token Turnover Data
+    , market_data.token_turnover_circulating as token_turnover_circulating
+    , market_data.token_turnover_fdv as token_turnover_fdv
+
 from crosschain_data
 left join axelar_chain_data using (date)
 left join github_data using (date)
-left join price_data using (date)
+left join market_data using (date)
 left join validator_fees_data using (date)
 left join mints_data using (date)
 left join supply_data using (date)
--- models/staging/axelar/fact_axelar_crosschain_dau_txns_fees_volume.sql
+++ models/staging/axelar/fact_axelar_crosschain_dau_txns_fees_volume.sql
@@ -1,26 +1,46 @@
-with
-max_extraction as (
-    select max(extraction_date) as max_date
-    from {{ source("PROD_LANDING", "raw_axelar_dau_txns_volume_fees") }}
-)
-,latest_data as (
-    select 
-        date(value:timestamp) as date
-        , value:users::number as bridge_daa
-        , value:fee::number as fees
-        , value:num_txs::number as bridge_txns
-        , value:volume::number as volume
-    from {{ source("PROD_LANDING", "raw_axelar_dau_txns_volume_fees") }},
-    lateral flatten(input => parse_json(source_json):data)
-    where extraction_date = (select max_date from max_extraction)
-    and value:timestamp <> 0
-)
-select
+{{ config(
+    materialized="table",
+    snowflake_warehouse="AXELAR"
+) }}
+
+WITH
+    max_extraction AS (
+        SELECT MAX(extraction_date) AS max_date
+        FROM {{ source("PROD_LANDING", "raw_axelar_dau_txns_volume_fees") }}
+    )
+    , latest_data AS (
+        SELECT 
+            DATE(value:timestamp) AS date
+            , NULL AS bridge_daa
+            , NULL AS fees
+            , value:num_txs::number AS bridge_txns
+            , value:volume::number AS volume
+        FROM LANDING_DATABASE.PROD_LANDING.raw_axelar_dau_txns_volume_fees,
+        LATERAL FLATTEN(input => parse_json(source_json):data)
+        WHERE DATE(extraction_date) = '2025-07-22' AND DATE(value:timestamp) > '2025-04-28'
+        AND value:timestamp <> 0
+
+        UNION ALL 
+
+        SELECT 
+            DATE(value:timestamp) AS date
+            , value:users::number AS bridge_daa
+            , value:fee::number AS fees
+            , value:num_txs::number AS bridge_txns
+            , value:volume::number AS volume
+        FROM LANDING_DATABASE.PROD_LANDING.raw_axelar_dau_txns_volume_fees,
+        LATERAL FLATTEN(input => parse_json(source_json):data)
+        WHERE DATE(extraction_date) = '2025-04-28' AND DATE(value:timestamp) <= '2025-04-28'
+        AND value:timestamp <> 0
+    )
+
+SELECT
     date
     , bridge_daa
     , fees
     , bridge_txns
     , volume
-    , 'axelar' as chain
-    , 'axelar' as app
-from latest_data
+    , 'axelar' AS chain
+    , 'axelar' AS app
+FROM latest_data
+
