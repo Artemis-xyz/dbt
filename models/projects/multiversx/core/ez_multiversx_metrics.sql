bb10134cc5262ee55c681fca72fa8bbb5e8006e7
Alex Weseley <pampawes@gmail.com>
2025-07-22 23:54:42 -0400

    multiversx and schema

--- models/projects/multiversx/core/ez_multiversx_metrics.sql
+++ models/projects/multiversx/core/ez_multiversx_metrics.sql
@@ -13,7 +13,7 @@ with
         select
             date
             , txns
-            , daa as dau
+            , daa::number as dau
             , gas as fees_native
             , gas_usd as fees
             , case when txns > 0 then fees / txns end as avg_txn_fee
@@ -27,13 +27,7 @@ with
 
 select
     f.date
-    , 'multiversx' as chain
-    , txns
-    , dau
-    , fees
-    , fees_native
-    , avg_txn_fee
-    , dex_volumes
+
     -- Standardized Metrics
     -- Market Data
     , price
@@ -41,24 +35,29 @@ select
     , fdmc
     , token_volume
     -- Chain Metrics
-    , txns as chain_txns
-    , dau::number as chain_dau
-    , avg_txn_fee as chain_avg_txn_fee
-    , dex_volumes as chain_spot_volume
-    -- Cash Flow Metrics
-    , fees as ecosystem_revenue
-    , fees_native as ecosystem_revenue_native
-    -- Crypto Metrics
-    , tvl
+    , f.txns as chain_txns
+    , f.txns
+    , f.dau as chain_dau
+    , f.dau
+    , f.avg_txn_fee as chain_avg_txn_fee
+    , dfl.dex_volumes as chain_spot_volume
+    , dfl.tvl
+
+    -- Fees Metrics
+    , f.fees
+    , f.fees_native
+
     -- Developer Metrics
     , weekly_commits_core_ecosystem
     , weekly_commits_sub_ecosystem
     , weekly_developers_core_ecosystem
     , weekly_developers_sub_ecosystem
+
+    -- Other Metrics
     , token_turnover_circulating
     , token_turnover_fdv
 from fundamental_data f
 left join github_data using (f.date)
-left join defillama_data using (f.date)
+left join defillama_data dfl using (f.date)
 left join price_data using (f.date)
 where f.date < to_date(sysdate())
