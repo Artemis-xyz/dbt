8cad16e3f69c48912aa6989e6ed19c616e3ec48e
kaviish <kaviishvsethi@gmail.com>
2025-07-23 15:16:55 -0400

    kav/stride_metric_deprecation

--- models/projects/stride/core/ez_stride_metrics.sql
+++ models/projects/stride/core/ez_stride_metrics.sql
@@ -15,59 +15,52 @@ with
     market_data as ({{ get_coingecko_metrics("stride") }})
 
 select
-    fundamental_data.date,
-    'stride' as app,
-    'DeFi' as category,
+    fundamental_data.date
+    , 'stride' as app
+    , 'DeFi' as category
     
-    --Old metrics needed for compatibility
-    fundamental_data.txns,
-    fundamental_data.dau,
-    fundamental_data.wau,
-    fundamental_data.mau,
-    fundamental_data.fees_native AS gas_fees_native,
-    fundamental_data.fees_usd AS gas_fees_usd,
-    fundamental_data.avg_txn_fee,
-    fundamental_data.total_staking_yield_usd,
-    fundamental_data.total_supply_side_revenue_usd,
-    fundamental_data.protocol_revenue_usd AS fees,
-    fundamental_data.protocol_revenue_usd AS revenue,
-    fundamental_data.operating_expenses_usd,
-    fundamental_data.protocol_earnings_usd AS earnings
-
-    --Standardized Metrics
-    --Market Metrics
+    --Market Data
     , market_data.price
     , market_data.market_cap
     , market_data.fdmc
     , market_data.token_volume
 
-    --Chain Usage Metrics
+    --Chain Data
     , fundamental_data.dau as chain_dau
     , fundamental_data.wau as chain_wau
     , fundamental_data.mau as chain_mau
-    , fundamental_data.txns as chain_txns
     , fundamental_data.returning_users as returning_users
     , fundamental_data.new_users as new_users
     , fundamental_data.low_sleep_users as low_sleep_users
     , fundamental_data.high_sleep_users as high_sleep_users
     , fundamental_data.sybil_users as sybil_users
     , fundamental_data.non_sybil_users as non_sybil_users
-    , fundamental_data.tvl as tvl
-    , fundamental_data.tvl_net_change as tvl_net_change
+    , fundamental_data.txns as chain_txns
     , fundamental_data.tvl as lst_tvl
-    , fundamental_data.tvl_net_change as lst_tvl_net_change
+    , fundamental_data.tvl as tvl
 
-    --Cashflow Metrics
+    -- Fee Data
+    , fundamental_data.avg_txn_fee
+    , fundamental_data.fees_native as chain_fees_native
     , fundamental_data.fees_usd as chain_fees
     , fundamental_data.total_staking_yield_usd as yield_generated
-    , chain_fees + yield_generated as ecosystem_revenue
-    , (yield_generated * .1) as treasury_fee_allocation
-    , (yield_generated * .9) as service_fee_allocation
-    , chain_fees as validator_fee_allocation
+    , (fundamental_data.total_staking_yield_usd * .1) as staking_reward_fees
+    , chain_fees + staking_reward_fees as fees
+
+    -- Fee allocations
+    , (fundamental_data.total_staking_yield_usd * .08) as burn_fee_allocation
+    , (fundamental_data.total_staking_yield_usd * .02) as service_fee_allocation
+
+    -- Financial Statement Metrics
+    , fundamental_data.total_supply_side_revenue_usd
+    , fundamental_data.protocol_revenue_usd * 0.8 as revenue
+    , 0 as operating_expenses_usd
+    , fundamental_data.protocol_revenue_usd * 0.8  AS earnings
 
     -- Other Metrics
     , market_data.token_turnover_circulating
     , market_data.token_turnover_fdv
+
 from fundamental_data
 left join market_data on fundamental_data.date = market_data.date
 where fundamental_data.date < to_date(sysdate())
--- models/projects/stride/core/ez_stride_metrics_by_chain.sql
+++ models/projects/stride/core/ez_stride_metrics_by_chain.sql
@@ -13,36 +13,12 @@ with
     fundamental_data as (select * EXCLUDE date, TO_TIMESTAMP_NTZ(date) AS date from {{ source('PROD_LANDING', 'ez_stride_metrics') }})
 
 select
-    fundamental_data.date,
-    'stride' as app,
-    'DeFi' as category,
-    fundamental_data.chain,
+    fundamental_data.date
+    , 'stride' as app
+    , 'DeFi' as category
+    , fundamental_data.chain
     
-    --Old metrics needed for compatibility
-    fundamental_data.txns,
-    fundamental_data.dau,
-    fundamental_data.wau,
-    fundamental_data.mau,
-    fundamental_data.tvl,
-    fundamental_data.tvl_net_change,
-    fundamental_data.fees_native AS gas_fees_native,
-    fundamental_data.fees_usd AS gas_fees_usd,
-    fundamental_data.avg_txn_fee,
-    fundamental_data.returning_users,
-    fundamental_data.new_users,
-    fundamental_data.low_sleep_users,
-    fundamental_data.high_sleep_users,
-    fundamental_data.sybil_users,
-    fundamental_data.non_sybil_users,
-    fundamental_data.total_staking_yield_usd,
-    fundamental_data.total_supply_side_revenue_usd,
-    fundamental_data.protocol_revenue_usd AS revenue,
-    fundamental_data.operating_expenses_usd,
-    fundamental_data.protocol_earnings_usd AS earnings
-
-    --Standardized Metrics
-    
-    --Chain Usage Metrics
+    --Chain Data
     , fundamental_data.dau as chain_dau
     , fundamental_data.wau as chain_wau
     , fundamental_data.mau as chain_mau
@@ -51,16 +27,28 @@ select
     , fundamental_data.new_users as chain_new_users
     , fundamental_data.low_sleep_users as chain_low_sleep_users
     , fundamental_data.avg_txn_fee as chain_avg_txn_fee
+    , fundamental_data.sybil_users
+    , fundamental_data.non_sybil_users
     , fundamental_data.tvl as lst_tvl
-    , fundamental_data.tvl_net_change as lst_tvl_net_change
-
+    , fundamental_data.tvl as tvl
     
-    --Cashflow Metrics
-    , fundamental_data.fees_usd as chain_fees
+    --Fee Data
+    , fundamental_data.avg_txn_fee
+    , fundamental_data.fees_native AS chain_fees_native
+    , fundamental_data.fees_usd AS chain_fees
     , fundamental_data.total_staking_yield_usd as yield_generated
-    , chain_fees + fundamental_data.protocol_revenue_usd AS fees
-    , (yield_generated * .1) as treasury_fee_allocation
-    , (yield_generated * .9) as service_fee_allocation
-    , chain_fees as validator_fee_allocation    
+    , (fundamental_data.total_staking_yield_usd * .1) as staking_reward_fees
+    , chain_fees + staking_reward_fees as fees
+
+    --Fee Allocations
+    , (fundamental_data.total_staking_yield_usd * .08) as burn_fee_allocation
+    , (fundamental_data.total_staking_yield_usd * .02) as service_fee_allocation
+
+    --Financial Statement Metrics
+    , fundamental_data.total_supply_side_revenue_usd
+    , fundamental_data.protocol_revenue_usd * 0.8 as revenue
+    , 0 as operating_expenses_usd
+    , fundamental_data.protocol_revenue_usd * 0.8  AS earnings
+
     
 from fundamental_data
