1b014a4ce323a22b81043deabf15c9d6a573593a
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-23 11:25:15 -0400

    Standardized metrics for blast

--- models/projects/blast/core/ez_blast_metrics.sql
+++ models/projects/blast/core/ez_blast_metrics.sql
@@ -12,7 +12,7 @@
 with
     fundamental_data as ({{ get_goldsky_chain_fundamental_metrics("blast", "_v2") }})
     , defillama_data as ({{ get_defillama_metrics("blast") }})
-    , price_data as ({{ get_coingecko_metrics("blast") }})
+    , market_data as ({{ get_coingecko_metrics("blast") }})
     , eth_price as ({{ get_coingecko_metrics("ethereum") }})
     , contract_data as ({{ get_contract_metrics("blast") }})
     -- NOTE, this says l1 data cost, but that's inaccurate
@@ -38,39 +38,33 @@ select
         contract_data.date,
         expenses_data.date
     )) as date
-    , 'blast' as chain
-    , txns
-    , daa as dau
-    , wau
-    , mau
-    , fees_native
-    , fees_native * eth_price.price AS fees
-    , fees / txns as avg_txn_fee
+
     , l1_data_cost_native
     , l1_data_cost
     , coalesce(fees_native, 0) - l1_data_cost_native as revenue_native  -- supply side: fees paid to squencer - fees paied to l1 (L2 Revenue)
     , coalesce(fees, 0) - l1_data_cost as revenue
-    -- , dau_over_100 omitting balances for blast
-    , dune_dex_volumes_blast.dex_volumes as dex_volumes
-    , dune_dex_volumes_blast.adjusted_dex_volumes as adjusted_dex_volumes
+
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price_data.price
-    , price_data.market_cap
-    , price_data.fdmc
+
+    -- Market Data 
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume as token_volume
     , tvl
-    -- Chain Usage metrics
-    , txns as chain_txns
-    , dau AS chain_dau
-    , wau AS chain_wau
-    , mau AS chain_mau
-    , avg_txn_fee AS chain_avg_txn_fee
-    -- , returning_users
-    -- , new_users
-    -- , low_sleep_users
-    -- , high_sleep_users
-    -- , dau_over_100 AS dau_over_100_balance
+
+    -- Usage Data
+    , daa AS chain_dau
+    , daa AS dau
+    , txns AS chain_txns
+    , txns 
     , dune_dex_volumes_blast.dex_volumes as chain_spot_volume
+    , dune_dex_volumes_blast.adjusted_dex_volumes as adjusted_chain_spot_volume
+
+    -- Fee Data
+    , fees_native
+    , fees_native * eth_price.price AS fees
+  
     -- Cashflow metrics
     , fees_native AS ecosystem_revenue_native
     , fees AS ecosystem_revenue
@@ -80,21 +74,26 @@ select
     , l1_data_cost AS l1_fee_allocation
     , revenue_native AS foundation_fee_allocation_native
     , revenue AS foundation_fee_allocation
-    -- Developer metrics
-    , weekly_contracts_deployed
-    , weekly_contract_deployers
 
     -- Supply Metrics
     , premine_unlocks_native
     , premine_unlocks_native as net_supply_change_native
     , circulating_supply_native
+
+    -- Developer metrics
+    , weekly_contracts_deployed
+    , weekly_contract_deployers
+
+    -- Turnover data
+    , market_data.token_turnover_circulating as token_turnover_circulating
+    , market_data.token_turnover_fdv as token_turnover_fdv
 from fundamental_data
 left join defillama_data on fundamental_data.date = defillama_data.date
 left join contract_data on fundamental_data.date = contract_data.date
 left join expenses_data on fundamental_data.date = expenses_data.date
 left join rolling_metrics on fundamental_data.date = rolling_metrics.date
 left join blast_dex_volumes as dune_dex_volumes_blast on fundamental_data.date = dune_dex_volumes_blast.date
-left join price_data on fundamental_data.date = price_data.date
+left join market_data on fundamental_data.date = market_data.date
 left join eth_price on fundamental_data.date = eth_price.date
 left join premine_emissions on fundamental_data.date = premine_emissions.date
 where fundamental_data.date < to_date(sysdate())
