46404ed1e4997f7ad8725334b1f8529aefa07e9b
Alex Weseley <pampawes@gmail.com>
2025-07-23 00:06:40 -0400

    worldchain schemas

--- models/projects/worldchain/core/ez_worldchain_metrics.sql
+++ models/projects/worldchain/core/ez_worldchain_metrics.sql
@@ -9,43 +9,47 @@
 }}
 
 with 
-     price_data as ({{ get_coingecko_metrics('worldcoin-wld') }})
-    , worldchain_dex_volumes as (
+     market_data as ({{ get_coingecko_metrics('worldcoin-wld') }})
+    , dex_volumes as (
         select date, daily_volume as dex_volumes, daily_volume_adjusted as adjusted_dex_volumes
         from {{ ref("fact_worldchain_daily_dex_volumes") }}
     )
 select
     f.date
-    , txns
-    , daa as dau
-    , fees_native
-    , fees
-    , cost
-    , cost_native
-    , revenue
-    , revenue_native
-    , dex_volumes
-    , adjusted_dex_volumes
     -- Standardized Metrics
     -- Market Data
-    , price
-    , market_cap
-    , fdmc
-    , token_volume
-    -- Chain Metrics
-    , dau as chain_dau
-    , txns as chain_txns
-    , dex_volumes as chain_spot_volume
-    -- Cash Flow Metrics
-    , fees as ecosystem_revenue
-    , fees_native as ecosystem_revenue_native
-    , cost as l1_fee_allocation
-    , cost_native as l1_fee_allocation_native
-    , revenue as foundation_fee_allocation
-    , revenue_native as foundation_fee_allocation_native
-    , token_turnover_circulating
-    , token_turnover_fdv
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+    -- Usage Metrics
+    , f.daa as chain_dau
+    , f.daa as dau
+    , f.txns as chain_txns
+    , f.txns
+    , dex.dex_volumes as chain_spot_volume
+
+    -- Fees Metrics
+    , f.fees as chain_fees
+    , f.fees
+    , f.fees_native
+    , f.cost as l1_fee_allocation
+    , f.cost_native as l1_fee_allocation_native
+    , f.revenue as foundation_fee_allocation
+    , f.revenue_native as foundation_fee_allocation_native
+    
+    -- Financial Metrics
+    , f.revenue
+    , f.revenue_native
+
+    -- Other Metrics
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
+    -- Legacy Metrics
+    , dex.adjusted_dex_volumes
+
 from {{ ref("fact_worldchain_fundamental_metrics") }} as f
-left join price_data on f.date = price_data.date
-left join worldchain_dex_volumes on f.date = worldchain_dex_volumes.date
+left join market_data on f.date = market_data.date
+left join dex_volumes dex on f.date = dex.date
 where f.date  < to_date(sysdate())
