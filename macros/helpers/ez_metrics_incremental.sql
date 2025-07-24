da9061dd16cc6b76f983f923b821ae29dc3d71a0
Broderick Bonelli <broderickbonelli@gmail.com>
2025-07-24 11:08:31 -0500

    amend macro to cast backfill_date to date

--- macros/helpers/ez_metrics_incremental.sql
+++ macros/helpers/ez_metrics_incremental.sql
@@ -1,7 +1,7 @@
 {% macro ez_metrics_incremental(date_column, backfill_date=None) %}
     {% if is_incremental() %}
         {% if backfill_date %}
-            where {{ date_column }} >= '{{ backfill_date }}'
+            where DATE({{ date_column }}) >= DATE('{{ backfill_date }}')
         {% else %}
             where {{ date_column }} > (select max(this.date) from {{ this }} as this)
         {% endif %}
