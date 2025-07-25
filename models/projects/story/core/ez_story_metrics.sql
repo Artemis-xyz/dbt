a6d5742f94d4df79f7a08c53372c5602291a41b6
kaviish <kaviishvsethi@gmail.com>
2025-07-25 11:14:36 -0400

    feature/story_issued_supply_and_float

--- models/projects/story/core/ez_story_metrics.sql
+++ models/projects/story/core/ez_story_metrics.sql
@@ -17,17 +17,37 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-select
-    date
-    , txns
-    , daa as dau
-    , fees_native
-    , fees
+WITH issued_supply_metrics AS (
+    SELECT 
+        date,
+        total_supply + cumulative_ip_burned AS max_supply_native,
+        total_supply AS total_supply_native,
+        total_ip_burned AS native_burns,
+        revenue,
+        issued_supply AS issued_supply_native,
+        circulating_supply AS circulating_supply_native
+    FROM {{ ref('fact_story_issued_supply_and_float') }}
+)
 
-    -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from {{ ref("fact_story_fundamental_metrics") }}
-where true
-{{ ez_metrics_incremental('date', backfill_date) }}
-and date < to_date(sysdate())
+SELECT
+    f.date,
+    f.txns,
+    f.daa AS dau,
+    f.fees_native,
+    f.fees,
+    i.revenue,
+    i.max_supply_native,
+    i.total_supply_native,
+    i.native_burns,
+    i.issued_supply_native,
+    i.circulating_supply_native,
+    TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on,
+    TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM {{ ref("fact_story_fundamental_metrics") }} f
+LEFT JOIN issued_supply_metrics i
+    ON DATE(f.date) = i.date
+WHERE TRUE
+{% if is_incremental() and backfill_date is not none %}
+  AND f.date >= {{ backfill_date }}
+{% endif %}
+AND f.date < TO_DATE(SYSDATE())
