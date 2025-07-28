fcec8af709b58c3a2050dfc9a338f7e0a7d7b9db
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-28 17:46:48 -0400

    Standardized goldfinch metrics

--- models/projects/goldfinch/core/ez_goldfinch_metrics.sql
+++ models/projects/goldfinch/core/ez_goldfinch_metrics.sql
@@ -10,7 +10,7 @@
         , on_schema_change="append_new_columns"
         , merge_update_columns=var("backfill_columns", [])
         , merge_exclude_columns=["created_on"] | reject('in', var("backfill_columns", [])) | list
-        , full_refresh=false
+        , full_refresh=false 
         , tags=["ez_metrics"]
     )
 }}
@@ -58,54 +58,46 @@ with fees_tvl_metrics as(
 
 SELECT
     m.date
+    , 'goldfinch' as artemis_id
+    
+    -- Standardized Metrics
+
+    -- Market Data
+    , pd.price
+    , pd.market_cap
+    , pd.fdmc
+    , pd.token_volume
+
+    -- Usage Data
+    , m.net_deposits as lending_deposits
+    , m.tvl as lending_loans
+    , m.tvl as lending_loan_capacity
+    , m.tvl as lending_tvl
+    , m.tvl as tvl
+    , th.token_holder_count
+
+    -- Fee Data
     , m.interest_fees
     , m.withdrawal_revenue as withdrawal_fees
     , coalesce(m.interest_fees,0) + coalesce(m.withdrawal_revenue,0) as fees
-    , m.supply_side_fees as primary_supply_side_revenue
-    , 0 as secondary_supply_side_revenue
-    , primary_supply_side_revenue + secondary_supply_side_revenue as total_supply_side_revenue
-    , m.interest_revenue
-    , m.withdrawal_revenue
+    , m.supply_side_fees as lp_fee_allocation
+    , coalesce(m.interest_revenue,0) + coalesce(m.withdrawal_revenue,0) as tokenholder_fee_allocation 
+        -- This is cashflow to the DAO-controlled treasury
+    
+    -- Financial Statements
     , coalesce(m.interest_revenue,0) + coalesce(m.withdrawal_revenue,0) as revenue
     , coalesce(ti.token_incentives,0) as token_incentives
-    , 0 as operating_expenses
-    , coalesce(ti.token_incentives,0) + coalesce(operating_expenses,0) as total_expenses
-    , coalesce(revenue,0) - coalesce(total_expenses,0) as earnings
-    , m.net_deposits as net_deposits
-    , 0 as outstanding_supply
-    , t.treasury as treasury_value
-    , t.treasury_native as treasury_value_native
-    , t.net_treasury as net_treasury_value
-    , {{ daily_pct_change('m.tvl') }} as tvl_growth
-    , th.token_holder_count
-    -- Standardized Metrics
-    -- Token Metrics
-    , coalesce(pd.price,0) as price
-    , coalesce(pd.market_cap,0) as market_cap
-    , coalesce(pd.fdmc,0) as fdmc
-    , coalesce(pd.token_volume,0) as token_volume
-    -- Lending Metrics
-    , coalesce(m.net_deposits,0) as lending_deposits
-    , coalesce(m.tvl,0) as lending_loan_capacity
-    , coalesce(m.interest_revenue,0) as lending_interest_fees
-    -- Crypto Metrics
-    , coalesce(m.tvl,0) as tvl
-    , coalesce(m.tvl,0) - LAG(coalesce(m.tvl,0)) OVER (ORDER BY date) as tvl_net_change
-    -- Cash Flow
-    , coalesce(m.interest_fees,0) + coalesce(m.withdrawal_revenue,0) as ecosystem_revenue
-    , coalesce(m.supply_side_fees,0) as service_fee_allocation
-    , coalesce(m.interest_revenue,0) + coalesce(m.withdrawal_revenue,0) as token_fee_allocation 
-        -- This is cashflow to the DAO-controlled treasury
-    -- Protocol Metrics
-    , coalesce(t.treasury,0) as treasury
-    , coalesce(t.treasury_native,0) as treasury_native
-    , coalesce(t.net_treasury,0) as net_treasury
-    , coalesce(t.net_treasury_native,0) as net_treasury_native
-    , coalesce(t.own_token_treasury,0) as own_token_treasury
-    , coalesce(t.own_token_treasury_native,0) as own_token_treasury_native
+    , coalesce(revenue,0) - coalesce(token_incentives,0) as earnings
+
+    -- Treasury Data
+    , t.treasury
+    , t.net_treasury
+    , t.own_token_treasury
+
     -- Turnover Metrics
-    , coalesce(pd.token_turnover_circulating,0) as token_turnover_circulating
-    , coalesce(pd.token_turnover_fdv,0) as token_turnover_fdv
+    , pd.token_turnover_circulating
+    , pd.token_turnover_fdv
+
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
