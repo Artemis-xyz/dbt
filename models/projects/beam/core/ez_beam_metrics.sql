8569c5c53b3410b25f7d50517326bd3b1f9c0c1d
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-28 16:40:19 -0400

    Standardized beam metrics

--- models/projects/beam/core/ez_beam_metrics.sql
+++ models/projects/beam/core/ez_beam_metrics.sql
@@ -40,36 +40,38 @@ with
     )
 select
     date_spine.date as date
-    , coalesce(chain, 'beam') as chain
-    , coalesce(dau, 0) as dau
-    , coalesce(txns, 0) as txns
-    , coalesce(fees_native, 0) as fees_native
-    , coalesce(fees_native, 0) * coalesce(price, 0) as fees
-    , coalesce(fees, 0) / coalesce(txns, 1) as avg_txn_fee
+    , 'beam' as artemis_id
     -- Standardized Metrics
-    -- Market Metrics
+    
+    -- Market Data
     , coalesce(price, 0) as price
     , coalesce(market_cap, 0) as market_cap
     , coalesce(fdmc, 0) as fdmc
     , coalesce(token_volume, 0) as token_volume
-    -- Chain Metrics
-    , coalesce(dau, 0) as chain_dau
-    , coalesce(txns, 0) as chain_txns
-    -- Cash Flow Metrics
-    , coalesce(fees, 0) as chain_fees
-    , coalesce(fees, 0) as ecosystem_revenue
-    , coalesce(fees_native, 0) as ecosystem_revenue_native
-    , coalesce(avg_txn_fee, 0) as chain_avg_txn_fee
-    , coalesce(burns.burns_native, 0) as burned_fee_allocation_native
-    , coalesce(burns.burns_native, 0) * price as burned_fee_allocation
-    -- Crypto Metrics
+
+    -- Usage Data
+    , dau
+    , dau as chain_dau
+    , txns
+    , txns as chain_txns
     , tvl
-    , token_turnover_circulating
-    , token_turnover_fdv
+    , tvl as chain_tvl
+
+    -- Fee Data
+    , fees_native
+    , fees_native * price as fees
+    , fees as chain_fees
+    , coalesce(burns.burns_native, 0) * price as burned_fee_allocation
+
     -- Supply Metrics
     , coalesce(premine_unlocks_native, 0) as premine_unlocks_native
     , coalesce(premine_unlocks_native, 0) - coalesce(burns.burns_native, 0) as net_supply_change_native
     , sum(net_supply_change_native) over (order by date) as circulating_supply_native
+
+    -- Turnover Data
+    , token_turnover_circulating
+    , token_turnover_fdv
+
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
