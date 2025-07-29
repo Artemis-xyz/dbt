a5bbec3ac891e03c0e99f94fd1c41c9b6040e9c7
limzhengjie <limzhengjie01@gmail.com>
2025-07-29 16:04:44 -0400

    standardize ez apex metrics

--- models/projects/apex/core/ez_apex_metrics.sql
+++ models/projects/apex/core/ez_apex_metrics.sql
@@ -10,7 +10,7 @@
         on_schema_change="append_new_columns",
         merge_update_columns=var("backfill_columns", []),
         merge_exclude_columns=["created_on"] if not var("backfill_columns", []) else none,
-        full_refresh=false,
+        full_refresh=var("full_refresh", false),
         tags=["ez_metrics"],
     )
 }}
@@ -19,30 +19,37 @@
 
 with
     trading_volume_data as (
-        select date, sum(trading_volume) as trading_volume
+        select date, coalesce(sum(trading_volume), 0) as trading_volume
         from {{ ref("fact_apex_trading_volume") }}
         group by date
     )
-    , price as ({{ get_coingecko_metrics("apex-token-2") }})
+    , market_metrics as ({{ get_coingecko_metrics("apex-token-2") }})
 select
-    date
-    , 'apex' as app
+    trading_volume_data.date
+    , 'apex' as artemis_id
     , 'DeFi' as category
-    , trading_volume
-    -- standardize metrics
-    , trading_volume as perp_volume
+    
+    -- Standardized Metrics
+
     -- Market Data
-    , price
-    , market_cap
-    , fdmc
-    , token_turnover_circulating
-    , token_turnover_fdv
-    , token_volume
+    , market_metrics.price
+    , market_metrics.market_cap
+    , market_metrics.fdmc
+    , market_metrics.token_volume
+
+    -- Usage Data
+    , trading_volume_data.trading_volume as perp_volume
+
+    -- Token Turnover/Other Data
+    , market_metrics.token_turnover_circulating
+    , market_metrics.token_turnover_fdv
+
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
+
 from trading_volume_data
-left join price using(date)
+left join market_metrics using(date)
 where true 
-{{ ez_metrics_incremental("date", backfill_date) }}
-and date < to_date(sysdate())
+{{ ez_metrics_incremental("trading_volume_data.date", backfill_date) }}
+and trading_volume_data.date < to_date(sysdate())
