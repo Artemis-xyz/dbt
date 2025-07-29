0068c08081916c59ed986a42587247489a27c160
limzhengjie <limzhengjie01@gmail.com>
2025-07-29 16:05:46 -0400

    standardize ez apex metrics by chain

--- models/projects/apex/core/ez_apex_metrics_by_chain.sql
+++ models/projects/apex/core/ez_apex_metrics_by_chain.sql
@@ -11,16 +11,30 @@
 
 with
     trading_volume_data as (
-        select date, trading_volume, chain
+        select date, coalesce(trading_volume, 0) as trading_volume, chain
         from {{ ref("fact_apex_trading_volume") }}
     )
+    , market_metrics as ({{ get_coingecko_metrics("apex-token-2") }})
 select
     date
-    , 'apex' as app
-    , 'DeFi' as category
+    , 'apex' as artemis_id
     , chain
-    , trading_volume
-    -- standardize metrics
+
+    -- Standardized Metrics
+
+    -- Market Data
+    , market_metrics.price
+    , market_metrics.market_cap
+    , market_metrics.fdmc
+    , market_metrics.token_volume
+
+    -- Usage Data
     , trading_volume as perp_volume
+
+    -- Token Turnover/Other Data
+    , market_metrics.token_turnover_circulating
+    , market_metrics.token_turnover_fdv
+
 from trading_volume_data
+left join market_metrics using(date)
 where date < to_date(sysdate())
