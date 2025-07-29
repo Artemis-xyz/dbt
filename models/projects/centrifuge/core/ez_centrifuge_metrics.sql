33b446ffed9e457dd7fc445b38e3be015a615fe5
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 17:34:40 -0400

    Standardized centrifuge metrics

--- models/projects/centrifuge/core/ez_centrifuge_metrics.sql
+++ models/projects/centrifuge/core/ez_centrifuge_metrics.sql
@@ -17,48 +17,49 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    fundamental_data as (
-        select
-            date, 
-            txns,
-            daa, 
-            fees_native, 
-            fees_usd
-        from {{ ref("fact_centrifuge_fundamental_metrics") }}
+WITH
+    fundamental_data AS (
+        SELECT
+            date
+            , txns
+            , daa
+            , fees_native
+            , fees_usd
+        FROM {{ ref("fact_centrifuge_fundamental_metrics") }}
     )
-    , market_data as (
+    , market_data AS (
         {{ get_coingecko_metrics("centrifuge") }}
     )
-select
+SELECT
     date
-    , txns
-    , daa as dau
-    , coalesce(fees_native, 0) as fees_native
-    , coalesce(fees_usd, 0) as fees
-    -- Standardized Metrics
-    -- Token Metrics
-    , coalesce(market_data.price, 0) as price
-    , coalesce(market_data.market_cap, 0) as market_cap
-    , coalesce(market_data.fdmc, 0) as fdmc
-    , coalesce(market_data.token_volume, 0) as token_volume 
-    -- Chain Metrics
-    , daa as chain_dau
-    , txns as chain_txns
-    , coalesce(fees_native, 0) as l1_fee_allocation_native
-    , coalesce(fees_usd, 0) as l1_fee_allocation
-    , coalesce(fees_usd, 0)/coalesce(txns, 1) as chain_avg_txn_fee
-    -- Cash Flow Metrics
-    , coalesce(fees_usd, 0) as ecosystem_revenue
-    , coalesce(fees_native, 0) as ecosystem_revenue_native
+    , 'centrifuge' AS artemis_id 
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume 
+
+    -- Usage Data
+    , daa AS chain_dau
+    , daa AS dau
+    , txns AS chain_txns
+    , txns AS txns
+
+    -- Fee Data
+    , fees_native
+    , fees_usd
+    , fees_usd AS l1_fee_allocation
+
     -- Turnover Metrics
-    , coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating
-    , coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from fundamental_data
-left join market_data using (date)
-where true
+FROM fundamental_data
+LEFT JOIN market_data USING(date)
+WHERE true
 {{ ez_metrics_incremental('fundamental_data.date', backfill_date) }}
-and fundamental_data.date < to_date(sysdate())
+AND fundamental_data.date < to_date(sysdate())
