1526584639f75b2984b013982cbdee2365d6c7fe
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 18:17:12 -0400

    Standardized akash metrics

--- models/projects/akash/core/ez_akash_metrics.sql
+++ models/projects/akash/core/ez_akash_metrics.sql
@@ -18,134 +18,141 @@
 {% set backfill_date = var("backfill_date", None) %}
 
 WITH
-active_providers AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_active_providers_silver") }}
-)
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2020-09-25' AND date < to_date(sysdate())
+    )
 
-, active_leases AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_active_leases_silver") }}
-)
+    , active_providers AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_active_providers_silver") }}
+    )
 
-, new_leases AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_new_leases_silver") }}
-)
+    , active_leases AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_active_leases_silver") }}
+    )
 
-, compute_fees_native AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_compute_fees_native_silver") }}
-)
+    , new_leases AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_new_leases_silver") }}
+    )
 
-, compute_fees_usdc AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_compute_fees_usdc_silver") }}
-)
+    , compute_fees_native AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_compute_fees_native_silver") }}
+    )
 
-, compute_fees_total_usd AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_compute_fees_total_usd_silver") }}
-)
+    , compute_fees_usdc AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_compute_fees_usdc_silver") }}
+    )
 
-, validator_fees_native AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_validator_fees_native_silver") }}
-)
-, validator_fees AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_validator_fees_silver") }}
-)
+    , compute_fees_total_usd AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_compute_fees_total_usd_silver") }}
+    )
 
-, total_fees AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_total_fees_silver") }}
-)
+    , validator_fees_native AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_validator_fees_native_silver") }}
+    )
+    , validator_fees AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_validator_fees_silver") }}
+    )
 
-, revenue AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_revenue_silver") }}
-)
+    , total_fees AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_total_fees_silver") }}
+    )
 
-, mints AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_mints_silver") }}
-)
+    , revenue AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_revenue_silver") }}
+    )
 
-, burns AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_burns_native_silver") }}
-)
-, price as ({{ get_coingecko_metrics("akash-network") }})
+    , mints AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_mints_silver") }}
+    )
 
-, premine_unlocks AS (
-    SELECT * 
-    FROM {{ ref("fact_akash_premine_unlocks") }}
-)
+    , burns AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_burns_native_silver") }}
+    )
+    , market_data AS ({{ get_coingecko_metrics("akash-network") }})
+
+    , premine_unlocks AS (
+        SELECT * 
+        FROM {{ ref("fact_akash_premine_unlocks") }}
+    )
 
 SELECT
     mints.date
-    , coalesce(active_leases.active_leases, 0) AS active_leases
-    , coalesce(active_providers.active_providers, 0) AS active_providers
-    , coalesce(new_leases.new_leases, 0) AS new_leases
-    , (coalesce(compute_fees_native.compute_fees_native, 0))
-    / 1e6 AS compute_fees_native
-    , (coalesce(compute_fees_usdc.compute_fees_usdc, 0))
-    / 1e6 AS compute_fees_usdc
-    , (coalesce(compute_fees_total_usd.compute_fees_total_usd, 0))
-    / 1e6 AS compute_fees_total_usd
-    , coalesce(validator_fees_native.validator_fees_native, 0)
-        AS validator_fees_native
-    , coalesce(validator_fees.validator_fees, 0) AS validator_fees
-    , coalesce(total_fees.total_fees, 0) AS total_fees
-    , coalesce(revenue.revenue, 0) AS revenue
-    , coalesce(burns.total_burned_native, 0) AS total_burned_native
+    , 'akash' AS artemis_id
 
     -- Standardized Metrics
 
-    -- Token Metrics
-    , coalesce(price, 0) as price
-    , coalesce(market_cap, 0) as market_cap
-    , coalesce(fdmc, 0) as fdmc
-    , coalesce(token_volume, 0) as token_volume  
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume  
 
-    -- Cashflow Metrics
+    -- Fee Data
     , (coalesce(compute_fees_total_usd.compute_fees_total_usd, 0))/ 1e6 AS compute_fees
     , coalesce(validator_fees.validator_fees, 0) AS gas_fees
     , compute_fees + gas_fees AS fees
     , validator_fees AS validator_fee_allocation
     , revenue.revenue AS treasury_fee_allocation
-    , compute_fees - treasury_fee_allocation AS service_fee_allocation
-    , coalesce(burns.total_burned_native, 0) AS burns_native
+    , compute_fees - treasury_fee_allocation AS other_fee_allocation
 
-    -- Supply Metrics
+    -- Financial Statements
+    , revenue.revenue + burns.total_burned_native AS revenue
+    , revenue.revenue AS earnings
+
+    -- Supply Data
     , coalesce(mints.mints, 0) AS gross_emissions_native
     , coalesce(premine_unlocks.pre_mine_unlocks, 0) AS premine_unlocks_native
-    , coalesce(mints.mints, 0) + coalesce(premine_unlocks.pre_mine_unlocks, 0) - coalesce(burns.total_burned_native, 0) AS net_supply_change_native
+    , coalesce(burns.total_burned_native, 0) AS burns
     , sum((coalesce(mints.mints, 0) - coalesce(burns.total_burned_native, 0)) + coalesce(premine_unlocks.pre_mine_unlocks, 0)) OVER (order by mints.date) AS circulating_supply_native
     
     -- Turnover Metrics
-    , coalesce(token_turnover_circulating, 0) as token_turnover_circulating
-    , coalesce(token_turnover_fdv, 0) as token_turnover_fdv 
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv 
+
+    -- Bespoke Metrics
+    , COALESCE(active_leases.active_leases, 0) AS active_leases
+    , COALESCE(active_providers.active_providers, 0) AS active_providers
+    , COALESCE(new_leases.new_leases, 0) AS new_leases
+    , (COALESCE(compute_fees_native.compute_fees_native, 0))
+    / 1e6 AS compute_fees_native
+    , (COALESCE(compute_fees_usdc.compute_fees_usdc, 0))
+    / 1e6 AS compute_fees_usdc
+    , (COALESCE(compute_fees_total_usd.compute_fees_total_usd, 0))
+    / 1e6 AS compute_fees_total_usd
 
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-FROM mints
-LEFT JOIN active_providers ON mints.date = active_providers.date
-LEFT JOIN new_leases ON mints.date = new_leases.date
-LEFT JOIN compute_fees_native ON mints.date = compute_fees_native.date
-LEFT JOIN compute_fees_usdc ON mints.date = compute_fees_usdc.date
-LEFT JOIN compute_fees_total_usd ON mints.date = compute_fees_total_usd.date
-LEFT JOIN validator_fees_native ON mints.date = validator_fees_native.date
-LEFT JOIN validator_fees ON mints.date = validator_fees.date
-LEFT JOIN total_fees ON mints.date = total_fees.date
-LEFT JOIN revenue ON mints.date = revenue.date
-LEFT JOIN burns ON mints.date = burns.date
-LEFT JOIN active_leases ON mints.date = active_leases.date
-LEFT JOIN price ON mints.date = price.date
-LEFT JOIN premine_unlocks ON mints.date = premine_unlocks.date
+FROM date_spine
+LEFT JOIN mints USING (date)
+LEFT JOIN active_providers USING (date)
+LEFT JOIN new_leases USING (date)
+LEFT JOIN compute_fees_native USING (date)
+LEFT JOIN compute_fees_usdc USING (date)
+LEFT JOIN compute_fees_total_usd USING (date)
+LEFT JOIN validator_fees_native USING (date)
+LEFT JOIN validator_fees USING (date)
+LEFT JOIN total_fees USING (date)
+LEFT JOIN revenue USING (date)
+LEFT JOIN burns USING (date)
+LEFT JOIN active_leases USING (date)
+LEFT JOIN market_data USING (date)
+LEFT JOIN premine_unlocks USING (date)
 WHERE true
 {{ ez_metrics_incremental("mints.date", backfill_date) }}
-and mints.date < to_date(sysdate())
+AND mints.date < to_date(sysdate())
 ORDER BY date DESC
