dcf204c3e3770b74c78cd7348a131c9470cd3bd6
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 18:56:30 -0400

    Standardized bsc metrics

--- models/projects/bsc/core/ez_bsc_metrics.sql
+++ models/projects/bsc/core/ez_bsc_metrics.sql
@@ -18,79 +18,73 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    fundamental_data as ({{ get_fundamental_data_for_chain("bsc", "v2") }}),
-    price_data as ({{ get_coingecko_metrics("binancecoin") }}),
-    defillama_data as ({{ get_defillama_metrics("bsc") }}),
-    stablecoin_data as ({{ get_stablecoin_metrics("bsc") }}),
-    github_data as ({{ get_github_metrics("Binance Smart Chain") }}),
-    contract_data as ({{ get_contract_metrics("bsc") }}),
-    nft_metrics as ({{ get_nft_metrics("bsc") }}),
-    rolling_metrics as ({{ get_rolling_active_address_metrics("bsc") }}),
-    binance_dex_volumes as (
-        select date, daily_volume as dex_volumes, daily_volume_adjusted as adjusted_dex_volumes
-        from {{ ref("fact_binance_daily_dex_volumes") }}
+WITH
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2020-08-29' AND date < TO_DATE(SYSDATE())
     )
-    , staked_eth_metrics as (
-        select
-            date,
-            sum(num_staked_eth) as num_staked_eth,
-            sum(amount_staked_usd) as amount_staked_usd,
-            sum(num_staked_eth_net_change) as num_staked_eth_net_change,
-            sum(amount_staked_usd_net_change) as amount_staked_usd_net_change
-        from {{ ref('fact_binance_staked_eth_count_with_usd_and_change') }}
-        group by 1
+    , fundamental_data AS ({{ get_fundamental_data_for_chain("bsc", "v2") }})
+    , market_data AS ({{ get_coingecko_metrics("binancecoin") }})
+    , defillama_data AS ({{ get_defillama_metrics("bsc") }})
+    , stablecoin_data AS ({{ get_stablecoin_metrics("bsc") }})
+    , github_data AS ({{ get_github_metrics("Binance Smart Chain") }})
+    , contract_data AS ({{ get_contract_metrics("bsc") }})
+    , nft_metrics AS ({{ get_nft_metrics("bsc") }})
+    , rolling_metrics AS ({{ get_rolling_active_address_metrics("bsc") }})
+    , binance_dex_volumes AS (
+        SELECT date, daily_volume AS dex_volumes, daily_volume_adjusted AS adjusted_dex_volumes
+        FROM {{ ref("fact_binance_daily_dex_volumes") }}
     )
-select
-    fundamental_data.date
-    , fundamental_data.chain
-    , txns
-    , dau
-    , wau
-    , mau
-    , fees_native
-    , fees
-    , avg_txn_fee
-    , median_txn_fee
-    , fees_native * .1 as revenue_native
-    , fees * .1 as revenue
-    , dau_over_100
-    , nft_trading_volume
-    , dune_dex_volumes_binance.dex_volumes
-    , dune_dex_volumes_binance.adjusted_dex_volumes
+    , staked_eth_metrics AS (
+        SELECT
+            date
+            , SUM(num_staked_eth) AS num_staked_eth
+            , SUM(amount_staked_usd) AS amount_staked_usd
+            , SUM(num_staked_eth_net_change) AS num_staked_eth_net_change
+            , SUM(amount_staked_usd_net_change) AS amount_staked_usd_net_change
+        FROM {{ ref('fact_binance_staked_eth_count_with_usd_and_change') }}
+        GROUP BY 1
+    )
+SELECT
+    date_spine.date
+    'bsc' AS artemis_id
+
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price
-    , market_cap
-    , fdmc
-    , tvl
-    -- Chain Usage Metrics
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data
     , dau AS chain_dau
-    , wau AS chain_wau
+    , dau 
+    , wau AS chain_mau
     , mau AS chain_mau
     , txns AS chain_txns
-    , avg_txn_fee AS chain_avg_txn_fee
-    , median_txn_fee AS chain_median_txn_fee
-    , returning_users
-    , new_users
-    , low_sleep_users
-    , high_sleep_users
-    , sybil_users
-    , non_sybil_users
-    , dau_over_100 AS dau_over_100_balance
+    , txns
+    , tvl AS chain_tvl
+    , tvl 
     , nft_trading_volume AS chain_nft_trading_volume
     , dune_dex_volumes_binance.dex_volumes AS chain_spot_volume
-    -- LST Metrics
+    , dune_dex_volumes_binance.adjusted_dex_volumes AS chain_spot_volume_adjusted
     , staked_eth_metrics.num_staked_eth as lst_tvl_native
     , staked_eth_metrics.amount_staked_usd as lst_tvl
     , staked_eth_metrics.num_staked_eth_net_change as lst_tvl_native_net_change
     , staked_eth_metrics.amount_staked_usd_net_change as lst_tvl_net_change
-    -- Cashflow metrics
+
+    -- Fee Data
+    , fees_native
+    , fees
     , fees as chain_fees
-    , fees_native AS ecosystem_revenue_native
-    , fees AS ecosystem_revenue
-    , fees_native * .1 AS burned_fee_allocation_native
     , fees * .1 AS burned_fee_allocation
+
+    -- Financial Statements
+    , fees * .1 AS revenue
+    , revenue AS earnings
+
     -- Developer metrics
     , weekly_commits_core_ecosystem
     , weekly_commits_sub_ecosystem
@@ -98,6 +92,7 @@ select
     , weekly_developers_sub_ecosystem
     , weekly_contracts_deployed
     , weekly_contract_deployers
+
     -- Stablecoin metrics
     , stablecoin_total_supply
     , stablecoin_txns
@@ -114,19 +109,34 @@ select
     , p2p_stablecoin_dau
     , p2p_stablecoin_mau
     , p2p_stablecoin_transfer_volume
+
+    -- Turnover Data
+    , token_turnover_circulating
+    , token_turnover_fdv
+
+    -- Bespoke Metrics
+    , returning_users
+    , new_users
+    , low_sleep_users
+    , high_sleep_users
+    , sybil_users
+    , non_sybil_users
+    , dau_over_100 AS dau_over_100_balance
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from fundamental_data
-left join price_data on fundamental_data.date = price_data.date
-left join defillama_data on fundamental_data.date = defillama_data.date
-left join stablecoin_data on fundamental_data.date = stablecoin_data.date
-left join github_data on fundamental_data.date = github_data.date
-left join contract_data on fundamental_data.date = contract_data.date
-left join nft_metrics on fundamental_data.date = nft_metrics.date
-left join rolling_metrics on fundamental_data.date = rolling_metrics.date
-left join binance_dex_volumes as dune_dex_volumes_binance on fundamental_data.date = dune_dex_volumes_binance.date
-left join staked_eth_metrics on fundamental_data.date = staked_eth_metrics.date
-where true
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN fundamental_data USING (date)
+LEFT JOIN market_data USING (date)
+LEFT JOIN defillama_data USING (date)
+LEFT JOIN stablecoin_data USING (date)
+LEFT JOIN github_data USING (date)
+LEFT JOIN contract_data USING (date)
+LEFT JOIN nft_metrics USING (date)
+LEFT JOIN rolling_metrics USING (date)
+LEFT JOIN binance_dex_volumes AS dune_dex_volumes_binance USING (date)
+LEFT JOIN staked_eth_metrics USING (date)
+WHERE true
 {{ ez_metrics_incremental('fundamental_data.date', backfill_date) }}
-    and fundamental_data.date < to_date(sysdate())
+    AND fundamental_data.date < TO_DATE(SYSDATE())
