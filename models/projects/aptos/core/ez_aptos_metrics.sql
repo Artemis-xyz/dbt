5883a8d26f47c02249651b6c22c728548bc7d615
limzhengjie <limzhengjie01@gmail.com>
2025-07-29 16:15:55 -0400

    standardize ez aptos metrics table

--- models/projects/aptos/core/ez_aptos_metrics.sql
+++ models/projects/aptos/core/ez_aptos_metrics.sql
@@ -10,7 +10,7 @@
         on_schema_change="append_new_columns",
         merge_update_columns=var("backfill_columns", []),
         merge_exclude_columns=["created_on"] if not var("backfill_columns", []) else none,
-        full_refresh=false,
+        full_refresh=var("full_refresh", false),
         tags=["ez_metrics"],
     )
 }}
@@ -22,13 +22,13 @@ with
         select
             date
             , chain
-            , txns
-            , daa as dau
-            , gas as fees_native
-            , gas_usd as fees
-            , fees / txns as avg_txn_fee
-            , revenue
-            , gas as revenue_native
+            , coalesce(txns, 0) as txns
+            , coalesce(daa, 0) as dau
+            , coalesce(gas, 0) as fees_native
+            , coalesce(gas_usd, 0) as fees
+            , coalesce(fees / txns, 0) as avg_txn_fee
+            , coalesce(revenue, 0) as revenue
+            , coalesce(gas, 0) as revenue_native
         from {{ ref("fact_aptos_daa_txns_gas_gas_usd_revenue") }}
     )
     , price_data as ({{ get_coingecko_metrics("aptos") }})
@@ -38,16 +38,14 @@ with
     , aptos_dex_volumes as (
         select 
             date
-            , volume_usd as dex_volumes
+            , coalesce(volume_usd, 0) as dex_volumes
         from {{ ref("fact_aptos_dex_volumes") }}
     )
 select
     fundamental_data.date
+    , 'aptos' as artemis_id
     , fundamental_data.chain
     , txns
-    , dau
-    , wau
-    , mau
     , fees_native
     , fees
     , avg_txn_fee
@@ -57,43 +55,47 @@ select
 
     -- Standardized Metrics
 
-    -- Token Metrics
-    , coalesce(price_data.price, 0) as price
-    , coalesce(price_data.market_cap, 0) as market_cap
-    , coalesce(price_data.fdmc, 0) as fdmc
-    , coalesce(price_data.token_volume, 0) as token_volume
+    -- Market Data
+    , price_data.price
+    , price_data.market_cap
+    , price_data.fdmc
+    , price_data.token_volume
 
     -- Chain Metrics
-    , coalesce(dau, 0) as chain_dau
-    , coalesce(wau, 0) as chain_wau
-    , coalesce(mau, 0) as chain_mau
-    , coalesce(txns, 0) as chain_txns
-    , coalesce(avg_txn_fee, 0) as chain_avg_txn_fee
-    , coalesce(aptos_dex_volumes.dex_volumes, 0) as chain_spot_volume
+    , fundamental_data.dau as chain_dau
+    , fundamental_data.dau as dau
+    , fundamental_data.wau as chain_wau
+    , fundamental_data.mau as chain_mau
+    , fundamental_data.txns as chain_txns
+    , fundamental_data.txns as txns
+    , fundamental_data.avg_txn_fee as chain_avg_txn_fee
+    , aptos_dex_volumes.dex_volumes as chain_spot_volume
+    , defillama_data.tvl as chain_tvl
+    , defillama_data.tvl as tvl
 
-    -- Crypto Metrics
-    , coalesce(tvl, 0) as tvl
+    -- Fee Data
+    , fundamental_data.fees as chain_fees
+    , fundamental_data.fees as burned_fee_allocation
+    , fundamental_data.fees_native as burned_fee_allocation_native
 
-    -- Cash Flow Metrics
-    , coalesce(fees, 0) as chain_fees
-    , coalesce(fees, 0) as ecosystem_revenue
-    , coalesce(fees_native, 0) as ecosystem_revenue_native
-    , coalesce(fees, 0) as burned_fee_allocation
-    , coalesce(fees_native, 0) as burned_fee_allocation_native
+    -- Financial Metrics
+    , fundamental_data.revenue as revenue
+    , fundamental_data.revenue_native as revenue_native
 
     -- Developer Metrics
-    , coalesce(weekly_commits_core_ecosystem, 0) as weekly_commits_core_ecosystem
-    , coalesce(weekly_commits_sub_ecosystem, 0) as weekly_commits_sub_ecosystem
-    , coalesce(weekly_developers_core_ecosystem, 0) as weekly_developers_core_ecosystem
-    , coalesce(weekly_developers_sub_ecosystem, 0) as weekly_developers_sub_ecosystem
+    , github_data.weekly_commits_core_ecosystem
+    , github_data.weekly_commits_sub_ecosystem
+    , github_data.weekly_developers_core_ecosystem
+    , github_data.weekly_developers_sub_ecosystem
 
     -- Turnover Metrics
-    , coalesce(price_data.token_turnover_circulating, 0) as token_turnover_circulating
-    , coalesce(price_data.token_turnover_fdv, 0) as token_turnover_fdv
+    , price_data.token_turnover_circulating
+    , price_data.token_turnover_fdv
 
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
+
 from fundamental_data
 left join price_data on fundamental_data.date = price_data.date
 left join defillama_data on fundamental_data.date = defillama_data.date
