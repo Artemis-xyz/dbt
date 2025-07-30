9d140d7c1ee8b01c9cfd3511967d14b157584052
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-30 15:35:59 -0400

    Standardized puffer metrics

--- models/projects/puffer_finance/core/ez_puffer_finance_metrics.sql
+++ models/projects/puffer_finance/core/ez_puffer_finance_metrics.sql
@@ -2,9 +2,9 @@
     config(
         materialized="incremental",
         snowflake_warehouse="PUFFER_FINANCE",
-        database="puffer_finance",
+        databASe="puffer_finance",
         schema="core",
-        alias="ez_metrics",
+        aliAS="ez_metrics",
         incremental_strategy="merge",
         unique_key="date",
         on_schema_change="append_new_columns",
@@ -17,63 +17,53 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with restaked_eth_metrics as (
-    select
-        date,
-        chain,
-        num_restaked_eth,
-        amount_restaked_usd,
-        num_restaked_eth_net_change,
-        amount_restaked_usd_net_change
-    from {{ ref('fact_puffer_finance_restaked_eth_count_with_usd_and_change') }}
-)
-, date_spine as (
-    select
-        ds.date
-    from {{ ref('dim_date_spine') }} ds
-    where ds.date between (select min(date) from restaked_eth_metrics) and to_date(sysdate())
-)
-, market_metrics as (
-    {{get_coingecko_metrics('puffer-finance')}}
-)
+WITH 
+    restaked_eth_metrics AS (
+        SELECT
+            date,
+            chain,
+            num_restaked_eth,
+            amount_restaked_usd,
+            num_restaked_eth_net_change,
+            amount_restaked_usd_net_change
+        FROM {{ ref('fact_puffer_finance_restaked_eth_count_with_usd_and_change') }}
+    )
+    , date_spine AS (
+        SELECT
+            ds.date
+        FROM {{ ref('dim_date_spine') }} ds
+        WHERE ds.date BETWEEN (SELECT min(date) FROM restaked_eth_metrics) AND to_date(sysdate())
+    )
+    , market_data AS (
+        {{get_coingecko_metrics('puffer-finance')}}
+    )
 
-select
+SELECT
     date_spine.date,
-    'puffer_finance' as app,
-    'DeFi' as category,
-    
-    --Old metrics needed for compatibility
-    restaked_eth_metrics.num_restaked_eth,
-    restaked_eth_metrics.amount_restaked_usd,
-    restaked_eth_metrics.num_restaked_eth_net_change,
-    restaked_eth_metrics.amount_restaked_usd_net_change
+    'puffer_finance' AS artemis_id,
 
     --Standardized Metrics
 
-    --Market Metrics
-    , market_metrics.price as price
-    , market_metrics.token_volume as token_volume
-    , market_metrics.market_cap as market_cap
-    , market_metrics.fdmc as fdmc
+    --Market Data
+    , market_data.price AS price
+    , market_data.token_volume AS token_volume
+    , market_data.market_cap AS market_cap
+    , market_data.fdmc AS fdmc
 
-    --Usage Metrics
-    , restaked_eth_metrics.num_restaked_eth as tvl_native
-    , restaked_eth_metrics.num_restaked_eth as lrt_tvl_native
-    , restaked_eth_metrics.amount_restaked_usd as tvl
-    , restaked_eth_metrics.amount_restaked_usd as lrt_tvl
-    , restaked_eth_metrics.num_restaked_eth_net_change as lrt_tvl_native_net_change
-    , restaked_eth_metrics.amount_restaked_usd_net_change as lrt_tvl_net_change
+    --Usage Data
+    , restaked_eth_metrics.amount_restaked_usd AS lrt_tvl
+    , restaked_eth_metrics.amount_restaked_usd AS tvl
 
-    --Other Metrics
-    , market_metrics.token_turnover_circulating as token_turnover_circulating
-    , market_metrics.token_turnover_fdv as token_turnover_fdv
+    --Turnover Data
+    , market_data.token_turnover_circulating AS token_turnover_circulating
+    , market_data.token_turnover_fdv AS token_turnover_fdv
 
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from date_spine
-left join restaked_eth_metrics using(date)
-left join market_metrics using(date)
-where true
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN restaked_eth_metrics USING (date)
+LEFT JOIN market_data USING (date)
+WHERE true
 {{ ez_metrics_incremental('date_spine.date', backfill_date) }}
-and date_spine.date < to_date(sysdate())
+AND date_spine.date < to_date(sysdate())
