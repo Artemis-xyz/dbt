8a82bc69cca8e1c9352f0feb4ba6d119534a0d3e
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 21:59:39 -0400

    Standardized venus metrics

--- models/projects/venus/core/ez_venus_metrics.sql
+++ models/projects/venus/core/ez_venus_metrics.sql
@@ -17,8 +17,14 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    venus_by_chain as (
+WITH
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2020-11-24'
+        AND date < to_date(sysdate())
+    )
+    , venus_by_chain AS (
         {{
             dbt_utils.union_relations(
                 relations=[
@@ -27,44 +33,53 @@ with
             )
         }}
     )
-    , venus_metrics as (
-        select
+    , venus_metrics AS (
+        SELECT
             date
             , sum(daily_borrows_usd) as daily_borrows_usd
             , sum(daily_supply_usd) as daily_supply_usd
-        from venus_by_chain
-        group by 1
+        FROM venus_by_chain
+        GROUP BY 1
     )
 
-    , token_incentives as (
-        select
-            date,
-            token_incentives as token_incentives
-        from {{ref('fact_venus_token_incentives')}}
+    , token_incentives AS (
+        SELECT
+            date
+            , token_incentives
+        FROM {{ ref("fact_venus_token_incentives") }}
     )
-    , price_data as ({{ get_coingecko_metrics("venus") }})
+    , market_data AS ({{ get_coingecko_metrics("venus") }})
 
-select
+SELECT
     venus_metrics.date
-    , 'venus' as app
-    , 'DeFi' as category
-    , venus_metrics.daily_borrows_usd
-    , venus_metrics.daily_supply_usd
-    -- Standardized metrics
+    , 'venus' as artemis_id
+
+    -- Standardized Metrics
+
+    -- Market Data 
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data
     , venus_metrics.daily_borrows_usd as lending_loans
     , venus_metrics.daily_supply_usd as lending_deposits
-    , price_data.price
-    , price_data.market_cap
-    , price_data.fdmc
+
+    -- Financial Statements 
     , coalesce(token_incentives.token_incentives, 0) as token_incentives
+
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from venus_metrics
-left join token_incentives
-    on venus_metrics.date = token_incentives.date
-left join price_data
-    on venus_metrics.date = price_data.date
-where true
-{{ ez_metrics_incremental('venus_metrics.date', backfill_date) }}
-and venus_metrics.date < to_date(sysdate())
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN venus_metrics USING (date)
+LEFT JOIN token_incentives USING (date)
+LEFT JOIN market_data USING (date)
+WHERE true
+{{ ez_metrics_incremental("venus_metrics.date", backfill_date) }}
+AND venus_metrics.date < to_date(sysdate())
