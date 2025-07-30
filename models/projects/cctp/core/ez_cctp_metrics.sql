8a39f475cbd1dcfeed7b45538569c9c84aaac426
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 21:43:36 -0400

    Standardized cctp

--- models/projects/cctp/core/ez_cctp_metrics.sql
+++ models/projects/cctp/core/ez_cctp_metrics.sql
@@ -17,27 +17,42 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    bridge_volume as (
-        select date, bridge_volume
-        from {{ ref("fact_cctp_bridge_volume") }}
-        where chain is null
-    ),
-    bridge_dau as (
-        select date, bridge_dau
-        from {{ ref("fact_cctp_bridge_dau") }}
+WITH
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2023-04-18'
+        AND date < to_date(sysdate())
     )
-select
-    bridge_volume.date as date,
-    'cctp' as protocol,
-    'Bridge' as category,
-    bridge_volume.bridge_volume,
-    bridge_dau.bridge_dau
+    , bridge_volume AS (
+        SELECT date
+            , bridge_volume
+        FROM {{ ref("fact_cctp_bridge_volume") }}
+        WHERE chain IS NULL
+    )
+    , bridge_dau AS (
+        SELECT date
+            , bridge_dau
+        FROM {{ ref("fact_cctp_bridge_dau") }}
+    )
+
+SELECT
+    bridge_volume.date AS date
+    , 'cctp' AS artemis_id
+
+    -- Standardized Metrics
+
+    -- Usage Data
+    , bridge_dau.bridge_dau
+    , bridge_dau.bridge_dau AS dau
+    , bridge_volume.bridge_volume
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from bridge_volume
-left join bridge_dau on bridge_volume.date = bridge_dau.date
-where true
-{{ ez_metrics_incremental('bridge_volume.date', backfill_date) }}
-and bridge_volume.date < to_date(sysdate())
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN bridge_volume USING (date)
+LEFT JOIN bridge_dau USING (date)
+WHERE true
+{{ ez_metrics_incremental("bridge_volume.date", backfill_date) }}
+AND bridge_volume.date < to_date(sysdate())
