0be9a4dff183a364de8244e553501b7bb7b7dd3e
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-30 15:07:37 -0400

    Standardized ethereum metrics

--- models/projects/ethereum/core/ez_ethereum_metrics.sql
+++ models/projects/ethereum/core/ez_ethereum_metrics.sql
@@ -23,54 +23,86 @@
 {% set backfill_date = var("backfill_date", None) %}
 
 with
-    fundamental_data as ({{ get_fundamental_data_for_chain("ethereum", "v2") }}),
-    price_data as ({{ get_coingecko_metrics("ethereum") }}),
-    defillama_data as ({{ get_defillama_metrics("ethereum") }}),
-    stablecoin_data as ({{ get_stablecoin_metrics("ethereum") }}),
-    staking_data as ({{ get_staking_metrics("ethereum") }}),
-    censored_block_metrics as ({{ get_censored_block_metrics("ethereum") }}),
-    revenue_data as (
-        select date, revenue, native_token_burn as revenue_native
-        from {{ ref("agg_daily_ethereum_revenue") }}
-    ),
-    github_data as ({{ get_github_metrics("ethereum") }}),
-    contract_data as ({{ get_contract_metrics("ethereum") }}),
-    validator_queue_data as (
-        select date, queue_entry_amount, queue_exit_amount, queue_active_amount
-        from {{ ref("fact_ethereum_beacon_chain_queue_entry_active_exit_silver") }}
-    ),
-    nft_metrics as ({{ get_nft_metrics("ethereum") }}),
-    p2p_metrics as ({{ get_p2p_metrics("ethereum") }}),
-    rolling_metrics as ({{ get_rolling_active_address_metrics("ethereum") }}),
-    da_metrics as (
-        select date, blob_fees_native, blob_fees, blob_size_mib, avg_mib_per_second, avg_cost_per_mib, avg_cost_per_mib_gwei, submitters
-        from {{ ref("fact_ethereum_da_metrics") }}
-    ),
-    etf_metrics as (
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2015-08-07'
+        AND date < to_date(sysdate())
+    )
+    , fundamental_data AS ({{ get_fundamental_data_for_chain("ethereum", "v2") }})
+    , market_data AS ({{ get_coingecko_metrics("ethereum") }})
+    , defillama_data AS ({{ get_defillama_metrics("ethereum") }})
+    , stablecoin_data AS ({{ get_stablecoin_metrics("ethereum") }})
+    , staking_data AS ({{ get_staking_metrics("ethereum") }})
+    , censored_block_metrics AS ({{ get_censored_block_metrics("ethereum") }})
+    , revenue_data AS (
+        SELECT 
+            date
+            , revenue
+            , native_token_burn AS revenue_native
+        FROM {{ ref("agg_daily_ethereum_revenue") }}
+    )
+    , github_data AS ({{ get_github_metrics("ethereum") }})
+    , contract_data AS ({{ get_contract_metrics("ethereum") }})
+    , validator_queue_data AS (
+        SELECT 
+            date
+            , queue_entry_amount
+            , queue_exit_amount
+            , queue_active_amount
+        FROM {{ ref("fact_ethereum_beacon_chain_queue_entry_active_exit_silver") }}
+    )
+    , nft_metrics AS ({{ get_nft_metrics("ethereum") }})
+    , p2p_metrics AS ({{ get_p2p_metrics("ethereum") }})
+    , rolling_metrics AS ({{ get_rolling_active_address_metrics("ethereum") }})
+    , da_metrics AS (
+        SELECT 
+            date
+            , blob_fees_native
+            , blob_fees
+            , blob_size_mib
+            , avg_mib_per_second
+            , avg_cost_per_mib
+            , avg_cost_per_mib_gwei
+            , submitters
+        FROM {{ ref("fact_ethereum_da_metrics") }}
+    )
+    , etf_metrics AS (
         SELECT
-            date,
-            sum(net_etf_flow_native) as net_etf_flow_native,
-            sum(net_etf_flow) as net_etf_flow,
-            sum(cumulative_etf_flow_native) as cumulative_etf_flow_native,
-            sum(cumulative_etf_flow) as cumulative_etf_flow
+            date
+            , SUM(net_etf_flow_native) AS net_etf_flow_native
+            , SUM(net_etf_flow) AS net_etf_flow
+            , SUM(cumulative_etf_flow_native) AS cumulative_etf_flow_native
+            , SUM(cumulative_etf_flow) AS cumulative_etf_flow
         FROM {{ ref("ez_ethereum_etf_metrics") }}
         GROUP BY 1
-    ),
-    ethereum_dex_volumes as (
-        select date, daily_volume as dex_volumes, daily_volume_adjusted as adjusted_dex_volumes
-        from {{ ref("fact_ethereum_daily_dex_volumes") }}
-    ),
-    block_rewards_data as (
-        select date, block_rewards_native
-        from {{ ref("fact_ethereum_block_rewards") }}
-    ),
-    adjusted_dau_metrics as (
-        select date, adj_daus as adjusted_dau
-        from {{ ref("ez_ethereum_adjusted_dau") }}
     )
-    , eth_supply as (
-        select date, issued_supply, circulating_supply
-        from {{ ref("fact_ethereum_eth_supply_estimated") }}
+    , ethereum_dex_volumes AS (
+        SELECT 
+            date
+            , SUM(daily_volume) AS dex_volumes
+            , SUM(daily_volume_adjusted) AS adjusted_dex_volumes
+        FROM {{ ref("fact_ethereum_daily_dex_volumes") }}
+        GROUP BY 1
+    )
+    , block_rewards_data AS (
+        SELECT 
+            date
+            , block_rewards_native
+        FROM {{ ref("fact_ethereum_block_rewards") }}
+    )
+    , adjusted_dau_metrics AS (
+        SELECT 
+            date
+            , adj_daus AS adjusted_dau
+        FROM {{ ref("ez_ethereum_adjusted_dau") }}
+    )
+    , eth_supply AS (
+        SELECT 
+            date
+            , issued_supply
+            , circulating_supply
+        FROM {{ ref("fact_ethereum_eth_supply_estimated") }}
     )
     , application_fees AS (
         SELECT 
@@ -82,37 +114,27 @@ with
     )
 
 select
-    fundamental_data.date
-    , fundamental_data.chain
-    , fundamental_data.txns
-    , dau
-    , adjusted_dau
-    , wau
-    , mau
-    , avg_txn_fee
-    , median_txn_fee
-    , case
-        when fees is null then (fees_native * price) - revenue else fees - revenue
-    end as priority_fee_usd
-    , nft_trading_volume
-    , dau_over_100
-    , percent_censored
-    , percent_semi_censored
-    , percent_non_censored
-    , dune_dex_volumes_ethereum.dex_volumes
-    , dune_dex_volumes_ethereum.adjusted_dex_volumes
-    , submitters
+    date_spine.date
+    , 'ethereum' as artemis_id
+
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price
-    , market_cap
-    , fdmc
-    , tvl
-    -- Chain Usage Metrics
-    , dau AS chain_dau
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data
+    , fundamental_data.dau AS chain_dau
+    , adjusted_dau.adjusted_dau AS chain_dau_adjusted
+    , fundamental_data.dau
+    , fundamental_data.txns AS chain_txns
+    , fundamental_data.txns
     , wau AS chain_wau
     , mau AS chain_mau
-    , txns AS chain_txns
+    , tvl AS chain_tvl
+    , tvl 
     , avg_txn_fee AS chain_avg_txn_fee
     , median_txn_fee AS chain_median_txn_fee
     , returning_users
@@ -139,41 +161,43 @@ select
     , p2p_native_transfer_volume
     , p2p_token_transfer_volume
     , p2p_transfer_volume
-    , coalesce(artemis_stablecoin_transfer_volume, 0) - coalesce(stablecoin_data.p2p_stablecoin_transfer_volume, 0) as non_p2p_stablecoin_transfer_volume
-    , coalesce(dune_dex_volumes_ethereum.dex_volumes, 0) + coalesce(nft_trading_volume, 0) + coalesce(p2p_transfer_volume, 0) as settlement_volume
+    , COALESCE(artemis_stablecoin_transfer_volume, 0) - COALESCE(stablecoin_data.p2p_stablecoin_transfer_volume, 0) AS non_p2p_stablecoin_transfer_volume
+    , COALESCE(ethereum_dex_volumes.dex_volumes, 0) + COALESCE(nft_trading_volume, 0) + COALESCE(p2p_transfer_volume, 0) AS settlement_volume
     , blob_fees_native
     , blob_fees
     , blob_size_mib
     , avg_mib_per_second
     , avg_cost_per_mib_gwei
     , avg_cost_per_mib
-    , submitters as da_dau
-    , dune_dex_volumes_ethereum.dex_volumes AS chain_spot_volume
-    , coalesce(fees, 0) + coalesce(blob_fees, 0) + coalesce(priority_fee_usd, 0) + coalesce(settlement_volume, 0) + coalesce(application_fees.application_fees, 0) as total_economic_activity
-    -- Cashflow metrics
-    , fees as chain_fees
-    , case when fees is null then (coalesce(blob_fees_native, 0) + fees_native) * price else fees + coalesce(blob_fees, 0) end as fees
+    , submitters AS da_dau
+    , ethereum_dex_volumes.dex_volumes AS chain_spot_volume
+    , COALESCE(fees, 0) + COALESCE(blob_fees, 0) + COALESCE(priority_fee_usd, 0) + COALESCE(settlement_volume, 0) + COALESCE(application_fees.application_fees, 0) AS total_economic_activity
+    , ethereum_dex_volumes.dex_volumes AS chain_spot_volume
+    , ethereum_dex_volumes.adjusted_dex_volumes AS chain_spot_volume_adjusted
+
+    -- Fee Data
     , fees_native
-    , revenue_native AS burned_fee_allocation_native
-    , revenue AS burned_fee_allocation
-    , fees_native - revenue_native as priority_fees_native
     , priority_fee_usd AS priority_fees
-    -- Financial Statement Metrics
-    , revenue_native + coalesce(blob_fees_native, 0) as revenue_native
-    , revenue + coalesce(blob_fees, 0) as revenue
-    , block_rewards_native  * price AS token_incentives
+    , fees AS chain_fees
+    , blob_fees 
+    , COALESCE(fees, 0) + COALESCE(blob_fees, 0) + COALESCE(priority_fee_usd, 0) AS fees
+    , revenue AS burned_fee_allocation
+
+    -- Financial Statements
+    , revenue_native + COALESCE(blob_fees_native, 0) AS revenue_native
+    , revenue + COALESCE(blob_fees, 0) AS revenue
+    , block_rewards_native * price AS token_incentives
     , revenue - token_incentives AS earnings
-    -- Developer metrics
+
+    -- Developer Data
     , weekly_commits_core_ecosystem
     , weekly_commits_sub_ecosystem
     , weekly_developers_core_ecosystem
     , weekly_developers_sub_ecosystem
     , weekly_contracts_deployed
     , weekly_contract_deployers
-    -- Supply metrics
-    , block_rewards_native AS gross_emissions_native
-    , block_rewards_native * price AS gross_emissions
-    -- Stablecoin metrics
+
+    -- Stablecoin Data
     , stablecoin_total_supply
     , stablecoin_txns
     , stablecoin_dau
@@ -189,36 +213,47 @@ select
     , p2p_stablecoin_dau
     , p2p_stablecoin_mau
     , stablecoin_data.p2p_stablecoin_transfer_volume
-    , eth_supply.issued_supply as issued_supply_native
-    , eth_supply.circulating_supply as circulating_supply_native
-    -- ETF Metrics
+
+    -- ETF Data
     , net_etf_flow_native
     , net_etf_flow
     , cumulative_etf_flow_native
     , cumulative_etf_flow
+
+    -- Supply Data
+    , block_rewards_native AS gross_emissions_native
+    , block_rewards_native * price AS gross_emissions
+    , eth_supply.issued_supply AS issued_supply_native
+    , eth_supply.circulating_supply AS circulating_supply_native
+
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from fundamental_data
-left join price_data on fundamental_data.date = price_data.date
-left join defillama_data on fundamental_data.date = defillama_data.date
-left join stablecoin_data on fundamental_data.date = stablecoin_data.date
-left join censored_block_metrics on fundamental_data.date = censored_block_metrics.date
-left join staking_data on fundamental_data.date = staking_data.date
-left join revenue_data on fundamental_data.date = revenue_data.date
-left join validator_queue_data on fundamental_data.date = validator_queue_data.date
-left join github_data on fundamental_data.date = github_data.date
-left join contract_data on fundamental_data.date = contract_data.date
-left join nft_metrics on fundamental_data.date = nft_metrics.date
-left join p2p_metrics on fundamental_data.date = p2p_metrics.date
-left join rolling_metrics on fundamental_data.date = rolling_metrics.date
-left join da_metrics on fundamental_data.date = da_metrics.date
-left join etf_metrics on fundamental_data.date = etf_metrics.date
-left join ethereum_dex_volumes as dune_dex_volumes_ethereum on fundamental_data.date = dune_dex_volumes_ethereum.date
-left join block_rewards_data on fundamental_data.date = block_rewards_data.date
-left join eth_supply on fundamental_data.date = eth_supply.date
-left join adjusted_dau_metrics on fundamental_data.date = adjusted_dau_metrics.date
-left join application_fees on fundamental_data.date = application_fees.date
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN fundamental_data USING (date)
+LEFT JOIN market_data USING (date)
+LEFT JOIN defillama_data USING (date)
+LEFT JOIN stablecoin_data USING (date)
+LEFT JOIN censored_block_metrics USING (date)
+LEFT JOIN staking_data USING (date)
+LEFT JOIN revenue_data USING (date)
+LEFT JOIN validator_queue_data USING (date)
+LEFT JOIN github_data USING (date)
+LEFT JOIN contract_data USING (date)
+LEFT JOIN nft_metrics USING (date)
+LEFT JOIN p2p_metrics USING (date)
+LEFT JOIN rolling_metrics USING (date)
+LEFT JOIN da_metrics USING (date)
+LEFT JOIN etf_metrics USING (date)
+LEFT JOIN ethereum_dex_volumes USING (date)
+LEFT JOIN block_rewards_data USING (date)
+LEFT JOIN eth_supply USING (date)
+LEFT JOIN adjusted_dau_metrics USING (date)
+LEFT JOIN application_fees USING (date)
 where true
-{{ ez_metrics_incremental('fundamental_data.date', backfill_date) }}
-and fundamental_data.date < to_date(sysdate())
+{{ ez_metrics_incremental('date_spine.date', backfill_date) }}
+AND date_spine.date < to_date(sysdate())
