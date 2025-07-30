e0e9e65ab5614df36231d113ac592babe51d4f8d
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 21:50:24 -0400

    standardized bob metrics

--- models/projects/bob/core/ez_bob_metrics.sql
+++ models/projects/bob/core/ez_bob_metrics.sql
@@ -17,9 +17,15 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with 
-    fundamental_data as (
-        select
+WITH 
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2024-04-11'
+        AND date < to_date(sysdate())
+    )
+    , fundamental_data as (
+        SELECT
             date 
             , txns
             , daa
@@ -29,41 +35,42 @@ with
             , cost_native
             , revenue
             , revenue_native
-        from {{ ref("fact_bob_fundamental_metrics") }}
-    ),
-    price_data as ({{ get_coingecko_metrics('bob-build-on-bitcoin') }})
+        FROM {{ ref("fact_bob_fundamental_metrics") }}
+    )
+    , market_data as ({{ get_coingecko_metrics('bob-build-on-bitcoin') }})
 select
-    f.date
-    , txns
-    , daa as dau
-    , fees_native
-    , fees
-    , cost
-    , cost_native
-    -- we leave revenue and revenue_native untouched as there isn't much information about bob
-    , revenue
-    , revenue_native
+    date_spine.date
+    , 'bob' AS artemis_id
+
     -- Standardized Metrics
+
     -- Market Data
-    , price
-    , market_cap
-    , fdmc
-    , token_volume
-    -- Chain Metrics
-    , txns as chain_txns
-    , dau as chain_dau
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Metrics
+    , fundamental_data.dau as chain_dau
+    , fundamental_data.daa as dau
+    , fundamental_data.txns as chain_txns
+    , fundamental_data.txns
+
     -- Cash Flow Metrics
-    , fees as ecosystem_revenue
-    , fees_native as ecosystem_revenue_native
-    , cost as l1_fee_allocation
-    , cost_native as l1_fee_allocation_native
-    , token_turnover_circulating
-    , token_turnover_fdv
+    , fundamental_data.fees_native
+    , fundamental_data.fees
+    , fundamental_data.cost as l1_fee_allocation
+
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from fundamental_data f
-left join price_data using(f.date)
-where true 
-{{ ez_metrics_incremental('f.date', backfill_date) }}
-and f.date  < to_date(sysdate())
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN fundamental_data USING (date)
+LEFT JOIN market_data USING (date)
+WHERE true
+{{ ez_metrics_incremental("date", backfill_date) }}
+AND date < to_date(sysdate())
