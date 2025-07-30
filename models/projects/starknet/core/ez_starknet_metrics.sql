8e8afb67390b98a6ae4fa3de794df6c9f4f117a6
limzhengjie <limzhengjie01@gmail.com>
2025-07-30 19:44:31 -0400

    standardize starknet metrics

--- models/projects/starknet/core/ez_starknet_metrics.sql
+++ models/projects/starknet/core/ez_starknet_metrics.sql
@@ -20,7 +20,7 @@
 
 with
     fundamental_data as ({{ get_fundamental_data_for_chain("starknet") }}),
-    price_data as ({{ get_coingecko_metrics("starknet") }}),
+    market_metrics as ({{ get_coingecko_metrics("starknet") }}),
     defillama_data as ({{ get_defillama_metrics("starknet") }}),
     expenses_data as (
         select date, chain, l1_data_cost_native, l1_data_cost
@@ -49,65 +49,65 @@ with
 
 select
     fundamental_data.date
-    , fundamental_data.chain
-    , txns
-    , dau
-    , wau
-    , mau
-    , fees
-    , fees_data.fees_native
-    , l1_data_cost_native
-    , l1_data_cost
-    , coalesce(fees_data.fees_native, 0) - l1_data_cost_native as revenue_native
-    , coalesce(fees, 0) -  l1_data_cost as revenue
-    , avg_txn_fee
-    , median_txn_fee
-    , dex_volumes
+    , 'starknet' as artemis_id
+    
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price
-    , market_cap
-    , fdmc
-    , tvl
-    -- Chain Usage Metrics
-    , dau AS chain_dau
-    , wau AS chain_wau
-    , mau AS chain_mau
-    , txns AS chain_txns
-    , avg_txn_fee AS chain_avg_txn_fee
-    , dex_volumes AS chain_spot_volume
-    , returning_users
-    , new_users
-    -- Cashflow Metrics
-    , fees as chain_fees
-    , fees AS ecosystem_revenue
-    , fees_data.fees_native AS ecosystem_revenue_native
-    , median_txn_fee AS chain_median_txn_fee
+
+    -- Market Data
+    , market_metrics.price
+    , market_metrics.market_cap
+    , market_metrics.fdmc
+    , market_metrics.tvl
+
+    -- Usage Data
+    , fundamental_data.dau as chain_dau
+    , fundamental_data.dau as dau
+    , fundamental_data.wau as chain_wau
+    , fundamental_data.mau as chain_mau
+    , fundamental_data.txns as chain_txns
+    , fundamental_data.txns as txns
+    , fundamental_data.avg_txn_fee as chain_avg_txn_fee
+    , fundamental_data.dex_volumes as chain_spot_volume
+    , fundamental_data.median_txn_fee as chain_median_txn_fee
+    , fundamental_data.returning_users
+    , fundamental_data.new_users
+
+    -- Fee Data
+    , fees_data.fees_native as fees_native
+    , fees_data.fees as chain_fees
+    , fees_data.fees as fees
+    , l1_data_cost_native as l1_fee_allocation_native
+    , l1_data_cost as l1_fee_allocation
     , coalesce(fees_data.fees_native, 0) - l1_data_cost_native as equity_fee_allocation_native
     , coalesce(fees, 0) -  l1_data_cost as equity_fee_allocation
-    , l1_data_cost_native AS l1_fee_allocation_native
-    , l1_data_cost AS l1_fee_allocation
-    -- Bridge Metrics,
-    , bridge_volume
-    , bridge_daa
+
+    -- Bridge Metrics
+    , bridge_volume_metrics.bridge_volume
+    , bridge_daa_metrics.bridge_daa
+
     -- Developer Metrics
-    , weekly_commits_core_ecosystem
-    , weekly_commits_sub_ecosystem
-    , weekly_developers_core_ecosystem
-    , weekly_developers_sub_ecosystem
+    , github_data.weekly_commits_core_ecosystem
+    , github_data.weekly_commits_sub_ecosystem
+    , github_data.weekly_developers_core_ecosystem
+    , github_data.weekly_developers_sub_ecosystem
 
     -- Supply Metrics
-    , premine_unlocks_native
-    , gross_emissions_native
-    , burns_native
-    , net_supply_change_native
-    , circulating_supply_native
+    , supply_data.premine_unlocks_native
+    , supply_data.gross_emissions_native
+    , supply_data.burns_native
+    , supply_data.net_supply_change_native
+    , supply_data.circulating_supply_native
+
+    -- Market Data
+    , market_metrics.token_turnover_fdv
+    , market_metrics.token_turnover_circulating
 
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
+
 from fundamental_data
-left join price_data on fundamental_data.date = price_data.date
+left join market_metrics on fundamental_data.date = market_metrics.date
 left join defillama_data on fundamental_data.date = defillama_data.date
 left join expenses_data on fundamental_data.date = expenses_data.date
 left join github_data on fundamental_data.date = github_data.date
