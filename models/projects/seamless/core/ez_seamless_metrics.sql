b48532e92a121cff211520310865bb3f8b35bbde
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-29 21:30:15 -0400

    Standardized seamless metrics

--- models/projects/seamless/core/ez_seamless_metrics.sql
+++ models/projects/seamless/core/ez_seamless_metrics.sql
@@ -17,8 +17,14 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    seamless_by_chain as (
+WITH
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2023-08-31'
+        AND date < to_date(sysdate())
+    )
+    , seamless_by_chain AS (
         {{
             dbt_utils.union_relations(
                 relations=[
@@ -27,35 +33,43 @@ with
             )
         }}
     )
-    , seamless_metrics as (
-        select
+    , seamless_metrics AS (
+        SELECT
             date
-            , sum(daily_borrows_usd) as daily_borrows_usd
-            , sum(daily_supply_usd) as daily_supply_usd
-        from seamless_by_chain
-        group by 1
+            , SUM(daily_borrows_usd) AS daily_borrows_usd
+            , SUM(daily_supply_usd) AS daily_supply_usd
+        FROM seamless_by_chain
+        GROUP BY 1
     )
 
-    , price_data as ({{ get_coingecko_metrics("seamless-protocol") }})
+    , market_data AS ({{ get_coingecko_metrics("seamless-protocol") }})
 
-select
+SELECT
     seamless_metrics.date
-    , 'seamless' as app
-    , 'DeFi' as category
-    , seamless_metrics.daily_borrows_usd
-    , seamless_metrics.daily_supply_usd
-    -- Standardized metrics
-    , seamless_metrics.daily_borrows_usd as lending_loans
-    , seamless_metrics.daily_supply_usd as lending_deposits
-    , price_data.price
-    , price_data.market_cap
-    , price_data.fdmc
+    , 'seamlessprotocol' AS artemis_id
+
+    -- Standardized Metrics
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data
+    , seamless_metrics.daily_borrows_usd AS lending_loans
+    , seamless_metrics.daily_supply_usd AS lending_deposits
+
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from seamless_metrics
-left join price_data
-    on seamless_metrics.date = price_data.date
-where true
-{{ ez_metrics_incremental('seamless_metrics.date', backfill_date) }}
-and seamless_metrics.date < to_date(sysdate())
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN seamless_metrics USING (date)
+LEFT JOIN market_data USING (date)
+WHERE true
+{{ ez_metrics_incremental("seamless_metrics.date", backfill_date) }}
+AND seamless_metrics.date < to_date(sysdate())
