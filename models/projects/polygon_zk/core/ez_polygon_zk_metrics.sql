98e77d99aff556a8cffcfedb6caeb754ee172262
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-30 16:30:00 -0400

    Standardized polygon zk metrics

--- models/projects/polygon_zk/core/ez_polygon_zk_metrics.sql
+++ models/projects/polygon_zk/core/ez_polygon_zk_metrics.sql
@@ -18,70 +18,92 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    fundamental_data as (
-        select date, chain, daa as dau, txns, gas as fees_native, gas_usd as fees
-        from {{ ref("fact_polygon_zk_daa_txns_gas_usd") }}
-    ),
-    price_data as ({{ get_coingecko_metrics("matic-network") }}),
-    defillama_data as ({{ get_defillama_metrics("polygon zkevm") }}),
-    l1_data_cost as (
-        select
-            date,
-            l1_data_cost_native,
-            l1_data_cost
-        from {{ ref("fact_polygon_zk_l1_data_cost") }}
-    ),
-    github_data as ({{ get_github_metrics("Polygon Hermez") }}),
-    polygon_zk_dex_volumes as (
-        select date, daily_volume as dex_volumes
-        from {{ ref("fact_polygon_zk_daily_dex_volumes") }}
+WITH 
+    date_spine as (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2023-03-24'
+        AND date < to_date(sysdate())
     )
-select
-    fundamental_data.date
-    , fundamental_data.chain
-    , txns
-    , dau
-    , l1_data_cost_native
-    , l1_data_cost
-    , fees
-    , fees / txns as avg_txn_fee
-    , coalesce(fees, 0) - l1_data_cost as revenue
-    , dune_dex_volumes_polygon_zk.dex_volumes
+    , fundamental_data AS (
+        SELECT 
+            date
+            , chain
+            , daa AS dau
+            , txns
+            , gas AS fees_native
+            , gas_usd AS fees
+        FROM {{ ref("fact_polygon_zk_daa_txns_gas_usd") }}
+    )
+    , market_data AS ({{ get_coingecko_metrics("polygon-ecosystem-token") }})
+    , defillama_data AS ({{ get_defillama_metrics("polygon zkevm") }})
+    , l1_data_cost AS (
+        SELECT 
+            date
+            , l1_data_cost_native
+            , l1_data_cost
+        FROM {{ ref("fact_polygon_zk_l1_data_cost") }}
+    )
+    , github_data AS ({{ get_github_metrics("Polygon Hermez") }})
+    , polygon_zk_dex_volumes AS (
+        SELECT 
+            date
+            , daily_volume AS dex_volumes
+        FROM {{ ref("fact_polygon_zk_daily_dex_volumes") }}
+    )
+SELECT
+    date_spine.date
+    , 'polygon_zk' AS artemis_id
+
     -- Standardized Metrics
-    -- Market Data Metrics
-    , price
-    , market_cap
-    , fdmc
-    , tvl
-    -- Chain Usage Metrics
-    , txns AS chain_txns
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data
     , dau AS chain_dau
-    , fees / txns as chain_avg_txn_fee
+    , dau 
+    , txns AS chain_txns
+    , txns
+    , tvl AS chain_tvl
+    , tvl
+    , fees / txns AS chain_avg_txn_fee
     , dune_dex_volumes_polygon_zk.dex_volumes AS chain_spot_volume
-    -- Cash Flow Metrics
+
+    -- Fee Data
     , fees AS chain_fees
-    , fees_native AS ecosystem_revenue_native
-    , fees AS ecosystem_revenue
-    , coalesce(fees_native, 0) - l1_data_cost_native as service_fee_allocation_native
-    , coalesce(fees, 0) - l1_data_cost as service_fee_allocation
-    , l1_data_cost_native AS l1_fee_allocation_native
+    , fees
+    , COALESCE(fees, 0) - l1_data_cost AS service_fee_allocation
     , l1_data_cost AS l1_fee_allocation
-    -- Developer Metrics
+
+    -- Financial Statements
+    , COALESCE(fees, 0) - l1_data_cost AS revenue
+    , revenue AS earnings 
+
+    -- Developer Data
     , weekly_commits_core_ecosystem
     , weekly_commits_sub_ecosystem
     , weekly_developers_core_ecosystem
     , weekly_developers_sub_ecosystem
 
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from fundamental_data
-left join price_data on fundamental_data.date = price_data.date
-left join defillama_data on fundamental_data.date = defillama_data.date
-left join l1_data_cost on fundamental_data.date = l1_data_cost.date
-left join github_data on fundamental_data.date = github_data.date
-left join polygon_zk_dex_volumes as dune_dex_volumes_polygon_zk on fundamental_data.date = dune_dex_volumes_polygon_zk.date
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN fundamental_data USING (date)
+LEFT JOIN market_data USING (date)
+LEFT JOIN defillama_data USING (date)
+LEFT JOIN l1_data_cost USING (date)
+LEFT JOIN github_data USING (date)
+LEFT JOIN polygon_zk_dex_volumes AS dune_dex_volumes_polygon_zk USING (date)
 where true
-{{ ez_metrics_incremental('fundamental_data.date', backfill_date) }}
-and fundamental_data.date < to_date(sysdate())
+{{ ez_metrics_incremental('date_spine.date', backfill_date) }}
+AND date_spine.date < to_date(sysdate())
+GROUP BY ALL
