21048a4d90a25c69788d653c06dfb0abd5e26a30
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-30 16:04:43 -0400

    Standardized dydx metrics

--- models/projects/dydx_v4/core/ez_dydx_v4_metrics.sql
+++ models/projects/dydx_v4/core/ez_dydx_v4_metrics.sql
@@ -17,51 +17,86 @@
 
 {% set backfill_date = var("backfill_date", None) %}
 
-with
-    trading_volume_data as (
-        select date, trading_volume
-        from {{ ref("fact_dydx_v4_trading_volume") }}
+WITH
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine") }}
+        WHERE date >= '2023-11-13'
+        AND date < to_date(sysdate())
     )
-    , fees_data as (
-        select date, maker_fees, taker_fees, fees
-        from {{ ref("fact_dydx_v4_fees") }}
+    , trading_volume_data AS (
+        SELECT 
+            date
+            , SUM(trading_volume) AS trading_volume
+        FROM {{ ref("fact_dydx_v4_trading_volume") }}
+        GROUP BY date
     )
-    , chain_data as (
-        select date, maker_fees, maker_rebates, txn_fees
-        from {{ ref("fact_dydx_v4_txn_fees") }}
+    , fees_data AS (
+        SELECT 
+            date
+            , maker_fees
+            , taker_fees
+            , fees
+        FROM {{ ref("fact_dydx_v4_fees") }}
     )
-    , trading_fees as (
-        select date, total_fees
-        from {{ ref("fact_dydx_v4_trading_fees") }}
+    , chain_data AS (
+        SELECT 
+            date
+            , maker_fees
+            , maker_rebates
+            , txn_fees
+        FROM {{ ref("fact_dydx_v4_txn_fees") }}
     )
-    , unique_traders_data as (
-        select date, unique_traders
-        from {{ ref("fact_dydx_v4_unique_traders") }}
+    , trading_fees AS (
+        SELECT 
+            date
+            , total_fees
+        FROM {{ ref("fact_dydx_v4_trading_fees") }}
     )
+    , unique_traders_data AS (
+        SELECT 
+            date
+            , SUM(unique_traders) AS unique_traders
+        FROM {{ ref("fact_dydx_v4_unique_traders") }}
+        GROUP BY date
+    )
+    , market_data AS ({{get_coingecko_metrics("dydx-chain")}})
 
-select 
-    unique_traders_data.date as date
-    , 'dydx_v4' as app
-    , 'DeFi' as category
-    , trading_volume
-    , unique_traders
+SELECT 
+    date_spine.date
+    , 'dydx_v4' AS artemis_id
+
+    -- Market Data
+    , market_data.price
+    , market_data.market_cap
+    , market_data.fdmc
+    , market_data.token_volume
+
+    -- Usage Data 
+    , unique_traders_data.unique_traders AS perp_dau
+    , unique_traders_data.unique_traders AS dau
+    , trading_volume_data.trading_volume AS perp_volume
+
+    -- Fee data
     , fees_data.maker_fees
-    , taker_fees
-    , fees
-    , fees_data.maker_fees + fees_data.taker_fees as trading_fees -- Trading fees is maker_fees+taker_fees
-    , txn_fees -- chain transaction fees (not really significant)
-    -- standardize metrics
-    , trading_volume as perp_volume
-    , unique_traders as perp_dau
-    , trading_fees + txn_fees as ecosystem_revenue
-    , case when unique_traders_data.date > '2025-03-25' then ecosystem_revenue * 0.25 else 0 end as buybacks
+    , fees_data.taker_fees
+    , chain_data.txn_fees
+    , fees_data.maker_fees + fees_data.taker_fees + chain_data.txn_fees AS fees
+    , CASE WHEN unique_traders_data.date > '2025-03-25' THEN fees * 0.25 ELSE 0 END AS buybacks
+    
+    -- Turnover Data
+    , market_data.token_turnover_circulating
+    , market_data.token_turnover_fdv
+    
     -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-from trading_volume_data
-left join fees_data on trading_volume_data.date = fees_data.date
-left join chain_data on trading_volume_data.date = chain_data.date
-left join unique_traders_data on trading_volume_data.date = unique_traders_data.date
-where true 
-{{ ez_metrics_incremental('unique_traders_data.date', backfill_date) }}
-and unique_traders_data.date < to_date(sysdate())
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+FROM date_spine
+LEFT JOIN trading_volume_data USING (date)
+LEFT JOIN fees_data USING (date)
+LEFT JOIN chain_data USING (date)
+LEFT JOIN unique_traders_data USING (date)
+LEFT JOIN market_data USING (date)
+WHERE true
+{{ ez_metrics_incremental("date_spine.date", backfill_date) }}
+AND date_spine.date < to_date(sysdate())
