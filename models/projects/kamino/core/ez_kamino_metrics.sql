d56a83c4b4368b28ce6b7aa868690eb2654537a5
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-31 12:09:39 -0400

    Standardized kamino metrics

--- models/projects/kamino/core/ez_kamino_metrics.sql
+++ models/projects/kamino/core/ez_kamino_metrics.sql
@@ -18,45 +18,75 @@
 {% set backfill_date = var("backfill_date", None) %}
 
 with 
-    kamino_tvl as (
-        select date, sum(usd_balance) as tvl
-        from {{ ref("fact_kamino_tvl") }}
-        group by date
-    ),
+    date_spine AS (
+        SELECT date
+        FROM {{ ref("dim_date_spine")}}
+        WHERE date >= '2023-11-01' AND date < to_date(sysdate())
+    )
+    
+    , kamino_tvl AS (
+        SELECT 
+            date, 
+            sum(usd_balance) as tvl
+        FROM {{ ref("fact_kamino_tvl") }}
+        GROUP BY date
+    )
+    , klend_fees_and_revenue AS (
+       SELECT 
+            date, 
+            klend_fees_usd as fees, 
+            klend_revenue_usd as revenue
+        FROM {{ ref("fact_kamino_fees_and_revenues") }}
+    )
+    , kamino_transactions AS (    
+        SELECT 
+            date, 
+            tx_count as txn, 
+            dau
+        FROM {{ ref("dim_kamino_transactions") }}
+    )
 
-    klend_fees_and_revenue as (
-        select date, klend_fees_usd as fees, klend_revenue_usd as revenue
-        from {{ ref("fact_kamino_fees_and_revenues") }}
-    ),
-
-    kamino_transactions as (    
-        select date, tx_count as txn, dau
-        from {{ ref("dim_kamino_transactions") }}
-    ),
-
-    market_data as (
+    , market_data AS (
         {{ get_coingecko_metrics('kamino') }}
     )
-    select
-        date,
-        tvl,
-        fees,
-        revenue,
-        txn as txns,
-        dau,
-        coalesce(market_data.price, 0) as price,
-        coalesce(market_data.market_cap, 0) as market_cap,
-        coalesce(market_data.fdmc, 0) as fdmc,
-        coalesce(market_data.token_turnover_circulating, 0) as token_turnover_circulating,
-        coalesce(market_data.token_turnover_fdv, 0) as token_turnover_fdv,
-        coalesce(market_data.token_volume, 0) as token_volume,
-        -- timestamp columns
-        TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on,
-        TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
-    from kamino_tvl
-    left join klend_fees_and_revenue using (date)
-    left join market_data using (date)
-    left join kamino_transactions using (date)
-    where true
-    {{ ez_metrics_incremental('date', backfill_date) }}
-    and date < to_date(sysdate())
+
+    SELECT
+        date_spine.date
+        , 'kamino' as artemis_id
+
+        -- Market Data
+        , market_data.price
+        , market_data.market_cap
+        , market_data.fdmc
+        , market_data.token_volume
+
+        -- Usage Data
+        , dau AS lending_dau
+        , dau
+        , txn AS lending_txns
+        , txn AS txns
+        , tvl AS lending_deposits
+        , tvl 
+
+        -- Fee Data
+        , fees AS lending_fees
+        , fees
+
+        -- Financial Statements
+        , revenue
+
+        -- Turnover Data
+        , market_data.token_turnover_circulating
+        , market_data.token_turnover_fdv
+
+        -- Timestamp Columns
+        TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on,
+        TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
+    FROM date_spine
+    LEFT JOIN kamino_tvl USING (date)
+    LEFT JOIN klend_fees_and_revenue USING (date)
+    LEFT JOIN market_data USING (date)
+    LEFT JOIN kamino_transactions USING (date)
+    WHERE true
+    {{ ez_metrics_incremental('date_spine.date', backfill_date) }}
+    AND date_spine.date < to_date(sysdate())
