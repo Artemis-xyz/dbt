9ac7eb157e0d14a35755fca495d5032446421623
Alex Kan <29241719+akan72@users.noreply.github.com>
2025-07-31 07:17:56 -0700

    Dev Schemas Implementation (#1546)

---
+++ macros/admin/generate_alias_name.sql
@@ -0,0 +1,29 @@
+{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+
+    {# 
+        'If using the dev target, use the name of the model file as its alias.
+        Otherwise, use the default behavior of dbt'
+    #}
+    {%- if target.name == "dev" -%}
+        {{ return(node.name) }}
+    {%- endif -%}
+
+    {#
+        'If a custom alias is provided, use it.
+        Otherwise, use the name of the node and append the version if it exists'
+    #}
+    {%- if custom_alias_name -%}
+
+        {{ custom_alias_name | trim }}
+
+    {%- elif node.version -%}
+
+        {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}
+
+    {%- else -%}
+
+        {{ node.name }}
+
+    {%- endif -%}
+
+{%- endmacro %}
