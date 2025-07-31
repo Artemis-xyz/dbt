39e80512dd139d03350067e2064408579761c6ba
limzhengjie <limzhengjie01@gmail.com>
2025-07-30 20:55:57 -0400

    standardize zksync models

--- models/projects/zksync/core/ez_zksync_metrics_by_chain.sql
+++ models/projects/zksync/core/ez_zksync_metrics_by_chain.sql
@@ -13,27 +13,17 @@ with
         select date, chain, inflow, outflow
         from {{ ref("fact_zksync_era_bridge_bridge_volume") }}
         where chain is not null
-    ),
-    price_data as (
-        {{ get_coingecko_metrics('zksync') }}
     )
 select
-    b.date
-    -- Old metrics needed for compatibility
-    , 'zksync' as app
-    , 'Bridge' as category
-    , b.chain
+    bridge_volume_metrics.date
+    , 'zksync' as artemis_id
+    , bridge_volume_metrics.chain
+
     -- Standardized Metrics
-    -- Market Data
-    , price
-    , market_cap
-    , fdmc
-    , token_volume
-    -- Bridge Metrics
-    , inflow
-    , outflow
-    , token_turnover_circulating
-    , token_turnover_fdv
-from bridge_volume_metrics as b
-left join price_data on b.date = price_data.date
-where b.date < to_date(sysdate())
+
+    -- Usage Data
+    , bridge_volume_metrics.inflow
+    , bridge_volume_metrics.outflow
+
+from bridge_volume_metrics
+where bridge_volume_metrics.date < to_date(sysdate())
