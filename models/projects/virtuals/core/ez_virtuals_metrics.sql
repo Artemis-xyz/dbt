3f8eb626611030cd42f91379a8f3735bda95b07a
limzhengjie <limzhengjie01@gmail.com>
2025-07-30 21:32:26 -0400

    standardize virtuals models

--- models/projects/virtuals/core/ez_virtuals_metrics.sql
+++ models/projects/virtuals/core/ez_virtuals_metrics.sql
@@ -22,38 +22,38 @@ with date_spine as (
     from {{ ref("dim_date_spine") }}
     where date between '2024-09-10' and to_date(sysdate())
 )
-, daily_agents as (
+, virtuals_daily_agents as (
     select
         date
-        , daily_agents
+        , coalesce(daily_agents, 0) as daily_agents
     from {{ ref("fact_virtuals_daily_agents") }}
-),
-dau as (
+)
+, virtuals_dau as (
     select
         date
-        , dau
+        , coalesce(dau, 0) as dau
     from {{ ref("fact_virtuals_dau") }}
-),
-volume as (
+)
+, virtuals_volume as (
     select
         date
-        , volume_native
-        , volume_usd
+        , coalesce(volume_native, 0) as volume_native
+        , coalesce(volume_usd, 0) as volume_usd
     from {{ ref("fact_virtuals_volume") }}
 )
-, fees as (
+, virtuals_fees as (
     select
         date
-        , fee_fun_native
-        , fee_fun_usd
-        , tax_usd
-        , fees
+        , coalesce(fee_fun_native, 0) as fee_fun_native
+        , coalesce(fee_fun_usd, 0) as fee_fun_usd
+        , coalesce(tax_usd, 0) as tax_usd
+        , coalesce(fees, 0) as fees
     from {{ ref("fact_virtuals_fees") }}
 )
-, market_data as (
+, market_metrics as (
     {{ get_coingecko_metrics('virtual-protocol') }}
 )
-, supply_data as (
+, virtuals_supply_data as (
     select
         date
         , virtuals_sablier_lockup
@@ -65,53 +65,52 @@ volume as (
 )
 
 select
-    date
-    , coalesce(daily_agents, 0) as daily_agents
-    , coalesce(dau, 0) as dau
-    , coalesce(volume_native, 0) as volume_native
-    , coalesce(volume_usd, 0) as volume_usd
-    , coalesce(fee_fun_native, 0) as fee_fun_native
-    , coalesce(fee_fun_usd, 0) as fee_fun_usd
-    , coalesce(tax_usd, 0) as tax_usd
-    , coalesce(fees, 0) as fees
+    date_spine.date
+    , 'virtuals' as artemis_id
 
     -- Standardized Metrics
 
+    -- Market Data
+    , market_metrics.price
+    , market_metrics.token_volume
+    , market_metrics.market_cap
+    , market_metrics.fdmc
+
+    -- Usage Data
+    , virtuals_dau.dau as spot_dau
+    , virtuals_dau.dau as dau
+    , virtuals_volume.volume_usd as spot_volume
+
+    -- Fee Data
+    , virtuals_fees.tax_usd + virtuals_fees.fee_fun_usd as spot_fees
+    , virtuals_fees.fee_fun_usd + virtuals_fees.tax_usd as fees
+    , virtuals_fees.fee_fun_usd as service_fee_allocation
+    , virtuals_fees.tax_usd as treasury_fee_allocation
+
+    -- Financial Statement
+    , virtuals_fees.tax_usd as revenue
+
     -- Supply Metrics
-    , premine_unlocks_native
-    , net_supply_change_native
-    , circulating_supply_native
-
-    -- Token Metrics
-    , coalesce(price, 0) as price
-    , coalesce(market_cap, 0) as market_cap
-    , coalesce(fdmc, 0) as fdmc
-    , coalesce(token_volume, 0) as token_volume
-
-    -- Spot DEX Metrics
-    , coalesce(dau, 0) as spot_dau
-    , coalesce(volume_usd, 0) as spot_volume
-    , coalesce(tax_usd, 0) + coalesce(fee_fun_usd, 0) as spot_fees
-
-    -- Cash Flow Metrics
-    , coalesce(fee_fun_usd,0)  + coalesce(tax_usd,0) as ecosystem_revenue
-    , coalesce(fee_fun_usd,0) as service_fee_allocation
-    , coalesce(tax_usd,0) as treasury_fee_allocation
-
+    , virtuals_supply_data.premine_unlocks_native
+    , virtuals_supply_data.circulating_supply_native
+    
     -- Turnover Metrics
-    , coalesce(token_turnover_circulating, 0) as token_turnover_circulating
-    , coalesce(token_turnover_fdv, 0) as token_turnover_fdv
+    , market_metrics.token_turnover_circulating
+    , market_metrics.token_turnover_fdv
+
+    -- Bespoke Metrics
+    , virtuals_daily_agents.daily_agents
 
     -- timestamp columns
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
     , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
+
 from date_spine
-left join daily_agents using (date)
-left join dau using (date)
-left join volume using (date)
-left join fees using (date)
-left join market_data using (date)
-left join supply_data using (date)
+left join virtuals_daily_agents using (date)
+left join virtuals_dau using (date)
+left join virtuals_volume using (date)
+left join virtuals_fees using (date)
+left join virtuals_supply_data using (date)
 where true
 {{ ez_metrics_incremental('date_spine.date', backfill_date) }}
 and date_spine.date < to_date(sysdate())
