27d27cbbb574e3ce7c9a49098e02a9abb68b6542
Akhil Vajjhala <akhil.vajjhala@gmail.com>
2025-07-31 13:16:32 -0400

    Standardized momentum metrics

--- models/projects/momentum/core/ez_momentum_metrics.sql
+++ models/projects/momentum/core/ez_momentum_metrics.sql
@@ -18,7 +18,7 @@
 {% set backfill_date = var("backfill_date", None) %}
 
 WITH
-    date_spine AS(
+    date_spine AS (
         SELECT
             date
         FROM {{ ref("dim_date_spine") }}
@@ -26,50 +26,65 @@ WITH
             AND date >= (SELECT MIN(date) FROM {{ ref("fact_raw_momentum_spot_swaps") }})
     )
     , spot_trading_volume AS (
-        SELECT date, SUM(volume_usd) AS spot_dex_volumes
+        SELECT 
+            date
+            , SUM(volume_usd) AS spot_dex_volumes
         FROM {{ ref("fact_momentum_spot_volume") }}
         GROUP BY 1
     )
     , spot_dau_txns AS (
-        SELECT date, daily_dau AS dau, daily_txns AS txns
+        SELECT 
+            date
+            , daily_dau AS dau
+            , daily_txns AS txns
         FROM {{ ref("fact_momentum_spot_dau_txns") }}
         QUALIFY ROW_NUMBER() OVER (PARTITION BY date ORDER BY date DESC) = 1
     )
     , spot_fees_revenue AS (
-        SELECT date, SUM(fees) AS fees, SUM(service_fee_allocation) AS service_fee_allocation, SUM(foundation_fee_allocation) AS foundation_fee_allocation
+        SELECT 
+            date
+            , SUM(fees) AS fees
+            , SUM(service_fee_allocation) AS service_fee_allocation
+            , SUM(foundation_fee_allocation) AS foundation_fee_allocation
         FROM {{ ref("fact_momentum_spot_fees_revenue") }}
         GROUP BY 1
     )
     , tvl AS (
-        SELECT date, SUM(tvl) AS tvl
+        SELECT 
+            date
+            , SUM(tvl) AS tvl
         FROM {{ ref("fact_momentum_spot_tvl") }}
         GROUP BY 1
     )
 select
-    date
-    , 'momentum' AS app
-    , 'DeFi' AS category
+    date_spine.date
+    , 'momentum' AS artemis_id
+
     -- Standardized Metrics
-    --Cashflow Metrics
-    , COALESCE(spot_fees_revenue.fees, 0) AS fees
-    , COALESCE(spot_fees_revenue.foundation_fee_allocation, 0) AS foundation_fee_allocation
-    , COALESCE((spot_fees_revenue.service_fee_allocation), 0) AS service_fee_allocation
-    -- Spot DEX Metrics
-    , COALESCE(spot_dau_txns.dau, 0) AS spot_dau
-    , COALESCE(spot_dau_txns.txns, 0) AS spot_txns
-    , COALESCE(spot_fees_revenue.fees, 0) AS spot_fees
-    , COALESCE(spot_trading_volume.spot_dex_volumes, 0) AS spot_volume
-    -- Crypto Metrics
-    , COALESCE(tvl.tvl, 0) AS tvl
-    , COALESCE(tvl.tvl, 0) - COALESCE(LAG(tvl.tvl) OVER (ORDER BY date), 0) AS tvl_net_change
-    -- timestamp columns
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as created_on
-    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) as modified_on
+
+    -- Usage Data
+    , spot_dau_txns.dau AS spot_dau
+    , spot_dau_txns.dau
+    , spot_dau_txns.txns AS spot_txns
+    , spot_dau_txns.txns
+    , spot_trading_volume.spot_dex_volumes AS spot_volume
+    , tvl.tvl AS spot_tvl
+    , tvl.tvl
+
+    --Fee Data
+    , spot_fees_revenue.fees AS spot_fees
+    , spot_fees_revenue.fees
+    , spot_fees_revenue.foundation_fee_allocation
+    , spot_fees_revenue.service_fee_allocation AS lp_fee_allocation
+
+    -- Timestamp Columns
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS created_on
+    , TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP()) AS modified_on
 FROM date_spine
 LEFT JOIN spot_trading_volume USING(date)
 LEFT JOIN spot_dau_txns USING(date)
 LEFT JOIN spot_fees_revenue USING(date)
 LEFT JOIN tvl USING(date)
-where true
+WHERE true
 {{ ez_metrics_incremental('date_spine.date', backfill_date) }}
-and date_spine.date < to_date(sysdate())
+AND date_spine.date < to_date(sysdate())
