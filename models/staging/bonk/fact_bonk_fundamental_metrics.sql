7b3c5458439cda95eda73b62cc4212ac9ea17161
Anish Toomu <anishtoomu@gmail.com>
2025-08-01 14:10:10 -0400

    rename

--- models/staging/bonk/fact_bonk_fundamental_metrics.sql
+++ models/staging/bonk/fact_bonk_fundamental_metrics.sql
@@ -5,10 +5,10 @@
         snowflake_warehouse='BONK',
     )
 }}
-with coins_minted as (
+with tokens_created as (
     select 
         date_trunc('day', block_timestamp) as date,
-        count (distinct mint_address) as unique_tokens_created_per_day
+        count(distinct mint_address) as unique_tokens_created_per_day
     from {{ ref('fact_bonk_coins_minted') }}
     group by 1
 ),
@@ -27,10 +27,10 @@ bonk_fees as (
         date
         , bonk_fees as launchpad_fees
     from {{ ref('fact_bonk_fees') }}
-),
+)
 select 
-    coalesce(cm.date, lm.date, bf.date, tsm.date) as date
-    , unique_tokens_created_per_day
+    coalesce(tc.date, lm.date, bf.date) as date
+    , tc.unique_tokens_created_per_day
     , launchpad_dau
     , launchpad_volume
     , launchpad_txns
@@ -38,15 +38,11 @@ select
     , unique_tokens_traded
     , launchpad_volume / nullif(launchpad_txns, 0) as avg_swap_size_usd
     , launchpad_fees / nullif(launchpad_volume, 0) as fee_to_volume_ratio
-    , total_token_supply
-    , avg_token_supply
-    , unique_symbols_created
-from coins_minted cm
-full outer join launchpad_metrics lm on cm.date = lm.date
-full outer join bonk_fees bf on coalesce(cm.date, lm.date) = bf.date
-full outer join token_supply_metrics tsm on coalesce(cm.date, lm.date, bf.date) = tsm.date
+from tokens_created tc
+full outer join launchpad_metrics lm on tc.date = lm.date
+full outer join bonk_fees bf on coalesce(tc.date, lm.date) = bf.date
 {% if is_incremental() %}
-where coalesce(cm.date, lm.date, bf.date, tsm.date) >= dateadd(day, -3, to_date(sysdate()))
+where coalesce(tc.date, lm.date, bf.date) >= dateadd(day, -3, to_date(sysdate()))
 {% else %}
-where coalesce(cm.date, lm.date, bf.date, tsm.date) >= '2025-04-20'
+where coalesce(tc.date, lm.date, bf.date) >= '2025-04-20'
 {% endif %}
